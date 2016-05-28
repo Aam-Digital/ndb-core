f240fe0db0ed8710e1a3e18fbdc6e79ff9efb3a2
Sebastian Leidig <sebastian.leidig@gmail.com>
2016-05-28 16:20:16 +0200

    implemented checkPassword function for User class
    added crypto-js library as new dependency
    see #3

--- app/user/user.ts
+++ app/user/user.ts
@@ -1,3 +1,29 @@
-export class User {
+import { Entity } from "../model/entity";
+
+
+export class User extends Entity {
+
     public name: string;
+    private password: any;
+
+
+    public checkPassword(givenPassword: string): boolean {
+        return (this.hashPassword(givenPassword) == this.password.hash);
+    }
+
+    private hashPassword(givenPassword: string): string {
+        let options = {
+            keySize: this.password.keysize,
+            iterations: this.password.iterations
+        };
+
+        return CryptoJS.PBKDF2(givenPassword, this.password.salt, options).toString();
+    }
+
+
+
+
+    public getPrefix(): string {
+        return "user:";
+    }
 }
