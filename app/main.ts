5a13675ab8931672e47641366541aa06acc225b6
Sebastian Leidig <sebastian.leidig@gmail.com>
2016-08-07 00:37:03 +0200

    changed forms to new api
    see #18

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,13 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import {disableDeprecatedForms, provideForms} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavigationItemsService } from "./navigation/navigation-items.service";
 import { appRouterProviders } from "./app.routes";
 
 bootstrap(AppComponent, [
+    disableDeprecatedForms(),
+    provideForms()
     appRouterProviders,
     NavigationItemsService
 ]);
