66fae028f66780dfdbcdce9ef324e821c06f92e6
Sebastian Leidig <sebastian.leidig@gmail.com>
2016-08-19 18:04:45 +0200

    fixed old reference names and removed unused index file

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,8 @@ import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
 import {routing} from "./app.routes";
-import {SessionService} from "./session/session.service";
 import {LoggedInGuard} from "./session/logged-in.guard";
-import {EntityMapperService} from "./model/entity-mapper.service";
-import {LoginComponent} from "./session/login.component";
+import {EntityMapperService} from "./entity/entity-mapper.service";
 import {FooterComponent} from "./footer.component";
 import {AlertsModule} from "./alerts/alerts.module";
 import {NG2BootstrapModule} from "./ng2-bootstrap.module";
