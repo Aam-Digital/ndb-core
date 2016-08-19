15d78d6b124dd78ddd3d4dc5aadec0711c35e40e
Frederik Schöll <frederik.schoell@gmail.com>
2016-08-19 18:18:30 +0200

    re-added index.ts to export the modules

---
+++ index.ts
@@ -0,0 +1,10 @@
+export * from './app/app.module';
+
+export * from './app/alerts/alerts.module';
+export * from './app/config/config.module';
+export * from './app/dashboard/dashboard.module';
+export * from './app/database/database.module';
+export * from './app/entity/entity.module';
+export * from './app/session/session.module';
+export * from './app/sync-status/sync-status.module';
+export * from './app/user/user.module';
