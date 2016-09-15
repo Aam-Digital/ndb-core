bc9355d1733dff378905a36442c49ed0aa7d9c68
Frederik Sch?ll <frederik.schoell@gmail.com>
2016-09-15 16:10:58 +0200

    upgraded to Angular2 and fixed a ng2-bootstrap issue

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, ViewContainerRef} from '@angular/core';
 
 @Component({
     selector: 'ndb-app',
     template: '<ndb-ui></ndb-ui>'
 })
 export class AppComponent {
+    private viewContainerRef: ViewContainerRef;
 
+    public constructor(viewContainerRef: ViewContainerRef) {
+        // You need this small hack in order to catch application root view container ref
+        this.viewContainerRef = viewContainerRef;
+    }
 }
