d41e570ce60f497601281c4d793657849b1f12f8
Frederik Schöll <frederik.schoell@gmail.com>
2017-07-05 12:04:07 +0200

    added gpl licensing

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,3 +1,20 @@
+/*
+ *     This file is part of ndb-core.
+ *
+ *     ndb-core is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     ndb-core is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { Entity } from '../entity/entity';
 import * as CryptoJS from 'crypto-js';
 
@@ -15,7 +32,9 @@ export class User extends Entity {
   public setNewPassword(password: string) {
     const cryptKeySize = 256 / 32;
     const cryptIterations = 128;
-    const cryptSalt = 'test'; // TODO CryptoJS.lib.WordArray.random(128 / 8).toString();
+
+    //noinspection TypeScriptUnresolvedVariable (out-of-date types for crypto-js, lib is a valid field)
+    const cryptSalt = CryptoJS.lib.WordArray.random(128 / 8).toString();
     const hash = CryptoJS.PBKDF2(password, cryptSalt, {
       keySize: cryptKeySize,
       iterations: cryptIterations
