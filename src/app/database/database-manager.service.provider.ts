8fc3370c2382098f56f4b0a880992860387a3798
Frederik Schöll <frederik.schoell@gmail.com>
2017-07-07 11:29:12 +0200

    test database is automatically selected now when not in production environment

---
+++ src/app/database/database-manager.service.provider.ts
@@ -0,0 +1,37 @@
+/*
+ *     This file is part of ndb-core.
+ *
+ *     ndb-core is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     ndb-core is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { environment } from '../../environments/environment';
+import { PouchDatabaseManagerService } from './pouch-database-manager.service';
+import { ConfigService } from '../config/config.service';
+import { PouchDatabaseManagerTestService } from './pouch-database-manager-test.service';
+import { DatabaseManagerService } from './database-manager.service';
+
+export let databaseManagerFactory = function(appConfig: ConfigService) {
+  if (environment.production) {
+    return new PouchDatabaseManagerService(appConfig);
+  } else {
+    return new PouchDatabaseManagerTestService();
+  }
+};
+
+export let databaseManagerProvider =
+  {
+    provide: DatabaseManagerService,
+    useFactory: databaseManagerFactory,
+    deps: [ConfigService]
+  };
