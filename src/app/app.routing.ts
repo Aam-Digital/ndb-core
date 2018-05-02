0ca16dff83564e03164c2e7f87a1a7e8bc113617
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-05-02 13:23:55 +0530

    fixed loggedIn checks for other pages

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,23 +15,23 @@
  *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { RouterModule, Routes } from '@angular/router';
-import { LoggedInGuard } from './session/logged-in.guard';
-import { ModuleWithProviders } from '@angular/core';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { SchoolsListComponent } from './schools/schools-list/schools/schools-list.component';
-import {SchoolDetailComponent} from "./schools/school-details/school-detail/school-detail.component";
-import {ChildDetailsComponent} from "./children/child-details/child-details.component";
-import {UserAccountComponent} from "./user/user-account/user-account.component";
+import {RouterModule, Routes} from '@angular/router';
+import {LoggedInGuard} from './session/logged-in.guard';
+import {ModuleWithProviders} from '@angular/core';
+import {DashboardComponent} from './dashboard/dashboard/dashboard.component';
+import {SchoolsListComponent} from './schools/schools-list/schools/schools-list.component';
+import {SchoolDetailComponent} from './schools/school-details/school-detail/school-detail.component';
+import {ChildDetailsComponent} from './children/child-details/child-details.component';
+import {UserAccountComponent} from './user/user-account/user-account.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [LoggedInGuard]},
   {path: 'user', component: UserAccountComponent, canActivate: [LoggedInGuard]},
-  {path: 'school', component: SchoolsListComponent},
-  {path: 'school/:id', component: SchoolDetailComponent},
-  //{path: 'child', component: ChildDetailsComponent},
-  {path: 'child/:id', component: ChildDetailsComponent}
+  {path: 'school', component: SchoolsListComponent, canActivate: [LoggedInGuard]},
+  {path: 'school/:id', component: SchoolDetailComponent, canActivate: [LoggedInGuard]},
+  //{path: 'child', component: ChildDetailsComponent, canActivate: [LoggedInGuard]},
+  {path: 'child/:id', component: ChildDetailsComponent, canActivate: [LoggedInGuard]}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
