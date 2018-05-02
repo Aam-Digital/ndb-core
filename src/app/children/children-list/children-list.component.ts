4123870c0321259380eaab968ad77a37a061d77b
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-05-02 20:45:40 +0530

    added basic code to display children from database

--- src/app/children/children-list/children-list.component.ts
+++ src/app/children/children-list/children-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EntityMapperService} from '../../entity/entity-mapper.service';
+import {AlertService} from '../../alerts/alert.service';
+import {Child} from '../child';
 
 @Component({
   selector: 'app-children-list',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildrenListComponent implements OnInit {
 
-  constructor() { }
+  children: Child[];
+
+  constructor(private entityMapper: EntityMapperService, private alertService: AlertService) { }
 
   ngOnInit() {
+    this.entityMapper.loadType<Child>(Child).then(
+      loadedEntities => this.children = loadedEntities,
+      reason => this.alertService.addWarning(reason)
+    );
   }
 
 }
