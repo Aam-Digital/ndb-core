d9f872087e5915c394339a6d781a2cb8d70bdcdb
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-05-23 17:03:06 +0530

    fixed broken unit tests

--- src/app/children/children-list/children-list.component.spec.ts
+++ src/app/children/children-list/children-list.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChildrenListComponent } from './children-list.component';
+import {
+  MatButtonModule, MatButtonToggleModule,
+  MatExpansionModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatSidenavModule, MatSortModule,
+  MatTableModule
+} from '@angular/material';
+import {CommonModule} from '@angular/common';
+import {ChildrenService} from '../children.service';
+import {EntityMapperService} from '../../entity/entity-mapper.service';
+import {MockDatabase} from '../../database/mock-database';
+import {Database} from '../../database/database';
+import {RouterTestingModule} from '@angular/router/testing';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('ChildrenListComponent', () => {
   let component: ChildrenListComponent;
@@ -8,7 +20,22 @@ describe('ChildrenListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChildrenListComponent ]
+      declarations: [ ChildrenListComponent ],
+      imports: [
+        CommonModule,
+        RouterTestingModule.withRoutes([]),
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatTableModule,
+        MatSortModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        NoopAnimationsModule,
+      ],
+      providers: [ChildrenService, EntityMapperService, { provide: Database, useClass: MockDatabase }],
     })
     .compileComponents();
   }));
