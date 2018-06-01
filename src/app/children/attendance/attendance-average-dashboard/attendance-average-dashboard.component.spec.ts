341010d586d4412634057ee269847cd6554763b6
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-06-01 15:02:05 +0530

    fixed unit tests of attendance-warning-dashboard

--- src/app/children/attendance/attendance-average-dashboard/attendance-average-dashboard.component.spec.ts
+++ src/app/children/attendance/attendance-average-dashboard/attendance-average-dashboard.component.spec.ts
@@ -33,39 +33,4 @@ describe('AttendanceAverageDashboardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-
-  function getAttendanceTestData() {
-    const data =  new Array<AttendanceMonth>();
-    const a1 = new AttendanceMonth('1');
-    a1.student = '22';
-    a1.month = new Date('2018-01-01');
-    a1.daysWorking = 20;
-    a1.daysAttended = 18;
-    data.push(a1);
-
-    const a2 = new AttendanceMonth('2');
-    a2.student = '22';
-    a2.month = new Date('2018-02-01');
-    a2.daysWorking = 22;
-    a2.daysAttended = 5;
-    data.push(a2);
-
-    const a3 = new AttendanceMonth('3');
-    a3.student = '22';
-    a3.month = new Date('2018-03-01');
-    a3.daysWorking = 19;
-    a3.daysAttended = 11;
-    a3.daysExcused = 3;
-    data.push(a3);
-
-    const a4 = new AttendanceMonth('4');
-    a4.student = '22';
-    a4.month = new Date('2018-04-01');
-    a4.daysWorking = 19;
-    a4.daysAttended = 11;
-    a4.daysExcused = 3;
-    data.push(a4);
-
-    return data;
-  }
 });
--- src/app/children/attendance/attendance-warnings-dashboard/attendance-warnings-dashboard.component.spec.ts
+++ src/app/children/attendance/attendance-warnings-dashboard/attendance-warnings-dashboard.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AttendanceWarningsDashboardComponent } from './attendance-warnings-dashboard.component';
+import {MatCardModule, MatIconModule} from '@angular/material';
+import {ChildBlockComponent} from '../../child-block/child-block.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MockDatabase} from '../../../database/mock-database';
+import {Database} from '../../../database/database';
+import {EntityMapperService} from '../../../entity/entity-mapper.service';
+import {ChildrenService} from '../../children.service';
 
 describe('AttendanceWarningsDashboardComponent', () => {
   let component: AttendanceWarningsDashboardComponent;
@@ -8,7 +15,9 @@ describe('AttendanceWarningsDashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AttendanceWarningsDashboardComponent ]
+      declarations: [ ChildBlockComponent, AttendanceWarningsDashboardComponent ],
+      imports: [MatIconModule, MatCardModule, RouterTestingModule],
+      providers: [ChildrenService, EntityMapperService, { provide: Database, useClass: MockDatabase }],
     })
     .compileComponents();
   }));
