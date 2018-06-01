766b2926294b6291dbd2ba2209416055478bdb24
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-06-01 12:25:12 +0530

    added unit tests for ChildrenCountDashboardComponent

--- src/app/children/children-count-dashboard/children-count-dashboard.component.spec.ts
+++ src/app/children/children-count-dashboard/children-count-dashboard.component.spec.ts
@@ -2,17 +2,31 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChildrenCountDashboardComponent } from './children-count-dashboard.component';
 import {MatCardModule, MatIconModule} from '@angular/material';
-import {MockDatabase} from '../../database/mock-database';
-import {EntityMapperService} from '../../entity/entity-mapper.service';
 import {ChildrenService} from '../children.service';
-import {Database} from '../../database/database';
 import {RouterTestingModule} from '@angular/router/testing';
+import {Observable} from 'rxjs/Rx';
+import {Child} from '../child';
 
 describe('ChildrenCountDashboardComponent', () => {
   let component: ChildrenCountDashboardComponent;
   let fixture: ComponentFixture<ChildrenCountDashboardComponent>;
 
+  let childrenService: ChildrenService;
+  let childrenObserver;
+
+  let _lastId = 0;
+  function createChild(center: string) {
+    _lastId++;
+    const child = new Child(_lastId.toString());
+    child.center = center;
+    return child;
+  }
+
   beforeEach(async(() => {
+    childrenService = new ChildrenService(null);
+    spyOn(childrenService, 'getChildren').and
+      .returnValue(new Observable((observer) => { childrenObserver = observer; }));
+
     TestBed.configureTestingModule({
       declarations: [ ChildrenCountDashboardComponent ],
       imports: [
@@ -20,7 +34,7 @@ describe('ChildrenCountDashboardComponent', () => {
         MatCardModule,
         RouterTestingModule,
       ],
-      providers: [ChildrenService, EntityMapperService, { provide: Database, useClass: MockDatabase }],
+      providers: [{ provide: ChildrenService, useValue: childrenService }],
     })
     .compileComponents();
   }));
@@ -31,7 +45,29 @@ describe('ChildrenCountDashboardComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should calculate totalChildren correctly', () => {
+    const children = [createChild('CenterA'), createChild('CenterB'), createChild('CenterA')];
+    childrenObserver.next(children);
+
+    expect(component.totalChildren).toBe(3);
+  });
+
+  it('should calculate childrens per center correctly', () => {
+    const centerA = 'CenterA';
+    const centerB = 'CenterB';
+    const children = [createChild(centerA), createChild(centerB), createChild(centerA)];
+    childrenObserver.next(children);
+
+    expect(component.childrenByCenter.length).toBe(2, 'unexpected number of centers');
+    const actualCenterAEntry = component.childrenByCenter.filter(e => e[0] === centerA)[0];
+    expect(actualCenterAEntry[1]).toBe(2, 'child count of CenterA not correct');
+    const actualCenterBEntry = component.childrenByCenter.filter(e => e[0] === centerB)[0];
+    expect(actualCenterBEntry[1]).toBe(1, 'child count of CenterB not correct');
+  });
+
 });
