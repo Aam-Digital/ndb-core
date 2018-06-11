d4627965c1d6667b19c5b6c0c4c683123bd44d86
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-06-11 18:09:49 +0530

    set browser title to app title

--- src/app/ui/ui/ui.component.ts
+++ src/app/ui/ui/ui.component.ts
@@ -18,6 +18,7 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { SessionService } from '../../session/session.service';
 import {AppConfig} from '../../app-config/app-config';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   moduleId: module.id,
@@ -31,12 +32,14 @@ export class UiComponent implements OnInit {
   viewContainerRef: ViewContainerRef;
 
   constructor(private _sessionService: SessionService,
-              viewContainerRef: ViewContainerRef) {
+              viewContainerRef: ViewContainerRef,
+              private titleService: Title ) {
     this.viewContainerRef = viewContainerRef;
   }
 
   ngOnInit(): void {
     this.title = AppConfig.settings.site_name;
+    this.titleService.setTitle(this.title);
   }
 
   isLoggedIn() {
