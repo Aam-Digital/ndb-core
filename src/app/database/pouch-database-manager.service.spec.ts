271b05c0ac406a3edaf3c8b21463bf0ecdd38b97
mspoeri <markus.spoeri@die-softwareklitsche.de>
2018-07-19 17:56:19 +0200

    fixed missing arguments in manual constructor calls inside tests

--- src/app/database/pouch-database-manager.service.spec.ts
+++ src/app/database/pouch-database-manager.service.spec.ts
@@ -33,7 +33,7 @@ describe('PouchDatabaseManagerService', () => {
       version: 'x',
       dev: { useRemoteDatabaseDuringDevelopment: false },
     };
-    dbManager = new PouchDatabaseManagerService(new AlertService(null));
+    dbManager = new PouchDatabaseManagerService(new AlertService(null, null));
   });
 
   it('returns database', function () {
