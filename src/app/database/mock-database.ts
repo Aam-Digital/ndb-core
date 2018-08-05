063a5ac7501936e60f804f2edb1d93ca614d2535
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-08-05 15:04:03 +0530

    added some mock implementation of database indices for MockDatabase

--- src/app/database/mock-database.ts
+++ src/app/database/mock-database.ts
@@ -16,6 +16,8 @@
  */
 
 import { Database } from './database';
+import {Note} from '../children/notes/note';
+import {AttendanceMonth} from '../children/attendance/attendance-month';
 
 /**
  * Wrapper for a PouchDB instance to decouple the code from
@@ -105,13 +107,102 @@ export class MockDatabase extends Database {
   }
 
 
-  query(fun: (doc: any, emit: any) => void, options: any): Promise<any> {
-    // TODO: implement mock query
+
+  query(fun: any, options?: any): Promise<any> {
+    // TODO: implement generic mock query function
+    /* SAMPLE INPUT:
+      query('notes_index/by_child', {key: childId, include_docs: true});
+      query('avg_attendance_index/three_months', {reduce: true, group: true});
+    */
+
+    // mock specific indices
+    let filter;
+    let reduce;
+    switch (fun) {
+      case 'notes_index/by_child':
+        filter = (e: Note) => e.getType() === Note.ENTITY_TYPE && e.children.includes(options.key);
+        break;
+      case 'attendences_index/by_child':
+        filter = (e: AttendanceMonth) => e.getType() === AttendanceMonth.ENTITY_TYPE && e.student === options.key;
+        break;
+      case 'attendences_index/by_month':
+        filter = (e: AttendanceMonth) => e.getType() === AttendanceMonth.ENTITY_TYPE && e.month === options.month;
+        break;
+      case 'avg_attendance_index/three_months':
+        filter = (e: AttendanceMonth) => e.getType() === AttendanceMonth.ENTITY_TYPE
+          && this.isWithinLastMonths(e.month, new Date(), 3);
+        reduce = this.getStatsReduceFun((e: AttendanceMonth) => e.student,
+          (e: AttendanceMonth) => (e.daysAttended / (e.daysWorking - e.daysExcused)));
+        break;
+      case 'avg_attendance_index/last_month':
+        filter = (e: AttendanceMonth) => e.getType() === AttendanceMonth.ENTITY_TYPE
+          && this.isWithinLastMonths(e.month, new Date(), 1);
+        reduce = this.getStatsReduceFun((e: AttendanceMonth) => e.student,
+          (e: AttendanceMonth) => (e.daysAttended / (e.daysWorking - e.daysExcused)));
+        break;
+    }
+    if (filter !== undefined) {
+      if (reduce !== undefined) {
+        return this.getAll().then(results => {
+          results = results.filter(filter);
+          results = results.reduce(reduce, []);
+          return { rows: results }
+        });
+      } else {
+        return this.getAll().then(results => {
+          return { rows: results.filter(filter) }
+        });
+      }
+    }
+
+
     console.warn('MockDatabase does not implement "query()"');
     return Promise.resolve({ rows: []});
   }
+
+  private isWithinLastMonths(date: Date, now: Date, numberOfMonths: number): boolean {
+    let months;
+    months = (now.getFullYear() - date.getFullYear()) * 12;
+    months -= date.getMonth();
+    months += now.getMonth();
+    if (months < 0) { return false; }
+    return months <= numberOfMonths;
+  }
+
+  private getStatsReduceFun(keyFun: (any) => string, valueFun: (any) => number) {
+    return (acc, value) => {
+      const stats = { key: keyFun(value), value: { count: 1, sum: valueFun(value) } };
+      const existing = acc.filter(x => x.key === keyFun(value));
+      if (existing.length > 0) {
+        const v = existing[0].value;
+        v.count++;
+        v.sum = v.sum + stats.value.sum;
+      } else {
+        acc.push(stats);
+      }
+
+      return acc;
+    };
+  }
+
+
   saveDatabaseIndex(designDoc) {
     // TODO: implement mock query
+    /* SAMPLE INPUT:
+    const designDoc = {
+      _id: '_design/attendance_index',
+      views: {
+        by_name: {
+          map: (doc) => { emit(doc.child); }
+        },
+        by_month: {
+          map: (doc) => { emit(doc.month); }
+        }
+      }
+    };
+     */
+
+
     console.warn('MockDatabase does not implement "saveDatabaseIndex()"');
     return Promise.resolve({});
   }
