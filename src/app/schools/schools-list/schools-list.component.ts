c5583da273f61b392bfd3e326376d28cb58564fe
SuppenGeist <johs.werner@googlemail.com>
2018-08-16 20:56:15 +0200

    Nearly finish implementation of school list

--- src/app/schools/schools-list/schools-list.component.ts
+++ src/app/schools/schools-list/schools-list.component.ts
@@ -1,34 +1,69 @@
-import {Component, OnInit, Output, EventEmitter, ViewChild, AfterViewInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, ViewChild} from '@angular/core';
 import {MatTableDataSource, MatSort} from '@angular/material';
 import {School} from '../schoolsShared/school';
 import {SchoolsServices} from '../schoolsShared/schools.services';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-schools',
+  selector: 'app-schools-list',
   templateUrl: './schools-list.component.html',
   styleUrls: ['./schools-list.component.css']
 })
 
 export class SchoolsListComponent implements OnInit, AfterViewInit {
   schoolList: School[];
-  schoolDataSource: MatTableDataSource<School>;
+  schoolDataSource: MatTableDataSource<School> = new MatTableDataSource<School>();
 
-  columnsToDisplay: ['name', 'address', 'medium'];
+  @ViewChild(MatSort) sort: MatSort;
+  columnsToDisplay: string[] = ['name', 'address', 'medium', 'privateSchool'];
+  mediums: string[];
+  mediumFilterSelection = '';
+  filterFunctionMedium: (s: School) => boolean = (s: School) => true;
 
   constructor(private schoolService: SchoolsServices,
               private router: Router
   ) {
     this.schoolService.getSchools().subscribe(data => {
       this.schoolList = data;
-      this.schoolDataSource = new MatTableDataSource<School>(data);
+      this.schoolDataSource.data = data;
+      this.setMediumFilteredList(this.mediumFilterSelection);
+
+      this.mediums = data.map(s => s.medium).filter((value, index, arr) => arr.indexOf(value) === index);
     });
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.schoolDataSource.filter = filterValue;
   }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
+    this.schoolDataSource.sort = this.sort;
+  }
+
+  applyFilterGroups() {
+    this.schoolDataSource.data = this.schoolList
+      .filter(this.filterFunctionMedium);
+  }
+
+  setMediumFilteredList(filteredSelection: string) {
+    if (filteredSelection === '') {
+      this.filterFunctionMedium = (s: School) => true;
+    } else {
+      this.filterFunctionMedium = (s: School) => s.medium === filteredSelection;
+    }
+
+    this.applyFilterGroups();
+  }
+
+  addSchoolClick() {
+    let route: string;
+    route = this.router.url + '/new';
+    this.router.navigate([route]);
   }
 
   showSchoolDetails(school: School) {
--- src/app/schools/schools.module.ts
+++ src/app/schools/schools.module.ts
@@ -2,14 +2,29 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SchoolsListComponent} from './schools-list/schools-list.component';
 import {
-  MatTableModule, MatFormFieldModule, MatInputModule, MatSortModule, MatExpansionModule,
-  MatIconModule, MatCheckboxModule, MatButtonModule, MatSnackBarModule
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSortModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatButtonToggleModule,
+  MatAutocompleteModule,
+  MatCardModule,
+  MatSidenavModule, MatDialogModule
 } from '@angular/material';
 import {SchoolsServices} from './schoolsShared/schools.services';
 import {SchoolDetailComponent} from './school-detail/school-detail.component';
 import { SchoolBlockComponent } from './school-block/school-block.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatPaginatorModule, MatProgressSpinnerModule } from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import {UiHelperModule} from '../ui-helper/ui-helper.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
@@ -24,6 +39,32 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     MatSnackBarModule,
     MatIconModule,
     MatCheckboxModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+    FormsModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatSortModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    UiHelperModule,
     ReactiveFormsModule
   ],
   declarations: [
