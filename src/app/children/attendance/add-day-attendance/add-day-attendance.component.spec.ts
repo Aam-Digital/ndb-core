39c5e86eef0d4f977d46735e72bf4fa57396f846
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-08-31 18:32:15 +0530

    implemented staged roll call UI for daily attendance

--- src/app/children/attendance/add-day-attendance/add-day-attendance.component.spec.ts
+++ src/app/children/attendance/add-day-attendance/add-day-attendance.component.spec.ts
@@ -1,6 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddDayAttendanceComponent } from './add-day-attendance.component';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatFormFieldModule, MatIconModule,
+  MatInputModule,
+  MatSelectModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {SchoolBlockComponent} from '../../../schools/school-block/school-block.component';
+import {ChildBlockComponent} from '../../child-block/child-block.component';
+import {ChildrenService} from '../../children.service';
+import {Database} from '../../../database/database';
+import {MockDatabase} from '../../../database/mock-database';
+import {EntityModule} from '../../../entity/entity.module';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AddDayAttendanceComponent', () => {
   let component: AddDayAttendanceComponent;
@@ -8,7 +23,14 @@ describe('AddDayAttendanceComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddDayAttendanceComponent ]
+      declarations: [ AddDayAttendanceComponent, SchoolBlockComponent, ChildBlockComponent ],
+      imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatButtonToggleModule,
+        MatIconModule, NoopAnimationsModule,
+        EntityModule],
+      providers: [
+        {provide: ChildrenService, useClass: ChildrenService},
+        {provide: Database, useClass: MockDatabase},
+      ],
     })
     .compileComponents();
   }));
