a278a7d8a4c52fe2a5cc4007d1cbd86ca00af7ce
Simon <therealslimv@yahoo.de>
2018-11-22 21:07:35 +0100

    Loading children that visit the school work with the current data model where each student saves the school it attends

--- src/app/schools/school-detail/school-detail.component.ts
+++ src/app/schools/school-detail/school-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {School} from '../schoolsShared/school';
 import {SchoolsServices} from '../schoolsShared/schools.services';
@@ -16,7 +16,7 @@ import {ChildSchoolRelation} from '../../children/childSchoolRelation';
   templateUrl: './school-detail.component.html',
   styleUrls: ['./school-detail.component.css']
 })
-export class SchoolDetailComponent {
+export class SchoolDetailComponent implements OnInit {
   school = new School('');
   studentDataSource: MatTableDataSource<Child> = new MatTableDataSource();
   displayedColumns = ['id', 'name', 'age'];
@@ -50,7 +50,27 @@ export class SchoolDetailComponent {
     private alertService: AlertService,
     private snackBar: MatSnackBar,
     private confirmationDialog: ConfirmationDialogService
-  ) {
+  ) { }
+
+  ngOnInit() {
+    // this.entityMapperService.loadType<Child>(Child)
+    //   .then(children => children.forEach(child => {
+    //     const relation: ChildSchoolRelation = new ChildSchoolRelation(uniqid());
+    //     relation.childId = child.getId();
+    //     relation.schoolId = this.school.getId();
+    //     this.entityMapperService.save<ChildSchoolRelation>(relation)
+    //       .then(r => console.log('relation', r));
+    //     const id = this.route.snapshot.params['id'];
+    //     if (id === 'new') {
+    //       this.creatingNew = true;
+    //       this.editing = true;
+    //       this.school = new School(uniqid());
+    //     } else {
+    //       this.loadSchool(id);
+    //     }
+    //     this.initializeForm();
+    //   }
+    // ));
     const id = this.route.snapshot.params['id'];
     if (id === 'new') {
       this.creatingNew = true;
@@ -73,22 +93,36 @@ export class SchoolDetailComponent {
   }
 
   loadSchool(id: string) {
-    this.entityMapperService.load(School, id)
-      .then(loadedEntities => this.school = loadedEntities)
-      .then(() => this.entityMapperService.loadType<ChildSchoolRelation>(ChildSchoolRelation))
-      .then(childSchoolRelations => childSchoolRelations.filter(relation => relation.schoolId === this.school.getId()))
-      .then(childSchoolRelations =>
-        childSchoolRelations.forEach(relation =>
-          this.entityMapperService.load<Child>(Child, relation.childId)
-            .then(child => {
-              console.log('child', child);
-              this.studentDataSource.data.push(child)
-            }))
-      ).catch((err) => {
-        console.log('Error', err);
-        this.alertService.addDanger('Could not load school with id "' + id + '": ' + err)
-    });
-
+    // this.entityMapperService.load(School, id)
+    //   .then(loadedEntities => this.school = loadedEntities)
+    //   .then(() => this.entityMapperService.loadType<ChildSchoolRelation>(ChildSchoolRelation))
+    //   .then(childSchoolRelations => childSchoolRelations.filter(relation => relation.schoolId === this.school.getId()))
+    //   .then(childSchoolRelations =>
+    //     childSchoolRelations.forEach(relation =>
+    //       this.entityMapperService.load<Child>(Child, relation.childId)
+    //         .then(child => {
+    //           console.log('child', child);
+    //           this.studentDataSource.data.push(child)
+    //         }))
+    //   ).catch((err) => {
+    //     console.log('Error', err);
+    //     this.alertService.addDanger('Could not load school with id "' + id + '": ' + err)
+    // });
+    this.entityMapperService.load<School>(School, id)
+      .then(schools => this.school = schools)
+      .then(() => this.entityMapperService.loadType<Child>(Child))
+      .then(children => {
+        console.log('children', children);
+        return children.filter(child => {
+          console.log('child', child.schoolId, this.school.getId(), child.schoolId === this.school.getId());
+          return child.schoolId === this.school.getId();
+        })
+      })
+      .then(children => {
+        console.log('filtered', children);
+        this.studentDataSource.data = children;
+        console.log('dataSource', this.studentDataSource.data);
+      });
   }
 
   removeSchool() {
@@ -116,23 +150,24 @@ export class SchoolDetailComponent {
     this.router.navigate([route]);
   }
   saveSchool() {
-    this.school.name = this.form.get('name').value;
-    this.school.address = this.form.get('address').value;
-    this.school.medium = this.form.get('medium').value;
-    this.school.schoolTiming = this.form.get('schoolTiming').value;
-    this.school.maxClass = this.form.get('maxClass').value;
-    this.school.remarks = this.form.get('remarks').value;
-    this.school.board = this.form.get('board').value;
-    this.school.workDays = this.form.get('workDays').value;
-    this.school.website = this.form.get('website').value;
-    this.school.privateSchool = this.form.get('privateSchool').value;
-
-    this.entityMapperService.save<School>(this.school)
-      .then(() => {
-        if (this.creatingNew) {
-          this.router.navigate(['/school', this.school.getId()]);
-        }
-        this.disableEdit();
-      })
-      .catch((err) => this.alertService.addDanger('Could not save School "' + this.school.name + '": ' + err));  }
+    // this.school.name = this.form.get('name').value;
+    // this.school.address = this.form.get('address').value;
+    // this.school.medium = this.form.get('medium').value;
+    // this.school.schoolTiming = this.form.get('schoolTiming').value;
+    // this.school.maxClass = this.form.get('maxClass').value;
+    // this.school.remarks = this.form.get('remarks').value;
+    // this.school.board = this.form.get('board').value;
+    // this.school.workDays = this.form.get('workDays').value;
+    // this.school.website = this.form.get('website').value;
+    // this.school.privateSchool = this.form.get('privateSchool').value;
+    //
+    // this.entityMapperService.save<School>(this.school)
+    //   .then(() => {
+    //     if (this.creatingNew) {
+    //       this.router.navigate(['/school', this.school.getId()]);
+    //     }
+    //     this.disableEdit();
+    //   })
+    //   .catch((err) => this.alertService.addDanger('Could not save School "' + this.school.name + '": ' + err));  }
+  }
 }
