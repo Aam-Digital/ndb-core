f1f40de4b23fecf094ff621e751890f4ea9667ab
Richard Nutt <richard.nutt@outlook.com>
2018-12-02 18:10:39 +0100

    fixed focus of the menu button on closing

--- src/app/ui/ui/ui.component.ts
+++ src/app/ui/ui/ui.component.ts
@@ -15,7 +15,7 @@
  *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {Component, OnDestroy, OnInit, ViewContainerRef} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import { SessionService } from '../../session/session.service';
 import {AppConfig} from '../../app-config/app-config';
 import {Title} from '@angular/platform-browser';
@@ -29,9 +29,8 @@ import {Subscription} from "rxjs";
   styleUrls: ['./ui.component.css']
 })
 export class UiComponent implements OnInit, OnDestroy {
-
+  @ViewChild('sideNav') sideNav;
   title: string;
-  showNav = true;
   viewContainerRef: ViewContainerRef;
   watcher: Subscription;
   sideNavMode: string;
@@ -44,18 +43,11 @@ export class UiComponent implements OnInit, OnDestroy {
     // watch screen width to change sidenav mode
     this.watcher = media.subscribe((change: MediaChange) => {
       this.sideNavMode = change.mqAlias == 'xs' ? 'over' : 'side';
-      if (change.mqAlias != 'xs') {
-        this.showNav = true;
-      }
     });
   }
   ngOnInit(): void {
     this.title = AppConfig.settings.site_name;
     this.titleService.setTitle(this.title);
-
-    if (window.innerWidth < 600) {
-      this.showNav = false;
-    }
   }
 
   ngOnDestroy() {
