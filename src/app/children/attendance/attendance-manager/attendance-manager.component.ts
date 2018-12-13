48d48178dd13ba6934d4eed3e7c94064b36d16c5
Sebastian Leidig <sebastian.leidig@gmail.com>
2018-12-13 15:45:23 +0530

    added a basic attendance data viewer to analyse attendance of multiple children at once

--- src/app/children/attendance/attendance-manager/attendance-manager.component.ts
+++ src/app/children/attendance/attendance-manager/attendance-manager.component.ts
@@ -1,15 +1,132 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {FilterSelection} from '../../../ui-helper/filter-selection';
+import {Child} from '../../child';
+import {ChildrenService} from '../../children.service';
+import {MatSort, MatTableDataSource} from '@angular/material';
+import {AttendanceMonth} from '../attendance-month';
 
 @Component({
   selector: 'app-attendance-manager',
   templateUrl: './attendance-manager.component.html',
   styleUrls: ['./attendance-manager.component.scss']
 })
-export class AttendanceManagerComponent implements OnInit {
+export class AttendanceManagerComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  filterFrom: Date;
+  filterUntil: Date;
+  attendanceType = '';
+  displayType = 'daily';
+
+  childrenAll: Child[];
+
+  dataSource = new MatTableDataSource<any>();
+  columnsToDisplay = ['child', 'attendanceType', 'averageAttendance', 'recordCount', 'attendance'];
+  @ViewChild(MatSort) sort: MatSort;
+  loading = 0;
+
+  centerFS = new FilterSelection('center', []);
+  dropoutFS = new FilterSelection('status', [
+    {key: 'active', label: 'Current Project Children', filterFun: (c: Child) => c.isActive()},
+    {key: 'dropout', label: 'Dropouts', filterFun: (c: Child) => !c.isActive()},
+    {key: '', label: 'All', filterFun: (c: Child) => true},
+  ]);
+  filterSelections = [
+    this.dropoutFS,
+    this.centerFS,
+  ];
+
+
+
+  constructor(private childrenService: ChildrenService) { }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit() {
+    this.filterFrom = new Date();
+    this.filterFrom.setDate(1);
+    this.filterUntil = new Date();
+    this.filterUntil.setDate(1);
+    this.filterUntil.setMonth(this.filterUntil.getMonth() + 1);
+
+    this.childrenService.getChildren().subscribe(data => {
+      this.childrenAll = data.filter(c => c.isActive());
+      this.initCenterFilterOptions();
+      this.applyFilterSelections();
+    });
+  }
+
+
+  private initCenterFilterOptions() {
+    const centers = this.childrenAll.map(c => c.center).filter((value, index, arr) => arr.indexOf(value) === index);
+
+    const options = [{key: '', label: 'All', filterFun: (c: Child) => true}];
+
+    centers.forEach(center => {
+      options.push({key: center.toLowerCase(), label: center, filterFun: (c: Child) => c.center === center});
+    });
+
+    this.centerFS.options = options;
+  }
+
+
+  applyFilterSelections() {
+    this.loading = 0;
+    let filteredData = this.childrenAll;
+
+    this.filterSelections.forEach(f => {
+      filteredData = filteredData.filter(f.getSelectedFilterFunction());
+    });
+
+    let data = [];
+    filteredData.forEach(child => data = data.concat(this.loadChildRecords(child)));
+
+    this.dataSource.data = data;
+  }
+
+  loadChildRecords(child: Child): any[] {
+    this.loading++;
+    const recordCoaching = { child: child, attendanceType: 'coaching', attendance: [], averageAttendance: 0 };
+    const recordSchool = { child: child, attendanceType: 'school', attendance: [], averageAttendance: 0 };
+
+    this.childrenService.getAttendancesOfChild(child.getId()).subscribe(attendances => {
+      attendances.forEach(att => {
+        if ((att.month.getFullYear() > this.filterFrom.getFullYear()
+              || (att.month.getFullYear() === this.filterFrom.getFullYear() && att.month.getMonth() >= this.filterFrom.getMonth()))
+            && (att.month.getFullYear() < this.filterUntil.getFullYear()
+              || (att.month.getFullYear() === this.filterUntil.getFullYear() && att.month.getMonth() <= this.filterUntil.getMonth()))) {
+
+          if (att.institution === 'school') {
+            recordSchool.attendance.push(att);
+          } else if (att.institution === 'coaching') {
+            recordCoaching.attendance.push(att);
+          }
+        }
+      });
+
+      this.calculateRecordStats(recordSchool);
+      this.calculateRecordStats(recordCoaching);
+
+      this.loading--;
+    });
+
+    return [recordSchool, recordCoaching];
+  }
+
+  calculateRecordStats(record) {
+    const stats = record.attendance
+      .reduce((acc, a: AttendanceMonth) => {
+          if (a.daysWorking > 0) {
+            acc.sum += a.getAttendancePercentage();
+            acc.count++;
+          }
+          return acc;
+        },
+        { sum: 0, count: 0 });
+    record.recordCount = stats.count;
+    record.averageAttendance = stats.sum / stats.count;
+
   }
 
 }
