9f1f7dc44a1262fe752c079fb3ecfa5bcbc405fd
fab kne <fabian.kneissl@gmx.de>
2019-05-16 20:37:38 +0200

    Added basic desplay of users

--- src/app/admin/user-list/user-list.component.ts
+++ src/app/admin/user-list/user-list.component.ts
@@ -12,11 +12,17 @@ export class UserListComponent implements OnInit {
 
   users = new Array<User>();
 
+  columns: Array<ColumnDescription> = [
+  new ColumnDescription('name', 'Username', 'string'),
+  new ColumnDescription('password', 'Password', 'string'),
+  new ColumnDescription('admin', 'Role', 'boolean'), 
+  ]
 
   constructor(private entityMapperService: EntityMapperService) { }
 
   ngOnInit() {
-    
+    this.loadData();
+    console.log(this.users);
   }
 
 
@@ -25,6 +31,10 @@ export class UserListComponent implements OnInit {
     this.entityMapperService.loadType<User>(User).then(
       users => this.users=users)
     }
+
+  generateNewRecordFactory(){
+    
+  }
   
 
 }
