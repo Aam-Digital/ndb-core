b15bacd5c90ce002ec5dfe12c675c9961ef1444f
Sebastian Leidig <sebastian.leidig@gmail.com>
2019-05-30 19:37:41 +0200

    add schema to ChildSchoolRelation Entity

--- src/app/children/childSchoolRelation.ts
+++ src/app/children/childSchoolRelation.ts
@@ -2,6 +2,13 @@ import {Entity} from '../entity/entity';
 
 export class ChildSchoolRelation extends Entity {
   static ENTITY_TYPE = 'ChildSchoolRelation';
+  static schema = Entity.schema.extend({
+    'childId': 'string',
+    'schoolId': 'string',
+    'schoolClass': 'string',
+    'start': 'string',
+    'end': 'string',
+  });
 
   public childId: string;
   public schoolId: string;
