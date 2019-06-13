e62bd7a0d4a1e35156c25ccf5b16db9d2fc55d67
Richard Nutt <richard.nutt@outlook.com>
2019-06-14 00:43:00 +0200

    implemented the BlobService constructor

--- src/app/webdav/blob-service.service.ts
+++ src/app/webdav/blob-service.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AppConfig } from '../app-config/app-config';
+import webdav from 'webdav';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlobServiceService {
 
-  constructor() { }
+  private client:any;
+  constructor() { 
+    //const { createClient } = require("webdav");
+
+    this.client = webdav.createClient(
+      AppConfig.settings.webdav.remote_url,
+      {
+        username:AppConfig.settings.webdav.username,
+        password:AppConfig.settings.webdav.password
+      }
+    )
+  };
 }
--- src/app/webdav/webdav.module.ts
+++ src/app/webdav/webdav.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BlobServiceService } from "./blob-service.service";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    BlobServiceService
   ]
 })
 export class WebdavModule { }
