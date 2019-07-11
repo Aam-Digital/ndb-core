0b44d3c38cab3bedb99ba4a469ddfc967ff52f16
Simon <therealslimv@yahoo.de>
2019-07-11 22:03:19 +0200

    added reflect metadata

--- src/app/entity/database-field.decorator.ts
+++ src/app/entity/database-field.decorator.ts
@@ -1,4 +1,4 @@
-
+import 'reflect-metadata'
 /**
  * Decorator (Annotation `@DatabaseEntity()`) to set the string ENTITY_TYPE to an Entity Type.
  * Note: Property decorators are called before class decorators.
@@ -6,7 +6,9 @@
  */
 export function DatabaseField(fieldSchema: string) {
   return (targetEntity, property: string) => {
-
+    let design =  Reflect.getMetadata('design:type', targetEntity, property);
+    let types =  Reflect.getMetadata('design:paramtypes', targetEntity, property);
+    console.log('design', design, 'types', types);
     // Adding a dummy object to store the schemas // TODO: document why "localSchema" is needed
     if (!targetEntity.constructor.hasOwnProperty('localSchema')) {
       targetEntity.constructor.localSchema = {};
