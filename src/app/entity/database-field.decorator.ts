462a06c075ad36ce2d4b20aaa4f14b47c6e88380
Simon <therealslimv@yahoo.de>
2019-07-13 17:56:52 +0200

    Added the reflect-metadata API to get decorators the ability to retrieve type info of the decorated properties

--- src/app/entity/database-field.decorator.ts
+++ src/app/entity/database-field.decorator.ts
@@ -1,12 +1,20 @@
+import 'reflect-metadata'
+import {SchemaFieldOptions} from './schema/entity-schema';
+
 /**
  * Decorator (Annotation `@DatabaseEntity()`) to set the string ENTITY_TYPE to an Entity Type.
  * Note: Property decorators are called before class decorators.
  * @param fieldSchema The schema definition for this property.
+ * @param arrayType (optional) needs to be provided if property is an array.
  */
-import {SchemaFieldOptions} from './schema/entity-schema';
-
-export function DatabaseField(dataType: string, options: SchemaFieldOptions = {}) {
+export function DatabaseField(dataType: string, options: SchemaFieldOptions = {}, arrayType = '') {
   return (targetEntity, property: string) => {
+    let expDataType = Reflect.getMetadata('design:type', targetEntity, property).name.toLowerCase();
+    if (expDataType === 'array') {
+        expDataType = arrayType;
+        options['isArray'] = true;
+    }
+    console.log('property', property, 'name', expDataType);
 
     // Adding a dummy object to store the schemas // TODO: document why "localSchema" is needed
     if (!targetEntity.constructor.hasOwnProperty('localSchema')) {
