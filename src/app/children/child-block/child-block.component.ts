a7d57a13e27d2cbf48fc6f7876ca793d3d5b3f2a
Richard Nutt <richard.nutt@outlook.com>
2019-08-15 23:58:24 +0200

    images now correctly converted to base64 in child-block

--- src/app/children/child-block/child-block.component.ts
+++ src/app/children/child-block/child-block.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {ChildrenService} from '../children.service';
 import { BlobServiceService } from 'app/webdav/blob-service.service';
 import {Child} from '../child';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-child-block',
@@ -15,7 +16,7 @@ export class ChildBlockComponent implements OnInit {
   @Input() linkDisabled: boolean;
   tooltip = false;
   tooltipTimeout;
-  imageURL: string;
+  image: SafeUrl = 'assets/child.png';
 
   constructor(private router: Router,
               private blobService: BlobServiceService,
@@ -27,11 +28,9 @@ export class ChildBlockComponent implements OnInit {
         this.entity = child;
       });
     }
-    if (this.entity.photoFile) {
-      this.imageURL = this.blobService.getImageDownloadLink(this.entity.getId().replace('child:', ''));
-    } else {
-      this.imageURL = 'http://localhost/remote.php/dav/files/nextclouduser/default.png';
-    }
+    // TODO: use entity.photoFile
+    this.blobService.getImage(this.entity.getId().replace('child:', ''))
+    .then( arrayBuffer => this.image = this.blobService.bufferArrayToBase64(arrayBuffer));
   }
 
   showTooltip() {
