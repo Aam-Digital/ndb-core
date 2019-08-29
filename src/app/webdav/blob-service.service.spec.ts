d176fe6ebcb1c3547f7763251e90c618e4dd4acf
Richard Nutt <richard.nutt@outlook.com>
2019-08-29 20:47:39 +0200

    added SessionService to blob-service tests

--- src/app/webdav/blob-service.service.spec.ts
+++ src/app/webdav/blob-service.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BlobServiceService } from './blob-service.service';
+import { SessionService } from 'app/session/session.service';
 
 describe('BlobServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SessionService
+      ],
+    })
+    .compileComponents();
+  }));
 
   it('should be created', () => {
     const service: BlobServiceService = TestBed.get(BlobServiceService);
