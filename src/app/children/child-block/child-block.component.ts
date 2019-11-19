afa01c99495c8115d8382fbd8a4712435422bab8
Richard Nutt <richard.nutt@outlook.com>
2019-11-19 20:59:10 +0100

    cleaned up child-block.componnt

--- src/app/children/child-block/child-block.component.ts
+++ src/app/children/child-block/child-block.component.ts
@@ -3,7 +3,6 @@ import {Router} from '@angular/router';
 import {ChildrenService} from '../children.service';
 import { BlobService } from 'app/webdav/blob-service.service';
 import {Child} from '../child';
-import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-child-block',
@@ -16,7 +15,6 @@ export class ChildBlockComponent implements OnInit, OnChanges {
   @Input() linkDisabled: boolean;
   tooltip = false;
   tooltipTimeout;
-  image: SafeUrl = 'assets/child.png';
 
   constructor(private router: Router,
               private blobService: BlobService,
@@ -28,8 +26,6 @@ export class ChildBlockComponent implements OnInit, OnChanges {
         this.entity = child;
       });
     }
-    //this.blobService.getImage(this.entity.getId().replace('child:', ''))
-    //.then( arrayBuffer => {this.image = this.blobService.bufferArrayToBase64(arrayBuffer); });
   }
 
   ngOnChanges() {
