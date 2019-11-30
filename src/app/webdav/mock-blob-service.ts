c18e8666a586c5416e2627c9334392a528633631
Richard Nutt <richard.nutt@outlook.com>
2019-11-30 01:03:24 +0100

    added getImage function to mock-blob-service

--- src/app/webdav/mock-blob-service.ts
+++ src/app/webdav/mock-blob-service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Injectable()
 export class MockBlobService {
 
   constructor() {
+  }
+
+  getImage(path: string): Promise<SafeUrl>{
+    return new Promise((resolve, reject) => {resolve('assets/child.png');});
   }
 }
