6aecf9ac1758a781f43a16dea22e25c57ddd2591
Richard Nutt <richard.nutt@outlook.com>
2019-12-02 23:01:45 +0100

    removed obsolete code from test

--- src/app/children/child-block/child-block.component.spec.ts
+++ src/app/children/child-block/child-block.component.spec.ts
@@ -10,21 +10,12 @@ import {MockDatabase} from '../../database/mock-database';
 import {ChildrenService} from '../children.service';
 import {Database} from '../../database/database';
 import {EntitySchemaService} from '../../entity/schema/entity-schema.service';
-import { SessionService } from 'app/session/session.service';
 import { BlobService } from 'app/webdav/blob-service.service';
-import { DatabaseManagerService } from 'app/database/database-manager.service';
-import { AlertService } from 'app/alerts/alert.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Overlay } from '@angular/cdk/overlay';
-import { userInfo } from 'os';
-import { User } from 'app/user/user';
 import { MockBlobService } from 'app/webdav/mock-blob-service';
 
 describe('ChildBlockComponent', () => {
   let component: ChildBlockComponent;
   let fixture: ComponentFixture<ChildBlockComponent>;
-//  let sessionService = { getCurrentUser: () => new User('TestUser')};
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
