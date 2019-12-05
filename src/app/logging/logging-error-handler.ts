2d186d4804be697f2f097a6b05436728b57c97e7
Sebastian <sebastian.leidig@gmail.com>
2019-12-05 21:37:06 +0100

    fix(logging): clean up and update LoggingService and its link to Sentry
    * update to latest sentry sdk and use LoggerService consistently
    * do not log debug events to remote
    * add Angular ErrorHandler to catch and log exceptions globally
    
    see PR #384

---
+++ src/app/logging/logging-error-handler.ts
@@ -0,0 +1,14 @@
+import {ErrorHandler, Injectable} from '@angular/core';
+import {LoggingService} from './logging.service';
+
+@Injectable()
+export class LoggingErrorHandler implements ErrorHandler {
+  constructor(private logger: LoggingService) {}
+
+  handleError(error) {
+    this.logger.error(error);
+    // It is possible to show a feedback dialog to the user through Sentry:
+    // const eventId = Sentry.captureException(error.originalError || error);
+    // Sentry.showReportDialog({ eventId });
+  }
+}
