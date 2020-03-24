e407cd601c2e3f773ee537dcd20ec8cd3bb80171
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-03-24 16:39:29 +0100

    fix some 404 errors for undefined school ids

--- src/app/child-dev-project/schools/school-block/school-block.component.ts
+++ src/app/child-dev-project/schools/school-block/school-block.component.ts
@@ -19,12 +19,11 @@ export class SchoolBlockComponent implements OnInit {
               private entityMapper: EntityMapperService) {
   }
 
-  ngOnInit() {
-    if (this.entityId !== undefined) {
-      this.entityMapper.load(School, this.entityId).then(school => {
-        this.entity = school;
-      });
+  async ngOnInit() {
+    if (!this.entityId) {
+      return;
     }
+    this.entity = await this.entityMapper.load(School, this.entityId);
   }
 
   showTooltip() {
