e58fc0dfbba0316a9adcf3f59f2ae9b54c23f6e1
Tom Winter <mail@tom-winter.com>
2020-07-28 22:16:07 +0200

    feat(analytics): (wip) add auth and navigation tracking events

--- src/app/core/analytics/analytics.service.ts
+++ src/app/core/analytics/analytics.service.ts
@@ -18,7 +18,18 @@ export class AnalyticsService {
     AnalyticsService.setVersion();
   }
 
-  static eventTrack(action: string, properties?: any): void {
+  static eventTrack(
+    action: string,
+    properties: {
+      category: string;
+      label: string;
+      value: number;
+    } = {
+      category: "no_category",
+      label: "no_label",
+      value: -1
+    }
+  ): void {
     AnalyticsService.angulartics2Piwik.eventTrack(action, properties);
   }
 
