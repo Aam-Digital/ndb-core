7df20ba967af1c7788d68971ec55fd1b3686c7ab
Simon <therealslimv@yahoo.de>
2020-10-28 21:21:28 +0100

    extended config-fix and component map with config for child details section

--- src/app/core/view/dynamic-components-map.ts
+++ src/app/core/view/dynamic-components-map.ts
@@ -5,6 +5,14 @@ import { ChildrenCountDashboardComponent } from "app/child-dev-project/children/
 import { NoRecentNotesDashboardComponent } from "app/child-dev-project/notes/dashboard-widgets/no-recent-notes-dashboard/no-recent-notes-dashboard.component";
 import { RecentNotesDashboardComponent } from "app/child-dev-project/notes/dashboard-widgets/recent-notes-dashboard/recent-notes-dashboard.component";
 import { ProgressDashboardComponent } from "../../child-dev-project/progress-dashboard-widget/progress-dashboard/progress-dashboard.component";
+import { PreviousSchoolsComponent } from "../../child-dev-project/previous-schools/previous-schools.component";
+import { AserComponent } from "../../child-dev-project/aser/aser-component/aser.component";
+import { GroupedChildAttendanceComponent } from "../../child-dev-project/children/child-details/grouped-child-attendance/grouped-child-attendance.component";
+import { NotesOfChildComponent } from "../../child-dev-project/notes/notes-of-child/notes-of-child.component";
+import { HealthFormComponent } from "../../child-dev-project/children/child-details/form-components/health-form/health-form.component";
+import { HealthCheckupComponent } from "../../child-dev-project/health-checkup/health-checkup-component/health-checkup.component";
+import { EducationalMaterialComponent } from "../../child-dev-project/educational-material/educational-material-component/educational-material.component";
+import { DropoutFormComponent } from "../../child-dev-project/children/child-details/form-components/dropout-form/dropout-form.component";
 
 export const DYNAMIC_COMPONENTS_MAP = new Map<string, any>([
   ["ChildrenCountDashboard", ChildrenCountDashboardComponent],
@@ -14,4 +22,12 @@ export const DYNAMIC_COMPONENTS_MAP = new Map<string, any>([
   ["ProgressDashboard", ProgressDashboardComponent],
   ["AttendanceAverageDashboard", AttendanceAverageDashboardComponent],
   ["AttendanceWarningsDashboard", AttendanceWarningsDashboardComponent],
+  ["PreviousSchools", PreviousSchoolsComponent],
+  ["Aser", AserComponent],
+  ["GroupedChildAttendance", GroupedChildAttendanceComponent],
+  ["NotesOfChild", NotesOfChildComponent],
+  ["HealthForm", HealthFormComponent],
+  ["HealthCheckup", HealthCheckupComponent],
+  ["EducationalMaterial", EducationalMaterialComponent],
+  ["DropoutForm", DropoutFormComponent],
 ]);
