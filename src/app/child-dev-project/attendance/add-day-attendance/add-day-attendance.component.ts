355447f9286dd7ffe566a19c67eccab43fd884e3
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-12-07 13:57:42 +0100

    use eventnote entities for roll call

--- src/app/child-dev-project/attendance/add-day-attendance/add-day-attendance.component.ts
+++ src/app/child-dev-project/attendance/add-day-attendance/add-day-attendance.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { Child } from "../../children/model/child";
+import { EventNote } from "../model/event-note";
+import { EntityMapperService } from "../../../core/entity/entity-mapper.service";
 
 @Component({
   selector: "app-add-day-attendance",
@@ -11,23 +13,48 @@ export class AddDayAttendanceComponent {
 
   day = new Date();
   attendanceType: string;
-  students: Child[] = [];
+  selectedChildren: Child[] = [];
+  event: EventNote;
 
   stages = ["Setup Roll Call", "Select Student Group", "Roll Call"];
 
-  constructor() {}
+  constructor(private entityService: EntityMapperService) {}
 
   finishBasicInformationStage() {
     this.currentStage = 1;
   }
 
   finishStudentSelectionStage(selectedStudents: Child[]) {
-    this.students = selectedStudents;
+    this.selectedChildren = selectedStudents;
+
+    this.event = EventNote.create(this.day, this.attendanceType);
+    this.event.addChildren(
+      selectedStudents.sort(sortByChildClass).map((c) => c.getId())
+    );
 
     this.currentStage = 2;
   }
 
-  finishRollCallState() {
+  async finishRollCallState() {
+    await this.entityService.save(this.event);
     this.currentStage = 0;
+  }
+}
+
+function sortByChildClass(a: Child, b: Child) {
+  {
+    if (a.schoolClass === b.schoolClass) {
+      return 0;
+    }
+
+    const diff = parseInt(a.schoolClass, 10) - parseInt(b.schoolClass, 10);
+    if (!Number.isNaN(diff)) {
+      return diff;
+    }
+
+    if (a.schoolClass < b.schoolClass || b.schoolClass === undefined) {
+      return -1;
+    }
+    return 1;
   }
 }
--- src/app/child-dev-project/attendance/model/event-note.spec.ts
+++ src/app/child-dev-project/attendance/model/event-note.spec.ts
@@ -1,6 +1,5 @@
 import { EntitySchemaService } from "../../../core/entity/schema/entity-schema.service";
 import { async } from "@angular/core/testing";
-import { Entity } from "../../../core/entity/entity";
 import { EventAttendance } from "./event-attendance";
 import { AttendanceStatus } from "./attendance-status";
 import { EventNote } from "./event-note";
@@ -14,18 +13,15 @@ describe("EventNote", () => {
   }));
 
   it("has correct _id and entityId", function () {
-    const id = "test1";
-    const entity = new EventNote(id);
+    const entity = EventNote.create(new Date());
 
-    expect(entity.getId()).toBe(id);
+    expect(entity.getId()).toBeDefined();
     expect(entity.getType()).toBe(ENTITY_TYPE);
-    expect(Entity.extractEntityIdFromId(entity._id)).toBe(id);
   });
 
   it("has all and only defined schema fields in rawData", function () {
-    const id = "1";
     const expectedData = {
-      _id: ENTITY_TYPE + ":" + id,
+      _id: ENTITY_TYPE + ":",
 
       children: [
         new EventAttendance("1"),
@@ -34,13 +30,13 @@ describe("EventNote", () => {
       ],
       date: new Date(),
       activity: "Coaching",
-      author: "Max Musterman",
 
       searchIndices: [],
     };
 
-    const entity = new EventNote(id);
+    const entity = EventNote.create(new Date());
     Object.assign(entity, expectedData);
+    expectedData._id += entity.getId();
 
     const rawData = entitySchemaService.transformEntityToDatabaseFormat(entity);
 
@@ -56,10 +52,9 @@ describe("EventNote", () => {
         new EventAttendance("5"),
       ],
       activity: "Coaching",
-      author: "Max Musterman",
     };
 
-    const entity = new EventNote("");
+    const entity = EventNote.create(new Date());
     entitySchemaService.loadDataIntoEntity(entity, data);
 
     expect(entity.date).toEqual(new Date(data.date));
@@ -69,7 +64,18 @@ describe("EventNote", () => {
     expect(entity.children[1]).toEqual(
       new EventAttendance("2", AttendanceStatus.ABSENT, "ill")
     );
-    expect(entity.author).toEqual("Max Musterman");
     expect(entity.activity).toEqual("Coaching");
+  });
+
+  it("adds childIds with full event attendance defaults", () => {
+    const testChildIds = ["1", "99"];
+
+    const entity = EventNote.create(new Date());
+    entity.addChildren(testChildIds);
+
+    expect(entity.children).toEqual([
+      new EventAttendance(testChildIds[0], AttendanceStatus.UNKNOWN, ""),
+      new EventAttendance(testChildIds[1], AttendanceStatus.UNKNOWN, ""),
+    ]);
   });
 });
