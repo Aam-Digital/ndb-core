7ea9990445a52ce1c8f0d9c0350e7dd1faa90ae6
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-12-08 11:43:22 +0100

    docs(storybook): add story for roll call

--- src/app/child-dev-project/children/child-block/child-block.stories.ts
+++ src/app/child-dev-project/children/child-block/child-block.stories.ts
@@ -1,8 +1,7 @@
 import { Story, Meta } from "@storybook/angular/types-6-0";
 import { ChildBlockComponent } from "./child-block.component";
 import { Child } from "../model/child";
-import { BehaviorSubject } from "rxjs";
-import { SafeUrl } from "@angular/platform-browser";
+import { addDefaultChildPhoto } from "../../../../../.storybook/utils/addDefaultChildPhoto";
 
 export default {
   title: "Child Dev Project/ChildBlock",
@@ -12,7 +11,7 @@ export default {
 
 const demoChild = new Child("1");
 demoChild.name = "John Doe";
-demoChild.photo = new BehaviorSubject<SafeUrl>("assets/child.png");
+addDefaultChildPhoto(demoChild);
 demoChild.projectNumber = "99";
 demoChild.phone = "+49 199 1234567";
 demoChild.schoolClass = "5";
