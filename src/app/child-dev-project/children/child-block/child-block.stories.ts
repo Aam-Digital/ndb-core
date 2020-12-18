d36407a99496947d7a30edd333082f6b2640807c
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-12-18 10:59:01 +0100

    fix(*): new default photo and round shape

--- src/app/child-dev-project/children/child-block/child-block.stories.ts
+++ src/app/child-dev-project/children/child-block/child-block.stories.ts
@@ -3,18 +3,26 @@ import { ChildBlockComponent } from "./child-block.component";
 import { Child } from "../model/child";
 import { BehaviorSubject } from "rxjs";
 import { SafeUrl } from "@angular/platform-browser";
+import { moduleMetadata } from "@storybook/angular";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { CommonModule } from "@angular/common";
 
 export default {
   title: "Child Dev Project/ChildBlock",
   component: ChildBlockComponent,
-  argTypes: {},
+  decorators: [
+    moduleMetadata({
+      imports: [CommonModule, FlexLayoutModule],
+    }),
+  ],
 } as Meta;
 
 const demoChild = new Child("1");
 demoChild.name = "John Doe";
 demoChild.photo = new BehaviorSubject<SafeUrl>("assets/child.png");
 demoChild.projectNumber = "99";
-demoChild.phone = "+49 199 1234567";
+// @ts-ignore
+demoChild.phone = "+49 199 1234567"; // @ts-ignore
 demoChild.schoolClass = "5";
 
 const Template: Story<ChildBlockComponent> = (args: ChildBlockComponent) => ({
