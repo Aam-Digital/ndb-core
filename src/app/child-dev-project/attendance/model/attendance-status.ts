16f31667aeec5a9bd6cc439024648d11b974fb05
Sebastian Leidig <sebastian.leidig@gmail.com>
2020-12-19 13:41:24 +0100

    simplify roll call component and make options configurable

--- src/app/child-dev-project/attendance/model/attendance-status.ts
+++ src/app/child-dev-project/attendance/model/attendance-status.ts
@@ -6,3 +6,26 @@ export enum AttendanceStatus {
   LATE = "L",
   EXCUSED = "E",
 }
+
+export class AttendanceStatusType {
+  static NONE = {
+    status: AttendanceStatus.UNKNOWN,
+    name: "?",
+    shortName: "?",
+    color: "#DDDDDD",
+  };
+
+  /**
+   * match to enum - can probably be removed once completed moved into config?
+   */
+  status: AttendanceStatus;
+
+  /** a short (one letter) representation e.g. used in calendar view */
+  shortName: string;
+
+  /** a clear, spelled out title of the status */
+  name: string;
+
+  /** a color code representing the status */
+  color?: string;
+}
