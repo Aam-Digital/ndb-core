a37c50808d73b214d9ce42579ee5572f5728ca43
Sebastian Leidig <sebastian.leidig@gmail.com>
2021-01-08 13:07:35 +0100

    make recurring activity styling for activity-card manually configurable

--- src/app/child-dev-project/attendance/activity-card/activity-card.component.ts
+++ src/app/child-dev-project/attendance/activity-card/activity-card.component.ts
@@ -7,6 +7,30 @@ import { Note } from "../../notes/model/note";
   styleUrls: ["./activity-card.component.scss"],
 })
 export class ActivityCardComponent {
+  /**
+   * The Note representing the event to be displayed
+   */
   @Input() event: Note;
+
+  /**
+   * Whether the card is visualized as selected
+   */
   @Input() highlighted: boolean;
+
+  private _displayAsRecurring: boolean | null = null;
+  /**
+   * Whether the event or activity is displayed in the style of events generated from a generic recurring activity.
+   *
+   * If not explicitly defined, it is inferred from the given event Note's reference to an activity.
+   */
+  @Input() set recurring(value: boolean) {
+    this._displayAsRecurring = value;
+  }
+  get recurring(): boolean {
+    if (this._displayAsRecurring !== null) {
+      return this._displayAsRecurring;
+    } else {
+      return !!this.event?.relatesTo;
+    }
+  }
 }
