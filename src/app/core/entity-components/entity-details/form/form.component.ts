c0d108bbd98761654669c9e5a387efc9c07486c3
Sebastian <sebastian.leidig@gmail.com>
2021-01-20 12:41:34 +0100

    Update src/app/core/entity-components/entity-details/form/form.component.ts
    Co-authored-by: Simon <33730997+TheSlimvReal@users.noreply.github.com>

--- src/app/core/entity-components/entity-details/form/form.component.ts
+++ src/app/core/entity-components/entity-details/form/form.component.ts
@@ -98,7 +98,6 @@ export class FormComponent implements OnInitDynamicComponent {
     const formConfig = {};
     this.config.cols.forEach((c: FormFieldConfig[]) =>
       c.forEach((r) => {
-        console.log("field", r);
         formConfig[r.id] = [
           { value: this.entity[r.id], disabled: !this.editing },
         ];
