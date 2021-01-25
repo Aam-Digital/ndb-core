05ca5ff2325c8ea2e55b21d6b01a63f52c62bb8c
Sebastian Leidig <sebastian.leidig@gmail.com>
2021-01-25 19:38:05 +0100

    feat(demo): generate events and attendance for recurring activities

--- src/app/core/config/config-fix.ts
+++ src/app/core/config/config-fix.ts
@@ -1,4 +1,8 @@
-{
+import { defaultAttendanceStatusTypes } from "./default-config/default-attendance-status-types";
+import { defaultInteractionTypes } from "./default-config/default-interaction-types";
+
+// prettier-ignore
+export const defaultConfig = {
   "navigationMenu": {
     "items": [
       {
@@ -60,154 +64,9 @@
   },
 
 
-  "enum:interaction-type": [
-    {
-      "id": "",
-      "label": ""
-    },
-    {
-      "id": "HOME_VISIT",
-      "label": "Home Visit"
-    },
-    {
-      "id": "GUARDIAN_TALK",
-      "label": "Talk with Guardians"
-    },
-    {
-      "id": "CHILD_TALK",
-      "label": "Talk with Child"
-    },
-    {
-      "id": "INCIDENT",
-      "label": "Incident"
-    },
-    {
-      "id": "DISCUSSION",
-      "label": "Discussion/Decision",
-      "color": "#E1BEE7"
-    },
-    {
-      "id": "VISIT",
-      "label": "School/Hostel Visit"
-    },
-    {
-      "id": "PHONE_CALL",
-      "label": "Phone Call"
-    },
-    {
-      "id": "COACHING_TALK",
-      "label": "Talk with Coaching Teacher"
-    },
-    {
-      "id": "PEER_TALK",
-      "label": "Talk with Peer"
-    },
-    {
-      "id": "NEIGHBOUR_TALK",
-      "label": "Talk with Neighbours"
-    },
-    {
-      "id": "GUARDIAN_MEETING",
-      "label": "Guardians' Meeting",
-      "color": "#E1F5FE",
-      "isMeeting": true
-    },
-    {
-      "id": "CHILDREN_MEETING",
-      "label": "Children's Meeting",
-      "color": "#E1F5FE",
-      "isMeeting": true
-    },
-    {
-      "id": "DAILY_ROUTINE",
-      "label": "Daily Routine",
-      "color": "#F1F8E9"
-    },
-    {
-      "id": "ANNUAL_SURVEY",
-      "label": "Annual Survey",
-      "color": "#FFFDE7"
-    },
-    {
-      "id": "EXCURSION",
-      "label": "Excursion/Trip",
-      "color": "#E1F5FE",
-      "isMeeting": true
-    },
-    {
-      "id": "PARTNER_CONTACT",
-      "label": "Contact with other partners (club/NGO/...)"
-    },
-    {
-      "id": "RATION_DISTRIBUTION",
-      "label": "Ration Distribution",
-      "color": "#E1F5FE",
-      "isMeeting": true
-    },
-    {
-      "id": "COACHING_CLASS",
-      "label": "Coaching Class",
-      "color": "#EEEEEE",
-      "isMeeting": true
-    },
-    {
-      "id": "SCHOOL_CLASS",
-      "label": "School Class",
-      "color": "#EEEEEE",
-      "isMeeting": true
-    },
-    {
-      "id": "LIFE_SKILLS",
-      "label": "Life Skills Workshop",
-      "color": "#E1F5FE",
-      "isMeeting": true
-    }
-  ],
+  "enum:interaction-type": defaultInteractionTypes,
 
-  "enum:attendance-status": [
-    {
-      "id": "PRESENT",
-      "shortlabel": "P",
-      "label": "Present",
-      "style": "attendance-P",
-      "countAs": "PRESENT"
-    },
-    {
-      "id": "ABSENT",
-      "shortlabel": "A",
-      "label": "Absent",
-      "style": "attendance-A",
-      "countAs": "ABSENT"
-    },
-    {
-      "id": "LATE",
-      "shortlabel": "L",
-      "label": "Late",
-      "style": "attendance-L",
-      "countAs": "PRESENT"
-    },
-    {
-      "id": "HOLIDAY",
-      "shortlabel": "H",
-      "label": "Holiday",
-      "style": "attendance-H",
-      "countAs": "IGNORE"
-    },
-    {
-      "id": "EXCUSED",
-      "shortlabel": "E",
-      "label": "Excused",
-      "style": "attendance-E",
-      "countAs": "IGNORE"
-    },
-    {
-      "id": "UNKNOWN",
-      "shortlabel": "?",
-      "label": "Skip",
-      "style": "attendance-U",
-      "countAs": "IGNORE"
-    }
-  ],
+  "enum:attendance-status": defaultAttendanceStatusTypes,
 
 
   "view:": {
