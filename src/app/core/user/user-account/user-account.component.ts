381482f2a8e2bda277b6c41a2b9b30ec9d0316a7
Simon <33730997+TheSlimvReal@users.noreply.github.com>
2021-01-29 16:21:06 +0100

    feat: allow users to change own password in their user profile (#602)
    closes #17 
    
    Co-authored-by: Sebastian Leidig <sebastian.leidig@gmail.com>

--- src/app/core/user/user-account/user-account.component.ts
+++ src/app/core/user/user-account/user-account.component.ts
@@ -20,6 +20,10 @@ import { User } from "../user";
 import { SessionService } from "../../session/session-service/session.service";
 import { EntityMapperService } from "../../entity/entity-mapper.service";
 import { WebdavModule } from "../../webdav/webdav.module";
+import { UserAccountService } from "./user-account.service";
+import { FormBuilder, ValidationErrors, Validators } from "@angular/forms";
+import { AppConfig } from "../../app-config/app-config";
+import { LoggingService } from "../../logging/logging.service";
 
 /**
  * User account form to allow the user to view and edit information.
@@ -36,30 +40,93 @@ export class UserAccountComponent implements OnInit {
   /** whether webdav integration is configured and the cloud settings section should be displayed */
   webdavEnabled = WebdavModule.isEnabled;
 
+  /** whether password change is disallowed because of demo mode */
+  disabledForDemoMode: boolean;
+  disabledForOfflineMode: boolean;
+
+  passwordChangeResult: { success: boolean; error?: any };
+
+  passwordForm = this.fb.group(
+    {
+      currentPassword: ["", Validators.required],
+      newPassword: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/[A-Z]/),
+          Validators.pattern(/[a-z]/),
+          Validators.pattern(/[0-9]/),
+          Validators.pattern(/[^A-Za-z0-9]/),
+        ],
+      ],
+      confirmPassword: ["", [Validators.required]],
+    },
+    { validators: () => this.passwordMatchValidator() }
+  );
+
   constructor(
     private entityMapperService: EntityMapperService,
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private userAccountService: UserAccountService,
+    private fb: FormBuilder,
+    private loggingService: LoggingService
   ) {}
 
   ngOnInit() {
+    this.checkIfPasswordChangeAllowed();
     this.user = this.sessionService.getCurrentUser();
   }
 
-  /**
-   * Change the user's password.
-   *
-   * @todo This is not implemented yet!
-   *
-   * @param pwd New password to be set
-   * @param rpwd Confirmation of new password (second input by the user to prevent typos)
-   */
-  changePassword(pwd, rpwd) {
-    if (pwd === rpwd) {
-      // TODO: update the password for this remote database user first and deny the password change if that fails
-      this.user.setNewPassword(pwd);
-      // TODO: Show success message
-    } else {
-      // TODO: Show error message
+  checkIfPasswordChangeAllowed() {
+    this.disabledForDemoMode = false;
+    this.disabledForOfflineMode = false;
+    this.passwordForm.enable();
+
+    if (AppConfig.settings.database.useTemporaryDatabase) {
+      this.disabledForDemoMode = true;
+      this.passwordForm.disable();
+    } else if (!navigator.onLine) {
+      this.disabledForOfflineMode = true;
+      this.passwordForm.disable();
     }
+  }
+
+  changePassword() {
+    const currentPassword = this.passwordForm.get("currentPassword").value;
+    if (!this.user.checkPassword(currentPassword)) {
+      this.passwordForm
+        .get("currentPassword")
+        .setErrors({ incorrectPassword: true });
+      return;
+    }
+
+    const newPassword = this.passwordForm.get("newPassword").value;
+    this.userAccountService
+      .changePassword(this.user, currentPassword, newPassword)
+      .then(() => this.sessionService.login(this.user.name, newPassword))
+      .then(() => {
+        this.passwordChangeResult = { success: true };
+      })
+      .catch((err: Error) => {
+        this.passwordChangeResult = { success: false, error: err.message };
+        this.loggingService.warn({
+          error: "password change failed",
+          details: err.message,
+        });
+      });
+  }
+
+  private passwordMatchValidator(): ValidationErrors | null {
+    const newPassword: string = this?.passwordForm?.get("newPassword")?.value;
+    const confirmPassword: string = this?.passwordForm?.get("confirmPassword")
+      ?.value;
+    if (newPassword !== confirmPassword) {
+      this.passwordForm
+        .get("confirmPassword")
+        .setErrors({ passwordConfirmationMismatch: true });
+      return { passwordConfirmationMismatch: true };
+    }
+    return null;
   }
 }
---
+++ src/app/core/user/user-account/user-account.service.spec.ts
@@ -0,0 +1,84 @@
+import { TestBed } from "@angular/core/testing";
+
+import { UserAccountService } from "./user-account.service";
+import { EntityMapperService } from "../../entity/entity-mapper.service";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../user";
+import { of } from "rxjs";
+
+describe("UserAccountService", () => {
+  let service: UserAccountService;
+  let mockEntityMapper: jasmine.SpyObj<EntityMapperService>;
+  let mockHttpClient: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    mockEntityMapper = jasmine.createSpyObj("mockEntityMapper", ["save"]);
+    mockHttpClient = jasmine.createSpyObj("mockHttpClient", ["get", "put"]);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: EntityMapperService, useValue: mockEntityMapper },
+        { provide: HttpClient, useValue: mockHttpClient },
+      ],
+    });
+    service = TestBed.inject(UserAccountService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should reject if old password is incorrect", (done) => {
+    const user = new User("TestUser");
+    user.setNewPassword("testPW");
+    service
+      .changePassword(user, "wrongPW", "")
+      .then(() => fail())
+      .catch((err) => {
+        expect(err).toBeDefined();
+        done();
+      });
+  });
+
+  it("should call report error when CouchDB not available", (done) => {
+    const user = new User("TestUser");
+    user.setNewPassword("testPW");
+    mockHttpClient.get.and.throwError(new Error("error"));
+    service
+      .changePassword(user, "testPW", "")
+      .then(() => fail())
+      .catch((err) => {
+        expect(mockHttpClient.get).toHaveBeenCalled();
+        expect(err).toBeDefined();
+        done();
+      });
+  });
+
+  it("should call report error when new Password cannot be saved", (done) => {
+    const user = new User("TestUser");
+    user.setNewPassword("testPW");
+    mockHttpClient.get.and.returnValues(of({}));
+    mockHttpClient.put.and.throwError(new Error("error"));
+    service
+      .changePassword(user, "testPW", "")
+      .then(() => fail())
+      .catch((err) => {
+        expect(mockHttpClient.get).toHaveBeenCalled();
+        expect(mockHttpClient.put).toHaveBeenCalled();
+        expect(err).toBeDefined();
+        done();
+      });
+  });
+
+  it("should return User with new password", (done) => {
+    const user = new User("TestUser");
+    user.setNewPassword("testPW");
+    mockHttpClient.get.and.returnValues(of({}));
+    mockHttpClient.put.and.returnValues(of({}));
+    mockEntityMapper.save.and.resolveTo(true);
+    service.changePassword(user, "testPW", "newPW").then((res) => {
+      expect(res.checkPassword("testPW")).toBeFalse();
+      expect(res.checkPassword("newPW")).toBeTrue();
+      done();
+    });
+  });
+});
