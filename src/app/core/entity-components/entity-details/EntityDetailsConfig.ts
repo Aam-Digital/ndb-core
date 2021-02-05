2736f2a689b34a3f194f1a1874c2d7a19e914081
Simon <therealslimv@yahoo.de>
2021-02-05 12:44:10 +0100

    created enum document status and referenced it in the config

--- src/app/core/entity-components/entity-details/EntityDetailsConfig.ts
+++ src/app/core/entity-components/entity-details/EntityDetailsConfig.ts
@@ -22,3 +22,37 @@ export class PanelConfig {
   creatingNew?: boolean;
   config?: any;
 }
+
+export class FormConfig {
+  /**
+   * The input type for the form.
+   * Available options: "photo", "text", "textarea", "checkbox", "age", "select", "configurable-enum-select", "datepicker"
+   */
+  input: string;
+
+  /**
+   * The id of the entity which should be accessed
+   */
+  id: string;
+
+  /**
+   * A placeholder or description of the expected input
+   */
+  placeholder: string;
+
+  /**
+   * If required is set to "true", the form cannot be saved if the field is empty.
+   * Default to false
+   */
+  required: boolean;
+
+  /**
+   * The options in case `input="select"` is used.
+   */
+  options?: string[];
+
+  /**
+   * The id of the enum in case `input="configurable-enum-select" is used.
+   */
+  enumId?: string;
+}
