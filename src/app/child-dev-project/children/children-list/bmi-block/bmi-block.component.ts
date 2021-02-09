d68793369b2675bb5a363df5e0bf810cea97c520
Lorsbyjm <jmuegge@gmx.de>
2021-02-09 20:53:50 +0100

    feat(354): BMI in visible in Child Details

---
+++ src/app/child-dev-project/children/children-list/bmi-block/bmi-block.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { HealthCheck } from "app/child-dev-project/health-checkup/model/health-check";
+import { ColumnCellConfig } from "app/core/entity-components/entity-list/EntityListConfig";
+import { OnInitDynamicComponent } from "app/core/view/dynamic-components/on-init-dynamic-component.interface";
+import { ChildrenService } from "../../children.service";
+
+@UntilDestroy()
+@Component({
+  selector: "app-bmi-block",
+  template: `<span
+    class="mat-elevation-z1 attendance-block w-{{
+      currentHealthCheck?.getWarningLevel()
+    }}"
+  >
+    {{ currentHealthCheck?.bmi.toFixed(2) }}
+  </span>`,
+  styleUrls: ["./bmi-block.component.scss"],
+})
+export class BmiBlockComponent implements OnInitDynamicComponent {
+  public currentHealthCheck: HealthCheck;
+
+  constructor(private childrenService: ChildrenService) {}
+
+  onInitFromDynamicConfig(config: ColumnCellConfig) {
+    this.childrenService
+      .getHealthChecksOfChild(config.entity.getId())
+      .pipe(untilDestroyed(this))
+      .subscribe((results) => {
+        if (results.length > 0) {
+          this.currentHealthCheck = results.reduce((prev, cur) =>
+            cur.date > prev.date ? cur : prev
+          );
+        }
+      });
+  }
+}
