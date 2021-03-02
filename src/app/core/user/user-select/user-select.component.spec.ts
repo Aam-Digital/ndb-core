4ba825d163251d5f89bcfbd54b2acb5697f52e29
Schottkyc137 <lukas2121@icloud.com>
2021-03-02 23:30:43 +0100

    Implemented User autocomplete dropdown

---
+++ src/app/core/user/user-select/user-select.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { UserSelectComponent } from "./user-select.component";
+
+describe("UserSelectComponent", () => {
+  let component: UserSelectComponent;
+  let fixture: ComponentFixture<UserSelectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserSelectComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/user/user-select/user-select.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { EntityMapperService } from "../../entity/entity-mapper.service";
+import { User } from "../user";
+import { MatAutocompleteTrigger } from "@angular/material/autocomplete";
+
+@Component({
+  selector: "app-user-select",
+  templateUrl: "./user-select.component.html",
+  styleUrls: ["./user-select.component.scss"],
+})
+export class UserSelectComponent implements OnInit {
+  @Input() user: string;
+  @Output() userChange = new EventEmitter<string>();
+
+  users: Array<User>;
+  suggestions: Array<User>;
+
+  @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
+
+  constructor(private entityMapperService: EntityMapperService) {}
+
+  ngOnInit(): void {
+    this.entityMapperService.loadType<User>(User).then((users) => {
+      this.users = users;
+      this.onInputChanged();
+    });
+  }
+
+  selectUser(user: User) {
+    this.user = user.name;
+    this.userChange.emit(this.user);
+  }
+
+  onInputChanged(): void {
+    this.suggestions = this.users.filter((otherUser) =>
+      otherUser.name.toLowerCase().startsWith(this.user)
+    );
+    this.userChange.emit(this.user);
+  }
+}
