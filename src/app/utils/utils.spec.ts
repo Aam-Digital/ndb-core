b4bf31c8871e3c1859a134b5ba3eb6681b4c45c7
Simon <therealslimv@yahoo.de>
2021-03-04 15:48:59 +0100

    added test for age calculation

--- src/app/utils/utils.spec.ts
+++ src/app/utils/utils.spec.ts
@@ -1,4 +1,5 @@
-import { isValidDate } from "./utils";
+import { calculateAge, isValidDate } from "./utils";
+import moment from "moment";
 
 describe("Utils", () => {
   it("isValidDate for normal Date object", () => {
@@ -14,5 +15,11 @@ describe("Utils", () => {
   it("not isValidDate for Date object with invalid value", () => {
     const testDate = new Date("foo");
     expect(isValidDate(testDate)).toBeFalse();
+  });
+
+  it("should calculate age correctly", () => {
+    const dob = moment().subtract(9, "years");
+    const age = calculateAge(dob.toDate());
+    expect(age).toBe(9);
   });
 });
