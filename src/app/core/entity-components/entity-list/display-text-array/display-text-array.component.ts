99b063ecca089e4946f834a570097612ce46193a
Schottkyc137 <lukas2121@icloud.com>
2021-03-04 14:02:15 +0100

    Text-size will not grow indefinitely

--- src/app/core/entity-components/entity-list/display-text-array/display-text-array.component.ts
+++ src/app/core/entity-components/entity-list/display-text-array/display-text-array.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { OnInitDynamicComponent } from "../../../view/dynamic-components/on-init-dynamic-component.interface";
 
 @Component({
@@ -11,7 +11,16 @@ export class DisplayTextArrayComponent implements OnInitDynamicComponent {
   constructor() {}
 
   textArrayAsText(): string {
-    return this.textArray.join(", ");
+    if (this.textArray.length > 2) {
+      return (
+        this.textArray.slice(0, 2).join(", ") +
+        " and " +
+        String(this.textArray.length - 2) +
+        " more"
+      );
+    } else {
+      return this.textArray.join(", ");
+    }
   }
 
   onInitFromDynamicConfig(config: any) {
