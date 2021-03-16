94f9d90ed91f10914a51934e72a530af1e9e76db
Simon <therealslimv@yahoo.de>
2021-03-16 18:59:10 +0100

    changed query function to return single result

--- src/app/child-dev-project/reporting/aggregation.service.ts
+++ src/app/child-dev-project/reporting/aggregation.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from "@angular/core";
 import { EntityMapperService } from "../../core/entity/entity-mapper.service";
 import { Entity } from "../../core/entity/entity";
 import { Child } from "../children/model/child";
+import { School } from "../schools/model/school";
+import { Note } from "../notes/model/note";
+import { RecurringActivity } from "../attendance/model/recurring-activity";
+import { EventNote } from "../attendance/model/event-note";
 
 export interface PropertyQuery {
   [key: string]: any;
@@ -11,33 +15,116 @@ export interface PropertyQuery {
   providedIn: "root",
 })
 export class AggregationService {
+  private static readonly ENTITY_CLASSES: typeof Entity[] = [
+    Child,
+    School,
+    RecurringActivity,
+    Note,
+    EventNote,
+  ];
+
   private entities = new Map<typeof Entity, Entity[]>();
-  private dataPromise: Promise<any>;
+  private dataLoaded: Promise<any>;
   constructor(private entityMapper: EntityMapperService) {}
 
-  public loadData() {
-    this.dataPromise = this.entityMapper
-      .loadType<Child>(Child)
-      .then((children) => this.entities.set(Child, children));
+  public loadData(): void {
+    this.dataLoaded = Promise.all(
+      AggregationService.ENTITY_CLASSES.map((entityClass) =>
+        this.entityMapper
+          .loadType(entityClass)
+          .then((entities) => this.entities.set(entityClass, entities))
+      )
+    ).then(() => this.resolveRelations());
+  }
+
+  private resolveRelations() {
+    const schoolMap = new Map(
+      this.entities
+        .get(School)
+        .map((school) => [school.getId(), school as School])
+    );
+    this.entities
+      .get(RecurringActivity)
+      .forEach((activity: RecurringActivity) =>
+        this.addSchoolToActivity(activity, schoolMap)
+      );
+    const activityMap = new Map(
+      this.entities
+        .get(RecurringActivity)
+        .map((activity) => [activity._id, activity as RecurringActivity])
+    );
+    this.entities.get(EventNote).forEach((event: EventNote) => {
+      this.addActivityToEvent(event, activityMap);
+    });
+  }
+
+  private addSchoolToActivity(
+    activity: RecurringActivity,
+    schoolMap: Map<string, School>
+  ) {
+    if (
+      activity.linkedGroups &&
+      activity.linkedGroups.length === 1 &&
+      schoolMap.has(activity.linkedGroups[0])
+    ) {
+      activity[School.ENTITY_TYPE] = schoolMap.get(activity.linkedGroups[0]);
+    }
+  }
+
+  private addActivityToEvent(
+    event: EventNote,
+    activityMap: Map<string, RecurringActivity>
+  ) {
+    if (event.relatesTo && activityMap.has(event.relatesTo)) {
+      event[RecurringActivity.ENTITY_TYPE] = activityMap.get(event.relatesTo);
+    }
   }
 
   public async countEntitiesByProperties(
     entityClass: typeof Entity,
-    propertyQueries: PropertyQuery[]
-  ): Promise<number[]> {
-    await this.dataPromise;
-    const count = Array<number>(propertyQueries.length).fill(0);
+    propertyQuery: PropertyQuery
+  ): Promise<number> {
+    await this.dataLoaded;
+    let count = 0;
     for (const entity of this.entities.get(entityClass)) {
-      for (let i = 0; i < propertyQueries.length; i++) {
-        if (
-          Object.keys(propertyQueries[i]).every(
-            (key) => entity[key] === propertyQueries[i][key]
-          )
-        ) {
-          count[i]++;
-        }
+      if (this.entitySatisfiesQuery(entity, propertyQuery)) {
+        count++;
       }
     }
     return count;
+  }
+
+  private entitySatisfiesQuery(
+    entity: Entity,
+    propertyQuery: PropertyQuery
+  ): boolean {
+    const entityTypes = AggregationService.ENTITY_CLASSES.map(
+      (type) => type.ENTITY_TYPE
+    );
+    return Object.keys(propertyQuery).every((key) => {
+      if (entity[key] && entityTypes.includes(key)) {
+        return this.entitySatisfiesQuery(entity[key], propertyQuery[key]);
+      } else if (typeof propertyQuery[key] === "object") {
+        return this.valueSatisfiesComplexChecks(
+          entity[key],
+          propertyQuery[key]
+        );
+      } else {
+        return entity[key] === propertyQuery[key];
+      }
+    });
+  }
+
+  private valueSatisfiesComplexChecks(value: any, checks: any): boolean {
+    return Object.keys(checks).every((check) => {
+      switch (check) {
+        case "gte":
+          return value >= checks[check];
+        case "not":
+          return !(value === checks[check]);
+        default:
+          return false;
+      }
+    });
   }
 }
