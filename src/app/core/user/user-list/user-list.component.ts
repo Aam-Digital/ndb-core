b60aeb1698f1fbb2d7e292578cd96f6827b474fe
Schottkyc137 <lukas2121@icloud.com>
2021-03-29 15:24:24 +0200

    Add promise catch

--- src/app/core/user/user-list/user-list.component.ts
+++ src/app/core/user/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import { User } from "../user";
 import { EntityMapperService } from "../../entity/entity-mapper.service";
 import { OnInitDynamicComponent } from "../../view/dynamic-components/on-init-dynamic-component.interface";
+import { LoggingService } from "../../logging/logging.service";
 
 @Component({
   selector: "app-user-list",
@@ -12,18 +13,26 @@ export class UserListComponent implements OnInitDynamicComponent {
 
   @Input() set entities(entities: (User | string)[]) {
     if (this.inputType === "id") {
-      this.entityMapperService.loadType<User>(User).then((users) => {
-        this._users = users.filter((u) =>
-          entities.find((e: string) => e === u.getId())
-        );
-      });
+      this.entityMapperService
+        .loadType<User>(User)
+        .then((users) => {
+          this._users = users.filter((u) =>
+            entities.find((e: string) => e === u.getId())
+          );
+        })
+        .catch((error) => {
+          this.loggingService.error(error);
+        });
     } else {
       this._users = entities as User[];
     }
   }
   @Input() inputType: "id" | "entity" = "id";
   @Input() maxUserThreshold = 2;
-  constructor(private entityMapperService: EntityMapperService) {}
+  constructor(
+    private entityMapperService: EntityMapperService,
+    private loggingService: LoggingService
+  ) {}
 
   get authorNames(): string {
     if (this._users.length <= this.maxUserThreshold) {
