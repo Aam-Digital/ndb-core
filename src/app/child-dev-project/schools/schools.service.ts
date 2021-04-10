e9230a7b5d17ff2cebfce28293ca9e724d730da2
Simon <therealslimv@yahoo.de>
2021-04-10 18:36:31 +0200

    undone performance optimization

--- src/app/child-dev-project/schools/schools.service.ts
+++ src/app/child-dev-project/schools/schools.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@angular/core";
+import { School } from "./model/school";
+import { EntityMapperService } from "../../core/entity/entity-mapper.service";
+import { from, Observable } from "rxjs";
 import { Child } from "../children/model/child";
 import { ChildrenService } from "../children/children.service";
 import { LoggingService } from "../../core/logging/logging.service";
@@ -6,22 +9,30 @@ import { LoggingService } from "../../core/logging/logging.service";
 @Injectable()
 export class SchoolsService {
   constructor(
-    private childrenService: ChildrenService
+    private entityMapper: EntityMapperService,
+    private childrenService: ChildrenService,
+    private log: LoggingService
   ) {}
+
+  getSchools(): Observable<School[]> {
+    return from(this.entityMapper.loadType<School>(School));
+  }
 
   async getChildrenForSchool(schoolId: string): Promise<Child[]> {
     const relations = await this.childrenService.queryRelationsOf(
       "school",
       schoolId
     );
-    const childrenPromises = relations.map((rel) =>
-      this.childrenService
-        .getChild(rel.childId)
-        .toPromise()
-        .catch(() => null)
-    );
-    let children = await Promise.all(childrenPromises);
-    children = children.filter((child) => !!child);
+    const children: Child[] = [];
+    for (const relation of relations) {
+      try {
+        children.push(
+          await this.childrenService.getChild(relation.childId).toPromise()
+        );
+      } catch (e) {
+        this.log.warn("Could not find child " + relation.childId);
+      }
+    }
     return children;
   }
 }
