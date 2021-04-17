b685da05652f83d1457cdb9de42c59630d8619f7
Schottkyc137 <lukas2121@icloud.com>
2021-04-17 17:51:52 +0200

    Refactored mock-entity-mapper-service

--- src/app/core/entity/mock-entity-mapper-service.ts
+++ src/app/core/entity/mock-entity-mapper-service.ts
@@ -3,23 +3,88 @@ import { EntityMapperService } from "./entity-mapper.service";
 import { UpdatedEntity } from "./entity-update";
 import { NEVER, Observable } from "rxjs";
 
-export function mockEntityMapper(withData: Entity[] = []): EntityMapperService {
+export function mockEntityMapper(
+  withData: Entity[] = []
+): MockEntityMapperService {
   const ems = new MockEntityMapperService();
-  const data = new Map<string, Entity[]>();
-  withData.forEach((e) => {
-    if (!data.get(e.getType())) {
-      data.set(e.getType(), []);
-    }
-    data.get(e.getType()).push(e);
-  });
-  ems.data = data;
+  ems.addAll(withData);
   return ems;
 }
 
+/**
+ * Mocked entity mapper. Can/should only be used whenever some data should be saved
+ * and loaded. This is not to be used to mock database/anything related to the Schema-service.
+ */
+
 export class MockEntityMapperService extends EntityMapperService {
-  data: Map<string, Entity[]> = new Map();
+  private data: Map<string, Map<string, Entity>> = new Map();
   constructor() {
     super(null, null);
+  }
+
+  /**
+   * like {@link save}, but synchronous
+   * @param entity The entity to add
+   */
+
+  public add(entity: Entity) {
+    const type = entity.getType();
+    if (!this.data.get(type)) {
+      this.data.set(type, new Map());
+    }
+    this.data.get(type).set(entity.getId(), entity);
+  }
+
+  /**
+   * returns whether or not the given entity is known
+   * @param entity
+   */
+
+  public contains(entity: Entity): boolean {
+    return (
+      this.data.has(entity.getType()) &&
+      this.data.get(entity.getType()).has(entity.getId())
+    );
+  }
+
+  /**
+   * Add a bunch of entities
+   * @param entities The entities to add
+   */
+
+  public addAll(entities: Entity[]) {
+    entities.forEach((e) => this.add(e));
+  }
+
+  /**
+   * like {@link load} but synchronous
+   * @param entityType
+   * @param id
+   */
+
+  public get(entityType: string, id: string): Entity {
+    return this.data.get(entityType)?.get(id);
+  }
+
+  /**
+   * like {@link loadType} but synchronous
+   * @param entityType
+   */
+
+  public getAll<T extends Entity>(entityType: string): T[] {
+    return ([...this.data.get(entityType).values()] || []) as T[];
+  }
+
+  /**
+   * like {@link remove} but synchronous
+   * @param entity
+   */
+
+  public delete(entity: Entity) {
+    const entities = this.data.get(entity.getType());
+    if (entities) {
+      entities.delete(entity.getId());
+    }
   }
 
   public async load<T extends Entity>(
@@ -27,34 +92,26 @@ export class MockEntityMapperService extends EntityMapperService {
     id: string
   ): Promise<T> {
     const type = new entityType().getType();
-    return this.data.get(type)?.find((e) => e.getId() === id) as T;
+    return this.get(type, id) as T;
   }
 
   async loadType<T extends Entity>(
     entityType: EntityConstructor<T>
   ): Promise<T[]> {
     const type = new entityType().getType();
-    return [...this.data.get(type)] as T[];
+    return this.getAll(type) as T[];
   }
 
   async save<T extends Entity>(
     entity: T,
     forceUpdate: boolean = false
   ): Promise<any> {
-    if (!this.data.get(entity.getType())) {
-      this.data.set(entity.getType(), []);
-    }
-    this.data.get(entity.getType()).push(entity);
+    this.add(entity);
+    return Promise.resolve();
   }
 
   remove<T extends Entity>(entity: T): Promise<any> {
-    const entities = this.data.get(entity.getType());
-    if (entities) {
-      const index = entities.findIndex((e) => e.getId() === entity.getId());
-      if (index !== -1) {
-        entities.splice(index, 1);
-      }
-    }
+    this.delete(entity);
     return Promise.resolve();
   }
 
