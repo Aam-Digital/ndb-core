99a0044705c8bbc3c709c4b2f6c4a5cc21139966
Simon <therealslimv@yahoo.de>
2021-04-26 12:24:18 +0200

    directly loading children in top level roll call component

--- src/app/child-dev-project/attendance/add-day-attendance/roll-call/roll-call.component.spec.ts
+++ src/app/child-dev-project/attendance/add-day-attendance/roll-call/roll-call.component.spec.ts
@@ -1,4 +1,10 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+  waitForAsync,
+} from "@angular/core/testing";
 
 import { RollCallComponent } from "./roll-call.component";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
@@ -6,6 +12,9 @@ import { Note } from "../../../notes/model/note";
 import { By } from "@angular/platform-browser";
 import { ConfigService } from "../../../../core/config/config.service";
 import { ConfigurableEnumConfig } from "../../../../core/configurable-enum/configurable-enum.interface";
+import { Child } from "../../../children/model/child";
+import { EntityMapperService } from "../../../../core/entity/entity-mapper.service";
+import { LoggingService } from "../../../../core/logging/logging.service";
 
 describe("RollCallComponent", () => {
   let component: RollCallComponent;
@@ -13,6 +22,8 @@ describe("RollCallComponent", () => {
 
   const testEvent = Note.create(new Date());
   let mockConfigService: jasmine.SpyObj<ConfigService>;
+  let mockEntityMapper: jasmine.SpyObj<EntityMapperService>;
+  let mockLoggingService: jasmine.SpyObj<LoggingService>;
 
   beforeEach(
     waitForAsync(() => {
@@ -20,11 +31,18 @@ describe("RollCallComponent", () => {
         "getConfig",
       ]);
       mockConfigService.getConfig.and.returnValue([]);
+      mockEntityMapper = jasmine.createSpyObj(["load"]);
+      mockEntityMapper.load.and.resolveTo();
+      mockLoggingService = jasmine.createSpyObj(["warn"]);
 
       TestBed.configureTestingModule({
         imports: [NoopAnimationsModule],
         declarations: [RollCallComponent],
-        providers: [{ provide: ConfigService, useValue: mockConfigService }],
+        providers: [
+          { provide: ConfigService, useValue: mockConfigService },
+          { provide: EntityMapperService, useValue: mockEntityMapper },
+          { provide: LoggingService, useValue: mockLoggingService },
+        ],
       }).compileComponents();
     })
   );
@@ -69,4 +87,24 @@ describe("RollCallComponent", () => {
     );
     expect(statusOptions.length).toBe(testStatusEnumConfig.length);
   });
+
+  it("should not record attendance if childId does not exist", fakeAsync(() => {
+    const existingChild = new Child("existingChild");
+    const noteWithNonExistingChild = new Note();
+    noteWithNonExistingChild.addChild(existingChild.getId());
+    noteWithNonExistingChild.addChild("notExistingChild");
+    component.eventEntity = noteWithNonExistingChild;
+
+    mockEntityMapper.load.and.callFake((con, id) =>
+      id === existingChild.getId()
+        ? Promise.resolve(existingChild as any)
+        : Promise.reject()
+    );
+
+    component.ngOnInit();
+    tick();
+
+    expect(component.entries.map((e) => e.child)).toEqual([existingChild]);
+    expect(mockLoggingService.warn).toHaveBeenCalled();
+  }));
 });
