39b0369e82078d1e6f1a0d05e4badce7ff050fb2
Sebastian Leidig <sebastian.leidig@gmail.com>
2021-04-27 10:44:26 +0200

    correct sort priorities of attendance events

--- src/app/child-dev-project/attendance/add-day-attendance/roll-call-setup/roll-call-setup.component.ts
+++ src/app/child-dev-project/attendance/add-day-attendance/roll-call-setup/roll-call-setup.component.ts
@@ -108,11 +108,11 @@ export class RollCallSetupComponent implements OnInit {
     const calculateEventPriority = (event: Note) => {
       let score = 0;
 
-      const assignedUsers = event.authors.concat(
-        this.allActivities
-          .filter((a) => a._id === event.relatesTo)
-          .map((a) => a.assignedTo)
-      );
+      const activityAssignedUsers = this.allActivities.find(
+        (a) => a._id === event.relatesTo
+      )?.assignedTo;
+      // use parent activities' assigned users and only fall back to event if necessary
+      const assignedUsers = activityAssignedUsers ?? event.authors;
 
       if (!RecurringActivity.isActivityEventNote(event)) {
         // show one-time events first
