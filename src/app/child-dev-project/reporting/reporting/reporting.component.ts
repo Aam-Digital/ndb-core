18d5916aee44ad06637a980ca84059ff5077edb2
Simon <therealslimv@yahoo.de>
2021-04-28 20:01:14 +0200

    added explanation comment

--- src/app/child-dev-project/reporting/reporting/reporting.component.ts
+++ src/app/child-dev-project/reporting/reporting/reporting.component.ts
@@ -33,6 +33,9 @@ export class ReportingComponent implements OnInit {
       this.fromDate,
       this.toDate
     );
+
+    // The ReportRowComponents manipulates the values array of the elements
+    // Using a timeout the calculation waits for this manipulation to be done
     setTimeout(() => (this.reportTable = this.getExportTable()));
   }
 
