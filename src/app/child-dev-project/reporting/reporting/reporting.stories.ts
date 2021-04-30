83c98430f6fe9b0e116ca9d8e1539efcc6c256fb
Sebastian Leidig <sebastian.leidig@gmail.com>
2021-04-30 11:00:21 +0200

    fix and simplify storybook

--- src/app/child-dev-project/reporting/reporting/reporting.stories.ts
+++ src/app/child-dev-project/reporting/reporting/reporting.stories.ts
@@ -1,26 +1,14 @@
 import { Story, Meta } from "@storybook/angular/types-6-0";
 import { moduleMetadata } from "@storybook/angular";
-import { CommonModule } from "@angular/common";
 import { RouterTestingModule } from "@angular/router/testing";
 import { ReportingComponent } from "./reporting.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatListModule } from "@angular/material/list";
-import { MatTableModule } from "@angular/material/table";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatStepperModule } from "@angular/material/stepper";
-import { MatIconModule } from "@angular/material/icon";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { FormsModule } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { of } from "rxjs";
 import { ReportingService } from "../reporting.service";
 import { MatNativeDateModule } from "@angular/material/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FontAwesomeIconsModule } from "../../../core/icons/font-awesome-icons.module";
-import { AdminModule } from "../../../core/admin/admin.module";
 import { BackupService } from "../../../core/admin/services/backup.service";
-import { ReportRowComponent } from "./report-row/report-row.component";
 import { Gender } from "../../children/model/Gender";
 import { ReportingModule } from "../reporting.module";
 
