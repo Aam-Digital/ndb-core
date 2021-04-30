fa2a183325f87adae9119993c354780913f836d5
Simon <therealslimv@yahoo.de>
2021-04-30 09:48:27 +0200

    added sort utils function

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -52,3 +52,20 @@ export function calculateAge(dateOfBirth: Date): number {
   }
   return age;
 }
+
+export function sortByAttribute<T, K extends keyof T>(
+  attribute: K,
+  order: "asc" | "desc" = "asc"
+): (e1: T, e2: T) => number {
+  return (e1, e2) => {
+    const value1 = e1[attribute];
+    const value2 = e2[attribute];
+    if (value1 === value2) {
+      return 0;
+    } else if (value1 < value2) {
+      return order === "asc" ? -1 : 1;
+    } else {
+      return order === "asc" ? 1 : -1;
+    }
+  };
+}
