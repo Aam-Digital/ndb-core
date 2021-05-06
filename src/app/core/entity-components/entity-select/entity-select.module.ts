d8db85fb791fbe4dfc5b0d94841d138bdfb3101d
Schottkyc137 <lukas2121@icloud.com>
2021-05-06 13:05:17 +0200

    cleanup module

--- src/app/core/entity-components/entity-select/entity-select.module.ts
+++ src/app/core/entity-components/entity-select/entity-select.module.ts
@@ -3,27 +3,21 @@ import { CommonModule } from "@angular/common";
 import { EntitySelectComponent } from "./entity-select/entity-select.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatChipsModule } from "@angular/material/chips";
-import { MatIconModule } from "@angular/material/icon";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatInputModule } from "@angular/material/input";
 import { MatTooltipModule } from "@angular/material/tooltip";
-import { ViewModule } from "../../view/view.module";
 
 @NgModule({
   declarations: [EntitySelectComponent],
   imports: [
     CommonModule,
     MatFormFieldModule,
-    FormsModule,
     MatChipsModule,
-    MatIconModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatTooltipModule,
-    ViewModule,
   ],
   exports: [EntitySelectComponent],
 })
