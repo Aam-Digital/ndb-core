65edf3ff3cce5c6970f797420f0e2d48ee041ff2
Simon <therealslimv@yahoo.de>
2021-05-14 16:49:56 +0200

    copied setup code from edit text component

--- src/app/core/entity-components/entity-details/form/edit-components/edit-configurable-enum/edit-configurable-enum.component.ts
+++ src/app/core/entity-components/entity-details/form/edit-components/edit-configurable-enum/edit-configurable-enum.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OnInitDynamicComponent } from "../../../../../view/dynamic-components/on-init-dynamic-component.interface";
+import { AbstractControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-edit-configurable-enum',
-  templateUrl: './edit-configurable-enum.component.html',
-  styleUrls: ['./edit-configurable-enum.component.scss']
+  selector: "app-edit-configurable-enum",
+  templateUrl: "./edit-configurable-enum.component.html",
+  styleUrls: ["./edit-configurable-enum.component.scss"],
 })
-export class EditConfigurableEnumComponent implements OnInit {
+export class EditConfigurableEnumComponent implements OnInitDynamicComponent {
+  tooltip: string;
+  formControlName: string;
+  placeholder: string;
+  formControl: AbstractControl;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  onInitFromDynamicConfig(config: any) {
+    this.formControlName = config["id"];
+    this.tooltip = config["tooltip"];
+    this.placeholder = config["placeholder"];
+    this.formControl = config["formControl"];
   }
-
 }
