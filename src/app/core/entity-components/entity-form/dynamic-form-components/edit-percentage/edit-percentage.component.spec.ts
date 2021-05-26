1816f17719b70d8deb407381155a74fe68907f66
Simon <therealslimv@yahoo.de>
2021-05-26 13:18:26 +0200

    added tests for edit percentage component

--- src/app/core/entity-components/entity-form/dynamic-form-components/edit-percentage/edit-percentage.component.spec.ts
+++ src/app/core/entity-components/entity-form/dynamic-form-components/edit-percentage/edit-percentage.component.spec.ts
@@ -2,7 +2,12 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { EditPercentageComponent } from "./edit-percentage.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { FormControl } from "@angular/forms";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
@@ -12,7 +17,12 @@ describe("EditPercentageComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatFormFieldModule, MatInputModule, NoopAnimationsModule],
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+      ],
       declarations: [EditPercentageComponent],
     }).compileComponents();
   });
@@ -20,11 +30,53 @@ describe("EditPercentageComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EditPercentageComponent);
     component = fixture.componentInstance;
-    component.formControl = new FormControl();
+    const formControl = new FormControl();
+    new FormGroup({ testProperty: formControl });
+    component.onInitFromDynamicConfig({
+      formControl: formControl,
+      propertySchema: {},
+      formFieldConfig: { id: "testProperty" },
+    });
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should only allow valid percentage values", () => {
+    component.formControl.setValue(101);
+    expect(component.formControl.invalid).toBeTrue();
+
+    component.formControl.setValue(100);
+    expect(component.formControl.valid).toBeTrue();
+
+    component.formControl.setValue(10);
+    expect(component.formControl.valid).toBeTrue();
+
+    component.formControl.setValue(0);
+    expect(component.formControl.valid).toBeTrue();
+
+    component.formControl.setValue(-1);
+    expect(component.formControl.invalid).toBeTrue();
+
+    component.formControl.setValue("one" as any);
+    expect(component.formControl.invalid).toBeTrue();
+  });
+
+  it("should keep existing validators", () => {
+    component.formControl.setValue(null);
+    expect(component.formControl.valid).toBeTrue();
+
+    const control = new FormControl(0, [Validators.required]);
+    new FormGroup({ testProperty: control });
+    component.onInitFromDynamicConfig({
+      formControl: control,
+      propertySchema: {},
+      formFieldConfig: { id: "testProperty" },
+    });
+
+    component.formControl.setValue(null);
+    expect(component.formControl.invalid).toBeTrue();
   });
 });
