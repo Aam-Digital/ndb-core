f3d71ef242baf767252c93fbc4dff9fc17c8d24c
Schottkyc137 <lukas2121@icloud.com>
2021-06-11 15:26:55 +0200

    fix prettier

--- src/app/child-dev-project/children/children-list/children-list.component.spec.ts
+++ src/app/child-dev-project/children/children-list/children-list.component.spec.ts
@@ -31,6 +31,8 @@ describe("ChildrenListComponent", () => {
   let fixture: ComponentFixture<ChildrenListComponent>;
   const routeData: EntityListConfig = {
     title: "Children List",
+    addNew: "Add child",
+    filterPlaceholder: "i.e. Participant name",
     columns: [
       { component: "DisplayText", title: "PN", id: "projectNumber" },
       { component: "ChildBlock", title: "Name", id: "name" },
@@ -80,10 +82,12 @@ describe("ChildrenListComponent", () => {
     data: of(routeData),
     queryParams: of({}),
   };
-  const mockChildrenService: jasmine.SpyObj<ChildrenService> =
-    jasmine.createSpyObj(["getChildren"]);
-  const mockEntityMapper: jasmine.SpyObj<EntityMapperService> =
-    jasmine.createSpyObj(["loadType", "save"]);
+  const mockChildrenService: jasmine.SpyObj<ChildrenService> = jasmine.createSpyObj(
+    ["getChildren"]
+  );
+  const mockEntityMapper: jasmine.SpyObj<EntityMapperService> = jasmine.createSpyObj(
+    ["loadType", "save"]
+  );
   beforeEach(
     waitForAsync(() => {
       mockEntityMapper.loadType.and.resolveTo([]);
