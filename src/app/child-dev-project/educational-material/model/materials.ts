37b811d53cf23ef9c62ac367d57da91e1f41a181
Simon <therealslimv@yahoo.de>
2021-07-17 17:47:14 +0200

    translated config

--- src/app/child-dev-project/educational-material/model/materials.ts
+++ src/app/child-dev-project/educational-material/model/materials.ts
@@ -7,124 +7,124 @@ export const materials: ConfigurableEnumValue[] = [
   },
   {
     id: "pencil",
-    label: $localize`pencil`,
+    label: $localize`:Label school material:pencil`,
   },
   {
     id: "eraser",
-    label: $localize`eraser`,
+    label: $localize`:Label school material:eraser`,
   },
   {
     id: "sharpener",
-    label: $localize`sharpener`,
+    label: $localize`:Label school material:sharpener`,
   },
   {
     id: "pen (black)",
-    label: $localize`pen (black)`,
+    label: $localize`:Label school material:pen (black)`,
   },
   {
     id: "oil pastels",
-    label: $localize`oil pastels`,
+    label: $localize`:Label school material:oil pastels`,
   },
   {
     id: "crayons",
-    label: $localize`crayons`,
+    label: $localize`:Label school material:crayons`,
   },
   {
     id: "sketch pens",
-    label: $localize`sketch pens`,
+    label: $localize`:Label school material:sketch pens`,
   },
   {
     id: "scale (big)",
-    label: $localize`scale (big)`,
+    label: $localize`:Label school material:scale (big)`,
   },
   {
     id: "scale (small)",
-    label: $localize`scale (small)`,
+    label: $localize`:Label school material:scale (small)`,
   },
   {
     id: "geometry box",
-    label: $localize`geometry box`,
+    label: $localize`:Label school material:geometry box`,
   },
   {
     id: "copy (single line, small)",
-    label: $localize`copy (single line, small)`,
+    label: $localize`:Label school material:copy (single line, small)`,
   },
   {
     id: "copy (single line, big)",
-    label: $localize`copy (single line, big)`,
+    label: $localize`:Label school material:copy (single line, big)`,
   },
   {
     id: "copy (four line)",
-    label: $localize`copy (four line)`,
+    label: $localize`:Label school material:copy (four line)`,
   },
   {
     id: "copy (squared)",
-    label: $localize`copy (squared)`,
+    label: $localize`:Label school material:copy (squared)`,
   },
   {
     id: "copy (plain)",
-    label: $localize`copy (plain)`,
+    label: $localize`:Label school material:copy (plain)`,
   },
   {
     id: "copy (line-plain)",
-    label: $localize`copy (line-plain)`,
+    label: $localize`:Label school material:copy (line-plain)`,
   },
   {
     id: "copy (drawing)",
-    label: $localize`copy (drawing)`,
+    label: $localize`:Label school material:copy (drawing)`,
   },
   {
     id: "copy (practical)",
-    label: $localize`copy (practical)`,
+    label: $localize`:Label school material:copy (practical)`,
   },
   {
     id: "graph book",
-    label: $localize`graph book`,
+    label: $localize`:Label school material:graph book`,
   },
   {
     id: "project papers",
-    label: $localize`project papers`,
+    label: $localize`:Label school material:project papers`,
   },
   {
     id: "project file",
-    label: $localize`project file`,
+    label: $localize`:Label school material:project file`,
   },
   {
     id: "scrap book",
-    label: $localize`scrap book`,
+    label: $localize`:Label school material:scrap book`,
   },
   {
     id: "exam board",
-    label: $localize`exam board`,
+    label: $localize`:Label school material:exam board`,
   },
   {
     id: "Bag",
-    label: $localize`Bag`,
+    label: $localize`:Label school material:Bag`,
     color: "#B3E5FC",
   },
   {
     id: "School Uniform",
-    label: $localize`School Uniform`,
+    label: $localize`:Label school material:School Uniform`,
     color: "#B3E5FC",
   },
   {
     id: "School Shoes",
-    label: $localize`School Shoes`,
+    label: $localize`:Label school material:School Shoes`,
     color: "#B3E5FC",
   },
   {
     id: "Sports Dress",
-    label: $localize`Sports Dress`,
+    label: $localize`:Label school material:Sports Dress`,
     color: "#B3E5FC",
   },
   {
     id: "Sports Shoes",
-    label: $localize`Sports Shoes`,
+    label: $localize`:Label school material:Sports Shoes`,
     color: "#B3E5FC",
   },
   {
     id: "Raincoat",
-    label: $localize`Raincoat`,
+    label: $localize`:Label school material:Raincoat`,
     color: "#B3E5FC",
   },
 ];
