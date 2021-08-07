5fd685695425d72e0fb8a8088143efc0f9d1f579
kirtijadhav <83791155+kirtijadhav@users.noreply.github.com>
2021-08-07 06:34:26 +0200

    fix: Tooltips open when clicking the question mark on mobile devices
    fixes #891

--- src/app/core/entity-components/entity-form/entity-form.module.ts
+++ src/app/core/entity-components/entity-form/entity-form.module.ts
@@ -6,6 +6,9 @@ import { MatButtonModule } from "@angular/material/button";
 import { FlexModule } from "@angular/flex-layout";
 import { ViewModule } from "../../view/view.module";
 import { PermissionsModule } from "../../permissions/permissions.module";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [EntityFormComponent],
@@ -15,6 +18,9 @@ import { PermissionsModule } from "../../permissions/permissions.module";
     FlexModule,
     ViewModule,
     PermissionsModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatFormFieldModule,
   ],
   providers: [EntityFormService],
   exports: [EntityFormComponent],
