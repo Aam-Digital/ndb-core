3d66fdde16baee2dbf282ebf86354b85183071b7
kirtijadhav <kirtijadhav029@gmail.com>
2021-08-09 16:39:14 +0200

    - add child new button

--- src/app/child-dev-project/schools/children-overview/children-overview.component.ts
+++ src/app/child-dev-project/schools/children-overview/children-overview.component.ts
@@ -5,6 +5,11 @@ import { Child } from "../../children/model/child";
 import { PanelConfig } from "../../../core/entity-components/entity-details/EntityDetailsConfig";
 import { FormFieldConfig } from "../../../core/entity-components/entity-form/entity-form/FormConfig";
 import { Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { EntityFormComponent } from "../../../core/entity-components/entity-form/entity-form/entity-form.component";
+import { ChildSchoolRelation } from "../../children/model/childSchoolRelation";
+import { School } from "../model/school";
+import { Entity } from "../../../core/entity/model/entity";
 
 /**
  * This component creates a table containing all children currently attending this school.
@@ -30,19 +35,41 @@ export class ChildrenOverviewComponent implements OnInitDynamicComponent {
   ];
 
   children: Child[] = [];
+  entity: Entity;
 
-  constructor(private schoolsService: SchoolsService, private router: Router) {}
+  constructor(private schoolsService: SchoolsService, private router: Router, private dialog: MatDialog) {}
 
   async onInitFromDynamicConfig(config: PanelConfig) {
     if (config?.config?.columns) {
       this.columns = config.config.columns;
     }
+    this.entity = config.entity;
     this.children = await this.schoolsService.getChildrenForSchool(
-      config.entity.getId()
+      this.entity.getId()
     );
   }
 
   routeToChild(child: Child) {
     this.router.navigate([`/${child.getType().toLowerCase()}`, child.getId()]);
+  }
+
+  addChildClick() {
+    console.log('addChildClick')
+    const dialogRef = this.dialog.open(EntityFormComponent, {
+      width: "80%",
+      maxHeight: "90vh",
+    });
+
+    dialogRef.componentInstance.columns = [["childId"], ["start"], ["end"], ["schoolClass"], ["result"]];
+    const newRelation = new ChildSchoolRelation();
+    newRelation.schoolId = this.entity.getId();
+    dialogRef.componentInstance.entity = newRelation;
+    dialogRef.componentInstance.editing = true;
+    dialogRef.componentInstance.onSave.subscribe(async () => {
+      dialogRef.close();
+      this.children = await this.schoolsService.getChildrenForSchool(
+        this.entity.getId())
+    });
+    dialogRef.componentInstance.onCancel.subscribe(() => dialogRef.close());
   }
 }
