27a9245494bc47ff6bc84c431f2b53892bc9317f
Simon <therealslimv@yahoo.de>
2021-08-11 12:02:46 +0200

    cleaned up login component and added tests

--- src/app/core/session/login/login.component.ts
+++ src/app/core/session/login/login.component.ts
@@ -15,12 +15,11 @@
  *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, Optional } from "@angular/core";
-import { SyncState } from "../session-states/sync-state.enum";
+import { Component } from "@angular/core";
 import { SessionService } from "../session-service/session.service";
 import { LoginState } from "../session-states/login-state.enum";
-import { ConnectionState } from "../session-states/connection-state.enum";
 import { ActivatedRoute, Router } from "@angular/router";
+import { LoggingService } from "../../logging/logging.service";
 
 /**
  * Form to allow users to enter their credentials and log in.
@@ -45,8 +44,9 @@ export class LoginComponent {
 
   constructor(
     private _sessionService: SessionService,
-    @Optional() private router: Router,
-    @Optional() private route: ActivatedRoute
+    private loggingService: LoggingService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   /**
@@ -54,55 +54,51 @@ export class LoginComponent {
    */
   login() {
     this.loginInProgress = true;
+    this.errorMessage = "";
 
     this._sessionService
       .login(this.username, this.password)
       .then((loginState) => {
-        if (loginState === LoginState.LOGGED_IN) {
-          this.onLoginSuccess();
-        } else {
-          if (
-            this._sessionService.getSyncState().getState() ===
-              SyncState.ABORTED &&
-            this._sessionService.getConnectionState().getState() ===
-              ConnectionState.OFFLINE
-          ) {
+        switch (loginState) {
+          case LoginState.LOGGED_IN:
+            this.onLoginSuccess();
+            break;
+          case LoginState.UNAVAILABLE:
             this.onLoginFailure(
-              $localize`Can't login for the first time when offline. Please try again later.`
+              $localize`:LoginError:Please connect to the internet and try again`
             );
-          } else if (
-            this._sessionService.getConnectionState().getState() ===
-            ConnectionState.OFFLINE
-          ) {
+            break;
+          case LoginState.LOGIN_FAILED:
             this.onLoginFailure(
-              $localize`Username or password incorrect!
-               You might also face this problem because you are currently offline.
-               Please connect to the internet to synchronize the latest user data.`
+              $localize`:LoginError:Username and/or password incorrect`
             );
-          } else {
-            this.onLoginFailure($localize`Username or password incorrect!`);
-          }
+            break;
+          default:
+            throw new Error(
+              $localize`:LoginError:Unexpected login state: ${loginState}`
+            );
         }
       })
-      .catch((reason) =>
-        this.onLoginFailure(
-          typeof reason === "string" ? reason : JSON.stringify(reason)
-        )
-      );
+      .catch((reason) => {
+        this.loggingService.error(`Unexpected login error: ${reason}`);
+        this.onLoginFailure($localize`
+          :LoginError:An unexpected error occurred. 
+          Please reload the the page and try again. 
+          If you keep seeing this error message, please contact your system administrator.
+        `);
+      });
   }
 
   private onLoginSuccess() {
-    // New routes are added at runtime,
-    if (this.router && this.route) {
-      this.router.navigate([], {
-        relativeTo: this.route,
-      });
-    }
+    // New routes are added at runtime
+    this.router.navigate([], {
+      relativeTo: this.route,
+    });
     this.reset();
     // login component is automatically hidden based on _sessionService.isLoggedIn()
   }
 
-  private onLoginFailure(reason: any) {
+  private onLoginFailure(reason: string) {
     this.reset();
     this.errorMessage = reason;
   }
