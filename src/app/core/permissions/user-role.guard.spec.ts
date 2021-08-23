8942d4b334e2d54b856cea1bdac1fb2e7b4e8a6d
Simon <therealslimv@yahoo.de>
2021-08-23 14:09:08 +0300

    refactored UserRoleGuard to get permissions from route.data

--- src/app/core/permissions/user-role.guard.spec.ts
+++ src/app/core/permissions/user-role.guard.spec.ts
@@ -1,29 +1,22 @@
 import { TestBed } from "@angular/core/testing";
 
 import { UserRoleGuard } from "./user-role.guard";
-import { ConfigService } from "../config/config.service";
 import { SessionService } from "../session/session-service/session.service";
 import { DatabaseUser } from "../session/session-service/local-user";
-import { PREFIX_VIEW_CONFIG } from "../view/dynamic-routing/view-config.interface";
 
 describe("UserRoleGuard", () => {
   let guard: UserRoleGuard;
-  let mockConfigService: jasmine.SpyObj<ConfigService>;
   let mockSessionService: jasmine.SpyObj<SessionService>;
   const normalUser: DatabaseUser = { name: "normalUser", roles: ["user_app"] };
   const adminUser: DatabaseUser = {
     name: "admin",
     roles: ["admin", "user_app"],
   };
-  const adminRouteConfig = { permittedUserRoles: ["admin"] };
 
   beforeEach(() => {
-    mockConfigService = jasmine.createSpyObj(["getConfig"]);
-    mockConfigService.getConfig.and.returnValue(adminRouteConfig);
     mockSessionService = jasmine.createSpyObj(["getCurrentUser"]);
     TestBed.configureTestingModule({
       providers: [
-        { provide: ConfigService, useValue: mockConfigService },
         { provide: SessionService, useValue: mockSessionService },
         UserRoleGuard,
       ],
@@ -38,33 +31,28 @@ describe("UserRoleGuard", () => {
   it("should return true if current user is allowed", () => {
     mockSessionService.getCurrentUser.and.returnValue(adminUser);
 
-    const result = guard.canActivate({ routeConfig: { path: "url" } } as any);
+    const result = guard.canActivate({
+      routeConfig: { path: "url" },
+      data: { permittedUserRoles: ["admin"] },
+    } as any);
 
-    expect(mockConfigService.getConfig).toHaveBeenCalledWith(
-      PREFIX_VIEW_CONFIG + "url"
-    );
     expect(result).toBeTrue();
   });
 
   it("should return false for a user without permissions", () => {
     mockSessionService.getCurrentUser.and.returnValue(normalUser);
 
-    const result = guard.canActivate({ routeConfig: { path: "url" } } as any);
+    const result = guard.canActivate({
+      routeConfig: { path: "url" },
+      data: { permittedUserRoles: ["admin"] },
+    } as any);
 
-    expect(mockConfigService.getConfig).toHaveBeenCalledWith(
-      PREFIX_VIEW_CONFIG + "url"
-    );
     expect(result).toBeFalse();
   });
 
   it("should return true if no config is set", () => {
-    mockConfigService.getConfig.and.returnValue({});
-
     const result = guard.canActivate({ routeConfig: { path: "url" } } as any);
 
-    expect(mockConfigService.getConfig).toHaveBeenCalledWith(
-      PREFIX_VIEW_CONFIG + "url"
-    );
     expect(result).toBeTrue();
   });
 });
--- src/app/core/permissions/user-role.guard.ts
+++ src/app/core/permissions/user-role.guard.ts
@@ -1,30 +1,21 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate } from "@angular/router";
 import { SessionService } from "../session/session-service/session.service";
-import { ConfigService } from "../config/config.service";
-import {
-  PREFIX_VIEW_CONFIG,
-  ViewConfig,
-} from "../view/dynamic-routing/view-config.interface";
+import { RouteData } from "../view/dynamic-routing/view-config.interface";
 
 @Injectable()
 /**
  * A guard that checks the roles of the current user against the permissions of the route config.
  */
 export class UserRoleGuard implements CanActivate {
-  constructor(
-    private sessionService: SessionService,
-    private configService: ConfigService
-  ) {}
+  constructor(private sessionService: SessionService) {}
 
   canActivate(route: ActivatedRouteSnapshot): boolean {
-    const config = this.configService.getConfig<ViewConfig>(
-      PREFIX_VIEW_CONFIG + route.routeConfig.path
-    );
+    const routeData: RouteData = route.data;
     const user = this.sessionService.getCurrentUser();
-    if (config.permittedUserRoles?.length > 0) {
+    if (routeData?.permittedUserRoles?.length > 0) {
       // Check if user has a role which is in the list of permitted roles
-      return config.permittedUserRoles.some((role) =>
+      return routeData.permittedUserRoles.some((role) =>
         user?.roles.includes(role)
       );
     } else {
