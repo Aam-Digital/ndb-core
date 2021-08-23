c172fefd18e5b6dab7cde0aa53970ab843bb884e
Simon <therealslimv@yahoo.de>
2021-08-23 12:44:22 +0300

    added a comment to createWithData function

--- src/app/core/database/pouch-database.ts
+++ src/app/core/database/pouch-database.ts
@@ -29,6 +29,12 @@ import { PerformanceAnalysisLogging } from "../../utils/performance-analysis-log
  * should be implemented in the abstract {@link Database}.
  */
 export class PouchDatabase extends Database {
+  /**
+   * Creates a PouchDB in-memory instance in which the passed documents are saved.
+   * The functions returns immediately but the documents are saved asynchronously.
+   * In tests use `tick()` or `waitForAsync()` to prevent accessing documents before they are saved.
+   * @param data an array of documents
+   */
   static createWithData(data: any[]): PouchDatabase {
     const instance = PouchDatabase.createWithInMemoryDB();
     data.forEach((doc) => instance.put(doc, true));
