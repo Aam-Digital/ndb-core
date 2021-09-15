ce8ca85c2fed12c573fa6145ad9ba3b49850858e
kirtijadhav <kirtijadhav029@gmail.com>
2021-09-15 17:28:47 +0200

    - run tests

--- src/app/child-dev-project/attendance/add-day-attendance/roll-call/roll-call.component.spec.ts
+++ src/app/child-dev-project/attendance/add-day-attendance/roll-call/roll-call.component.spec.ts
@@ -182,7 +182,7 @@ describe("RollCallComponent", () => {
 
   it("should complete roll call after clicking on save button", () => {
     const note = new Note();
-    const confirmationDialogService = TestBed.inject(ConfirmationDialogService)
+    const confirmationDialogService = TestBed.inject(ConfirmationDialogService);
     spyOn(component.complete, "emit");
     component.eventEntity = note;
     spyOn(confirmationDialogService, "openDialog").and.returnValue({
@@ -195,7 +195,7 @@ describe("RollCallComponent", () => {
   });
 
   it("should not open the dialog when the roll call is finished", () => {
-    const confirmationDialogService = TestBed.inject(ConfirmationDialogService)
+    const confirmationDialogService = TestBed.inject(ConfirmationDialogService);
     spyOn(confirmationDialogService, "openDialog");
     spyOn(component, "isFinished").and.returnValue(true);
 
@@ -205,7 +205,7 @@ describe("RollCallComponent", () => {
   });
 
   it("should open the dialog when the roll call is not finished", () => {
-    const confirmationDialogService = TestBed.inject(ConfirmationDialogService)
+    const confirmationDialogService = TestBed.inject(ConfirmationDialogService);
     spyOn(confirmationDialogService, "openDialog").and.returnValue({
       afterClosed: () => of(true),
     } as any);
@@ -217,7 +217,7 @@ describe("RollCallComponent", () => {
   });
 
   it("should not open the dialog for exit button when the roll call finished", () => {
-    const confirmationDialogService = TestBed.inject(ConfirmationDialogService)
+    const confirmationDialogService = TestBed.inject(ConfirmationDialogService);
     spyOn(confirmationDialogService, "openDialog");
     spyOn(component, "isFinished").and.returnValue(true);
 
