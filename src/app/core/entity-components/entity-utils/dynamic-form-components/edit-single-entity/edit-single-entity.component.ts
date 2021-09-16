4c0e7fea7116b10a9d786c1f06da1c5953ed8266
kirtijadhav <kirtijadhav029@gmail.com>
2021-09-16 19:59:57 +0200

    - fix child list for add child field

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-single-entity/edit-single-entity.component.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-single-entity/edit-single-entity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from "@angular/core";
 import { EditComponent, EditPropertyConfig } from "../edit-component";
 import { ENTITY_MAP } from "../../../entity-details/entity-details.component";
 import { EntityMapperService } from "../../../../entity/entity-mapper.service";
@@ -6,6 +6,7 @@ import { Entity } from "../../../../entity/model/entity";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { FormControl } from "@angular/forms";
+import { MatAutocompleteSelectedEvent, MatAutocompleteTrigger } from "@angular/material/autocomplete";
 
 @Component({
   selector: "app-edit-single-entity",
@@ -19,7 +20,23 @@ export class EditSingleEntityComponent
   placeholder: string;
   filteredEntities: Observable<Entity[]>;
   entityNameFormControl: FormControl;
+  selection_: Entity[] = [];
+  selectedEntity?: Entity;
+  editingSelectedEntity: boolean = false;
 
+  @Input() showEntities: boolean = true;
+  @Input() selectionInputType: "id" | "entity" = "id";
+  @Output() selectionChange = new EventEmitter<(string | Entity)[]>();
+  @ViewChild("inputField") inputField: ElementRef<HTMLInputElement>;
+  @ViewChild(MatAutocompleteTrigger) autocomplete: MatAutocompleteTrigger;
+
+  selectEntity(entity: Entity) {
+    this.selection_.push(entity);
+    this.emitChange();
+    this.inputField.nativeElement.value = "";
+    this.formControl.setValue(null);
+    setTimeout(() => this.autocomplete.openPanel());
+  }
   constructor(private entityMapper: EntityMapperService) {
     super();
     this.entityNameFormControl = new FormControl();
@@ -52,7 +69,30 @@ export class EditSingleEntityComponent
       (entity) => entity.getId() === this.formControl.value
     );
     if (selectedEntity) {
+      this.selectedEntity = selectedEntity;
+      this.editingSelectedEntity = false;
       this.entityNameFormControl.setValue(selectedEntity.toString());
+    } else {
+      this.entityNameFormControl.setValue("");
+    }
+  }
+
+  select(event: MatAutocompleteSelectedEvent) {
+    const entity = this.entities.find(
+      (e) => e.toString().toLowerCase() === event.option.value.toLowerCase()
+    );
+    if (entity) {
+      this.selectedEntity = entity;
+      this.editingSelectedEntity = false;
+      this.formControl.setValue(entity.getId());
+    }
+  }
+
+  private emitChange() {
+    if (this.selectionInputType === "id") {
+      this.selectionChange.emit(this.selection_.map((e) => e.getId()));
+    } else {
+      this.selectionChange.emit(this.selection_);
     }
   }
 }
