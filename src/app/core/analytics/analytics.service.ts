645b486876875eaae1d50811b104674a576c1012
Sebastian Leidig <sebastian.leidig@gmail.com>
2021-09-18 09:17:35 +0200

    simplify

--- src/app/core/analytics/analytics.service.ts
+++ src/app/core/analytics/analytics.service.ts
@@ -110,10 +110,7 @@ export class AnalyticsService {
     window["_paq"].push(["trackPageView"]);
     window["_paq"].push(["enableLinkTracking"]);
     (() => {
-      let u = url;
-      if (!u.endsWith("/")) {
-        u = u + "/";
-      }
+      const u = url.endsWith("/") ? url : url + "/";
       window["_paq"].push(["setTrackerUrl", u + "matomo.php"]);
       window["_paq"].push(["setSiteId", id]);
       const d = document;
