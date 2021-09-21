07d970f61493470d3c3128d1d7c2ca83c3239d0e
Schottkyc137 <lukas2121@icloud.com>
2021-09-21 14:37:23 +0200

    fix specs

--- src/app/core/entity-components/entity-subrecord/row-details/row-details.component.spec.ts
+++ src/app/core/entity-components/entity-subrecord/row-details/row-details.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { RowDetailsComponent } from "./row-details.component";
+import {
+  DetailsComponentData,
+  RowDetailsComponent,
+} from "./row-details.component";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { EntityFormService } from "../../entity-form/entity-form.service";
+import { FormBuilder } from "@angular/forms";
+import { EntityMapperService } from "../../../entity/entity-mapper.service";
+import { mockEntityMapper } from "../../../entity/mock-entity-mapper-service";
+import { EntitySchemaService } from "../../../entity/schema/entity-schema.service";
+import { Entity } from "../../../entity/model/entity";
 
 describe("RowDetailsComponent", () => {
-  let component: RowDetailsComponent;
-  let fixture: ComponentFixture<RowDetailsComponent>;
+  let component: RowDetailsComponent<any>;
+  let fixture: ComponentFixture<RowDetailsComponent<any>>;
+  const detailsComponentData: DetailsComponentData<any> = {
+    record: new Entity(),
+    rows: [],
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RowDetailsComponent],
+      imports: [MatDialogModule],
+      providers: [
+        EntityFormService,
+        FormBuilder,
+        EntitySchemaService,
+        { provide: EntityMapperService, useValue: mockEntityMapper() },
+        { provide: MAT_DIALOG_DATA, useValue: detailsComponentData },
+        { provide: MatDialogRef, useValue: {} },
+      ],
     }).compileComponents();
   });
 
