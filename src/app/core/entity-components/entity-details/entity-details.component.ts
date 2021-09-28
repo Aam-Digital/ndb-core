405dd84e8e76e76a62bc164dfef3d13b4122bf69
Simon <therealslimv@yahoo.de>
2021-09-28 09:19:51 +0200

    fixed some icons

--- src/app/core/entity-components/entity-details/entity-details.component.ts
+++ src/app/core/entity-components/entity-details/entity-details.component.ts
@@ -57,7 +57,7 @@ export class EntityDetailsComponent {
   operationType = OperationType;
 
   panels: Panel[] = [];
-  classNamesWithIcon: string;
+  iconName: string;
   config: EntityDetailsConfig;
 
   constructor(
@@ -72,7 +72,7 @@ export class EntityDetailsComponent {
   ) {
     this.route.data.subscribe((data: RouteData<EntityDetailsConfig>) => {
       this.config = data.config;
-      this.classNamesWithIcon = "fa fa-" + data.config.icon + " fa-fw";
+      this.iconName = data.config.icon;
       this.route.paramMap.subscribe((params) =>
         this.loadEntity(params.get("id"))
       );
