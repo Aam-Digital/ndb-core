e73b660aaab7f7909a032e4a33571c2e1811af6d
Simon <therealslimv@yahoo.de>
2021-10-20 10:05:43 +0200

    changed @Directive to @Component

--- src/app/core/entity-components/entity-utils/view-components/view-component.ts
+++ src/app/core/entity-components/entity-utils/view-components/view-component.ts
@@ -1,9 +1,9 @@
 import { OnInitDynamicComponent } from "../../../view/dynamic-components/on-init-dynamic-component.interface";
 import { ViewPropertyConfig } from "../../entity-list/EntityListConfig";
 import { Entity } from "../../../entity/model/entity";
-import { Directive, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-@Directive()
+@Component({})
 export abstract class ViewComponent implements OnInitDynamicComponent {
   @Input() entity: Entity;
   property: string;
