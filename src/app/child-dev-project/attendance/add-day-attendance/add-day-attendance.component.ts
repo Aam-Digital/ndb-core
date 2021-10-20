2573d6d13f32cc93d14aa51ddc47669d3386b1bb
Schottkyc137 <lukas2121@icloud.com>
2021-10-20 12:10:09 +0200

    reorder for readability

--- src/app/child-dev-project/attendance/add-day-attendance/add-day-attendance.component.ts
+++ src/app/child-dev-project/attendance/add-day-attendance/add-day-attendance.component.ts
@@ -55,15 +55,15 @@ export class AddDayAttendanceComponent {
   }
 
   exit() {
-    if (!this.rollCallComponent?.isDirty) {
-      this.finishRollCallState();
-    } else {
+    if (this.rollCallComponent?.isDirty) {
       this.confirmationDialog.openDialog(
         "Exit",
         "Do you want to save your progress before going back?",
         this.buttons,
         true
       );
+    } else {
+      this.finishRollCallState();
     }
   }
 
