cfdfcd692627c0375a4a5aed4a723e7a12a01522
Simon <therealslimv@yahoo.de>
2021-11-16 15:37:14 +0100

    added documentation

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-component.spec.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-component.spec.ts
@@ -3,6 +3,10 @@ import { FormControl, FormGroup } from "@angular/forms";
 
 export const FORM_CONTROL_NAME = "testProperty";
 
+/**
+ * A simple helper class that sets up a EditComponent with the required FormGroup
+ * @param component that extends EditComponent
+ */
 export function setupEditComponent<T>(component: EditComponent<T>): FormGroup {
   const formControl = new FormControl();
   const formGroup = new FormGroup({ testProperty: formControl });
