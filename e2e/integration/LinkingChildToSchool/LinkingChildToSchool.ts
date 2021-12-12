a810861e85c02e484267939f7dff15a366168d08
Artem Suttar <artem.suttar@gmail.com>
2021-12-12 20:03:22 +0100

    Fix LinkingChildToSchool
    Delete static variable with school name, now we get this variable from
    /school/1 url

--- e2e/integration/LinkingChildToSchool/LinkingChildToSchool.ts
+++ e2e/integration/LinkingChildToSchool/LinkingChildToSchool.ts
@@ -1,11 +1,12 @@
 describe("Scenario: Linking a child to a school - E2E test", () => {
   before(() => {
-    // GIVEN I am on the details page of a child
-    cy.wrap("Aaditya School").as("schoolName");
-    // cy.wrap(0).as('studentsCount')
-    cy.visit("http://localhost:4200/child/1");
-    cy.wait(10000);
 
+
+    // GIVEN I am on the details page of a child
+    cy.visit("http://localhost:4200/school/1");
+    cy.get(".page-header > .ng-star-inserted").invoke("text").as("schoolName");
+
+    cy.visit("http://localhost:4200/child/1");
     cy.get(".page-header > .ng-star-inserted").invoke("text").as("studentName");
   });
 
