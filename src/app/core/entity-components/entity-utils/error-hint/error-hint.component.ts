b4e2c9197dfd9689307c0b0b5a5c0aed907ccbb4
Schottkyc137 <lukas2121@icloud.com>
2022-01-11 19:45:47 +0100

    Add a component that hints at errors

---
+++ src/app/core/entity-components/entity-utils/error-hint/error-hint.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { DynamicValidatorsService } from "../../entity-form/dynamic-form-validators/dynamic-validators.service";
+
+@Component({
+  selector: "app-error-hint",
+  templateUrl: "./error-hint.component.html",
+  styleUrls: ["./error-hint.component.scss"],
+})
+export class ErrorHintComponent {
+  @Input() form: FormControl;
+
+  constructor(public validatorService: DynamicValidatorsService) {}
+}
