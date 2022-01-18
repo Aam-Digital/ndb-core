39c9fe320dd868ae6f9afec066aaf193bb9d736e
Christoph Scheuing <47225324+christophscheuing@users.noreply.github.com>
2022-01-18 19:17:23 +0100

    Progressing

--- src/app/core/navigation/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation/navigation.component.ts
@@ -41,6 +41,8 @@ export class NavigationComponent {
   /** all menu items to be displayed */
   public menuItems: MenuItem[] = [];
 
+  public installText: String = '';
+
   constructor(
     private userRoleGuard: UserRoleGuard,
     private configService: ConfigService
@@ -48,6 +50,81 @@ export class NavigationComponent {
     this.configService.configUpdates
       .pipe(untilDestroyed(this))
       .subscribe(() => this.initMenuItemsFromConfig());
+    
+    const whichOS = () => {
+      let OS = "";
+      const userAgent = window.navigator.userAgent;
+      if (/iphone|ipad|ipod|macintosh/i.test(userAgent)) {
+        if (window.innerWidth < 1025) {
+          OS = "iOS";
+        }
+        else {
+          OS = "MacOS";
+        }
+      }
+      else if (/windows|win32|win64|WinCE/i.test(userAgent)) {
+        OS = "Windows";
+      }
+      else if (/linux|X11/i.test(userAgent)) {
+        OS = "Linux";
+      }
+      return OS;
+    }
+
+    const whichBrowser = () => {
+      let browser = "";
+      if (navigator.userAgent.indexOf('Opera') != -1) {
+        browser = "Opera";
+      }
+      else if (navigator.userAgent.indexOf('MSIE') != -1) {
+        browser = "Microsoft Internet Explorer";
+      }
+      else if (navigator.userAgent.indexOf('MSIE') != -1) {
+        browser = "Microsoft Internet Explorer";
+      }
+      else if (navigator.userAgent.indexOf('Trident') != -1) {
+        browser = "Microsoft Internet Explorer";
+      }
+      else if (navigator.userAgent.indexOf('Chrome') != -1) {
+        browser = "Chrome";
+      }
+      else if (navigator.userAgent.indexOf('Safari') != -1) {
+        browser = "Safari";
+        if (navigator.userAgent.indexOf('CriOS') != -1) {
+          browser = "Chrome";
+        }
+      }
+      else if (navigator.userAgent.indexOf('Firefox') != -1) {
+        browser = "Firefox";
+      }
+      return browser;
+    }
+    
+
+
+    // Detects if device is in standalone mode
+    const isInStandaloneMode = () => ('standalone' in window.navigator); // && (window.navigator.standalone);
+    
+    // Detects if device is on iOS
+    const isSafari = () => {
+      return navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+          navigator.userAgent &&
+          navigator.userAgent.indexOf('CriOS') === -1 &&
+          navigator.userAgent.indexOf('FxiOS') === -1;
+    }       
+
+    const isMobile = () => {
+      return window.innerWidth < 1025
+    }  
+
+
+    if (!isInStandaloneMode()) {
+      this.installText = this.installText + ", kein Standalone";
+    }
+
+    this.installText = whichOS() + ", " + whichBrowser(); // window.navigator.userAgent + " ---isMobile: " + isMobile();
+    console.log("UserAgent: "+  window.navigator.userAgent);
+
   }
 
   /**
