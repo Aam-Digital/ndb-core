81819f399bd382459c7ae1d60040ab4243afe7ed
Artem Suttar <artem.suttar@gmail.com>
2022-02-04 23:25:20 +0100

    Fix LinkingChildToSchool.ts

--- e2e/integration/LinkingChildToSchool.ts
+++ e2e/integration/LinkingChildToSchool.ts
@@ -2,19 +2,17 @@ describe("Scenario: Linking a child to a school - E2E test", () => {
   before(() => {
     // GIVEN I am on the details page of a child
     cy.visit("school/1");
-    cy.get(".page-header > .ng-star-inserted").invoke("text").as("schoolName");
+    cy.get(".header-row > .header-title").invoke("text").as("schoolName");
 
     cy.visit("child/1");
-    cy.get(".page-header > .ng-star-inserted").invoke("text").as("studentName");
+    cy.get(".header-row > .header-title").invoke("text").as("studentName");
   });
 
   // WHEN I add an entry in the 'Previous Schools' section with a specific school
   // (with todays date - that might be added to this e2e test later)
   it("Add an entry in the Previous School section", function () {
     // get the Education button and click on it
-    cy.get("#mat-expansion-panel-header-1 > .mat-content")
-      .should("contain", "Education")
-      .click();
+    cy.get("#mat-tab-label-0-1").should("contain", "Education").click();
     // get the Show All button and toggle it, forcing the click is needed because the element has a hidden feature
     cy.get(
       "#mat-slide-toggle-1 > .mat-slide-toggle-label > .mat-slide-toggle-bar > .mat-slide-toggle-thumb-container > .mat-slide-toggle-thumb"
@@ -26,7 +24,7 @@ describe("Scenario: Linking a child to a school - E2E test", () => {
 
     // choose the school to add
     cy.get('[ng-reflect-placeholder="Select School"]')
-      .type(this.schoolName)
+      .type(this.schoolName.trim())
       .click();
     // save school in child profile
     cy.contains("button", "Save").click();
@@ -35,26 +33,24 @@ describe("Scenario: Linking a child to a school - E2E test", () => {
   // THEN I can see that child in the 'Children Overview' of the details page of this school
   it("Check for child in Children Overview of specific school", function () {
     // Go to the school overview page
-    cy.contains("mat-list-item", "Schools").click();
+    // cy.contains("mat-list-item", "Schools").click();
 
-    // Choose the school that was added to the child profile
+    // Choose the school that was added to the child profile "app-previous-schools.ng-star-inserted > app-entity-subrecord"
     cy.contains(
-      "tbody > :nth-child(1) > .cdk-column-name",
-      this.schoolName
+      ":nth-child(1) > .cdk-column-schoolId > app-display-entity.ng-star-inserted > .ng-star-inserted > :nth-child(1) > .underline-on-hover",
+      this.schoolName.trim()
     ).click();
     // Open the students overview
-    cy.contains("mat-expansion-panel-header", "Students").click();
+    cy.contains("#mat-tab-label-2-1", "Students").click();
 
-    // Here a wait function is implemented because cypress is faster then the website
-    cy.wait(2000);
-
-    // Toggle 'Show All' switch to display all students
-    cy.get(".mat-slide-toggle-content").click({ force: true });
+    // Choose Items per page: 50 to display all students
+    cy.get(".mat-select-arrow-wrapper").click();
+    cy.get("[ng-reflect-value=50]").click();
 
     // Check if student is in the school students list
     cy.contains(
-      "#cdk-accordion-child-10 > .mat-expansion-panel-body",
-      this.studentName
+      "app-children-overview > app-entity-subrecord",
+      this.studentName.trim()
     );
   });
 });
