bf30e1486c1e33317ff5c990856a79812c4f1ac0
Christoph Scheuing <47225324+christophscheuing@users.noreply.github.com>
2022-02-22 21:32:20 +0100

    Merged master and styling

--- src/app/pwa-install/pwa-install.module.ts
+++ src/app/pwa-install/pwa-install.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PwaInstallComponent } from './pwa-install.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { TranslationModule } from 'app/core/translation/translation.module';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [PwaInstallComponent],
   imports: [
     CommonModule,
+    FontAwesomeModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    TranslationModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [PwaInstallComponent]
 })
