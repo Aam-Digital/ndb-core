d3ffce33c09910580692094638cc97c994383098
Simon <33730997+TheSlimvReal@users.noreply.github.com>
2022-03-02 10:57:45 +0100

    Update src/app/child-dev-project/attendance/attendance.service.ts

--- src/app/child-dev-project/attendance/attendance.service.ts
+++ src/app/child-dev-project/attendance/attendance.service.ts
@@ -281,7 +281,7 @@ export class AttendanceService {
     const childIdPromises = linkedGroups.map((groupId) =>
       this.childrenService
         .queryRelationsOf("school", groupId)
-        .then((relations) => relations.map((r) => r.childId).filter((r) => !!r))
+        .then((relations) => relations.map((r) => r.childId).filter((id) => !!id))
     );
     const allParticipants = await Promise.all(childIdPromises);
     // flatten and remove duplicates
