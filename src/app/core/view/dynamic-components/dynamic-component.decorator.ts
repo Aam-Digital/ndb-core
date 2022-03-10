a6bc53ff8212008daf4617a73980628063651251
Schottkyc137 <lukas2121@icloud.com>
2022-03-10 22:05:36 +0100

    doc

--- src/app/core/view/dynamic-components/dynamic-component.decorator.ts
+++ src/app/core/view/dynamic-components/dynamic-component.decorator.ts
@@ -1,6 +1,28 @@
 import { OnInitDynamicComponent } from "./on-init-dynamic-component.interface";
 import { viewRegistry } from "../../registry/dynamic-registry";
 
+/**
+ * Decorator to annotate a class that serves as dynamic component
+ * A dynamic component can be referenced from the config.
+ * The name to reference the component from is the name of the class without the
+ * `Component`, or one of the registered aliases
+ * @example
+ * ```
+ * @DynamicComponent()
+ * @Component(...)
+ * class DoSomethingComponent {
+ *   // Component definition
+ * }
+ *
+ * // Later in some config:
+ *
+ * {
+ *   view: "DoSomething"
+ * }
+ * ```
+ * @param aliases Aliases that the component is also known under
+ * @constructor
+ */
 export function DynamicComponent<
   Constructor extends new (...args: any[]) => OnInitDynamicComponent
 >(...aliases: string[]) {
