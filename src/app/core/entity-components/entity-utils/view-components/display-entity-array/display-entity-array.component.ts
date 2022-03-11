a5ef9433f278c7c590c9904c94377977068aba38
Schottkyc137 <lukas2121@icloud.com>
2022-03-11 20:21:12 +0100

    Remove registry in favor of entity mapper

--- src/app/core/entity-components/entity-utils/view-components/display-entity-array/display-entity-array.component.ts
+++ src/app/core/entity-components/entity-utils/view-components/display-entity-array/display-entity-array.component.ts
@@ -1,13 +1,9 @@
-import { Component, Inject } from "@angular/core";
+import { Component } from "@angular/core";
 import { Entity } from "../../../../entity/model/entity";
 import { EntityMapperService } from "../../../../entity/entity-mapper.service";
 import { ViewDirective } from "../view.directive";
 import { ViewPropertyConfig } from "../../../entity-list/EntityListConfig";
 import { DynamicComponent } from "../../../../view/dynamic-components/dynamic-component.decorator";
-import {
-  ENTITIES,
-  EntityRegistry,
-} from "../../../../registry/dynamic-registry";
 
 @DynamicComponent()
 @Component({
@@ -18,10 +14,7 @@ import {
 export class DisplayEntityArrayComponent extends ViewDirective {
   readonly aggregationThreshold = 5;
   entities: Entity[];
-  constructor(
-    private entityMapper: EntityMapperService,
-    @Inject(ENTITIES) private entityRegistry: EntityRegistry
-  ) {
+  constructor(private entityMapper: EntityMapperService) {
     super();
   }
 
@@ -30,9 +23,8 @@ export class DisplayEntityArrayComponent extends ViewDirective {
     const entityIds: string[] = this.entity[this.property] || [];
     if (entityIds.length < this.aggregationThreshold) {
       const entityType = this.entity.getSchema().get(this.property).additional;
-      const entityConstructor = this.entityRegistry.get(entityType);
       const entityPromises = entityIds.map((entityId) =>
-        this.entityMapper.load(entityConstructor, entityId)
+        this.entityMapper.load(entityType, entityId)
       );
       this.entities = await Promise.all(entityPromises);
     }
