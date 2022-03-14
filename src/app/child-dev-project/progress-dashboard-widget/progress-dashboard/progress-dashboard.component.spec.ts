c98786d456fa0bef2282ebfa50db7071ea400b75
Schottkyc137 <lukas2121@icloud.com>
2022-03-14 20:52:21 +0100

    Progress is saved correctly after closing the dialog

--- src/app/child-dev-project/progress-dashboard-widget/progress-dashboard/progress-dashboard.component.spec.ts
+++ src/app/child-dev-project/progress-dashboard-widget/progress-dashboard/progress-dashboard.component.spec.ts
@@ -1,27 +1,40 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  waitForAsync,
+} from "@angular/core/testing";
 
 import { ProgressDashboardComponent } from "./progress-dashboard.component";
 import { EntityMapperService } from "../../../core/entity/entity-mapper.service";
 import { AlertService } from "../../../core/alerts/alert.service";
 import { ProgressDashboardWidgetModule } from "../progress-dashboard-widget.module";
 import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
+import { MatDialog } from "@angular/material/dialog";
+import { Subject } from "rxjs";
+import { take } from "rxjs/operators";
 
 describe("ProgressDashboardComponent", () => {
   let component: ProgressDashboardComponent;
   let fixture: ComponentFixture<ProgressDashboardComponent>;
+  const mockDialog = jasmine.createSpyObj<MatDialog>("matDialog", ["open"]);
+  let mockEntityService: jasmine.SpyObj<any>;
 
   beforeEach(
     waitForAsync(() => {
-      const mockEntityService = jasmine.createSpyObj("mockEntityService", [
+      mockEntityService = jasmine.createSpyObj("mockEntityService", [
         "load",
         "save",
       ]);
       mockEntityService.load.and.resolveTo({ title: "test", parts: [] });
+      mockEntityService.save.and.resolveTo();
+      // mockEntityService.save.and.callFake((next) => console.log("called with ", next));
 
       TestBed.configureTestingModule({
         imports: [ProgressDashboardWidgetModule, FontAwesomeTestingModule],
         providers: [
           { provide: EntityMapperService, useValue: mockEntityService },
+          { provide: MatDialog, useValue: mockDialog },
           {
             provide: AlertService,
             useValue: jasmine.createSpyObj([
@@ -44,4 +57,14 @@ describe("ProgressDashboardComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("saves data after the dialog was closed", fakeAsync(() => {
+    const closeNotifier = new Subject();
+    mockDialog.open.and.returnValue({
+      afterClosed: () => closeNotifier.pipe(take(1)),
+    } as any);
+    component.showEditComponent();
+    closeNotifier.next({});
+    expect(mockEntityService.save).toHaveBeenCalled();
+  }));
 });
