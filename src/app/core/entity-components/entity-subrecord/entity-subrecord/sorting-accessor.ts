e55a6b9c5018cb18070050fb1d978a6b8c29ad22
Simon <therealslimv@yahoo.de>
2022-03-21 09:26:33 +0100

    refactor sorting data accessor

--- src/app/core/entity-components/entity-subrecord/entity-subrecord/sorting-accessor.ts
+++ src/app/core/entity-components/entity-subrecord/entity-subrecord/sorting-accessor.ts
@@ -6,17 +6,21 @@
  * @param sortingHeader The active sorting header key; passed in by the data source
  */
 export function entityListSortingAccessor(data: Object, sortingHeader: string) {
-  if (
-    typeof data[sortingHeader] === "object" &&
-    data[sortingHeader] &&
-    "label" in data[sortingHeader]
-  ) {
+  if (isConfigurableEnum(data, sortingHeader)) {
     return data[sortingHeader].label;
   } else if (typeof data[sortingHeader] === "string") {
     return tryNumber(data[sortingHeader]);
   } else {
     return data[sortingHeader];
   }
+}
+
+function isConfigurableEnum(data: Object, sortingHeader: string) {
+  return (
+    typeof data[sortingHeader] === "object" &&
+    data[sortingHeader] &&
+    "label" in data[sortingHeader]
+  );
 }
 
 function tryNumber(input?: any): any {
