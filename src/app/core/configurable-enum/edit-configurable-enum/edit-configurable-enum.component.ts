99e5ff5179a9d25e3bcdfe0280cd5ab8fdbfd61c
Simon <therealslimv@yahoo.de>
2022-03-24 22:16:14 +0100

    added multi option to edit-configurable-enum.component

--- src/app/core/configurable-enum/edit-configurable-enum/edit-configurable-enum.component.ts
+++ src/app/core/configurable-enum/edit-configurable-enum/edit-configurable-enum.component.ts
@@ -5,6 +5,7 @@ import {
 } from "../../entity-components/entity-utils/dynamic-form-components/edit-component";
 import { ConfigurableEnumValue } from "../configurable-enum.interface";
 import { DynamicComponent } from "../../view/dynamic-components/dynamic-component.decorator";
+import { arrayEntitySchemaDatatype } from "../../entity/schema-datatypes/datatype-array";
 
 @DynamicComponent("EditConfigurableEnum")
 @Component({
@@ -14,9 +15,13 @@ import { DynamicComponent } from "../../view/dynamic-components/dynamic-componen
 })
 export class EditConfigurableEnumComponent extends EditComponent<ConfigurableEnumValue> {
   enumId: string;
+  multi = false;
 
   onInitFromDynamicConfig(config: EditPropertyConfig) {
     super.onInitFromDynamicConfig(config);
+    if (config.propertySchema.dataType === arrayEntitySchemaDatatype.name) {
+      this.multi = true;
+    }
     this.enumId =
       config.formFieldConfig.additional ||
       config.propertySchema.additional ||
