88faf8422d74aedd2f98c45f089487c116e34338
Schottkyc137 <lukas2121@icloud.com>
2022-03-31 13:44:59 +0200

    resolve errors

--- src/app/core/database/pouch-database.ts
+++ src/app/core/database/pouch-database.ts
@@ -141,17 +141,22 @@ export class PouchDatabase extends Database {
     });
   }
 
-  putAll(objects: any[], options?: PutAllOptions): Promise<any> {
+  async putAll(objects: any[], options?: PutAllOptions): Promise<any> {
     if (options?.force) {
       objects.forEach((obj) => (obj._rev = undefined));
     }
-    return this._pouchDB.bulkDocs(objects, options).catch((err) => {
-      if (err.status === 409) {
-        return this.resolveConflict(objects, false, err);
-      } else {
-        throw err;
-      }
-    });
+    try {
+      await this._pouchDB.bulkDocs(objects, options);
+    } catch (errors) {
+      return errors.map((err: PouchDB.Core.Error) => {
+        if (err.status === 409) {
+          const object = objects.find((obj) => obj.id === err.id);
+          return this.resolveConflict(object, options?.force, err);
+        } else {
+          throw err;
+        }
+      });
+    }
   }
 
   /**
