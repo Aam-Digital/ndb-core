b14ad07d83b56d4fd908ad4874b440dea396b5b6
Simon <therealslimv@yahoo.de>
2022-03-31 17:25:34 +0200

    added test for summary component

--- src/app/child-dev-project/attendance/attendance-summary/attendance-summary.component.spec.ts
+++ src/app/child-dev-project/attendance/attendance-summary/attendance-summary.component.spec.ts
@@ -1,16 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AttendanceSummaryComponent } from './attendance-summary.component';
+import { AttendanceSummaryComponent } from "./attendance-summary.component";
+import { AttendanceModule } from "../attendance.module";
 
-describe('AttendanceSummaryComponent', () => {
+describe("AttendanceSummaryComponent", () => {
   let component: AttendanceSummaryComponent;
   let fixture: ComponentFixture<AttendanceSummaryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AttendanceSummaryComponent ]
-    })
-    .compileComponents();
+      imports: [AttendanceModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +19,26 @@ describe('AttendanceSummaryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should set/overwrite the from and to column", () => {
+    component.columns = [
+      { id: "periodFrom", label: "Month" },
+      { id: "totalEvents", label: "Total" },
+      { id: "attendancePercentage", label: "Attendance" },
+    ];
+
+    expect(component._columns).toEqual([
+      jasmine.objectContaining({ id: "periodFrom" }),
+      jasmine.objectContaining({ id: "periodTo" }),
+      { id: "totalEvents", label: "Total" },
+      { id: "attendancePercentage", label: "Attendance" },
+    ]);
+    expect(component._columns).not.toContain({
+      id: "periodFrom",
+      label: "Month",
+    });
   });
 });
