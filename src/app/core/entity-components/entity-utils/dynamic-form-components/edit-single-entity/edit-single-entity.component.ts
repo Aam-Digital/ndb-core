34141dda7a53e9eae289da1020062595c507e8cd
Simon <33730997+TheSlimvReal@users.noreply.github.com>
2022-04-22 11:59:50 +0200

    fix(#1207): Errors in form dialogs are correctly shown
    Co-authored-by: Sebastian <sebastian.leidig@gmail.com>

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-single-entity/edit-single-entity.component.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-single-entity/edit-single-entity.component.ts
@@ -1,19 +1,10 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  ViewChild,
-} from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { EditComponent, EditPropertyConfig } from "../edit-component";
 import { Entity } from "../../../../entity/model/entity";
-import { Observable } from "rxjs";
-import { filter, map } from "rxjs/operators";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { FormControl } from "@angular/forms";
+import { BehaviorSubject } from "rxjs";
 import { DynamicComponent } from "../../../../view/dynamic-components/dynamic-component.decorator";
 import { EntityMapperService } from "../../../../entity/entity-mapper.service";
 
-@UntilDestroy()
 @DynamicComponent("EditSingleEntity")
 @Component({
   selector: "app-edit-single-entity",
@@ -23,86 +14,64 @@ import { EntityMapperService } from "../../../../entity/entity-mapper.service";
 export class EditSingleEntityComponent extends EditComponent<string> {
   entities: Entity[] = [];
   placeholder: string;
-  filteredEntities: Observable<Entity[]>;
+  autocompleteEntities = new BehaviorSubject<Entity[]>([]);
   selectedEntity?: Entity;
-  editingSelectedEntity: boolean = false;
-  entityNameFormControl = new FormControl();
+  editingSelectedEntity = false;
 
   @ViewChild("inputElement") input: ElementRef;
 
-  constructor(
-    private changeDetection: ChangeDetectorRef,
-    private entityMapperService: EntityMapperService
-  ) {
+  constructor(private entityMapperService: EntityMapperService) {
     super();
-    this.filteredEntities = this.entityNameFormControl.valueChanges.pipe(
-      untilDestroyed(this),
-      filter((s) => s !== null),
-      map((searchText?: string) => this.filter(searchText))
-    );
   }
 
-  private filter(searchText?: string): Entity[] {
-    if (!searchText) {
-      return this.entities;
-    } else {
-      const filterValue = searchText.toLowerCase();
-      return this.entities.filter((entity) =>
-        entity.toString().toLowerCase().includes(filterValue)
+  updateAutocomplete(inputText: string) {
+    let filteredEntities = this.entities;
+    if (inputText) {
+      filteredEntities = this.entities.filter((entity) =>
+        entity.toString().toLowerCase().includes(inputText.toLowerCase())
       );
     }
+    this.autocompleteEntities.next(filteredEntities);
   }
 
   async onInitFromDynamicConfig(config: EditPropertyConfig) {
     super.onInitFromDynamicConfig(config);
-    this.connectFormControlDisabledStatus();
     this.placeholder = $localize`:Placeholder for input to set an entity|context Select User:Select ${this.label}`;
     const entityType: string =
       config.formFieldConfig.additional || config.propertySchema.additional;
     this.entities = await this.entityMapperService.loadType(entityType);
     this.entities.sort((e1, e2) => e1.toString().localeCompare(e2.toString()));
-    this.entityNameFormControl.setValidators(this.formControl.validator);
     const selectedEntity = this.entities.find(
       (entity) => entity.getId() === this.formControl.value
     );
     if (selectedEntity) {
       this.selectedEntity = selectedEntity;
       this.editingSelectedEntity = false;
-      this.entityNameFormControl.setValue(selectedEntity.toString());
+    }
+  }
+
+  select(selected: string | Entity) {
+    let entity: Entity;
+    if (typeof selected === "string") {
+      entity = this.entities.find(
+        (e) => e.toString().toLowerCase() === selected.toLowerCase()
+      );
     } else {
-      this.entityNameFormControl.setValue("");
+      entity = selected;
     }
-    this.changeDetection.detectChanges();
-  }
 
-  private connectFormControlDisabledStatus() {
-    if (this.formControl.disabled) {
-      this.entityNameFormControl.disable();
-    }
-    this.formControl.registerOnDisabledChange((isDisabled) => {
-      if (isDisabled) {
-        this.entityNameFormControl.disable();
-      } else {
-        this.entityNameFormControl.enable();
-      }
-    });
-  }
-
-  select(entityName: string) {
-    const entity = this.entities.find(
-      (e) => e.toString().toLowerCase() === entityName.toLowerCase()
-    );
     if (entity) {
       this.selectedEntity = entity;
       this.editingSelectedEntity = false;
       this.formControl.setValue(entity.getId());
+    } else {
+      this.selectedEntity = undefined;
+      this.formControl.setValue(undefined);
     }
   }
 
   editSelectedEntity() {
     this.editingSelectedEntity = true;
-    this.selectedEntity = undefined;
-    this.formControl.setValue(null);
     setTimeout(() => {
       this.input.nativeElement.focus();
     });
