101c3ae9c8052146f3e15cbf809105e47cbf760a
Simon <33730997+TheSlimvReal@users.noreply.github.com>
2022-04-26 16:47:33 +0200

    feat: Added a user support panel (#1220)
    fixes: #1214
    
    Co-authored-by: Sebastian <sebastian.leidig@gmail.com>

---
+++ src/app/core/support/support/support.component.spec.ts
@@ -0,0 +1,118 @@
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from "@angular/core/testing";
+
+import { SupportComponent } from "./support.component";
+import { SupportModule } from "../support.module";
+import { SessionService } from "../../session/session-service/session.service";
+import { BehaviorSubject, of } from "rxjs";
+import { SyncState } from "../../session/session-states/sync-state.enum";
+import { SwUpdate } from "@angular/service-worker";
+import { Database } from "../../database/database";
+import { LOCATION_TOKEN, WINDOW_TOKEN } from "../../../utils/di-tokens";
+import { TEST_USER } from "../../../utils/mocked-testing.module";
+import { RemoteSession } from "../../session/session-service/remote-session";
+import { ConfirmationDialogService } from "../../confirmation-dialog/confirmation-dialog.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClient } from "@angular/common/http";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+
+describe("SupportComponent", () => {
+  let component: SupportComponent;
+  let fixture: ComponentFixture<SupportComponent>;
+  const testUser = { name: TEST_USER, roles: [] };
+  let mockSessionService: jasmine.SpyObj<SessionService>;
+  const mockSW = { isEnabled: false };
+  let mockDB: jasmine.SpyObj<Database>;
+  const mockWindow = {
+    navigator: {
+      userAgent: "mock user agent",
+      serviceWorker: { getRegistrations: () => [], ready: Promise.resolve() },
+    },
+  };
+  let mockLocation: jasmine.SpyObj<Location>;
+
+  beforeEach(async () => {
+    localStorage.clear();
+    mockSessionService = jasmine.createSpyObj(["getCurrentUser"], {
+      syncState: new BehaviorSubject(SyncState.UNSYNCED),
+    });
+    mockSessionService.getCurrentUser.and.returnValue(testUser);
+    mockDB = jasmine.createSpyObj(["destroy"]);
+    mockLocation = jasmine.createSpyObj(["reload"]);
+    await TestBed.configureTestingModule({
+      imports: [SupportModule, HttpClientTestingModule, NoopAnimationsModule],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: SwUpdate, useValue: mockSW },
+        { provide: Database, useValue: mockDB },
+        { provide: WINDOW_TOKEN, useValue: mockWindow },
+        { provide: LOCATION_TOKEN, useValue: mockLocation },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SupportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should initialize application information", () => {
+    expect(component.currentUser).toBe(testUser);
+    expect(component.currentSyncState).toBe("unsynced");
+    expect(component.lastSync).toBe("never");
+    expect(component.lastRemoteLogin).toBe("never");
+    expect(component.swStatus).toBe("not enabled");
+    expect(component.userAgent).toBe("mock user agent");
+  });
+
+  it("should correctly read sync and remote login status from local storage", () => {
+    const lastSync = new Date("2022-01-01").toISOString();
+    localStorage.setItem(SupportComponent.LAST_SYNC_KEY, lastSync);
+    const lastRemoteLogin = new Date("2022-01-02").toISOString();
+    localStorage.setItem(RemoteSession.LAST_LOGIN_KEY, lastRemoteLogin);
+
+    component.ngOnInit();
+
+    expect(component.lastSync).toBe(lastSync);
+    expect(component.lastRemoteLogin).toBe(lastRemoteLogin);
+    localStorage.clear();
+  });
+
+  it("should reset the application after confirmation", async () => {
+    const confirmationDialog = TestBed.inject(ConfirmationDialogService);
+    spyOn(confirmationDialog, "getConfirmation").and.returnValue({
+      afterClosed: () => of(true),
+    } as any);
+    localStorage.setItem("someItem", "someValue");
+    const unregisterSpy = jasmine.createSpy();
+    mockWindow.navigator.serviceWorker.getRegistrations = () => [
+      { unregister: unregisterSpy },
+    ];
+
+    await component.resetApplication();
+
+    expect(mockDB.destroy).toHaveBeenCalled();
+    expect(unregisterSpy).toHaveBeenCalled();
+    expect(localStorage.getItem("someItem")).toBeNull();
+    expect(mockLocation.reload).toHaveBeenCalled();
+  });
+
+  it("should display the service worker logs after they are available", fakeAsync(() => {
+    const exampleLog = "example service worker log";
+    spyOn(TestBed.inject(HttpClient), "get").and.returnValue(of(exampleLog));
+
+    component.ngOnInit();
+    tick();
+
+    expect(component.swLog).toBe(exampleLog);
+  }));
+});
---
+++ src/app/core/support/support/support.component.ts
@@ -0,0 +1,126 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { SessionService } from "../../session/session-service/session.service";
+import { DatabaseUser } from "../../session/session-service/local-user";
+import { LOCATION_TOKEN, WINDOW_TOKEN } from "../../../utils/di-tokens";
+import { SyncState } from "../../session/session-states/sync-state.enum";
+import { SwUpdate } from "@angular/service-worker";
+import { Database } from "../../database/database";
+import * as Sentry from "@sentry/browser";
+import { Severity } from "@sentry/browser";
+import { RemoteSession } from "../../session/session-service/remote-session";
+import { RouteTarget } from "../../../app.routing";
+import { ConfirmationDialogService } from "../../confirmation-dialog/confirmation-dialog.service";
+import { HttpClient } from "@angular/common/http";
+
+@RouteTarget("Support")
+@Component({
+  selector: "app-support",
+  templateUrl: "./support.component.html",
+  styleUrls: ["./support.component.scss"],
+})
+export class SupportComponent implements OnInit {
+  static readonly LAST_SYNC_KEY = "LAST_SYNC";
+  currentUser: DatabaseUser;
+  currentSyncState: string;
+  lastSync: string;
+  lastRemoteLogin: string;
+  swStatus: string;
+  swLog = "not available";
+  userAgent = this.window.navigator.userAgent;
+
+  constructor(
+    private sessionService: SessionService,
+    private sw: SwUpdate,
+    private database: Database,
+    private confirmationDialog: ConfirmationDialogService,
+    private http: HttpClient,
+    @Inject(WINDOW_TOKEN) private window: Window,
+    @Inject(LOCATION_TOKEN) private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.sessionService.getCurrentUser();
+    this.initCurrentSyncState();
+    this.initLastSync();
+    this.initLastRemoteLogin();
+    this.initSwStatus();
+  }
+
+  private initCurrentSyncState() {
+    switch (this.sessionService.syncState.value) {
+      case SyncState.COMPLETED:
+        this.currentSyncState = "synced";
+        return;
+      case SyncState.STARTED:
+        this.currentSyncState = "in progress";
+        return;
+      default:
+        this.currentSyncState = "unsynced";
+    }
+  }
+
+  private initLastSync() {
+    this.lastSync =
+      localStorage.getItem(SupportComponent.LAST_SYNC_KEY) || "never";
+  }
+
+  private initLastRemoteLogin() {
+    this.lastRemoteLogin =
+      localStorage.getItem(RemoteSession.LAST_LOGIN_KEY) || "never";
+  }
+
+  private initSwStatus() {
+    if (this.sw.isEnabled) {
+      this.swStatus = "enabled";
+    } else {
+      this.swStatus = "not enabled";
+    }
+    this.window.navigator.serviceWorker.ready
+      .then(() =>
+        this.http.get("/ngsw/state", { responseType: "text" }).toPromise()
+      )
+      .then((res) => (this.swLog = res));
+  }
+
+  sendReport() {
+    // This is sent even without submitting the crash report.
+    Sentry.captureMessage("report information", {
+      user: { name: this.currentUser.name },
+      level: Severity.Debug,
+      extra: {
+        currentSyncState: this.currentSyncState,
+        lastSync: this.lastSync,
+        lastRemoteLogin: this.lastRemoteLogin,
+        swStatus: this.swStatus,
+        userAgent: this.userAgent,
+        swLog: this.swLog,
+      },
+    });
+    Sentry.showReportDialog({
+      user: {
+        name: this.currentUser.name,
+        email: "example@email.com",
+      },
+      title: $localize`:Title user feedback dialog:Support request`,
+      subtitle: $localize`:Subtitle user feedback dialog:Please describe the problem you are facing.`,
+      subtitle2: "",
+    });
+  }
+
+  async resetApplication() {
+    const choice = await this.confirmationDialog.getConfirmation(
+      "Reset Application",
+      "Are you sure you want to reset the application? This will delete all application data from your device and you will have to synchronize again."
+    );
+    if (!choice) {
+      return;
+    }
+
+    await this.database.destroy();
+    const registrations = await this.window.navigator.serviceWorker.getRegistrations();
+    const unregisterPromises = registrations.map((reg) => reg.unregister());
+    await Promise.all(unregisterPromises);
+    localStorage.clear();
+    this.location.reload();
+  }
+}
