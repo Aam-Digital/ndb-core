7f93328ff03bfd12c4155162b3457edd668131bd
Radhika Thalla <radhikathalla9@gmail.com>
2022-04-27 10:54:54 +0200

    changes

--- src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
+++ src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input,OnInit,AfterViewInit } from "@angular/core";
+import { Component,OnInit,AfterViewInit, } from "@angular/core";
 import { EntityMapperService } from "../../../../core/entity/entity-mapper.service";
 import { Child } from "../../model/child";
 import { DynamicComponent } from "../../../../core/view/dynamic-components/dynamic-component.decorator";
@@ -7,7 +7,9 @@ import { MatTableDataSource } from "@angular/material/table";
 import { MatSort } from "@angular/material/sort";
 import { MatPaginator } from "@angular/material/paginator";
 import { ViewChild } from "@angular/core";
-import { calculateAge } from "app/utils/utils";
+import { ChildrenBmiDashboardComponent } from "../children-bmi-dashboard/children-bmi-dashboard.component";
+import childBlockStories from "../../child-block/child-block.stories";
+
 @DynamicComponent("BirthdayDashboard")
 @Component({
   selector: "app-birthday-dashboard",
@@ -15,43 +17,39 @@ import { calculateAge } from "app/utils/utils";
   styleUrls: ["./birthday-dashboard.component.scss"],
 })
 export class BirthdayDashboardComponent implements OnInitDynamicComponent,AfterViewInit {
-  @Input() dateOfBirth: Date;
-  @Input() age:number;
-
 
   children: Child[] = [];
-  Child: any ;
-  /**  displayedColumns: string[] = ['entityid', 'dateOfBirth', 'age'];*/
-  childrenDataSource = new  MatTableDataSource<Child>();
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  isLoading: boolean = true;
-  dataSource: MatTableDataSource<Child>;
-  
-
+  displayedColumns: string[] = ['entityid', 'dateOfBirth', 'age'];
+  childrendataSource = new  MatTableDataSource<Child>();
+  @ViewChild("Paginator",{static:true}) paginator: MatPaginator;
+  @ViewChild("Sort",{static:true}) sort: MatSort;
+  isLoading: boolean = false;
 
   constructor(private entityMapper: EntityMapperService) {}
   ngOnInit(){
    // TODO sort the children based on the date of birth
     this.entityMapper.loadType(Child).then((res) => {
       this.children = res;
-      console.log(res);
+      console.log(this.children);
+      this.displayedColumns = ['entityid', 'dateOfBirth', 'age'];
+      this.childrendataSource = new MatTableDataSource(this.children);
+      
+      this.childrendataSource.paginator=this.paginator;
       
       //  Now the children are set
     });
+    
+    
+
   
   }
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.childrendataSource= new MatTableDataSource(this.children);
+    this.childrendataSource.paginator = this.paginator;
+    this.childrendataSource.sort = this.sort;
   }
   onInitFromDynamicConfig(config: any) {
-    if (config?.dateOfBirth) {
-      this.dateOfBirth = config.dateOfBirth;
-    }
-    if (config?.age) {
-      this.age = config.age;
-    }
+    
   }
  
 }
