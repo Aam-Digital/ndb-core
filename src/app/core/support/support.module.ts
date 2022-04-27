fac23fd0a37d4f7f3e19d1251f723e01daf27df8
Simon <therealslimv@yahoo.de>
2022-04-27 13:22:26 +0200

    fix: last sync state is correctly set

--- src/app/core/support/support.module.ts
+++ src/app/core/support/support.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SupportComponent } from "./support/support.component";
 import { MatButtonModule } from "@angular/material/button";
-import { SessionService } from "../session/session-service/session.service";
-import { filter } from "rxjs/operators";
-import { SyncState } from "../session/session-states/sync-state.enum";
 import { ConfirmationDialogModule } from "../confirmation-dialog/confirmation-dialog.module";
 import { FlexModule } from "@angular/flex-layout";
 import { MatExpansionModule } from "@angular/material/expansion";
@@ -22,14 +19,4 @@ import { MatExpansionModule } from "@angular/material/expansion";
 })
 export class SupportModule {
   static dynamicComponents = [SupportComponent];
-  constructor(sessionService: SessionService) {
-    sessionService.syncState
-      .pipe(filter((state) => state === SyncState.COMPLETED))
-      .subscribe(() =>
-        localStorage.setItem(
-          SupportComponent.LAST_SYNC_KEY,
-          new Date().toISOString()
-        )
-      );
-  }
 }
