6d10c7b0fd55f69345e705344db996489f90666c
Simon <therealslimv@yahoo.de>
2022-05-04 23:08:31 +0200

    using directive instead of manually loading config

--- src/app/child-dev-project/attendance/attendance-status-select/attendance-status-select.component.ts
+++ src/app/child-dev-project/attendance/attendance-status-select/attendance-status-select.component.ts
@@ -1,18 +1,4 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges,
-} from "@angular/core";
-import { ConfigService } from "../../../core/config/config.service";
-import {
-  CONFIGURABLE_ENUM_CONFIG_PREFIX,
-  ConfigurableEnumConfig,
-} from "../../../core/configurable-enum/configurable-enum.interface";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import {
   ATTENDANCE_STATUS_CONFIG_ID,
   AttendanceStatusType,
@@ -24,37 +10,9 @@ import {
   templateUrl: "./attendance-status-select.component.html",
   styleUrls: ["./attendance-status-select.component.scss"],
 })
-export class AttendanceStatusSelectComponent implements OnInit, OnChanges {
+export class AttendanceStatusSelectComponent {
   @Input() value: AttendanceStatusType = NullAttendanceStatusType;
   @Input() disabled: boolean = false;
   @Output() valueChange = new EventEmitter<AttendanceStatusType>();
-
-  statusValues: AttendanceStatusType[];
   statusID = ATTENDANCE_STATUS_CONFIG_ID;
-
-  constructor(
-    private configService: ConfigService,
-    private changeDetection: ChangeDetectorRef
-  ) {
-    this.valueChange.subscribe((change) =>
-      console.log("change", typeof this.value, change, this.value)
-    );
-  }
-
-  ngOnInit() {
-    console.log("before", typeof this.value);
-    console.log("value", this.value.label.toLowerCase());
-    this.statusValues = this.configService.getConfig<
-      ConfigurableEnumConfig<AttendanceStatusType>
-    >(CONFIGURABLE_ENUM_CONFIG_PREFIX + ATTENDANCE_STATUS_CONFIG_ID);
-    console.log("statusValues", this.statusValues);
-    this.changeDetection.detectChanges();
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log("changes", changes, this.value.label.toLowerCase());
-  }
-  compareStatuses(a: AttendanceStatusType, b: AttendanceStatusType) {
-    return a.id === b.id;
-  }
 }
