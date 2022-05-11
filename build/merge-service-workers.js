7cb006a06bac8e9cda5594bdf75e399b5ee95aab
Simon <therealslimv@yahoo.de>
2022-05-11 17:21:51 +0200

    not editing ngsw-worker file anymore

--- build/merge-service-workers.js
+++ build/merge-service-workers.js
@@ -47,15 +47,15 @@ fs.writeFileSync(`${distFolder}/ngsw.json`, JSON.stringify(combined));
 fs.unlinkSync(`${distFolder}/${firstLocale}/ngsw.json`);
 
 // Adjust service worker to allow changing language offline
-const swFile = fs
-  .readFileSync(`${distFolder}/${firstLocale}/ngsw-worker.js`)
-  .toString();
+// const swFile = fs
+//   .readFileSync(`${distFolder}/${firstLocale}/ngsw-worker.js`)
+//   .toString();
 //TODO this might be an issue in the root request
 // const patchedSw = swFile.replace(
 //   "return this.handleFetch(this.adapter.newRequest(this.indexUrl), context);",
 //   "return this.handleFetch(this.adapter.newRequest('/' + this.adapter.normalizeUrl(req.url).split('/')[1] + '/index.html'), context);"
 // );
-fs.writeFileSync(`${distFolder}/ngsw-worker.js`, patchedSw);
+// fs.writeFileSync(`${distFolder}/ngsw-worker.js`, patchedSw);
 fs.unlinkSync(`${distFolder}/${firstLocale}/ngsw-worker.js`);
 fs.renameSync(
   `${distFolder}/${firstLocale}/safety-worker.js`,
