91dc12f34e641bb8065369329fd4fea342331b01
Simon <therealslimv@yahoo.de>
2022-05-20 07:57:40 +0200

    fixed tests

--- e2e/integration/MarkingChildAsDropout.ts
+++ e2e/integration/MarkingChildAsDropout.ts
@@ -1,14 +1,12 @@
-describe("Scenario: Marking a child as dropout - E2E test", () => {
-  before(() => {
-    // GIVEN I am on the details page of a specific child
+describe("Scenario: Marking a child as dropout - E2E test", function () {
+  before("GIVEN I am on the details page of a specific child", function () {
     // go to the url with the Child
     cy.visit("child/1");
     // save the name of this Child to the variable
     cy.get(".header-row > .header-title").invoke("text").as("childName");
   });
 
-  // WHEN I select a dropout date for this child
-  it("Select a dropout date for the child", function () {
+  it("WHEN I select a dropout date for this child", () => {
     // click on "Dropout" menu
     cy.contains("Dropout").click();
     cy.get("#mat-tab-label-0-7").click();
@@ -21,8 +19,7 @@ describe("Scenario: Marking a child as dropout - E2E test", () => {
     cy.contains("span", "Save").should("be.visible").click();
   });
 
-  // THEN I should not see this child in the list of all children at first
-  it("This child is not in the list of all children at first", function () {
+  it("THEN I should not see this child in the list of all children at first", function () {
     // click on "Children" menu in navigation
     cy.get('[ng-reflect-angulartics-label="Children"]').click();
     // type to the input "Filter" the name of child
@@ -31,8 +28,7 @@ describe("Scenario: Marking a child as dropout - E2E test", () => {
     cy.get("table").contains(this.childName.trim()).should("not.exist");
   });
 
-  // AND I should see the child when I activate the 'inactive' filter
-  it("The child is seen when the 'inactive' filter activated", function () {
+  it("AND I should see the child when I activate the 'inactive' filter", function () {
     // click on the button with the content "Inactive"
     cy.get('[ng-reflect-placeholder="isActive"]').click();
     cy.contains("span", "Inactive").should("be.visible").click();
--- e2e/integration/RecordingAttendanceOfChild.ts
+++ e2e/integration/RecordingAttendanceOfChild.ts
@@ -1,17 +1,14 @@
-describe("Scenario: Recording attendance of a child - E2E test", () => {
-  before(() => {
-    // GIVEN A specific child is attending a specific class
+describe("Scenario: Recording attendance of a child - E2E test", function () {
+  before("GIVEN A specific child is attending a specific class", function () {
     cy.visit("attendance");
   });
 
-  // WHEN I record attendance for this class
-  it("Record attendance for the class", () => {
+  it("WHEN I record attendance for this class", () => {
     cy.get(".mat-card", { timeout: 10000 }).should("be.visible").eq(0).click();
     cy.contains("button", "Record").click();
   });
 
-  // AND I set the attendance of the specific child to 'present'
-  it("set the attendance of the specific child to 'present'", () => {
+  it("AND I set the attendance of the specific child to 'present'", function () {
     cy.contains("button", "Show more").click({ scrollBehavior: "center" });
     cy.contains("mat-card", "School Class")
       .eq(0)
@@ -21,19 +18,16 @@ describe("Scenario: Recording attendance of a child - E2E test", () => {
     cy.contains("button", "Save").click();
   });
 
-  // THEN in the details page of this child under 'attendance' for the specific class I should see a green background for the current day
-  it("In the details page of this child under 'attendance' for the specific class should be a green background for the current day", () => {
+  it("THEN in the details page of this child under 'attendance' for the specific class I should see a green background for the current day", function () {
     cy.get(".mat-card", { timeout: 10000 })
       .should("be.visible")
       .eq(0)
       .click({ force: true });
-    // Click on ChildBlock inside roll-call to navigate to child
+    // Navigate to child through search bar
     cy.get(".navbar-wrapper > :nth-child(1)").click();
-    cy.get(".mat-body-1").then((el) => {
-      console.log(el);
-      console.log(el.text());
-      cy.get("#mat-input-2").focus().type(el.text());
-    });
+    cy.get(".mat-body-1").then((el) =>
+      cy.get("#mat-input-2").type(el.text()).type("{downArrow}").type("{enter}")
+    );
     cy.get(".mat-option-text", { timeout: 500 }).click();
     cy.get("#mat-tab-label-0-2").click();
     cy.get(".attendance-P").should(
