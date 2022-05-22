ec324bf2d97c63975b303a2a5b2c9b63b6215cb4
Schottkyc137 <45085299+Schottkyc137@users.noreply.github.com>
2022-05-22 20:09:57 +0200

    fix: Improved animation for roll call and enabled swipe to skipp participants (#1253)
    Co-authored-by: Simon <33730997+TheSlimvReal@users.noreply.github.com>
    Co-authored-by: Simon <therealslimv@yahoo.de>

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,10 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
+// Import hammer.js to enable gestures
+// on mobile devices
+import "hammerjs";
+
 if (environment.production) {
   enableProdMode();
 }
