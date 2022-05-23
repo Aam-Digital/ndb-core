12b2ce02fd62552c11e5445b96401d1c913e32a5
Radhika Thalla <radhikathalla9@gmail.com>
2022-05-23 16:38:20 +0200

    sort date by day and month

--- src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
+++ src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
@@ -6,6 +6,9 @@ import { OnInitDynamicComponent } from "../../../../core/view/dynamic-components
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
 import { ViewChild } from "@angular/core";
+import { months } from "moment";
+import { C } from "@angular/cdk/keycodes";
+import { date } from "faker";
 
 @DynamicComponent("BirthdayDashboard")
 @Component({
@@ -25,9 +28,46 @@ export class BirthdayDashboardComponent
   async ngOnInit() {
     this.children = (await this.entityMapper.loadType(Child))
       .filter((child: Child) => child.isActive)
-      .sort((a, b) => a.dateOfBirth.getTime() - b.dateOfBirth.getTime());
+      //*.sort((a, b) => a.dateOfBirth.getTime() - b.dateOfBirth.getTime());
     this.childrendataSource.data = this.children;
+    var dateOfBirth: any[], today: Date, bday: Date, diff: number, days: number;
+  dateOfBirth = [months,days]; // 6th of February
+        today = new Date();
+        bday = new Date(today.getFullYear(),dateOfBirth[1]-1,dateOfBirth[0]);
+    if( today.getTime() > bday.getTime()) {
+        bday.setFullYear(bday.getFullYear()+1);
+          }
+       diff = bday.getTime()-today.getTime();// calculating difference between days
+        days = Math.floor(diff/(1000*60*60*24));
+   // const children=[]  ;
+     
+//function distanceToBirthday(dateOfBirth)
+//{
+   // let currDate = new Date();
+   // currDate.setHours(0, 0, 0, 0);
+    //let currYear = currDate.getFullYear();
+
+   // let offset = new Date();
+   // offset.setHours(0, 0, 0, 0);
+   // offset.setFullYear(currYear + 1);
+
+    //dateOfBirth = new Date(dateOfBirth + " 00:00");
+    //dateOfBirth.setFullYear(currYear);
+
+    //let diff = dateOfBirth - currDate;
+   // return (diff < 0) ? diff + offset.getTime() : diff;
+//}
+
+//function getUpcomingBirthdays(bdays)
+//{
+   // return bdays.slice(0).sort(
+       // (a: any[], b: any[]) => distanceToBirthday(a[1]) - distanceToBirthday(b[1])
+   // );
+//}
+
+//console.log(getUpcomingBirthdays(children));
   }
+  
 
   ngAfterViewInit() {
     this.childrendataSource.paginator = this.paginator;
