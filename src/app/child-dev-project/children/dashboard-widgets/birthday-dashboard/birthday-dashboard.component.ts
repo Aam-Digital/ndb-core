d4b5b3bacc3e4f4b5cb6bfa02825d5ef9834e0ff
Radhika Thalla <radhikathalla9@gmail.com>
2022-07-04 14:13:13 +0200

    styled code using link

--- src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
+++ src/app/child-dev-project/children/dashboard-widgets/birthday-dashboard/birthday-dashboard.component.ts
@@ -6,9 +6,6 @@ import { OnInitDynamicComponent } from "../../../../core/view/dynamic-components
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
 import { ViewChild } from "@angular/core";
-import { ConditionalExpr } from "@angular/compiler";
-import { Console } from "console";
-
 
 @DynamicComponent("BirthdayDashboard")
 @Component({
@@ -26,72 +23,33 @@ export class BirthdayDashboardComponent
   constructor(private entityMapper: EntityMapperService) {}
 
   async ngOnInit() {
-      this.children = (await this.entityMapper.loadType(Child))
+  this.children = (await this.entityMapper.loadType(Child))
       .filter((child: Child) => child.isActive);
      
-     this.childrendataSource.data= this.children.map((child)=>({child:child,birthday:getNextBirthday(child.dateOfBirth)}))
-       .sort((a,b) =>daysUntilBirthday(a.birthday) - daysUntilBirthday(b.birthday))
-       .filter((a)=>daysUntilBirthday(a.birthday)<32)
+  this.childrendataSource.data= this.children.map((child)=>( {child:child,birthday:getNextBirthday(child.dateOfBirth)} ))
+      .sort((a,b) =>daysUntilBirthday(a.birthday) - daysUntilBirthday(b.birthday))
+      .filter((a)=>daysUntilBirthday(a.birthday)<32);
 
-     
-   
-       /** Takes a date as input and returns the number of days till the next birthday (i.e. this or next year).
-*/
-      function daysUntilBirthday(birthday: Date): number {
-         let today = new Date();
-         today.setHours(0,0,0,0);
-       
-         let diff = birthday.getTime() - today.getTime();
-         let daysTillNextBirthday = Math.floor(diff / (1000 * 60 * 60 * 24));
+ function daysUntilBirthday(birthday: Date): number {
+  let today = new Date();
+      today.setHours(0,0,0,0);
+  let diff = birthday.getTime() - today.getTime();
+  let daysTillNextBirthday = Math.floor(diff / (1000 * 60 * 60 * 24));
+      return daysTillNextBirthday;
+}
+ function getNextBirthday(dateOfBirth:Date) :Date {
+  let today = new Date();
+      today.setHours(0,0,0,0);
+  let birthday = new Date(today.getFullYear(),dateOfBirth.getMonth(), dateOfBirth.getDate());
 
-         return daysTillNextBirthday;
-            }
-            function getNextBirthday(dateOfBirth:Date) :Date{
-              let today = new Date();
-              today.setHours(0,0,0,0);
-              let birthday = new Date(today.getFullYear(),dateOfBirth.getMonth(),
-                  dateOfBirth.getDate());
-                       
-     
-             // If the birthday has already occured this year. Then their next birthday is next year.
-              if (today.getTime() > birthday.getTime()) { 
-                 birthday.setFullYear(birthday.getFullYear() + 1);
-                    }
-                    return birthday;
-
-
-            }
-           
-  
-
-    // TASK A: sort this.children so that the child whose birthday comes next is listed first
-
-    // TASK B: filter this.children so that only those children are included whose birthday is less than 30 days from today
-
-  
-             }
-
-        ngAfterViewInit() {
-        this.childrendataSource.paginator = this.paginator;
-          }
-
-        onInitFromDynamicConfig(config: any) {}
-      }
-
-/**
-* Takes a date as input and returns the number of days till the next birthday (i.e. this or next year).
-*/
-//function daysUntilBirthday(dateOfBirth: Date) : Number {
-  //let today = new Date();
-  //let birthday = new Date(today.getFullYear(), dateOfBirth.getMonth() - 1, dateOfBirth.getDate());
-
-  // If the birthday has already occured this year. Then their next birthday is next year.
-  //if (today.getTime() > birthday.getTime()) {
-    //birthday.setFullYear(birthday.getFullYear() + 1);
- // }
-
-  //let diff = birthday.getTime() - today.getTime();
-  //let daysTillNextBirthday = Math.floor(diff/(1000*60*60*24));
-
-  //return daysTillNextBirthday;
-//}
+  if (today.getTime() > birthday.getTime()) { 
+      birthday.setFullYear(birthday.getFullYear() + 1);
+}
+    return birthday;
+}
+}
+ ngAfterViewInit() {
+ this.childrendataSource.paginator = this.paginator;
+}
+ onInitFromDynamicConfig(config: any) {}
+}
