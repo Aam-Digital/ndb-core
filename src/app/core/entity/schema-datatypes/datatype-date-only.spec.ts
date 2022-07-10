a2bdfc48bda4b09a4400efcfe061ba9eb6b3e0d9
Simon <therealslimv@yahoo.de>
2022-07-10 14:29:48 -0600

    added test

--- src/app/core/entity/schema-datatypes/datatype-date-only.spec.ts
+++ src/app/core/entity/schema-datatypes/datatype-date-only.spec.ts
@@ -2,9 +2,23 @@ import { dateOnlyEntitySchemaDatatype } from "./datatype-date-only";
 
 describe("Schema data type:Date", () => {
   it("should not fail on null values", () => {
-    const nullDateRes = dateOnlyEntitySchemaDatatype.transformToDatabaseFormat(
-      null
-    );
+    const nullDateRes =
+      dateOnlyEntitySchemaDatatype.transformToDatabaseFormat(null);
     expect(nullDateRes).toBeUndefined();
+  });
+
+  it("should correctly transform values", () => {
+    const date = new Date(2022, 0, 1);
+
+    const dbFormat =
+      dateOnlyEntitySchemaDatatype.transformToDatabaseFormat(date);
+    expect(dbFormat).toBe("2022-01-01");
+
+    const objFormat: Date =
+      dateOnlyEntitySchemaDatatype.transformToObjectFormat(dbFormat);
+    expect(objFormat).toBeInstanceOf(Date);
+    expect(objFormat.getFullYear()).toBe(2022);
+    expect(objFormat.getMonth()).toBe(0);
+    expect(objFormat.getDate()).toBe(1);
   });
 });
