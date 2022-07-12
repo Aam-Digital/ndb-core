89838406ca8b2c03f4e76788efdcdcbbfcbcee54
Simon <therealslimv@yahoo.de>
2022-07-12 14:13:51 +0200

    refactored language setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,11 +23,6 @@ import { environment } from "./environments/environment";
 import { loadTranslations } from "@angular/localize";
 import { registerLocaleData } from "@angular/common";
 import { AppConfig } from "./app/core/app-config/app-config";
-import localeDe from "@angular/common/locales/de";
-
-// Import hammer.js to enable gestures
-// on mobile devices
-import "hammerjs";
 import { parseTranslationsForLocalize } from "./app/utils/utils";
 
 if (environment.production) {
@@ -35,46 +30,32 @@ if (environment.production) {
 }
 
 const locale = localStorage.getItem("locale") || "en-US";
-
 if (locale !== "en-US") {
-  fetch("/assets/locale/messages." + locale + ".xlf")
-    .then((response) => response.text())
-    // fetch("/assets/locale/messages.json")
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error("HTTP error " + response.status);
-    //     }
-    //     return response.json().then((res) => res.translations);
-    //   })
-    .then((res) => parseTranslationsForLocalize(res))
-    .then(async (json) => {
-      loadTranslations(json);
-      $localize.locale = locale;
-
-      // This is needed for locale-aware components & pipes to work.
-      // Add the required locales to `webpackInclude` to keep the bundle size small
-      // const localeModule = await import(
-      //   /* webpackInclude: /(en-US|fr|de)\.js/ */
-      //   `@angular/common/locales/${locale}`
-      // );
-      registerLocaleData(localeDe, locale);
-
-      /**
-       * Loading AppConfig before bootstrap process (see {@link https://stackoverflow.com/a/66957293/10713841})
-       */
-      AppConfig.load().then(() => {
-        // Bootstrap app
-        platformBrowserDynamic()
-          .bootstrapModule(AppModule)
-          .catch((err) => console.error(err));
-      });
-    })
-    .catch((err) => console.log("error", err));
+  initLanguage(locale).then(() => buildApp());
 } else {
+  buildApp();
+}
+
+async function initLanguage(locale: string): Promise<void> {
+  const response = await fetch("/assets/locale/messages." + locale + ".xlf");
+  const res = await response.text();
+  const json = await parseTranslationsForLocalize(res);
+  loadTranslations(json);
+  $localize.locale = locale;
+  // This is needed for locale-aware components & pipes to work.
+  // Add the required locales to `webpackInclude` to keep the bundle size small
+  const localeModule = await import(
+    /* webpackInclude: /(fr|de)\.mjs/ */
+    `../node_modules/@angular/common/locales/${locale}`
+  );
+  registerLocaleData(localeModule.default);
+}
+
+function buildApp(): Promise<void> {
   /**
    * Loading AppConfig before bootstrap process (see {@link https://stackoverflow.com/a/66957293/10713841})
    */
-  AppConfig.load().then(() => {
+  return AppConfig.load().then(() => {
     // Bootstrap app
     platformBrowserDynamic()
       .bootstrapModule(AppModule)
