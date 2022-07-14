12f720ba7e8e2e685f29a50ca2d1204876e8dc80
Simon <therealslimv@yahoo.de>
2022-07-14 13:45:21 +0200

    removed unused prebuilt school filter

--- src/app/child-dev-project/children/children-list/children-list.component.ts
+++ src/app/child-dev-project/children/children-list/children-list.component.ts
@@ -1,17 +1,9 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Child } from "../model/child";
 import { ActivatedRoute, Router } from "@angular/router";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { ChildrenService } from "../children.service";
-import { FilterSelectionOption } from "../../../core/filter/filter-selection/filter-selection";
-import {
-  EntityListConfig,
-  PrebuiltFilterConfig,
-} from "../../../core/entity-components/entity-list/EntityListConfig";
-import { EntityMapperService } from "../../../core/entity/entity-mapper.service";
-import { School } from "../../schools/model/school";
-import { LoggingService } from "../../../core/logging/logging.service";
-import { EntityListComponent } from "../../../core/entity-components/entity-list/entity-list.component";
+import { EntityListConfig } from "../../../core/entity-components/entity-list/EntityListConfig";
 import { RouteData } from "../../../core/view/dynamic-routing/view-config.interface";
 import { RouteTarget } from "../../../app.routing";
 
@@ -34,16 +26,12 @@ export class ChildrenListComponent implements OnInit {
   childrenList: Child[] = [];
   listConfig: EntityListConfig;
   childConstructor = Child;
-  @ViewChild(EntityListComponent)
-  entityListComponent: EntityListComponent<Child>;
-  isLoading: boolean = true;
+  isLoading = true;
 
   constructor(
     private childrenService: ChildrenService,
-    private entityMapper: EntityMapperService,
     private route: ActivatedRoute,
-    private router: Router,
-    private log: LoggingService
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -55,7 +43,6 @@ export class ChildrenListComponent implements OnInit {
       .pipe(untilDestroyed(this))
       .subscribe((children) => {
         this.childrenList = children;
-        this.addPrebuiltFilters();
         this.isLoading = false;
       });
   }
@@ -63,46 +50,5 @@ export class ChildrenListComponent implements OnInit {
   routeTo(route: string) {
     const path = "/" + Child.ENTITY_TYPE.toLowerCase();
     this.router.navigate([path, route]);
-  }
-
-  private async addPrebuiltFilters() {
-    for (const prebuiltFilter of this.listConfig.filters.filter(
-      (filter) => filter.type === "prebuilt"
-    )) {
-      switch (prebuiltFilter.id) {
-        case "school": {
-          (prebuiltFilter as PrebuiltFilterConfig<Child>).options = await this.buildSchoolFilter();
-          (prebuiltFilter as PrebuiltFilterConfig<Child>).default = "";
-          break;
-        }
-        default: {
-          this.log.warn(
-            "[ChildrenListComponent] No filter options available for prebuilt filter: " +
-              prebuiltFilter.id
-          );
-          (prebuiltFilter as PrebuiltFilterConfig<Child>).options = [];
-        }
-      }
-    }
-    // Triggering the initialization of the filter selections
-    this.entityListComponent.ngOnChanges({
-      entityList: null,
-    });
-  }
-
-  private async buildSchoolFilter(): Promise<FilterSelectionOption<Child>[]> {
-    const schools: School[] = await this.entityMapper.loadType(School);
-    const options: FilterSelectionOption<Child>[] = [
-      { key: "", label: $localize`All`, filterFun: () => true },
-    ];
-    schools.sort((s1, s2) => s1.toString().localeCompare(s2.toString()));
-    schools.forEach((school) =>
-      options.push({
-        key: school.getId(),
-        label: school.toString(),
-        filterFun: (c) => c.schoolId === school.getId(),
-      })
-    );
-    return options;
   }
 }
