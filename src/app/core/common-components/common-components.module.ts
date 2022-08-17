ae7b8275ed7bb2e81b82b966c5b7aae4b209a4a4
Schottkyc137 <lukas2121@icloud.com>
2022-08-17 11:57:03 +0200

    Added pill

--- src/app/core/common-components/common-components.module.ts
+++ src/app/core/common-components/common-components.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { DialogCloseComponent } from "./dialog-close/dialog-close.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatButtonModule } from "@angular/material/button";
 import { CustomizableTooltipDirective } from "./customizable-tooltip/customizable-tooltip.directive";
 import { CustomizableTooltipComponent } from "./customizable-tooltip/customizable-tooltip.component";
 import { BorderHighlightDirective } from "./border-highlight/border-highlight.directive";
-import { AddNewButtonComponent } from './add-new-button/add-new-button.component';
+import { AddNewButtonComponent } from "./add-new-button/add-new-button.component";
+import { PillComponent } from "./pill/pill.component";
 
 @NgModule({
   declarations: [
@@ -15,17 +16,15 @@ import { AddNewButtonComponent } from './add-new-button/add-new-button.component
     CustomizableTooltipComponent,
     BorderHighlightDirective,
     AddNewButtonComponent,
+    PillComponent,
   ],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    MatButtonModule,
-  ],
+  imports: [CommonModule, FontAwesomeModule, MatButtonModule],
   exports: [
     DialogCloseComponent,
     CustomizableTooltipDirective,
     BorderHighlightDirective,
-    AddNewButtonComponent
-  ]
+    AddNewButtonComponent,
+    PillComponent,
+  ],
 })
 export class CommonComponentsModule {}
