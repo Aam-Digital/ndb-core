d3af770242862e9c70e76172adc37b4215fea500
Schottkyc137 <lukas2121@icloud.com>
2022-08-25 21:41:11 +0200

    Add dialog close button

--- src/app/child-dev-project/attendance/attendance.module.ts
+++ src/app/child-dev-project/attendance/attendance.module.ts
@@ -69,6 +69,7 @@ import { ConfigurableEnumModule } from "../../core/configurable-enum/configurabl
 import * as Hammer from "hammerjs";
 import { TabStateModule } from "../../utils/tab-state/tab-state.module";
 import { CommonComponentsModule } from "../../core/common-components/common-components.module";
+import { MatDialogModule } from "@angular/material/dialog";
 
 @Injectable()
 // Only allow horizontal swiping
@@ -132,6 +133,7 @@ export class HorizontalHammerConfig extends HammerGestureConfig {
     HammerModule,
     TabStateModule,
     CommonComponentsModule,
+    MatDialogModule,
   ],
   exports: [
     ActivityCardComponent,
