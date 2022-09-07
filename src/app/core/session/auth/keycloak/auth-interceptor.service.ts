950d405e17ffee53dbd46810f468fd759b5b44c2
Simon <simon@aam-digital.com>
2022-09-07 12:28:34 +0200

    added documentation for AuthInterceptor

--- src/app/core/session/auth/keycloak/auth-interceptor.service.ts
+++ src/app/core/session/auth/keycloak/auth-interceptor.service.ts
@@ -8,6 +8,10 @@ import {
 import { Observable } from "rxjs";
 import { AuthService } from "../auth.service";
 
+/**
+ * This interceptor adds the required auth headers to all outgoing requests of the HttpClient service.
+ * This allows other backend services to verify, whether a user is properly logged in.
+ */
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
@@ -16,6 +20,7 @@ export class AuthInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    // The request needs to be cloned as they are immutable
     const headers = {} as any;
     this.authService.addAuthHeader(headers);
     return next.handle(request.clone({ setHeaders: headers }));
