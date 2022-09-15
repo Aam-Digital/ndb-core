4e52038bdfdf2528be83497a1b3e7cd685b8b6fc
matt nichols <mattknichols@gmail.com>
2022-09-15 19:40:31 +0200

    changes to enum and rating-answers

--- src/app/core/configurable-enum/display-configurable-enum/display-configurable-enum.component.ts
+++ src/app/core/configurable-enum/display-configurable-enum/display-configurable-enum.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
+import { ViewPropertyConfig } from "app/core/entity-components/entity-list/EntityListConfig";
 import { ViewDirective } from "../../entity-components/entity-utils/view-components/view.directive";
 import { DynamicComponent } from "../../view/dynamic-components/dynamic-component.decorator";
 import { ConfigurableEnumValue } from "../configurable-enum.interface";
@@ -11,4 +12,11 @@ import { ConfigurableEnumValue } from "../configurable-enum.interface";
   selector: "app-display-configurable-enum",
   template: `{{ value?.label }}`,
 })
-export class DisplayConfigurableEnumComponent extends ViewDirective<ConfigurableEnumValue> {}
+export class DisplayConfigurableEnumComponent extends ViewDirective<ConfigurableEnumValue> {
+  @HostBinding ('style.background-color') style;
+  @HostBinding ('style.padding') padding = '5px';
+  @HostBinding ('style.border-radius') radius = '4px';
+  onInitFromDynamicConfig(config: ViewPropertyConfig) {
+    super.onInitFromDynamicConfig(config)
+    this.style = this.value.color;
+} }
--- src/app/features/historical-data/model/rating-answers.ts
+++ src/app/features/historical-data/model/rating-answers.ts
@@ -2,21 +2,26 @@ export const ratingAnswers = [
   {
     id: "notTrueAtAll",
     label: $localize`:Rating answer:not at all`,
+    color: 'rgba(253, 114, 114, 0.5)',
   },
   {
     id: "rarelyTrue",
     label: $localize`:Rating answer:rarely`,
+    color: 'rgba(255, 165, 0, 0.5)',
   },
   {
     id: "usuallyTrue",
     label: $localize`:Rating answer:usually`,
+    color: 'rgba(255, 242, 0, 0.5)',
   },
   {
     id: "absolutelyTrue",
     label: $localize`:Rating answer:absolutely`,
+    color: 'rgba(144, 238, 144, 0.5)',
   },
   {
     id: "noAnswerPossible",
     label: $localize`:Rating answer:N/A`,
+    color: '#b0b0b0',
   },
 ];
