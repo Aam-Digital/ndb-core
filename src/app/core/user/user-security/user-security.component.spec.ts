6d79f0604bdfc71864f2a94dd026a87d85c41b5e
Simon <simon@aam-digital.com>
2022-09-30 15:54:32 +0200

    fixed test

--- src/app/core/user/user-security/user-security.component.spec.ts
+++ src/app/core/user/user-security/user-security.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { UserSecurityComponent } from './user-security.component';
+import { UserSecurityComponent } from "./user-security.component";
+import { UserModule } from "../user.module";
+import { MockedTestingModule } from "../../../utils/mocked-testing.module";
 
-describe('UserSecurityComponent', () => {
+describe("UserSecurityComponent", () => {
   let component: UserSecurityComponent;
   let fixture: ComponentFixture<UserSecurityComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserSecurityComponent ]
-    })
-    .compileComponents();
+      imports: [UserModule, MockedTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserSecurityComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
