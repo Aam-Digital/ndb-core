e398b2191e61beeb9a73fdb88d8d8668c24fa0d3
Schottkyc137 <lukas2121@icloud.com>
2022-10-20 19:34:35 +0200

    Dialog close button is no longer always focused

--- src/app/core/common-components/dialog-close/dialog-close.component.ts
+++ src/app/core/common-components/dialog-close/dialog-close.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ViewChild } from "@angular/core";
+import { MatButton } from "@angular/material/button";
 
 @Component({
-  selector: 'app-dialog-close',
-  templateUrl: './dialog-close.component.html',
-  styleUrls: ['./dialog-close.component.scss'],
+  selector: "app-dialog-close",
+  templateUrl: "./dialog-close.component.html",
+  styleUrls: ["./dialog-close.component.scss"],
 })
-export class DialogCloseComponent {}
+export class DialogCloseComponent implements AfterViewChecked {
+  @ViewChild("button") button: MatButton;
+
+  ngAfterViewChecked() {
+    (<HTMLButtonElement>this.button._elementRef.nativeElement).blur();
+  }
+}
