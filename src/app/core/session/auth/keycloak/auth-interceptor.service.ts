aa49a54cb0a65bb71519338fbb513df69510ae48
Simon <simon@aam-digital.com>
2022-10-21 17:05:53 +0200

    fix: GitHub API can be used again

--- src/app/core/session/auth/keycloak/auth-interceptor.service.ts
+++ src/app/core/session/auth/keycloak/auth-interceptor.service.ts
@@ -4,9 +4,21 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
+  HttpContextToken,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { AuthService } from "../auth.service";
+
+/**
+ * This context can be used to prevent the Bearer token to be set by this interceptor.
+ * This can be useful when a request goes to a 3rd party service e.g. GitHub.
+ *
+ * Usage:
+ * ```javascript
+ * this.httpClient.get(`...`, { context: new HttpContext().set(AUTH_ENABLED, false) });
+ * ```
+ */
+export const AUTH_ENABLED = new HttpContextToken(() => true);
 
 /**
  * This interceptor adds the required auth headers to all outgoing requests of the HttpClient service.
@@ -20,9 +32,12 @@ export class AuthInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    // The request needs to be cloned as they are immutable
-    const headers = {} as any;
-    this.authService.addAuthHeader(headers);
-    return next.handle(request.clone({ setHeaders: headers }));
+    if (request.context.get(AUTH_ENABLED)) {
+      const headers = {} as any;
+      this.authService.addAuthHeader(headers);
+      // The request needs to be cloned as they are immutable
+      request = request.clone({ setHeaders: headers });
+    }
+    return next.handle(request);
   }
 }
