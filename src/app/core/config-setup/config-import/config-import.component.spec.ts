d38e0f48e2e9902c901a577d6458cf48cb96ba2e
Sebastian Leidig <sebastian.leidig@gmail.com>
2022-10-26 11:28:20 +0200

    cleanup

--- src/app/core/config-setup/config-import/config-import.component.spec.ts
+++ src/app/core/config-setup/config-import/config-import.component.spec.ts
@@ -1,23 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ConfigImportComponent } from './config-import.component';
+import { ConfigImportComponent } from "./config-import.component";
+import { ConfigImportParserService } from "../config-import-parser.service";
 
-describe('ConfigImportComponent', () => {
+describe("ConfigImportComponent", () => {
   let component: ConfigImportComponent;
   let fixture: ComponentFixture<ConfigImportComponent>;
 
+  const mockConfigParser: ConfigImportParserService = jasmine.createSpyObj([
+    "parseImportDefinition",
+  ]);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfigImportComponent ]
-    })
-    .compileComponents();
+      declarations: [ConfigImportComponent],
+      providers: [
+        { provide: ConfigImportParserService, useValue: mockConfigParser },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfigImportComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
