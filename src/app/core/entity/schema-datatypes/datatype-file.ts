eb0678b4d892e54cc7e146831bd6291d020d27e8
Simon <simon@aam-digital.com>
2022-11-02 12:42:50 +0100

    Update src/app/core/entity/schema-datatypes/datatype-file.ts
    Co-authored-by: Sebastian <sebastian.leidig@gmail.com>

--- src/app/core/entity/schema-datatypes/datatype-file.ts
+++ src/app/core/entity/schema-datatypes/datatype-file.ts
@@ -1,5 +1,9 @@
 import { EntitySchemaDatatype } from "../schema/entity-schema-datatype";
 
+/**
+ * Datatype to make a file attachment property available in an entity.
+ * Attachment (file) itself is not stored as part of the database document but uploaded separatedly.
+ */
 export const fileEntitySchemaDatatype: EntitySchemaDatatype = {
   name: "file",
   editComponent: "edit-file",
