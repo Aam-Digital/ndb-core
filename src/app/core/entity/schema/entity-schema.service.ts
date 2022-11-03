dedeb164f22719ad8fc06d57c5a77d46015e1012
Simon <simon@aam-digital.com>
2022-11-03 13:41:26 +0100

    removed file datatype

--- src/app/core/entity/schema/entity-schema.service.ts
+++ src/app/core/entity/schema/entity-schema.service.ts
@@ -30,7 +30,6 @@ import { schemaEmbedEntitySchemaDatatype } from "../schema-datatypes/datatype-sc
 import { dateOnlyEntitySchemaDatatype } from "../schema-datatypes/datatype-date-only";
 import { mapEntitySchemaDatatype } from "../schema-datatypes/datatype-map";
 import { booleanEntitySchemaDatatype } from "../schema-datatypes/datatype-boolean";
-import { fileEntitySchemaDatatype } from "../schema-datatypes/datatype-file";
 
 /**
  * Transform between entity instances and database objects
@@ -66,7 +65,6 @@ export class EntitySchemaService {
     this.registerSchemaDatatype(schemaEmbedEntitySchemaDatatype);
     this.registerSchemaDatatype(mapEntitySchemaDatatype);
     this.registerSchemaDatatype(booleanEntitySchemaDatatype);
-    this.registerSchemaDatatype(fileEntitySchemaDatatype);
   }
 
   /**
