b96aa8b6b11942e4af060f0a51f1e0f331b7ac87
Simon <simon@aam-digital.com>
2022-11-05 08:03:15 +0100

    added tests for mock file service

--- src/app/core/file/mock-file.service.spec.ts
+++ src/app/core/file/mock-file.service.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { MockFileService } from './mock-file.service';
+import { MockFileService } from "./mock-file.service";
+import { EntityMapperService } from "../entity/entity-mapper.service";
+import { Entity } from "../entity/model/entity";
+import { firstValueFrom } from "rxjs";
 
-describe('MockFileService', () => {
+describe("MockFileService", () => {
   let service: MockFileService;
+  let mockEntityMapper: jasmine.SpyObj<EntityMapperService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockEntityMapper = jasmine.createSpyObj(["save"]);
+    TestBed.configureTestingModule({
+      providers: [
+        MockFileService,
+        { provide: EntityMapperService, useValue: mockEntityMapper },
+      ],
+    });
     service = TestBed.inject(MockFileService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should allow to open a file that has been uploaded before", async () => {
+    spyOn(URL, "createObjectURL").and.returnValue("object.url");
+    spyOn(window, "open");
+    const entity = new Entity();
+    const prop = "fileProp";
+    const file = { name: "test.file " } as File;
+
+    await firstValueFrom(service.uploadFile(file, entity, prop));
+
+    expect(entity[prop]).toBe(file.name);
+    expect(URL.createObjectURL).toHaveBeenCalledWith(file);
+    expect(mockEntityMapper.save).toHaveBeenCalledWith(entity);
+
+    service.showFile(entity, prop);
+
+    expect(window.open).toHaveBeenCalledWith("object.url", "_blank");
+  });
+
+  it("should remove a file from a entity", async () => {
+    const entity = new Entity();
+    const prop = "testProp";
+    entity[prop] = "test.file";
+
+    await firstValueFrom(service.removeFile(entity, prop));
+
+    expect(entity[prop]).toBe(undefined);
+    expect(mockEntityMapper.save).toHaveBeenCalledWith(entity);
   });
 });
