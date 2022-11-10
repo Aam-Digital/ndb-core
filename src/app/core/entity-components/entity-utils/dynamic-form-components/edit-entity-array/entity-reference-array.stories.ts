f25d25686847f34766985511777021a33b98d5f9
Sebastian Leidig <sebastian.leidig@gmail.com>
2022-11-10 15:38:16 +0100

    fix(ui): show label for form fields of "single entity select" type
    closes #1525

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-entity-array/entity-reference-array.stories.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-entity-array/entity-reference-array.stories.ts
@@ -7,13 +7,20 @@ import { EntityFormModule } from "../../../entity-form/entity-form.module";
 import { FormFieldConfig } from "../../../entity-form/entity-form/FormConfig";
 import { EntityMapperService } from "../../../../entity/entity-mapper.service";
 import { AlertsModule } from "../../../../alerts/alerts.module";
+import { StorybookBaseModule } from "../../../../../utils/storybook-base.module";
+import { DatabaseEntity } from "../../../../entity/database-entity.decorator";
 import { Entity } from "../../../../entity/model/entity";
 import { DatabaseField } from "../../../../entity/database-field.decorator";
-import { DatabaseEntity } from "../../../../entity/database-entity.decorator";
-import { StorybookBaseModule } from "../../../../../utils/storybook-base.module";
+import { mockEntityMapper } from "../../../../entity/mock-entity-mapper-service";
+import { User } from "../../../../user/user";
+
+const testUser = new User("1");
+testUser.name = "test entity";
+const user2 = new User("2");
+user2.name = "other entity";
 
 export default {
-  title: "Core/EntityComponents/Entity Property Fields/Number",
+  title: "Core/EntityComponents/Entity Property Fields/Entity Reference Array",
   component: EntityFormComponent,
   decorators: [
     moduleMetadata({
@@ -27,7 +34,7 @@ export default {
         EntitySchemaService,
         {
           provide: EntityMapperService,
-          useValue: { save: () => Promise.resolve() },
+          useValue: mockEntityMapper([testUser, user2]),
         },
       ],
     }),
@@ -44,23 +51,42 @@ const Template: Story<EntityFormComponent> = (args: EntityFormComponent) => ({
   props: args,
 });
 
-@DatabaseEntity("TestEntity")
-class TestEntity extends Entity {
-  @DatabaseField() test: number;
-}
 const fieldConfig: FormFieldConfig = {
-  id: "test",
-  view: "DisplayNumber",
-  edit: "EditNumber",
-  label: "test field label",
+  id: "relatedEntities",
+  view: "DisplayEntityArray",
+  edit: "EditEntityArray",
+  label: "test related entities label",
   tooltip: "test tooltip",
 };
+const otherField: FormFieldConfig = {
+  id: "x",
+  view: "DisplayNumber",
+  edit: "EditNumber",
+  label: "other label",
+};
+
+@DatabaseEntity("TestEntityReferenceArrayEntity")
+class TestEntity extends Entity {
+  @DatabaseField({
+    dataType: "entity-reference-array",
+    additional: User.ENTITY_TYPE,
+  })
+  relatedEntities: string[];
+  @DatabaseField() x: number = 1;
+}
 
 const testEntity = new TestEntity();
-testEntity.test = 5;
+testEntity.relatedEntities = [testUser.getId()];
 
 export const Primary = Template.bind({});
 Primary.args = {
-  columns: [[fieldConfig]],
+  columns: [[otherField, fieldConfig, otherField, otherField]],
   entity: testEntity,
+};
+
+export const Edit = Template.bind({});
+Edit.args = {
+  columns: [[otherField, fieldConfig, otherField, otherField]],
+  entity: testEntity,
+  editing: true,
 };
