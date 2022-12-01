f142da2ee912533bd0109141d19ce87b3e30e129
Simon <simon@aam-digital.com>
2022-12-01 13:16:24 +0100

    improved styling of clear button

--- src/app/features/location/location.module.ts
+++ src/app/features/location/location.module.ts
@@ -9,6 +9,7 @@ import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatInputModule } from "@angular/material/input";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [EditLocationComponent, ViewLocationComponent],
@@ -20,6 +21,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
     ReactiveFormsModule,
     FormsModule,
     FontAwesomeModule,
+    MatButtonModule,
   ],
   exports: [EditLocationComponent],
 })
