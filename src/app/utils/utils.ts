26c0b7ea150c41aba2b900b9c482329ec116aeed
Sebastian <sebastian.leidig@gmail.com>
2022-12-02 11:14:02 +0100

    Update src/app/utils/utils.ts
    Co-authored-by: Simon <simon@aam-digital.com>

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -133,9 +133,9 @@ export function parseJwt(token): {
 }
 
 export function addAlphaToHexColor(color, opacity) {
-  // coerce values so ti is between 0 and 1.
-  var _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
-  return color + _opacity.toString(16).toUpperCase();
+  // coerce values so it is between 0 and 1.
+  const opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+  return color + opacity.toString(16).toUpperCase();
 }
 
 /**
