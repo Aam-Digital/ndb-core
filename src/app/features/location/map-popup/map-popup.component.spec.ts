b2fbf256405650e9bafb047195ceb2583e314b6a
Simon <simon@aam-digital.com>
2022-12-07 16:19:34 +0100

    added test for map-popup

--- src/app/features/location/map-popup/map-popup.component.spec.ts
+++ src/app/features/location/map-popup/map-popup.component.spec.ts
@@ -4,17 +4,20 @@ import { MapPopupComponent } from "./map-popup.component";
 import { LocationModule } from "../location.module";
 import { EntitySchemaService } from "../../../core/entity/schema/entity-schema.service";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ConfigService } from "../../../core/config/config.service";
 
 describe("MapPopupComponent", () => {
   let component: MapPopupComponent;
   let fixture: ComponentFixture<MapPopupComponent>;
+  const coordinates = { lat: 1, lon: 1 };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LocationModule],
       providers: [
         EntitySchemaService,
-        { provide: MAT_DIALOG_DATA, useValue: { lat: 1, lon: 1 } },
+        { provide: MAT_DIALOG_DATA, useValue: { coordinates } },
+        { provide: ConfigService, useValue: { getConfig: () => undefined } },
       ],
     }).compileComponents();
 
@@ -25,5 +28,22 @@ describe("MapPopupComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should use coordinates from dialog data", () => {
+    expect(component.coordinates).toEqual(coordinates);
+  });
+
+  it("should update coordinates on map clicks", () => {
+    component.select({ lat: 1, lon: 2 });
+
+    expect(component.coordinates).toEqual({ lat: 1, lon: 2 });
+  });
+
+  it("should update coordinates when disabled", () => {
+    component.disabled = true;
+    component.select({ lat: 1, lon: 2 });
+
+    expect(component.coordinates).toEqual(coordinates);
   });
 });
