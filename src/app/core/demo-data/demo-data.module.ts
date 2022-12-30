21257b75c36d353a9d27818175d353a956f9f66f
Simon <simon@aam-digital.com>
2022-12-30 20:57:50 +0100

    review cleanups

--- src/app/core/demo-data/demo-data.module.ts
+++ src/app/core/demo-data/demo-data.module.ts
@@ -34,12 +34,12 @@ import { DemoDataInitializerService } from "./demo-data-initializer.service";
  * Import this module in the root AppModule to automatically write demo data into the database on loading of the module.
  * You need to pass providers for {@link DemoDataGenerator} implementations to the `forRoot()` method to register them.
  *
- ```
- DemoDataModule.forRoot([
- ...DemoChildGenerator.provider({count: 150}),
- { provide: DemoUserGeneratorService, useClass: DemoUserGeneratorService }
- ])
- ```
+ *```javascript
+ *  DemoDataModule.forRoot([
+ *    ...DemoChildGenerator.provider({count: 150}),
+ *    { provide: DemoUserGeneratorService, useClass: DemoUserGeneratorService }
+ *   ])
+ * ```
  *
  * In addition to importing the `DemoDataModule` you need to call the {@link DemoDataService}'s `publishDemoData()` method
  * to actually start the data generation.
