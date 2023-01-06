3d76dac8ad6aacc4f80c26c58109d95972029bf2
Simon <simon@aam-digital.com>
2023-01-06 10:31:10 +0100

    fixed test

--- e2e/integration/HidingFilterOnSmallScreen.cy.ts
+++ e2e/integration/HidingFilterOnSmallScreen.cy.ts
@@ -1,36 +1,21 @@
 describe("When on a big screen, the filter component above the entity list should be visible", () => {
   before("GIVEN that the screen is big", () => {
-    cy.visit("");
+    cy.visit("child");
     cy.viewport("macbook-15");
   });
 
-  it("WHEN I am on an entities page", () => {
-    cy.get(
-      '[ng-reflect-angulartics-label="Children"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .flex-row'
-    ).click();
-  });
-
-  it("THEN I should see filter options", () => {
+  it("THEN I should see filter options on a list page", () => {
     cy.get("app-filter").should("be.visible");
   });
 });
 
 describe("When on a small screen, the filter component above the entity list should not be visible", () => {
   before("GIVEN that the screen is small", () => {
-    cy.visit("");
+    cy.visit("child");
     cy.viewport("iphone-6");
   });
 
-  it("WHEN I am on an entities page", () => {
-    cy.get(
-      ":nth-child(1) > .ng-star-inserted > .mdc-icon-button > .mat-mdc-button-touch-target"
-    ).click();
-    cy.get(
-      '[ng-reflect-angulartics-label="Children"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .flex-row'
-    ).click();
-  });
-
-  it("THEN I should not see filter options", () => {
-    cy.get("app-filter").should("not.be.visible");
+  it("THEN I should not see filter options on a list page", () => {
+    cy.get("app-filter").should("not.exist");
   });
 });
