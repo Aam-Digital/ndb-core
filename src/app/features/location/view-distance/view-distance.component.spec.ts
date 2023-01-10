e478de3806ebbd47e49d1e22cde49d9009c00c93
Simon <simon@aam-digital.com>
2023-01-10 11:01:29 +0100

    distance component support multiple properties

--- src/app/features/location/view-distance/view-distance.component.spec.ts
+++ src/app/features/location/view-distance/view-distance.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import {
   ViewDistanceComponent,
   ViewDistanceConfig,
@@ -12,43 +11,64 @@ import { Coordinates } from "../coordinates";
 describe("ViewDistanceComponent", () => {
   let component: ViewDistanceComponent;
   let fixture: ComponentFixture<ViewDistanceComponent>;
+  let compareCoordinates: Subject<Coordinates[]>;
+  let entity: Child;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ViewDistanceComponent],
     }).compileComponents();
 
+    entity = new Child();
+    entity["address"] = { lat: 52, lon: 13 };
+    compareCoordinates = new Subject();
+    const config: ViewPropertyConfig<ViewDistanceConfig> = {
+      id: "distance",
+      entity,
+      value: undefined,
+      config: {
+        compareCoordinates,
+        coordinatesProperty: ["address", "otherAddress"],
+      },
+    };
     fixture = TestBed.createComponent(ViewDistanceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.onInitFromDynamicConfig(config);
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it("update function and trigger change detection when new coordiantes are emitted", () => {
-    const entity = new Child();
-    entity["address"] = { lat: 52, lon: 13 };
-    const compareCoordinates = new Subject<Coordinates>();
-    const config: ViewPropertyConfig<ViewDistanceConfig> = {
-      id: "distance",
-      entity,
-      value: undefined,
-      config: { compareCoordinates, coordinatesProperty: "address" },
-    };
+  it("should display a dash if no address is set", () => {
+    entity["address"] = undefined;
+    compareCoordinates.next([{ lat: 52.0001, lon: 13 }]);
+
+    expect(component.distanceFunction(entity)).toBe("-");
+  });
+
+  it("update function and trigger change detection when new coordinates are emitted", () => {
     const detectChangesSpy = spyOn(
       component["changeDetector"],
       "detectChanges"
     );
-    component.onInitFromDynamicConfig(config);
 
-    compareCoordinates.next({ lat: 52.0001, lon: 13 });
+    compareCoordinates.next([{ lat: 52.0001, lon: 13 }]);
     expect(detectChangesSpy).toHaveBeenCalledTimes(1);
-    expect(component.distanceFunction(entity)).toEqual("0.01 km");
+    expect(component.distanceFunction(entity)).toBe("0.01 km");
 
-    compareCoordinates.next({ lat: 52.001, lon: 13 });
+    compareCoordinates.next([{ lat: 52.001, lon: 13 }]);
     expect(detectChangesSpy).toHaveBeenCalledTimes(2);
-    expect(component.distanceFunction(entity)).toEqual("0.11 km");
+    expect(component.distanceFunction(entity)).toBe("0.11 km");
+  });
+
+  it("should display the shortest distance", () => {
+    entity["otherAddress"] = { lat: 52, lon: 14 };
+    const c1 = { lat: 53, lon: 14 };
+    const c2 = { lat: 52.0001, lon: 14 };
+    compareCoordinates.next([c1, c2]);
+
+    expect(component.distanceFunction(entity)).toBe("0.01 km");
   });
 });
