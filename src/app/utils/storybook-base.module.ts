d062a426abc6d7602dc83679c36ee560b886ba92
Simon <simon@aam-digital.com>
2023-01-12 14:50:01 +0100

    fixed stories

--- src/app/utils/storybook-base.module.ts
+++ src/app/utils/storybook-base.module.ts
@@ -7,13 +7,9 @@ import {
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatNativeDateModule } from "@angular/material/core";
 import { Angulartics2Module } from "angulartics2";
 import { RouterTestingModule } from "@angular/router/testing";
-import {
-  entityRegistry,
-  EntityRegistry,
-} from "../core/entity/database-entity.decorator";
+import { entityRegistry } from "../core/entity/database-entity.decorator";
 import { ConfigService } from "../core/config/config.service";
 import { AbilityService } from "../core/permissions/ability/ability.service";
 import { BehaviorSubject, Subject } from "rxjs";
@@ -21,7 +17,6 @@ import { EntityAbility } from "../core/permissions/ability/entity-ability";
 import { defineAbility } from "@casl/ability";
 import { SessionService } from "../core/session/session-service/session.service";
 import { SyncState } from "../core/session/session-states/sync-state.enum";
-import { WINDOW_TOKEN } from "./di-tokens";
 import { createTestingConfigService } from "../core/config/testing-config-service";
 import { componentRegistry } from "../dynamic-components";
 import { AppModule } from "../app.module";
@@ -34,6 +29,7 @@ export const entityFormStorybookDefaulParameters = {
 
 export const mockAbilityService = {
   abilityUpdated: new Subject<void>(),
+  initializeRules: () => {},
 };
 
 /**
@@ -46,24 +42,22 @@ export const mockAbilityService = {
     CommonModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
-    MatNativeDateModule,
     Angulartics2Module.forRoot(),
     RouterTestingModule,
   ],
   providers: [
-    { provide: EntityRegistry, useValue: entityRegistry },
     { provide: ConfigService, useValue: createTestingConfigService() },
     { provide: AbilityService, useValue: mockAbilityService },
     {
       provide: EntityAbility,
       useValue: defineAbility((can) => can("manage", "all")),
     },
-    { provide: WINDOW_TOKEN, useValue: window },
     {
       provide: SessionService,
       useValue: {
         getCurrentUser: () => ({ name: "demo-user" }),
         syncState: new BehaviorSubject(SyncState.COMPLETED),
+        isLoggedIn: () => true,
       },
     },
   ],
