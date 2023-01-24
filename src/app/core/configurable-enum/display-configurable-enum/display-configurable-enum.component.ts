fc8904e2d86bc6382a05df83f2c87dadab9f6884
Simon <simon@aam-digital.com>
2023-01-24 14:46:37 +0100

    DisplayConfigurableEnum works with arrays

--- src/app/core/configurable-enum/display-configurable-enum/display-configurable-enum.component.ts
+++ src/app/core/configurable-enum/display-configurable-enum/display-configurable-enum.component.ts
@@ -7,25 +7,32 @@ import { ConfigurableEnumValue } from "../configurable-enum.interface";
 /**
  * This component displays a {@link ConfigurableEnumValue} as text.
  * If the value has a `color` property, it is used as the background color.
- * TODO should also work for multi-select
  */
 @DynamicComponent("DisplayConfigurableEnum")
 @Component({
   selector: "app-display-configurable-enum",
-  template: `{{ value?.label }}`,
+  template: `{{ templateString }}`,
   standalone: true,
 })
-export class DisplayConfigurableEnumComponent extends ViewDirective<ConfigurableEnumValue> {
+export class DisplayConfigurableEnumComponent extends ViewDirective<
+  ConfigurableEnumValue | ConfigurableEnumValue[]
+> {
   @HostBinding("style.background-color") private style;
   @HostBinding("style.padding") private padding;
   @HostBinding("style.border-radius") private radius;
+  templateString = "";
 
   onInitFromDynamicConfig(config: ViewPropertyConfig) {
     super.onInitFromDynamicConfig(config);
-    if (this.value?.color) {
-      this.style = this.value.color;
-      this.padding = "5px";
-      this.radius = "4px";
+    if (Array.isArray(this.value)) {
+      this.templateString = this.value.map((v) => v.label).join(", ");
+    } else if (this.value) {
+      if (this.value.color) {
+        this.style = this.value.color;
+        this.padding = "5px";
+        this.radius = "4px";
+      }
+      this.templateString = this.value.label;
     }
   }
 }
