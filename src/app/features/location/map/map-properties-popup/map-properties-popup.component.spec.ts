b3149cd03e66ef779fc6188749e18c0befecc90e
Simon <simon@aam-digital.com>
2023-01-26 15:38:01 +0100

    fixed tests for MapPropertiesPopup

--- src/app/features/location/map/map-properties-popup/map-properties-popup.component.spec.ts
+++ src/app/features/location/map/map-properties-popup/map-properties-popup.component.spec.ts
@@ -1,8 +1,12 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { MapPropertiesPopupComponent } from "./map-properties-popup.component";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
+import {
+  entityRegistry,
+  EntityRegistry,
+} from "../../../../core/entity/database-entity.decorator";
 
 describe("MapPropertiesPopupComponent", () => {
   let component: MapPropertiesPopupComponent;
@@ -11,7 +15,11 @@ describe("MapPropertiesPopupComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MapPropertiesPopupComponent, FontAwesomeTestingModule],
-      providers: [{ provide: MAT_DIALOG_DATA, useValue: [] }],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: [] },
+        { provide: EntityRegistry, useValue: entityRegistry },
+        { provide: MatDialogRef, useValue: undefined },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MapPropertiesPopupComponent);
