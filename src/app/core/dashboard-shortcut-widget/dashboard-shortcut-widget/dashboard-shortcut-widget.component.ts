4571e417f82a06505c5818fa866a735ce14ea502
Simon <simon@aam-digital.com>
2023-02-22 13:45:43 +0100

    fix: added pagination to shortcut widget

--- src/app/core/dashboard-shortcut-widget/dashboard-shortcut-widget/dashboard-shortcut-widget.component.ts
+++ src/app/core/dashboard-shortcut-widget/dashboard-shortcut-widget/dashboard-shortcut-widget.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { MenuItem } from "../../navigation/menu-item";
 import { OnInitDynamicComponent } from "../../view/dynamic-components/on-init-dynamic-component.interface";
-import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { MatTableModule } from "@angular/material/table";
 import { DynamicComponent } from "../../view/dynamic-components/dynamic-component.decorator";
 import { FaDynamicIconComponent } from "../../view/fa-dynamic-icon/fa-dynamic-icon.component";
 import { RouterLink } from "@angular/router";
-import { DashboardWidgetComponent } from "../../dashboard/dashboard-widget/dashboard-widget.component";
-import { WidgetContentComponent } from "../../dashboard/dashboard-widget/widget-content/widget-content.component";
+import { DashboardListWidgetComponent } from "../../dashboard/dashboard-list-widget/dashboard-list-widget.component";
 
 /**
  * A simple list of shortcuts displayed as a dashboard widget for easy access to important navigation.
@@ -17,28 +16,20 @@ import { WidgetContentComponent } from "../../dashboard/dashboard-widget/widget-
   templateUrl: "./dashboard-shortcut-widget.component.html",
   styleUrls: ["./dashboard-shortcut-widget.component.scss"],
   imports: [
+    DashboardListWidgetComponent,
     MatTableModule,
     FaDynamicIconComponent,
     RouterLink,
-    DashboardWidgetComponent,
-    WidgetContentComponent,
   ],
   standalone: true,
 })
 export class DashboardShortcutWidgetComponent
-  implements OnInitDynamicComponent, OnInit
+  implements OnInitDynamicComponent
 {
   /** displayed entries, each representing one line displayed as a shortcut */
   @Input() shortcuts: MenuItem[] = [];
 
-  tableDataSource = new MatTableDataSource<MenuItem>();
-
   onInitFromDynamicConfig(config: any) {
     this.shortcuts = config.shortcuts;
-    this.tableDataSource.data = config.shortcuts;
-  }
-
-  ngOnInit(): void {
-    this.tableDataSource.data = this.shortcuts;
   }
 }
