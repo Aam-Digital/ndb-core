3205eeea1aae7460c399af01ab43889140b2e731
Simon <simon@aam-digital.com>
2023-02-23 16:23:58 +0100

    note details component uses forms in UI

--- src/app/child-dev-project/notes/note-details/note-details.component.ts
+++ src/app/child-dev-project/notes/note-details/note-details.component.ts
@@ -1,18 +1,18 @@
-import { Component, Input, OnInit, ViewChild } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 import { Note } from "../model/note";
 import { ShowsEntity } from "../../../core/form-dialog/shows-entity.interface";
-import { EntityConstructor } from "../../../core/entity/model/entity";
-import { INTERACTION_TYPE_CONFIG_ID } from "../model/interaction-type.interface";
 import { Child } from "../../children/model/child";
-import { User } from "../../../core/user/user";
-import { School } from "../../schools/model/school";
 import { ExportColumnConfig } from "../../../core/export/data-transformation-service/export-column-config";
 import { ConfigService } from "../../../core/config/config.service";
 import { EntityListConfig } from "../../../core/entity-components/entity-list/EntityListConfig";
-import { compareEnums } from "../../../utils/utils";
 import { FormDialogWrapperComponent } from "../../../core/form-dialog/form-dialog-wrapper/form-dialog-wrapper.component";
 import { ScreenWidthObserver } from "../../../utils/media/screen-size-observer.service";
-import { EntitySchemaField } from "../../../core/entity/schema/entity-schema-field";
 import { DatePipe, NgIf } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
@@ -20,14 +20,6 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { MatMenuModule } from "@angular/material/menu";
 import { ExportDataDirective } from "../../../core/export/export-data-directive/export-data.directive";
 import { Angulartics2Module } from "angulartics2";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatSelectModule } from "@angular/material/select";
-import { ConfigurableEnumDirective } from "../../../core/configurable-enum/configurable-enum-directive/configurable-enum.directive";
-import { BorderHighlightDirective } from "../../../core/common-components/border-highlight/border-highlight.directive";
-import { EntitySelectComponent } from "../../../core/entity-components/entity-select/entity-select/entity-select.component";
-import { MatCheckboxModule } from "@angular/material/checkbox";
 import { ChildMeetingNoteAttendanceComponent } from "./child-meeting-attendance/child-meeting-note-attendance.component";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import {
@@ -35,6 +27,8 @@ import {
   EntityFormService,
 } from "../../../core/entity-components/entity-form/entity-form.service";
 import { toFormFieldConfig } from "../../../core/entity-components/entity-subrecord/entity-subrecord/entity-subrecord-config";
+import { EntityFormComponent } from "../../../core/entity-components/entity-form/entity-form/entity-form.component";
+import { DynamicComponentDirective } from "../../../core/view/dynamic-components/dynamic-component.directive";
 
 /**
  * Component responsible for displaying the Note creation/view window
@@ -46,51 +40,26 @@ import { toFormFieldConfig } from "../../../core/entity-components/entity-subrec
   styleUrls: ["./note-details.component.scss"],
   imports: [
     FormDialogWrapperComponent,
+    ChildMeetingNoteAttendanceComponent,
+    EntityFormComponent,
+    DynamicComponentDirective,
+    NgIf,
     DatePipe,
     FormsModule,
+    MatMenuModule,
     MatButtonModule,
     FontAwesomeModule,
-    MatMenuModule,
     ExportDataDirective,
     Angulartics2Module,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatSelectModule,
-    ConfigurableEnumDirective,
-    BorderHighlightDirective,
-    EntitySelectComponent,
-    MatCheckboxModule,
-    NgIf,
-    ChildMeetingNoteAttendanceComponent,
-    MatDialogModule,
   ],
   standalone: true,
+  encapsulation: ViewEncapsulation.None,
 })
 export class NoteDetailsComponent implements ShowsEntity<Note>, OnInit {
   @Input() entity: Note;
   @ViewChild("dialogForm", { static: true })
   formDialogWrapper: FormDialogWrapperComponent<Note>;
 
-  readonly Child: EntityConstructor<Child> = Child;
-  readonly School: EntityConstructor<School> = School;
-  readonly User: EntityConstructor<User> = User;
-  readonly dateLabel = Note.schema.get("date").label;
-  readonly statusLabel = Note.schema.get("warningLevel").label;
-  readonly categoryLabel = Note.schema.get("category").label;
-  readonly authorsLabel = Note.schema.get("authors").label;
-  readonly authorsPlaceholder = this.getPlaceholder(this.authorsLabel);
-  readonly subjectLabel = Note.schema.get("subject").label;
-  readonly textLabel = Note.schema.get("text").label;
-  readonly childrenLabel = Note.schema.get("children").label;
-  readonly childrenPlaceholder = this.getPlaceholder(this.childrenLabel);
-  readonly schoolsLabel = Note.schema.get("schools").label;
-  readonly schoolsPlaceholder = this.getPlaceholder(this.schoolsLabel);
-  readonly relatedEntitiesSchema: EntitySchemaField =
-    Note.schema.get("relatedEntities");
-
-  readonly INTERACTION_TYPE_CONFIG = INTERACTION_TYPE_CONFIG_ID;
-  readonly compareFn = compareEnums;
   includeInactiveChildren: boolean = false;
 
   /** export format for notes to be used for downloading the individual details */
@@ -98,7 +67,17 @@ export class NoteDetailsComponent implements ShowsEntity<Note>, OnInit {
 
   /** Is it mobile view or not */
   mobile = false;
+  topForm = ["date", "warningLevel", "category", "authors"].map((field) => [
+    toFormFieldConfig(field),
+  ]);
+  middleForm = ["subject", "text"].map(toFormFieldConfig);
+  // TODO related entities is not necessarily set
+  // TODO make this configurable
+  bottomForm = ["children", "schools", "relatedEntities"].map(
+    toFormFieldConfig
+  );
   form: EntityForm<Note>;
+  tmpEntity: Note;
 
   constructor(
     private configService: ConfigService,
@@ -116,19 +95,14 @@ export class NoteDetailsComponent implements ShowsEntity<Note>, OnInit {
 
   ngOnInit() {
     this.form = this.entityFormService.createFormGroup(
-      [
-        "date",
-        "warning-level",
-        "category",
-        "authors",
-        "subject",
-        "text",
-        "children",
-        "schools",
-        "additional",
-      ].map(toFormFieldConfig),
+      this.middleForm.concat(...this.topForm, this.bottomForm),
       this.entity
     );
+    this.tmpEntity = this.entity;
+    this.form.valueChanges.pipe(untilDestroyed(this)).subscribe((value) => {
+      const dynamicConstructor: any = this.entity.getConstructor();
+      this.tmpEntity = Object.assign(new dynamicConstructor(), value);
+    });
   }
 
   toggleIncludeInactiveChildren() {
@@ -143,5 +117,13 @@ export class NoteDetailsComponent implements ShowsEntity<Note>, OnInit {
 
   private getPlaceholder(label: string): string {
     return $localize`:Placeholder for input to add entities|context Add User(s):Add ${label}`;
+  }
+
+  removeChild(id: string) {
+    // TODO type is broken
+    const children = this.form.get("children").value as any as string[];
+    const index = children.indexOf(id);
+    children.splice(index, 1);
+    this.form.get("children").setValue([...children] as any);
   }
 }
