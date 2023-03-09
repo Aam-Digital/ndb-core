6779202a49ac489ee2889c7adbd9c15fcf8263e3
Simon <simon@aam-digital.com>
2023-03-09 13:19:51 +0100

    testing timeout increase earlier

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -46,6 +46,7 @@ describe("AppComponent", () => {
   const defaultTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
 
   beforeEach(waitForAsync(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     environment.session_type = SessionType.mock;
     const entityMapper = mockEntityMapper();
     entityUpdates = new Subject();
@@ -69,12 +70,11 @@ describe("AppComponent", () => {
     // hide angular component so that test results are visible in test browser window
     fixture.debugElement.nativeElement.style.visibility = "hidden";
     await TestBed.inject(Database).destroy();
+    await new Promise((resolve) => setTimeout(resolve, 10000));
     jasmine.DEFAULT_TIMEOUT_INTERVAL = defaultTimeout;
   });
 
   it("should be created", () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-
     createComponent();
     expect(component).toBeTruthy();
   });
