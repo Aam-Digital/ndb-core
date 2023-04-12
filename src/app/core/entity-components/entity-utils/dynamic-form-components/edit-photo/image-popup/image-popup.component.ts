b05827613e87abfdc4b03a190a832320e031c679
Simon <simon@aam-digital.com>
2023-04-12 07:51:35 +0200

    fix: photo popup closes when clicking X button

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-photo/image-popup/image-popup.component.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-photo/image-popup/image-popup.component.ts
@@ -1,12 +1,12 @@
 import { Component, Inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
 import { DialogCloseComponent } from "../../../../../common-components/dialog-close/dialog-close.component";
 
 @Component({
   selector: "app-image-popup",
   standalone: true,
-  imports: [CommonModule, DialogCloseComponent],
+  imports: [CommonModule, DialogCloseComponent, MatDialogModule],
   templateUrl: "./image-popup.component.html",
   styleUrls: ["./image-popup.component.scss"],
 })
