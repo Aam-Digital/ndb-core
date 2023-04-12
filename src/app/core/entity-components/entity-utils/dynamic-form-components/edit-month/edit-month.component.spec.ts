4b6a7874e33fd64c93558e0c14f4fbcbd3dea3d8
Simon <simon@aam-digital.com>
2023-04-12 14:24:46 +0200

    fixed test

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-month/edit-month.component.spec.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-month/edit-month.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { EditMonthComponent } from './edit-month.component';
+import { EditMonthComponent } from "./edit-month.component";
+import { FormControl } from "@angular/forms";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import moment from "moment";
 
-describe('EditMonthComponent', () => {
+describe("EditMonthComponent", () => {
   let component: EditMonthComponent;
   let fixture: ComponentFixture<EditMonthComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ EditMonthComponent ]
-    })
-    .compileComponents();
+      imports: [EditMonthComponent, NoopAnimationsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EditMonthComponent);
     component = fixture.componentInstance;
+    component.formControl = new FormControl();
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should save the selected moment on the form control", () => {
+    const selected = new Date("2021-05-01");
+    const closeSpy = jasmine.createSpy();
+
+    component.setMonthAndYear(moment(selected), { close: closeSpy } as any);
+
+    expect(component.formControl).toHaveValue(selected);
+    expect(closeSpy).toHaveBeenCalled();
   });
 });
