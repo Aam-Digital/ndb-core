5ef921dde906c6250d8634c6b8b9b7b3330d6df1
Simon <simon@aam-digital.com>
2023-05-08 13:10:51 +0200

    fix: photos in child block component are correctly updated (#1855)

--- src/app/child-dev-project/children/child-block/child-block.component.spec.ts
+++ src/app/child-dev-project/children/child-block/child-block.component.spec.ts
@@ -1,27 +1,31 @@
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { ChildBlockComponent } from "./child-block.component";
-import { RouterTestingModule } from "@angular/router/testing";
 import { ChildrenService } from "../children.service";
 import { Child } from "../model/child";
 import { FileService } from "app/features/file/file.service";
+import { of } from "rxjs";
+import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
 
 describe("ChildBlockComponent", () => {
   let component: ChildBlockComponent;
   let fixture: ComponentFixture<ChildBlockComponent>;
   let mockChildrenService: jasmine.SpyObj<ChildrenService>;
+  let mockFileService: jasmine.SpyObj<FileService>;
 
   beforeEach(waitForAsync(() => {
     mockChildrenService = jasmine.createSpyObj("mockChildrenService", [
       "getChild",
     ]);
     mockChildrenService.getChild.and.resolveTo(new Child(""));
+    mockFileService = jasmine.createSpyObj(["loadFile"]);
+    mockFileService.loadFile.and.returnValue(of("success"));
 
     TestBed.configureTestingModule({
-      imports: [ChildBlockComponent, RouterTestingModule],
+      imports: [ChildBlockComponent, FontAwesomeTestingModule],
       providers: [
         { provide: ChildrenService, useValue: mockChildrenService },
-        { provide: FileService, useValue: {} },
+        { provide: FileService, useValue: mockFileService },
       ],
     }).compileComponents();
   }));
@@ -35,5 +39,25 @@ describe("ChildBlockComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should reset picture if child has none", async () => {
+    const withPicture = new Child();
+    withPicture.photo = "some-picture";
+    component.entity = withPicture;
+
+    await component.ngOnChanges({ entity: undefined });
+
+    expect(mockFileService.loadFile).toHaveBeenCalled();
+    expect(component.imgPath).toBeDefined();
+
+    mockFileService.loadFile.calls.reset();
+    // without picture
+    component.entity = new Child();
+
+    await component.ngOnChanges({ entity: undefined });
+
+    expect(mockFileService.loadFile).not.toHaveBeenCalled();
+    expect(component.imgPath).toBeUndefined();
   });
 });
