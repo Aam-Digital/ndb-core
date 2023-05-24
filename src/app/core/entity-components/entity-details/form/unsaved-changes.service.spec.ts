0b5cf3d068e79c913fb2723c1adf0023508c7d55
Simon <simon@aam-digital.com>
2023-05-24 12:21:38 +0200

    added tests for unsaved changes service

--- src/app/core/entity-components/entity-details/form/unsaved-changes.service.spec.ts
+++ src/app/core/entity-components/entity-details/form/unsaved-changes.service.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { UnsavedChangesService } from './unsaved-changes.service';
+import { UnsavedChangesService } from "./unsaved-changes.service";
+import { ConfirmationDialogService } from "../../../confirmation-dialog/confirmation-dialog.service";
 
-describe('UnsavedChangesService', () => {
+describe("UnsavedChangesService", () => {
   let service: UnsavedChangesService;
+  let mockConfirmation: jasmine.SpyObj<ConfirmationDialogService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockConfirmation = jasmine.createSpyObj(["getSaveConfirmation"]);
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: ConfirmationDialogService, useValue: mockConfirmation },
+      ],
+    });
     service = TestBed.inject(UnsavedChangesService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should only ask for confirmation if changes are pending", async () => {
+    mockConfirmation.getSaveConfirmation.and.resolveTo(false);
+
+    await expectAsync(service.checkUnsavedChanges()).toBeResolvedTo(true);
+    expect(mockConfirmation.getSaveConfirmation).not.toHaveBeenCalled();
+
+    service.pending = true;
+
+    await expectAsync(service.checkUnsavedChanges()).toBeResolvedTo(false);
+    expect(mockConfirmation.getSaveConfirmation).toHaveBeenCalled();
+
+    mockConfirmation.getSaveConfirmation.and.resolveTo(true);
+
+    await expectAsync(service.checkUnsavedChanges()).toBeResolvedTo(true);
   });
 });
