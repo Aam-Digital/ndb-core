c21ba4ab251f315499adf42c4858652f4b2eaf4a
Simon <simon@aam-digital.com>
2023-05-24 09:32:10 +0200

    fixed auth guard tests

--- src/app/core/session/auth.guard.spec.ts
+++ src/app/core/session/auth.guard.spec.ts
@@ -2,10 +2,10 @@ import { TestBed } from "@angular/core/testing";
 
 import { AuthGuard } from "./auth.guard";
 import { SessionService } from "./session-service/session.service";
-import { Router } from "@angular/router";
+import { CanActivateFn, Router } from "@angular/router";
 
 describe("AuthGuard", () => {
-  let guard: AuthGuard;
+  let guard: CanActivateFn;
   let mockSession: jasmine.SpyObj<SessionService>;
   let mockRouter: jasmine.SpyObj<Router>;
 
@@ -28,15 +28,13 @@ describe("AuthGuard", () => {
   it("should return true if user is logged in", () => {
     mockSession.isLoggedIn.and.returnValue(true);
 
-    expect(guard.canActivate(undefined, undefined)).toBeTrue();
+    expect(guard(undefined, undefined)).toBeTrue();
   });
 
   it("should navigate to login page with redirect url if not logged in", () => {
     mockSession.isLoggedIn.and.returnValue(false);
 
-    expect(
-      guard.canActivate(undefined, { url: "/some/url" } as any)
-    ).toBeFalse();
+    expect(guard(undefined, { url: "/some/url" } as any)).toBeFalse();
     expect(mockRouter.navigate).toHaveBeenCalledWith(["/login"], {
       queryParams: { redirect_uri: "/some/url" },
     });
