a4200c2aeb5822c13eb3e8d09a1f00312fb0b02a
Simon <simon@aam-digital.com>
2023-06-09 10:41:11 +0200

    removed compodoc from preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,7 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import "@angular/localize/init";
-import docJson from "../documentation.json";
 // polyfill buffer here as well
 import * as buffer from "buffer";
-
-setCompodocJson(docJson);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
