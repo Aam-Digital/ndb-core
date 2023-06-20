c798a15cb0c39b7a61dcee7e97b5e8b4e8b35f47
Simon <simon@aam-digital.com>
2023-06-20 10:33:31 +0200

    moved unsubscription back to form service

--- src/app/core/entity-components/entity-details/form/unsaved-changes.service.ts
+++ src/app/core/entity-components/entity-details/form/unsaved-changes.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@angular/core";
 import { ConfirmationDialogService } from "../../../confirmation-dialog/confirmation-dialog.service";
 
+/**
+ * This service handles the state whether there are currently some unsaved changes in the app.
+ * These pending changes might come from a form component or popup.
+ * If there are pending changes, certain actions in the app should trigger a user confirmation if the changes should be discarded.
+ */
 @Injectable({
   providedIn: "root",
 })
 export class UnsavedChangesService {
+  /**
+   * Set to true if the user has pending changes that are not yet saved.
+   * Set to false once the changes have been saved or discarded.
+   */
   pending = false;
 
   constructor(private confirmation: ConfirmationDialogService) {
@@ -17,6 +26,9 @@ export class UnsavedChangesService {
     };
   }
 
+  /**
+   * Shows a user confirmation popup if there are unsaved changes which will be discarded.
+   */
   async checkUnsavedChanges() {
     if (this.pending) {
       const confirmed = await this.confirmation.getDiscardConfirmation();
