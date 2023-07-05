555ffc6dfde69a74f3ea2bfcf995f1f63d43326a
Sebastian Leidig <sebastian.leidig@gmail.com>
2023-07-05 14:31:34 +0200

    entity type select

--- src/app/features/import/import-entity-type/import-entity-type.component.ts
+++ src/app/features/import/import-entity-type/import-entity-type.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ParsedData } from "../../data-import/input-file/input-file.component";
+import { EntityRegistry } from "../../../core/entity/database-entity.decorator";
 
 /**
  * Import sub-step: Let user select which entity type data should be imported as.
@@ -19,4 +20,8 @@ export class ImportEntityTypeComponent {
   @Output() entityTypeChange = new EventEmitter<string>();
   /** pre-selected entity type */
   @Input() entityType: string;
+
+  constructor(public entityTypes: EntityRegistry) {
+    // TODO: display EntityType label (!)
+  }
 }
