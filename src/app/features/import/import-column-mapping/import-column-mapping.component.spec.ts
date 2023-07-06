3105fa94a6fc933df060cc38ab3a16e0e820b55c
Simon <simon@aam-digital.com>
2023-07-06 11:54:06 +0200

    added test for column mapping cmp

--- src/app/features/import/import-column-mapping/import-column-mapping.component.spec.ts
+++ src/app/features/import/import-column-mapping/import-column-mapping.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ImportColumnMappingComponent } from "./import-column-mapping.component";
 import { MockedTestingModule } from "../../../utils/mocked-testing.module";
 import { ImportModule } from "../import.module";
+import { MatDialog } from "@angular/material/dialog";
+import { EnumValueMappingComponent } from "./enum-value-mapping/enum-value-mapping.component";
+import { Child } from "../../../child-dev-project/children/model/child";
 
 describe("ImportMapColumnsComponent", () => {
   let component: ImportColumnMappingComponent;
@@ -21,5 +24,29 @@ describe("ImportMapColumnsComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should open mapping component with required data", () => {
+    component.rawData = [
+      { name: "first", gender: "male" },
+      { name: "second", gender: "female" },
+      { name: "third", gender: "female" },
+    ];
+    component.entityType = "Child";
+    component.columnMapping = [{ column: "name" }, { column: "gender" }];
+    const openSpy = spyOn(TestBed.inject(MatDialog), "open");
+
+    const genderColumn = component.columnMapping[1];
+    genderColumn.propertyName = "gender";
+    component.openMappingComponent(genderColumn);
+
+    expect(openSpy).toHaveBeenCalledWith(EnumValueMappingComponent, {
+      data: {
+        col: genderColumn,
+        values: ["male", "female"],
+        entityType: Child,
+      },
+      disableClose: true,
+    });
   });
 });
