b50be15d60a243d5db6ed4e34b4617af24997ac6
Sebastian <sebastian.leidig@gmail.com>
2023-07-17 12:18:54 +0200

    refactor: help-button for consistent UI of contextual explanations (#1928)
    Co-authored-by: Simon <simon@aam-digital.com>

--- src/app/core/entity-components/entity-form/entity-form/entity-form.component.ts
+++ src/app/core/entity-components/entity-form/entity-form/entity-form.component.ts
@@ -14,11 +14,9 @@ import { ConfirmationDialogService } from "../../../confirmation-dialog/confirma
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { NgClass, NgForOf, NgIf } from "@angular/common";
 import { DynamicComponentDirective } from "../../../view/dynamic-components/dynamic-component.directive";
-import { MatButtonModule } from "@angular/material/button";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { Subscription } from "rxjs";
 import moment from "moment";
+import { HelpButtonComponent } from "../../../common-components/help-button/help-button.component";
 
 /**
  * A general purpose form component for displaying and editing entities.
@@ -41,10 +39,8 @@ import moment from "moment";
     NgForOf,
     DynamicComponentDirective,
     NgIf,
-    MatButtonModule,
-    MatTooltipModule,
-    FontAwesomeModule,
     NgClass,
+    HelpButtonComponent,
   ],
   standalone: true,
 })
