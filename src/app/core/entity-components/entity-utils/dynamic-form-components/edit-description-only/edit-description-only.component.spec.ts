3018f3db23a11fe91e1824f4ed53bc0a29c5d19b
Simon <simon@aam-digital.com>
2023-07-31 13:18:45 +0200

    removed unnecessary code

--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-description-only/edit-description-only.component.spec.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-description-only/edit-description-only.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { EditDescriptionOnlyComponent } from "./edit-description-only.component";
-import { setupEditComponent } from "../edit-component.spec";
 
 describe("EditDescriptionOnlyComponent", () => {
   let component: EditDescriptionOnlyComponent;
@@ -16,7 +14,6 @@ describe("EditDescriptionOnlyComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EditDescriptionOnlyComponent);
     component = fixture.componentInstance;
-    setupEditComponent(component);
     fixture.detectChanges();
   });
 
--- src/app/core/entity-components/entity-utils/dynamic-form-components/edit-description-only/edit-description-only.component.ts
+++ src/app/core/entity-components/entity-utils/dynamic-form-components/edit-description-only/edit-description-only.component.ts
@@ -1,31 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { EditComponent } from "../edit-component";
+import { Component, Input } from "@angular/core";
 import { DynamicComponent } from "../../../../view/dynamic-components/dynamic-component.decorator";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
-import { ErrorHintComponent } from "../../error-hint/error-hint.component";
-import { NgIf } from "@angular/common";
+import { FormFieldConfig } from "../../../entity-form/entity-form/FormConfig";
 
 @DynamicComponent("EditDescriptionOnly")
 @Component({
   selector: "app-edit-description-only",
-  template: "{{label}}",
-  imports: [
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    ErrorHintComponent,
-    NgIf,
-  ],
+  template: "{{formFieldConfig?.label}}",
   standalone: true,
 })
-export class EditDescriptionOnlyComponent
-  extends EditComponent<string>
-  implements OnInit
-{
-  ngOnInit() {
-    // override base class on init because this does not have a valid formControl!
-    this.label = this.formFieldConfig?.label;
-  }
+export class EditDescriptionOnlyComponent {
+  @Input() formFieldConfig: FormFieldConfig;
 }
