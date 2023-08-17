8fcbafb93476b77cac6e78822733b427043496d5
Sebastian Leidig <sebastian.leidig@gmail.com>
2023-08-17 11:30:32 +0200

    feat(import): import address by lookup of location data
    closes #1957

---
+++ src/app/features/location/location.datatype.spec.ts
@@ -0,0 +1,78 @@
+import { TestBed } from "@angular/core/testing";
+import { of } from "rxjs";
+import { LocationDatatype } from "./location.datatype";
+import { GeoResult, GeoService } from "./geo.service";
+
+describe("Schema data type: location", () => {
+  let service: LocationDatatype;
+  let mockGeoService: jasmine.SpyObj<GeoService>;
+
+  beforeEach(() => {
+    mockGeoService = jasmine.createSpyObj(["lookup"]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: GeoService, useValue: mockGeoService },
+        LocationDatatype,
+      ],
+    });
+
+    service = TestBed.inject(LocationDatatype);
+  });
+
+  async function testImportMapping(
+    importedValue: string,
+    mockedLookup: GeoResult[],
+    expectedResult,
+  ) {
+    mockGeoService.lookup.and.returnValue(of(mockedLookup));
+
+    const actualResult = await service.importMapFunction(importedValue, null);
+
+    expect(mockGeoService.lookup).toHaveBeenCalledWith(importedValue);
+    expect(actualResult).toEqual(expectedResult);
+  }
+
+  it("should import lookup location data when importing address string", async () => {
+    const importedAddress = "21 MyStreet, MyCity";
+    const locationResult: GeoResult = {
+      lat: 1,
+      lon: 2,
+      display_name: importedAddress,
+    };
+
+    await testImportMapping(importedAddress, [locationResult], locationResult);
+  });
+
+  it("should import first lookup location when importing address string resulting in multiple results", async () => {
+    const importedAddress = "21 MyStreet, MyCity";
+    const locationResult: GeoResult = {
+      lat: 1,
+      lon: 2,
+      display_name: importedAddress,
+    };
+
+    await testImportMapping(
+      importedAddress,
+      [locationResult, { display_name: "other result", lat: 0, lon: 0 }],
+      locationResult,
+    );
+  });
+
+  it("should import nothing when importing address that isn't found with lookup", async () => {
+    const importedAddress = "21 MyStreet, MyCity";
+
+    // TODO: when implementing #1844, extend this to import only custom address string without location object
+    await testImportMapping(importedAddress, [], undefined);
+  });
+
+  it("should not lookup empty address when importing", async () => {
+    const res1 = await service.importMapFunction(undefined, null);
+    expect(res1).toBeUndefined();
+    expect(mockGeoService.lookup).not.toHaveBeenCalled();
+
+    const res2 = await service.importMapFunction("", null);
+    expect(res2).toBeUndefined();
+    expect(mockGeoService.lookup).not.toHaveBeenCalled();
+  });
+});
--- src/app/features/location/location.datatype.ts
+++ src/app/features/location/location.datatype.ts
@@ -1,9 +1,29 @@
 import { DefaultDatatype } from "../../core/entity/schema/default.datatype";
 import { Injectable } from "@angular/core";
+import { EntitySchemaField } from "../../core/entity/schema/entity-schema-field";
+import { GeoResult, GeoService } from "./geo.service";
+import { lastValueFrom } from "rxjs";
 
 @Injectable()
-export class LocationDatatype extends DefaultDatatype {
+export class LocationDatatype extends DefaultDatatype<GeoResult, GeoResult> {
   static dataType = "location";
   editComponent = "EditLocation";
   viewComponent = "ViewLocation";
+
+  constructor(private geoService: GeoService) {
+    super();
+  }
+
+  async importMapFunction(
+    val: any,
+    schemaField: EntitySchemaField,
+    additional?: any,
+  ): Promise<any> {
+    if (!val) {
+      return undefined;
+    }
+
+    const geoResults = await lastValueFrom(this.geoService.lookup(val));
+    return geoResults[0];
+  }
 }
