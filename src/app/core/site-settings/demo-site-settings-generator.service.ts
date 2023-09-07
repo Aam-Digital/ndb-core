4e8fb1940db2563bae9aa4409697d9d6a837168f
Sebastian <sebastian@aam-digital.com>
2023-09-07 18:11:04 +0200

    Update src/app/core/site-settings/demo-site-settings-generator.service.ts

--- src/app/core/site-settings/demo-site-settings-generator.service.ts
+++ src/app/core/site-settings/demo-site-settings-generator.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@angular/core";
 import { DemoDataGenerator } from "../demo-data/demo-data-generator";
 import { SiteSettings } from "./site-settings";
 
+/**
+ * Generates SiteSettings entity. Defaults are defined in the SiteSettings class.
+ */
 @Injectable()
 export class DemoSiteSettingsGeneratorService extends DemoDataGenerator<SiteSettings> {
   static provider() {
