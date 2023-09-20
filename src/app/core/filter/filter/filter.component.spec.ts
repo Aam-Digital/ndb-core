37faae2ce89941b3a2a9d31c37e29b409e1424f6
Sebastian Leidig <sebastian.leidig@gmail.com>
2023-09-20 15:47:49 +0200

    move tests to more appropriate place

--- src/app/core/filter/filter/filter.component.spec.ts
+++ src/app/core/filter/filter/filter.component.spec.ts
@@ -7,15 +7,11 @@ import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
 import { HarnessLoader } from "@angular/cdk/testing";
 import { MatSelectHarness } from "@angular/material/select/testing";
 import { MockedTestingModule } from "../../../utils/mocked-testing.module";
-import { BooleanFilterConfig } from "../../entity-list/EntityListConfig";
-import { Entity } from "../../entity/model/entity";
-import { FilterService } from "../filter.service";
 
 describe("FilterComponent", () => {
   let component: FilterComponent;
   let fixture: ComponentFixture<FilterComponent>;
   let loader: HarnessLoader;
-  let filterService: FilterService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,8 +22,6 @@ describe("FilterComponent", () => {
     loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    filterService = TestBed.inject(FilterService);
   });
 
   it("should create", () => {
@@ -60,59 +54,4 @@ describe("FilterComponent", () => {
     expect(selected).toEqual(t1.label);
     expect(component.filterObj).toEqual({ "category.id": t1.id } as any);
   });
-
-  it("should support a boolean filter", async () => {
-    class EntityWithBooleanFlag extends Entity {
-      value: boolean;
-
-      static create(value: boolean) {
-        const e = new EntityWithBooleanFlag();
-        e.value = value;
-        return e;
-      }
-    }
-
-    const recordFalse = EntityWithBooleanFlag.create(false);
-    const recordTrue = EntityWithBooleanFlag.create(true);
-    const recordUndefined = new EntityWithBooleanFlag();
-    component.entities = [recordFalse, recordTrue, recordUndefined];
-    component.entityType = EntityWithBooleanFlag;
-    component.filterConfig = [
-      {
-        id: "value",
-        type: "boolean",
-        default: "true",
-        true: "is true",
-        false: "is not true",
-        all: "All",
-      } as BooleanFilterConfig,
-    ];
-
-    await component.ngOnChanges({ filterConfig: true } as any);
-
-    const selection = await loader.getHarness(MatSelectHarness);
-    let selected = await selection.getValueText();
-    expect(selected).toBe("is true");
-    testFilter(component.filterObj, component.entities, [recordTrue]);
-
-    await selection.open();
-    const options = await selection.getOptions();
-    const falseOption = options[2];
-    const label = await falseOption.getText();
-    expect(label).toBe("is not true");
-
-    await falseOption.click();
-    selected = await selection.getValueText();
-    expect(selected).toBe("is not true");
-    testFilter(component.filterObj, component.entities, [
-      recordFalse,
-      recordUndefined,
-    ]);
-  });
-
-  function testFilter(filterObj, entities, expectedFilteredResult) {
-    const filterPred = filterService.getFilterPredicate(filterObj);
-    const filtered = entities.filter(filterPred);
-    expect(filtered).toEqual(expectedFilteredResult);
-  }
 });
