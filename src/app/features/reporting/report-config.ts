5f535547becf54f8ea78d80640ed002a3a1486a4
Simon <simon@aam-digital.com>
2023-12-01 10:02:14 +0100

    added documentation
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/features/reporting/report-config.ts
+++ src/app/features/reporting/report-config.ts
@@ -4,11 +4,16 @@ import { Aggregation } from "./data-aggregation.service";
 import { ExportColumnConfig } from "../../core/export/data-transformation-service/export-column-config";
 import { DatabaseField } from "../../core/entity/database-field.decorator";
 
+/**
+ * A report can be accessed by users to generate aggregated statistics or customized exports calculated from available data.
+ * "read" permission for a ReportConfig entity is also used to control which users can generate the report's results.
+ */
 @DatabaseEntity("ReportConfig")
 export class ReportConfig extends Entity {
   static create(data: Partial<ReportConfig>) {
     return Object.assign(new ReportConfig(), data);
   }
+  
   /** human-readable title of the report */
   @DatabaseField() title: string;
 
