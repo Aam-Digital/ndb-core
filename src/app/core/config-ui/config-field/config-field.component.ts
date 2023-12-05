2f3fcc4fbcd1ab30fa449a9d986da162da4355e6
Sebastian <sebastian@aam-digital.com>
2023-12-05 14:09:54 +0100

    Update src/app/core/config-ui/config-field/config-field.component.ts
    Co-authored-by: Simon <simon@aam-digital.com>

--- src/app/core/config-ui/config-field/config-field.component.ts
+++ src/app/core/config-ui/config-field/config-field.component.ts
@@ -276,7 +276,7 @@ type SimpleDropdownValue = { label: string; value: string };
 
 export function generateSimplifiedId(label: string) {
   return label
-    .replace(/[^a-z0-9_]/g, "_")
+    .replace(/[^a-zA-Z0-9_]/g, "_")
     .replace(/\s/g, "_")
     .replace(/_+/g, "_")
     .replace(/^_*/, "")
