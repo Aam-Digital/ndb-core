c2e06e677289f1df0b224ec922628bf621557bbb
Sebastian Leidig <sebastian@aam-digital.com>
2023-12-07 12:34:29 +0100

    removed unused code

--- src/app/utils/generate-id-from-label/generate-id-from-label.ts
+++ src/app/utils/generate-id-from-label/generate-id-from-label.ts
@@ -1,12 +1,3 @@
-function generateSimplifiedId(label: string) {
-  return label
-    .replace(/[^a-zA-Z0-9_]/g, "_")
-    .replace(/\s/g, "_")
-    .replace(/_+/g, "_")
-    .replace(/^_*/, "")
-    .replace(/_*$/, "");
-}
-
 /**
  * Create a simplified id string from the given text.
  * This generates a camelCase string, so that it can be used as an id.
