4df0a8fc438a32ac8614a7912e324ab255c489a6
Simon <simon@aam-digital.com>
2023-12-22 10:55:22 +0100

    added test

--- src/app/features/todos/todo-completion/display-todo-completion/display-todo-completion.component.spec.ts
+++ src/app/features/todos/todo-completion/display-todo-completion/display-todo-completion.component.spec.ts
@@ -1,14 +1,29 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from "@angular/core/testing";
 
 import { DisplayTodoCompletionComponent } from "./display-todo-completion.component";
+import {
+  mockEntityMapper,
+  MockEntityMapperService,
+} from "../../../../core/entity/entity-mapper/mock-entity-mapper-service";
+import { EntityMapperService } from "../../../../core/entity/entity-mapper/entity-mapper.service";
+import { Child } from "../../../../child-dev-project/children/model/child";
+import { User } from "../../../../core/user/user";
 
 describe("DisplayTodoCompletionComponent", () => {
   let component: DisplayTodoCompletionComponent;
   let fixture: ComponentFixture<DisplayTodoCompletionComponent>;
+  let entityMapper: MockEntityMapperService;
 
   beforeEach(async () => {
+    entityMapper = mockEntityMapper();
     await TestBed.configureTestingModule({
       imports: [DisplayTodoCompletionComponent],
+      providers: [{ provide: EntityMapperService, useValue: entityMapper }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DisplayTodoCompletionComponent);
@@ -19,4 +34,34 @@ describe("DisplayTodoCompletionComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should load the entity in completedBy when it has full ID", fakeAsync(() => {
+    const completingChild = new Child("1");
+    const otherChild = new Child("2");
+    entityMapper.addAll([completingChild, otherChild]);
+
+    component.value = {
+      completedBy: completingChild.getId(true),
+      completedAt: new Date(),
+    };
+    component.ngOnChanges({ value: true } as any);
+    tick();
+
+    expect(component.completedBy).toEqual(completingChild);
+  }));
+
+  it("should load the user entity when completedBy does not contain entity type", fakeAsync(() => {
+    const completingUser = new User("1");
+    const otherUser = new User("2");
+    entityMapper.addAll([completingUser, otherUser]);
+
+    component.value = {
+      completedBy: completingUser.getId(),
+      completedAt: new Date(),
+    };
+    component.ngOnChanges({ value: true } as any);
+    tick();
+
+    expect(component.completedBy).toEqual(completingUser);
+  }));
 });
