62eb72ab7f114818e373089cc8da273e474c12d3
Sebastian Leidig <sebastian@aam-digital.com>
2024-01-25 11:57:52 +0100

    re-enable , not causing the issue

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ import { LoggingService } from "./app/core/logging/logging.service";
 import { PwaInstallService } from "./app/core/pwa-install/pwa-install.service";
 
 // write browser details to initial loading page for debugging
-//document.getElementById("browserDetails").innerHTML = navigator.userAgent;
+document.getElementById("browserDetails").innerHTML = navigator.userAgent;
 
 if (environment.production) {
   enableProdMode();
