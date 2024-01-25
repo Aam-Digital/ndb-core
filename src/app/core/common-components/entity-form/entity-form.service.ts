b1533b1a2bb304088908672471d97a872d19ff8d
Tom Winter <tom@aam-digital.com>
2024-01-25 11:30:16 +0100

    feat(permissions): filter fields in forms based on read/write permissions (#2180)
    closes #1912 
    
    Co-authored-by: Simon <simon@aam-digital.com>

--- src/app/core/common-components/entity-form/entity-form.service.ts
+++ src/app/core/common-components/entity-form/entity-form.service.ts
@@ -112,11 +112,13 @@ export class EntityFormService {
    * @param formFields
    * @param entity
    * @param forTable
+   * @param withPermissionCheck if true, fields without 'update' permissions will stay disabled when enabling form
    */
   public createFormGroup<T extends Entity>(
     formFields: ColumnConfig[],
     entity: T,
     forTable = false,
+    withPermissionCheck = true,
   ): EntityForm<T> {
     const formConfig = {};
     const copy = entity.copy();
@@ -130,10 +132,18 @@ export class EntityFormService {
     }
     const group = this.fb.group<Partial<T>>(formConfig);
 
-    const sub = group.valueChanges.subscribe(
+    const valueChangesSubscription = group.valueChanges.subscribe(
       () => (this.unsavedChanges.pending = group.dirty),
     );
-    this.subscriptions.push(sub);
+    this.subscriptions.push(valueChangesSubscription);
+
+    if (withPermissionCheck) {
+      this.disableReadOnlyFormControls(group, entity);
+      const statusChangesSubscription = group.statusChanges
+        .pipe(filter((status) => status !== "DISABLED"))
+        .subscribe(() => this.disableReadOnlyFormControls(group, entity));
+      this.subscriptions.push(statusChangesSubscription);
+    }
 
     return group;
   }
@@ -194,6 +204,18 @@ export class EntityFormService {
     return newVal;
   }
 
+  private disableReadOnlyFormControls<T extends Entity>(
+    form: EntityForm<T>,
+    entity: T,
+  ) {
+    const action = entity.isNew ? "create" : "update";
+    Object.keys(form.controls).forEach((fieldId) => {
+      if (this.ability.cannot(action, entity, fieldId)) {
+        form.get(fieldId).disable({ onlySelf: true, emitEvent: false });
+      }
+    });
+  }
+
   /**
    * This function applies the changes of the formGroup to the entity.
    * If the form is invalid or the entity does not pass validation after applying the changes, an error will be thrown.
@@ -228,7 +250,7 @@ export class EntityFormService {
   }
 
   private checkFormValidity<T extends Entity>(form: EntityForm<T>) {
-    // errors regarding invalid fields wont be displayed unless marked as touched
+    // errors regarding invalid fields won't be displayed unless marked as touched
     form.markAllAsTouched();
     if (form.invalid) {
       throw new InvalidFormFieldError();
