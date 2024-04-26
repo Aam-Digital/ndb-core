ff86e819251fd2913889c6c678e23b26c7eeaf1f
Abhishek Negi <negiabhishek253@gmail.com>
2024-04-26 14:58:08 +0530

    updated test cases

--- src/app/core/admin/admin-entity-details/admin-entity-field/admin-entity-text/admin-entity-text.component.spec.ts
+++ src/app/core/admin/admin-entity-details/admin-entity-field/admin-entity-text/admin-entity-text.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { AdminEntityTextComponent } from "./admin-entity-text.component";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Entity } from "app/core/entity/model/entity";
+import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
 fdescribe("AdminEntityTextComponent", () => {
   let component: AdminEntityTextComponent;
@@ -8,7 +12,21 @@ fdescribe("AdminEntityTextComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminEntityTextComponent],
+      imports: [
+        AdminEntityTextComponent,
+        FontAwesomeTestingModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            entitySchemaField: {},
+            entityType: Entity,
+          },
+        },
+        { provide: MatDialogRef, useValue: { close: () => null } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminEntityTextComponent);
