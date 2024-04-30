08a5e4afe65635880913b33206b076bf794a3e79
Tom Winter <tom@aam-digital.com>
2024-04-30 11:51:17 +0200

    Update src/app/core/entity/schema/default-field-value-config.ts
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/core/entity/schema/default-field-value-config.ts
+++ src/app/core/entity/schema/default-field-value-config.ts
@@ -3,6 +3,18 @@
  * i.e. that this field should automatically be filled with a value when creating a new entity
  */
 export interface DefaultFieldValueConfig {
+  /**
+   * What kind of logic is used to generate the default value:
+   *
+   *  mode: inherited
+   *  use the value from linked entity field
+   *
+   *  mode: static
+   *  use a static default value
+   *
+   *  mode: dynamic
+   *  use a placeholder value, see PLACEHOLDERS enum for available options
+   */
   mode: "inherited" | "static" | "dynamic";
 
   /** used as default value in "static" and "dynamic" mode */
