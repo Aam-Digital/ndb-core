a2deb2eedf62766541005fc29a810c159201f15a
Tom Winter <tom@aam-digital.com>
2024-06-13 16:28:06 +0200

    test: add sql-report test cases

--- src/app/features/reporting/sql-report/sql-report.service.ts
+++ src/app/features/reporting/sql-report/sql-report.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from "@angular/core";
-import { SqlTables, SqlType, SqsSchema } from "./sqs-schema";
 import { EntityRegistry } from "../../../core/entity/database-entity.decorator";
-import { EntitySchemaField } from "../../../core/entity/schema/entity-schema-field";
-import { NumberDatatype } from "../../../core/basic-datatypes/number/number.datatype";
-import { BooleanDatatype } from "../../../core/basic-datatypes/boolean/boolean.datatype";
 import { SqlReport } from "../report-config";
 import { HttpClient } from "@angular/common/http";
 import moment from "moment";
-import { EntityMapperService } from "../../../core/entity/entity-mapper/entity-mapper.service";
-import { isEqual } from "lodash-es";
 import { map, switchMap, takeWhile } from "rxjs/operators";
 import { firstValueFrom, interval, lastValueFrom, Observable } from "rxjs";
 
@@ -25,10 +19,16 @@ export interface ReportData {
 
 export interface ReportCalculation {
   id: string;
-  startDate: string;
-  endDate: string;
-  args: Map<String, String>[];
+  report: {
+    id: string;
+  };
+  startDate: string | null;
+  endDate: string | null;
+  args: Map<String, String>;
   status: "PENDING" | "RUNNING" | "FINISHED_SUCCESS" | "FINISHED_ERROR";
+  outcome: {
+    result_hash: string;
+  };
 }
 
 /**
@@ -39,10 +39,11 @@ export interface ReportCalculation {
 })
 export class SqlReportService {
   static QUERY_PROXY = "/query";
+
   constructor(
     private entities: EntityRegistry,
     private http: HttpClient,
-    private entityMapper: EntityMapperService,
+    // private entityMapper: EntityMapperService,
   ) {}
 
   /**
@@ -52,7 +53,7 @@ export class SqlReportService {
    * @param to
    */
   async query(report: SqlReport, from: Date, to: Date): Promise<ReportData> {
-    await this.updateSchemaIfNecessary();
+    // await this.updateSchemaIfNecessary();
 
     return firstValueFrom(
       this.http
@@ -125,24 +126,6 @@ export class SqlReportService {
     );
   }
 
-  /**
-   * Create a valid SQS schema object for all registered entities
-   */
-  generateSchema(): SqsSchema {
-    const tables: SqlTables = {};
-    for (const [name, ctr] of this.entities.entries()) {
-      tables[name] = {};
-      for (const [attr, attrSchema] of ctr.schema) {
-        if (attr === "_rev") {
-          // skip internal property
-          continue;
-        }
-        tables[name][attr] = this.getSqlType(attrSchema);
-      }
-    }
-    return SqsSchema.create(tables);
-  }
-
   private handleReportCalculationResponse(value: ReportCalculation) {
     switch (value.status) {
       case "FINISHED_SUCCESS":
@@ -163,44 +146,23 @@ export class SqlReportService {
     );
   }
 
-  /**
-   * Update SQS schema if entities have changed
-   * @private
-   */
-  private async updateSchemaIfNecessary() {
-    const existing = await this.entityMapper
-      .load(SqsSchema, SqsSchema.SQS_SCHEMA_ID)
-      .catch(() => new SqsSchema());
-
-    const newSchema = this.generateSchema();
-    if (isEqual(newSchema.sql, existing.sql)) {
-      return;
-    }
-
-    existing.sql = newSchema.sql;
-    await this.entityMapper.save(existing);
-  }
-
   private filterFromToDates(
     value: ReportCalculation,
     from: Date,
     to: Date,
   ): boolean {
+    let argFrom = value.args.get("from");
+    let argTo = value.args.get("to");
+
+    if (!argFrom || !argTo) {
+      return false;
+    }
+
     return (
-      moment(value.args["from"]).format("YYYY-MM-DD") ==
+      moment(argFrom.toString()).format("YYYY-MM-DD") ==
         moment(from).format("YYYY-MM-DD") &&
-      moment(value.args["to"]).format("YYYY-MM-DD") ==
+      moment(argTo.toString()).format("YYYY-MM-DD") ==
         moment(to).format("YYYY-MM-DD")
     );
-  }
-
-  private getSqlType(schema: EntitySchemaField): SqlType {
-    switch (schema.dataType) {
-      case NumberDatatype.dataType:
-      case BooleanDatatype.dataType:
-        return "INTEGER";
-      default:
-        return "TEXT";
-    }
   }
 }
