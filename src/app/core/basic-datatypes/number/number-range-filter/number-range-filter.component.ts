ab412c9c595f7c923d1273e12cfd16e53a1bcb6e
Christoph Scheuing <47225324+christophscheuing@users.noreply.github.com>
2024-06-20 17:24:06 +0200

    Working on it

--- src/app/core/basic-datatypes/number/number-range-filter/number-range-filter.component.ts
+++ src/app/core/basic-datatypes/number/number-range-filter/number-range-filter.component.ts
@@ -2,27 +2,55 @@ import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Entity } from "../../../entity/model/entity";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { FormsModule } from "@angular/forms";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
   selector: "app-date-range-filter",
   templateUrl: "./number-range-filter.component.html",
   styleUrls: ["./number-range-filter.component.scss"],
   standalone: true,
-  imports: [MatFormFieldModule, FormsModule],
+  imports: [MatFormFieldModule, FormsModule, ReactiveFormsModule],
 })
 export class NumberRangeFilterComponent<T extends Entity> implements OnChanges {
+  @Input() filterConfig;
+
   fromValue: number;
   toValue: number;
 
+  form: FormGroup;
+
   // @Input() filterConfig: NumberFilter<T>;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog) {
+    this.form = new FormGroup({
+      range: new FormControl(
+        {
+          minimum: 10,
+          maximum: 100,
+        },
+        [
+          Validators.required, //optional
+          Validators.min(10), //optional
+          Validators.max(100), //optional
+        ],
+      ),
+    });
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.filterConfig) {
       // this.initDates();
     }
+  }
+
+  onNumericRangeChanged(value: any): void {
+    console.log("Changed value: ", value);
   }
 
   // private initDates() {
@@ -61,3 +89,8 @@ export class NumberRangeFilterComponent<T extends Entity> implements OnChanges {
   //     .subscribe(() => this.initDates());
   // }
 }
+
+type INumericRange = {
+  minimum: number;
+  maximum: number;
+};
