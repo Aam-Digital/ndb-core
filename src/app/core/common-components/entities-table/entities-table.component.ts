2edd7e1dca950310f799fe550fffa4bcf39820e4
Abhinegi2 <negiabhishek253@gmail.com>
2024-07-10 15:59:47 +0530

    lint fixes

--- src/app/core/common-components/entities-table/entities-table.component.ts
+++ src/app/core/common-components/entities-table/entities-table.component.ts
@@ -233,7 +233,7 @@ export class EntitiesTableComponent<T extends Entity> {
 
   selectRow(row: TableRow<T>, checked: boolean) {
     if (checked) {
-        this.selectedRecords.push(row.record);
+      this.selectedRecords.push(row.record);
     } else {
       const index = this.selectedRecords.indexOf(row.record);
       if (index > -1) {
