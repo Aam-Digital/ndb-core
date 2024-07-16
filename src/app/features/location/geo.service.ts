d0fa77919795f883f49a08c56b5a07f11acdc17e
Sebastian Leidig <sebastian@aam-digital.com>
2024-07-16 13:08:57 +0200

    rewrite display_name of address search to simpler format

--- src/app/features/location/geo.service.ts
+++ src/app/features/location/geo.service.ts
@@ -6,6 +6,7 @@ import { ConfigService } from "../../core/config/config.service";
 import { AnalyticsService } from "../../core/analytics/analytics.service";
 import { environment } from "../../../environments/environment";
 import { MAP_CONFIG_KEY, MapConfig } from "./map-config";
+import { map } from "rxjs/operators";
 
 export interface GeoResult extends Coordinates {
   display_name: string;
@@ -48,13 +49,35 @@ export class GeoService {
       category: "Map",
       value: searchTerm.length,
     });
-    return this.http.get<GeoResult[]>(`${this.remoteUrl}/search`, {
-      params: {
-        ...this.defaultOptions,
-        q: searchTerm,
-        countrycodes: this.countrycodes,
-      },
-    });
+    return this.http
+      .get<OpenStreetMapsSearchResult[]>(`${this.remoteUrl}/search`, {
+        params: {
+          ...this.defaultOptions,
+          q: searchTerm,
+          countrycodes: this.countrycodes,
+        },
+      })
+      .pipe(
+        map((results) =>
+          Array.isArray(results)
+            ? results.map((x) => this.reformatDisplayName(x))
+            : [],
+        ),
+      );
+  }
+
+  private reformatDisplayName(result: OpenStreetMapsSearchResult): GeoResult {
+    const addr = result?.address;
+    if (addr) {
+      result.display_name = [
+        addr.amenity ?? addr.office,
+        addr.road ? addr.road + " " + addr.house_number : undefined,
+        addr.postcode ? addr.postcode + " " + addr.city : addr.city,
+      ]
+        .filter((x) => !!x)
+        .join(", ");
+    }
+    return result;
   }
 
   /**
@@ -74,3 +97,19 @@ export class GeoService {
     });
   }
 }
+
+type OpenStreetMapsSearchResult = GeoResult & {
+  address: {
+    amenity?: string;
+    office?: string;
+    house_number?: string;
+    road?: string;
+    neighbourhood?: string;
+    suburb?: string;
+    borough?: string;
+    city?: string;
+    postcode?: number;
+    country?: string;
+    country_code?: string;
+  };
+};
