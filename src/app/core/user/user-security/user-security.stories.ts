5a8fcc55c019355380b335b6c22340980c99cd5d
sadaf895 <116058905+sadaf895@users.noreply.github.com>
2024-07-30 10:51:04 +0530

    Given blue accent color and slighlty animation effect to the button whenever the form is valid.

--- src/app/core/user/user-security/user-security.stories.ts
+++ src/app/core/user/user-security/user-security.stories.ts
@@ -3,13 +3,40 @@ import { UserSecurityComponent } from "./user-security.component";
 import { StorybookBaseModule } from "../../../utils/storybook-base.module";
 import { User } from "../user";
 import { importProvidersFrom } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { KeycloakAuthService } from "app/core/session/auth/keycloak/keycloak-auth.service";
+import { BehaviorSubject, of } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import {
+  SessionInfo,
+  SessionSubject,
+} from "app/core/session/auth/session-info";
 
 export default {
   title: "Core/Admin/User Security",
   component: UserSecurityComponent,
   decorators: [
     applicationConfig({
-      providers: [importProvidersFrom(StorybookBaseModule)],
+      providers: [
+        importProvidersFrom(BrowserAnimationsModule),
+        {
+          provide: KeycloakAuthService,
+          useValue: {
+            getRoles: () => of(["account_manager", "user_app"]),
+            getUser: () => {
+              throw new Error("Not implemented");
+            },
+          },
+        },
+        { provide: HttpClient, useValue: {} },
+        {
+          provide: SessionSubject,
+          useValue: new BehaviorSubject<SessionInfo>({
+            roles: [KeycloakAuthService.ACCOUNT_MANAGER_ROLE],
+            name: "tester",
+          }),
+        },
+      ],
     }),
   ],
 } as Meta;
