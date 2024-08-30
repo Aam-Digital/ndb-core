8dbbeaa7ca0572d63a33cab6b10281dd156791cc
Sebastian Leidig <sebastian@aam-digital.com>
2024-08-30 13:38:55 +0200

    refactor: extract entity-actions into own service
    to allow registration of context menu actions from feature modules

---
+++ src/app/core/entity-details/entity-actions-menu/entity-actions-menu.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { EntityActionsMenuService } from './entity-actions-menu.service';
+
+describe('EntityActionsMenuService', () => {
+  let service: EntityActionsMenuService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(EntityActionsMenuService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
