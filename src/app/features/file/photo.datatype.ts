c4b6cd2b0a0d1eade755dadc04e7bf528e534ab6
Sebastian <sebastian@aam-digital.com>
2024-09-04 15:40:26 +0200

    Update src/app/features/file/photo.datatype.ts

--- src/app/features/file/photo.datatype.ts
+++ src/app/features/file/photo.datatype.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { FileDatatype } from "./file.datatype";
 
 /** Datatype for saving a photo on an entity property.*/
-
 @Injectable()
 export class PhotoDatatype extends FileDatatype {
   static override dataType = "photo";
