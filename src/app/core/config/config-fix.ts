78d9ce75485d01a6fee6e9dfdf05870e16e72fb9
Sebastian Leidig <sebastian@aam-digital.com>
2024-09-09 09:34:20 +0200

    rename photo -> image

--- src/app/core/config/config-fix.ts
+++ src/app/core/config/config-fix.ts
@@ -7,6 +7,7 @@ import { INTERACTION_TYPE_CONFIG_ID } from "../../child-dev-project/notes/model/
 import { EventAttendanceMap } from "../../child-dev-project/attendance/model/event-attendance";
 import { LongTextDatatype } from "../basic-datatypes/string/long-text.datatype";
 import { RecurringActivity } from "../../child-dev-project/attendance/model/recurring-activity";
+import { EntityConfig } from "../entity/entity-config";
 
 // prettier-ignore
 export const defaultJsonConfig = {
@@ -956,7 +957,7 @@ export const defaultJsonConfig = {
     "label": $localize`:Label for child:Child`,
     "labelPlural": $localize`:Plural label for child:Children`,
     "toStringAttributes": ["name"],
-    "toBlockDetailsAttributes": { "title": "name", "photo": "photo", "fields": ["phone", "schoolId", "schoolClass"] },
+    "toBlockDetailsAttributes": { "title": "name", "image": "photo", "fields": ["phone", "schoolId", "schoolClass"] },
     "icon": "child",
     "color": "#1565C0",
     "hasPII": true,
@@ -1051,7 +1052,7 @@ export const defaultJsonConfig = {
         "label": $localize`:Label for a child attribute:Birth certificate`
       }
     },
-  },
+  } as EntityConfig,
   "entity:School": {
     "toStringAttributes": ["name"],
     "icon": "university",
