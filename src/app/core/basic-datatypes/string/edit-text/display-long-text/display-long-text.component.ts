eee5244813bb8734a76aedfb8e0f80f70615b858
sadaf895 <116058905+sadaf895@users.noreply.github.com>
2024-09-11 16:01:20 +0530

    Updated the display-long-text.component.ts file

--- src/app/core/basic-datatypes/string/edit-text/display-long-text/display-long-text.component.ts
+++ src/app/core/basic-datatypes/string/edit-text/display-long-text/display-long-text.component.ts
@@ -5,7 +5,20 @@ import { DynamicComponent } from "app/core/config/dynamic-components/dynamic-com
 @DynamicComponent("DisplayLongText")
 @Component({
   selector: "app-display-long-text",
-  template: 'Display Text{{ value !== undefined ? value + " " + config : "" }}',
+  template: `<div [innerHTML]="formattedValue"></div>`,
   standalone: true,
 })
-export class DisplayLongTextComponent extends ViewDirective<string, string> {}
+export class DisplayLongTextComponent extends ViewDirective<string, string> {
+  get formattedValue(): string {
+    if (this.value === undefined) return "";
+
+    const MaxLines = 4;
+    const lines = this.value.split("\n");
+
+    const truncatedText =
+      lines.length > MaxLines
+        ? lines.slice(0, MaxLines).join("<br>") + "..."
+        : lines.join("<br>");
+    return truncatedText;
+  }
+}
