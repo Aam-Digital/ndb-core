673fec3364e8ca5519e80c99bda44589d9981c89
Sebastian Leidig <sebastian@aam-digital.com>
2024-09-12 13:39:48 +0200

    remove test code duplication

--- src/app/core/entity/edit-entity-type-dropdown/edit-entity-type-dropdown.component.spec.ts
+++ src/app/core/entity/edit-entity-type-dropdown/edit-entity-type-dropdown.component.spec.ts
@@ -1,40 +1,28 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { EditEntityTypeDropdownComponent } from "./edit-entity-type-dropdown.component";
-import { FormControl, FormGroup } from "@angular/forms";
 import { MockedTestingModule } from "../../../utils/mocked-testing.module";
-import { EntitySchemaField } from "../schema/entity-schema-field";
-import { Entity } from "../model/entity";
+import { setupEditComponent } from "../default-datatype/edit-component.spec";
 
 describe("EditConfigurableEnumComponent", () => {
   let component: EditEntityTypeDropdownComponent;
   let fixture: ComponentFixture<EditEntityTypeDropdownComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
         EditEntityTypeDropdownComponent,
         MockedTestingModule.withState(),
       ],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(EditEntityTypeDropdownComponent);
     component = fixture.componentInstance;
-    initWithSchema({});
+    setupEditComponent(component);
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
   });
-
-  function initWithSchema(schema: EntitySchemaField) {
-    const fromGroup = new FormGroup({ test: new FormControl() });
-    component.formControl = fromGroup.get("test") as FormControl;
-    component.formFieldConfig = { id: "test", ...schema }; // EditComponents are ensure to receive fully extended formFieldConfig
-    component.entity = new Entity();
-    component.ngOnInit();
-  }
 });
