04b667246ce793dc962fa97caa6d7bc6f73ac1a7
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-11-29 11:06:24 +0530

    feat(*): create the wrapper function for the firebase messaging

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+const { environments } = require("eslint-plugin-prettier");
+
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 firebase.initializeApp({
@@ -10,3 +12,32 @@ firebase.initializeApp({
     measurementId: "G-9Z03T96S55"
 });
 const messaging = firebase.messaging();
+messaging.onBackgroundMessage(function (payload) {
+    console.log('Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+});
+
+messaging.getToken({ vapidKey: environments.vapidKey }).then((currentToken) => {
+    if (currentToken) {
+        console.log('Token:', currentToken);
+    } else {
+        console.log('No registration token available. Request permission to generate one.');
+    }
+}
+).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+});
+
+
