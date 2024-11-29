c29861ff2d0096a3f0cfb772bf70bc67ec111d7d
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-11-29 18:23:03 +0530

    feat(*): create the push notification UI

--- src/app/core/ui/ui/ui.component.ts
+++ src/app/core/ui/ui/ui.component.ts
@@ -39,6 +39,7 @@ import { LoginStateSubject } from "../../session/session-type";
 import { LoginState } from "../../session/session-states/login-state.enum";
 import { SessionManagerService } from "../../session/session-service/session-manager.service";
 import { SetupWizardButtonComponent } from "../../admin/setup-wizard/setup-wizard-button/setup-wizard-button.component";
+import { NotificationsComponent } from '../../../features/notifications/notifications.component';
 
 /**
  * The main user interface component as root element for the app structure
@@ -67,6 +68,7 @@ import { SetupWizardButtonComponent } from "../../admin/setup-wizard/setup-wizar
     PrimaryActionComponent,
     DisplayImgComponent,
     SetupWizardButtonComponent,
+    NotificationsComponent,
   ],
   standalone: true,
 })
---
+++ src/app/features/notifications/notifications.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { MatBadgeModule } from "@angular/material/badge";
+import { NgIf } from "@angular/common";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MatMenu } from '@angular/material/menu';
+import { NgFor } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-notifications',
+  standalone: true,
+  imports: [
+    MatBadgeModule,
+    NgIf,
+    FontAwesomeModule,
+    MatMenu,
+    NgFor,
+    MatButtonModule,
+    MatMenuTrigger,
+    MatMenuModule,
+    MatSlideToggle,
+    FormsModule,
+  ],
+  templateUrl: './notifications.component.html',
+  styleUrl: './notifications.component.scss'
+})
+export class NotificationsComponent {
+  isEnableNotification = false;
+  showSettings = false;
+
+  notifications = [
+    { user: 'Brigid Dawson', message: 'followed you', time: '4 hours ago', avatar: 'avatar1.jpg', isUnread: true },
+    { user: 'John Dwyer', message: 'liked your post', time: 'Yesterday', avatar: 'avatar2.jpg', isUnread: true },
+    { user: 'Tim Hellman', message: 'liked your post', time: 'Tuesday', avatar: 'avatar3.jpg', isUnread: false },
+    { user: 'System', message: 'Running low on storage space', time: 'Monday', avatar: 'system.jpg', isUnread: false },
+    { user: 'Shannon Shaw', message: 'commented on your post', time: '4 days ago', avatar: 'avatar4.jpg', isUnread: false }
+  ];
+
+  toggleNotifications(event: Event) {
+    event.stopPropagation();
+    this.isEnableNotification = !this.isEnableNotification;
+  }
+
+  toggleSettings(event: Event) {
+    event.stopPropagation();
+    this.showSettings = !this.showSettings;
+  }
+
+  notificationClicked(notification) {
+    notification.isUnread = false;
+    console.log(notification.user + ' clicked');
+  }
+}
