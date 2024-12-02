a9a971da54125ed63d543589482b00c839cf22d9
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-12-02 19:51:01 +0530

    fix(*): linting fixes

--- src/app/features/notifications/notifications.component.spec.ts
+++ src/app/features/notifications/notifications.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { NotificationsComponent } from './notifications.component';
+import { NotificationsComponent } from "./notifications.component";
 
-describe('NotificationsComponent', () => {
+describe("NotificationsComponent", () => {
   let component: NotificationsComponent;
   let fixture: ComponentFixture<NotificationsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotificationsComponent]
-    })
-    .compileComponents();
+      imports: [NotificationsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
