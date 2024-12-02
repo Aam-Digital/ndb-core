1da4fdf75f7c53e05faa72ff65c2556705e6c977
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-12-02 20:10:10 +0530

    fix(*): fix the linting checks in the notification component

--- src/app/features/notifications/notifications.component.ts
+++ src/app/features/notifications/notifications.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { MatBadgeModule } from "@angular/material/badge";
 import { NgIf } from "@angular/common";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { MatMenu } from '@angular/material/menu';
-import { NgFor } from '@angular/common';
+import { MatMenu } from "@angular/material/menu";
+import { NgFor } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
-import { MatMenuTrigger } from '@angular/material/menu';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
+import { MatMenuTrigger } from "@angular/material/menu";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSlideToggle } from "@angular/material/slide-toggle";
+import { FormsModule } from "@angular/forms";
 import { MatTooltipModule } from "@angular/material/tooltip";
+import { Logging } from "app/core/logging/logging.service";
 
 @Component({
-  selector: 'app-notifications',
+  selector: "app-notifications",
   standalone: true,
   imports: [
     MatBadgeModule,
@@ -27,17 +28,35 @@ import { MatTooltipModule } from "@angular/material/tooltip";
     FormsModule,
     MatTooltipModule,
   ],
-  templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.scss'
+  templateUrl: "./notifications.component.html",
+  styleUrl: "./notifications.component.scss",
 })
 export class NotificationsComponent {
   isEnableNotification = false;
   showSettings = false;
 
   notifications = [
-    { user: 'Brigid Dawson', message: 'update child entity.', time: '4 hours ago', avatar: 'avatar1.jpg', isUnread: true },
-    { user: 'John Dwyer', message: 'create new student.', time: 'Yesterday', avatar: 'avatar2.jpg', isUnread: true },
-    { user: 'Tim Hellman', message: 'update the config.', time: 'Tuesday', avatar: 'avatar3.jpg', isUnread: false },
+    {
+      user: "Brigid Dawson",
+      message: "update child entity.",
+      time: "4 hours ago",
+      avatar: "avatar1.jpg",
+      isUnread: true,
+    },
+    {
+      user: "John Dwyer",
+      message: "create new student.",
+      time: "Yesterday",
+      avatar: "avatar2.jpg",
+      isUnread: true,
+    },
+    {
+      user: "Tim Hellman",
+      message: "update the config.",
+      time: "Tuesday",
+      avatar: "avatar3.jpg",
+      isUnread: false,
+    },
   ];
 
   toggleNotifications(event: Event) {
@@ -52,7 +71,7 @@ export class NotificationsComponent {
 
   notificationClicked(notification) {
     notification.isUnread = false;
-    console.log(notification.user + ' clicked');
+    Logging.log(notification.user + " clicked");
   }
 
   onNotificationBellClick() {
