35f34434cb3b0ac3e0d483e5c7da89d71551ca3c
Abhinegi2 <negiabhishek253@gmail.com>
2024-12-10 16:36:28 +0530

    test file updates

--- src/app/core/common-components/description-only/edit-description-only/edit-description-only.component.spec.ts
+++ src/app/core/common-components/description-only/edit-description-only/edit-description-only.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { EditDescriptionOnlyComponent } from "./edit-description-only.component";
+import { ComponentRegistry } from "app/dynamic-components";
 
-describe("EditDescriptionOnlyComponent", () => {
+fdescribe("EditDescriptionOnlyComponent", () => {
   let component: EditDescriptionOnlyComponent;
   let fixture: ComponentFixture<EditDescriptionOnlyComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EditDescriptionOnlyComponent],
+      providers: [ComponentRegistry],
     }).compileComponents();
   });
 
