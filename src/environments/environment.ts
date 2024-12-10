7af43444a218387d8f2c4013b957150ae6d7677e
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-12-10 21:50:25 +0530

    fix(*): added the firebase config in the environment prod and spec file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -52,4 +52,4 @@ export const firebaseConfig = {
   messagingSenderId: "189059495005",
   appId: "1:189059495005:web:c6bdb0c8c665864b37c9b4",
   vapidKey: "BKkE6EgJBIwRa9-DUSKZpmkMuG7Fak2lZgxda_DPx5kYkeK8cgQM_xqurHqxRNa1b2MuW7-_t9iFbgfuXUsWF5I"
-}
+};
