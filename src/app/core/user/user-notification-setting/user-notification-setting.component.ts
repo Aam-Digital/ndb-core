c3095254aeaf3def3668a46ff4fd2c06dbcf34ca
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-12-13 16:10:30 +0530

    fix(*): implement for the add new notification rule functionality and cleanups

--- src/app/core/user/user-notification-setting/user-notification-setting.component.ts
+++ src/app/core/user/user-notification-setting/user-notification-setting.component.ts
@@ -1,41 +1,61 @@
 import { Component } from '@angular/core';
 import { MatSlideToggle } from "@angular/material/slide-toggle";
-import {
-  MatExpansionPanel,
-  MatExpansionPanelDescription,
-  MatExpansionPanelHeader,
-  MatExpansionPanelTitle,
-} from "@angular/material/expansion";
-import { NgFor, NgIf } from "@angular/common";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MatOption, MatSelect } from "@angular/material/select";
+import { MatInputModule } from "@angular/material/input";
+import { NgFor } from "@angular/common";
+import { FaIconComponent, FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { Logging } from 'app/core/logging/logging.service';
-import { eventTypes, EventType } from "app/core/config/eventTypes";
+import { eventTypes, EventType } from "app/core/config/event-types";
+import { FormsModule } from '@angular/forms';
+import { Angulartics2OnModule } from "angulartics2";
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+interface Notification {
+  selectedOption: string;
+  inputValue: string;
+  toggleValue: boolean;
+}
 
 @Component({
   selector: 'app-user-notification-setting',
   standalone: true,
   imports: [
     MatSlideToggle,
-    MatExpansionPanel,
-    MatExpansionPanelHeader,
-    MatExpansionPanelTitle,
-    MatExpansionPanelDescription,
+    MatSelect,
+    MatInputModule,
     NgFor,
     FontAwesomeModule,
-    NgIf,
+    FormsModule,
+    MatFormFieldModule,
+    MatOption,
+    Angulartics2OnModule,
+    MatTooltip,
+    FaIconComponent,
+    MatButtonModule,
+    Angulartics2OnModule,
+    MatTooltipModule,
   ],
   templateUrl: './user-notification-setting.component.html',
   styleUrl: './user-notification-setting.component.scss'
 })
 export class UserNotificationSettingComponent {
   eventTypes: EventType[] = eventTypes;
+  notifications: Notification[] = [
+    { selectedOption: '', inputValue: '', toggleValue: false }
+  ];
 
-  onEventOptionToggle(eventTypeId: string, option: string) {
-    // TODO: Implement your logic to handle option toggle
-    Logging.log(`Toggled option: ${option} for event type: ${eventTypeId}`);
+  addNewRule() {
+    this.notifications.push({ selectedOption: '', inputValue: '', toggleValue: false });
+  }
+
+  removeRule(index: number) {
+    this.notifications.splice(index, 1);
+    // TODO: Need to add the logic to remove the rule from the backend
   }
 
   onEnableNotification() {
-    // TODO: Implement your logic to handle browser toggle
+    Logging.log('Browser notifications toggled.');
   }
 }
