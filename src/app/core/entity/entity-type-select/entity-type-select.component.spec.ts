9990ddf5f3517d45e66d7d52e5f0e29ee30c68bc
Ayush8923 <ayush.uniyal@coloredcow.in>
2024-12-17 16:10:55 +0530

    fix(*): update the entity type select component test cases'

--- src/app/core/entity/entity-type-select/entity-type-select.component.spec.ts
+++ src/app/core/entity/entity-type-select/entity-type-select.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { EntityTypeSelectComponent } from "./entity-type-select.component";
 import { MockedTestingModule } from "../../../utils/mocked-testing.module";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 describe("EntityTypeSelectComponent", () => {
   let component: EntityTypeSelectComponent;
@@ -8,11 +9,16 @@ describe("EntityTypeSelectComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EntityTypeSelectComponent, MockedTestingModule.withState()],
+      imports: [
+        EntityTypeSelectComponent,
+        MockedTestingModule.withState(),
+        ReactiveFormsModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EntityTypeSelectComponent);
     component = fixture.componentInstance;
+    component.formControl = new FormControl();
     fixture.detectChanges();
   });
 
