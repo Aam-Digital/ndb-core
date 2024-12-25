5e5dfe897ac6fa2152d2a30a938fc73a285dc305
Abhinegi2 <negiabhishek253@gmail.com>
2024-12-25 12:47:39 +0530

    lint fixes

--- src/app/core/admin/admin-entity-public-forms/admin-entity-public-forms-component.ts
+++ src/app/core/admin/admin-entity-public-forms/admin-entity-public-forms-component.ts
@@ -6,32 +6,22 @@ import { DynamicComponentDirective } from "app/core/config/dynamic-components/dy
   selector: "app-admin-entity-public-forms-component",
   standalone: true,
   templateUrl: "./admin-entity-public-forms-component.html",
-  styleUrls: [
-    "./admin-entity-public-forms-component.scss",
-  ],
-  imports: [
-    DynamicComponentDirective,
-  ],
+  styleUrls: ["./admin-entity-public-forms-component.scss"],
+  imports: [DynamicComponentDirective],
 })
 export class AdminEntityPublicFormsComponent implements OnInit {
   @Input() entityConstructor: EntityConstructor;
-config: any;
+  config: any;
 
   ngOnInit(): void {
-    console.log(this.entityConstructor.ENTITY_TYPE,"chjebciueb")
-    this.config =               {
+    console.log(this.entityConstructor.ENTITY_TYPE, "chjebciueb");
+    this.config = {
       component: "RelatedEntities",
       config: {
         entityType: "PublicFormConfig",
         property: this.entityConstructor.ENTITY_TYPE,
-        columns: [
-          "title",
-          "description",
-          "entity",
-          "route"
-        ]
-      }
-    }
+        columns: ["title", "description", "entity", "route"],
+      },
+    };
   }
- 
 }
