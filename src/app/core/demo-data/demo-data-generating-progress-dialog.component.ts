8ea138bbd9cb288b46a8fb5bda752ce32a774364
Sebastian Leidig <sebastian@aam-digital.com>
2024-12-30 18:07:01 +0100

    update translatable texts 3

--- src/app/core/demo-data/demo-data-generating-progress-dialog.component.ts
+++ src/app/core/demo-data/demo-data-generating-progress-dialog.component.ts
@@ -27,7 +27,7 @@ import { Component } from "@angular/core";
     <mat-dialog-content>
       <p i18n>Generating sample data for this demo ...</p>
       <mat-progress-bar mode="indeterminate"></mat-progress-bar>
-      <div i18n="test">new sample data text XX</div>
+      <div i18n="sample test|a test string">new sample data text X3</div>
     </mat-dialog-content>
   `,
 })
