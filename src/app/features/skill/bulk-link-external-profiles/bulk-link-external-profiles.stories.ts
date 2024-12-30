666c559260cf697c37f9ff4ec76f7bc620ea22c3
Sebastian Leidig <sebastian@aam-digital.com>
2024-12-30 14:15:13 +0100

    more storybook

--- src/app/features/skill/bulk-link-external-profiles/bulk-link-external-profiles.stories.ts
+++ src/app/features/skill/bulk-link-external-profiles/bulk-link-external-profiles.stories.ts
@@ -5,16 +5,17 @@ import { Entity } from "../../../core/entity/model/entity";
 import { SkillApiService } from "../skill-api/skill-api.service";
 import { StorybookBaseModule } from "../../../utils/storybook-base.module";
 import { importProvidersFrom } from "@angular/core";
-import { mockSkillApi } from "../skill-api/skill-api-mock";
 import { ExternalProfileLinkConfig } from "../external-profile-link-config";
 import { FormFieldConfig } from "../../../core/common-components/entity-form/FormConfig";
-import { throwError } from "rxjs";
+import { NEVER, throwError } from "rxjs";
+import { mockSkillApi } from "../skill-api/skill-api-mock";
 
 const entities: Entity[] = [
   TestEntity.create({ name: "Test Entity 1" }),
   TestEntity.create({ name: "Test Entity 2" }),
   TestEntity.create({ name: "Test Entity 3" }),
 ];
+entities[0]["ref"] = "1";
 
 export default {
   title: "Features/Skill Integration/BulkLinkExternalProfiles",
@@ -23,7 +24,14 @@ export default {
     applicationConfig({
       providers: [
         importProvidersFrom(StorybookBaseModule),
-        { provide: SkillApiService, useValue: mockSkillApi },
+        {
+          provide: SkillApiService,
+          useValue: {
+            generateDefaultSearchParams: () => ({}),
+            getExternalProfiles: () => NEVER,
+            getExternalProfileById: (x) => x,
+          },
+        },
         // importProvidersFrom(StorybookBaseModule.withData([...entities])),
       ],
     }),
@@ -41,6 +49,15 @@ const profileLinkConfig: ExternalProfileLinkConfig = {
 };
 
 export const Default: Story = {
+  args: {
+    entities: entities,
+    config: { id: "ref", additional: profileLinkConfig } as FormFieldConfig,
+    // @ts-ignore
+    skillApi: mockSkillApi,
+  },
+};
+
+export const Loading: Story = {
   args: {
     entities: entities,
     config: { id: "ref", additional: profileLinkConfig } as FormFieldConfig,
