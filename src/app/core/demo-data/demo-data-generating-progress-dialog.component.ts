2b87de550a22527e9f8bb8ecab34e70d7bf9cd2e
Sebastian Leidig <sebastian@aam-digital.com>
2024-12-30 17:30:05 +0100

    test trans

--- src/app/core/demo-data/demo-data-generating-progress-dialog.component.ts
+++ src/app/core/demo-data/demo-data-generating-progress-dialog.component.ts
@@ -25,8 +25,8 @@ import { Component } from "@angular/core";
 @Component({
   template: `
     <mat-dialog-content>
-      <p i18n>Generating sample data for this demo ... with changes XXX</p>
-      <p i18n>more sample data text</p>
+      <p i18n>Generating sample data for this demo ... with changes XXXXXX</p>
+      <p i18n>etxra sample data text</p>
       <mat-progress-bar mode="indeterminate"></mat-progress-bar>
     </mat-dialog-content>
   `,
