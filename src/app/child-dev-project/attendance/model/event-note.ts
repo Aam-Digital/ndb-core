39d47f8124485e1b761e9811af3f493992f2ca92
Sebastian Leidig <sebastian@aam-digital.com>
2025-01-02 13:22:05 +0100

    fix(Admin UI): hide special EventNote entity type from admin lists

--- src/app/child-dev-project/attendance/model/event-note.ts
+++ src/app/child-dev-project/attendance/model/event-note.ts
@@ -1,25 +1,11 @@
-/*
- *     This file is part of ndb-core.
- *
- *     ndb-core is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     ndb-core is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import { DatabaseEntity } from "../../../core/entity/database-entity.decorator";
 import { Note } from "../../notes/model/note";
 
 @DatabaseEntity("EventNote")
 export class EventNote extends Note {
+  static override label = undefined; // hide the EventNote entity type from Admin UIs to avoid confusion with the Note entity type
+  static override labelPlural = undefined;
+
   static override create(date: Date, subject: string = ""): EventNote {
     const instance = new EventNote();
     instance.date = date;
