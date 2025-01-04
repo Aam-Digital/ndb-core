36a806a2e6341cadf0748b0ce14b9a14972b48fc
Ayush8923 <ayush.uniyal@coloredcow.in>
2025-01-04 11:14:33 +0530

    fix(*): update the test cases

--- src/app/features/notification/notification-settings/notification-settings.component.spec.ts
+++ src/app/features/notification/notification-settings/notification-settings.component.spec.ts
@@ -6,10 +6,20 @@ import {
 } from "app/core/entity/database-entity.decorator";
 import { FontAwesomeTestingModule } from "@fortawesome/angular-fontawesome/testing";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MockEntityMapperService } from "../../../core/entity/entity-mapper/mock-entity-mapper-service";
+import { EntityMapperService } from "../../../core/entity/entity-mapper/entity-mapper.service";
+import {
+  SessionInfo,
+  SessionSubject,
+} from "../../../core/session/auth/session-info";
+import { BehaviorSubject } from "rxjs";
+import { TEST_USER } from "../../../core/user/demo-user-generator.service";
 
 describe("NotificationSettingComponent", () => {
   let component: NotificationSettingsComponent;
   let fixture: ComponentFixture<NotificationSettingsComponent>;
+  let entityMapper: MockEntityMapperService;
+  const testUser: SessionInfo = { name: TEST_USER, id: TEST_USER, roles: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,7 +28,14 @@ describe("NotificationSettingComponent", () => {
         FontAwesomeTestingModule,
         BrowserAnimationsModule,
       ],
-      providers: [{ provide: EntityRegistry, useValue: entityRegistry }],
+      providers: [
+        { provide: EntityRegistry, useValue: entityRegistry },
+        { provide: EntityMapperService, useValue: entityMapper },
+        {
+          provide: SessionSubject,
+          useValue: new BehaviorSubject(testUser),
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificationSettingsComponent);
