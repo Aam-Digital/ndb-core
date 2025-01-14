4f732e7d1e0a079f8eb936bdd880883dc69bb2bd
Ayush8923 <ayush.uniyal@coloredcow.in>
2025-01-14 19:05:32 +0530

    fix: added the jsDoc in the notification event

--- src/app/features/notification/model/notification-event.ts
+++ src/app/features/notification/model/notification-event.ts
@@ -9,12 +9,43 @@ import { NotificationType } from "./notification-config";
  */
 @DatabaseEntity("NotificationEvent")
 export class NotificationEvent extends Entity {
+  /*
+   * The title of the notification.
+   */
   @DatabaseField() title: string;
+
+  /*
+   * The body of the notification.
+   */
   @DatabaseField() body: string;
+
+  /*
+   * The URL to redirect the user to when the notification is clicked.
+   */
   @DatabaseField() actionURL: string;
+
+  /*
+   * The user ID for whom the notification is intended
+   */
   @DatabaseField() notificationFor: string;
+
+  /*
+   * The notification token to be used for the notification.
+   */
   @DatabaseField() notificationToken: string;
+
+  /*
+   * The type of notification.
+   */
   @DatabaseField() notificationType: NotificationType;
+
+  /*
+   * The entity type of the notification.
+   */
   @DatabaseField() entityType: string;
+
+  /*
+   * The status of the notification.
+   */
   @DatabaseField() readStatus: boolean;
 }
