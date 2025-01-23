06aae28f3586d9d8adcb7e5b5d458775fdba3282
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-01-23 16:31:30 +0530

    Update src/app/features/public-form/public-form.component.spec.ts
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/features/public-form/public-form.component.spec.ts
+++ src/app/features/public-form/public-form.component.spec.ts
@@ -96,7 +96,7 @@ describe("PublicFormComponent", () => {
 
     expect(saveSpy).toHaveBeenCalledWith(component.form, component.entity);
     tick();
-    expect(navigateSpy).toHaveBeenCalledWith(["/submission-success"]);
+    expect(navigateSpy).toHaveBeenCalledWith(["/public-form/submission-success"]);
   }));
 
   it("should show a snackbar error and not reset when trying to submit invalid form", fakeAsync(() => {
