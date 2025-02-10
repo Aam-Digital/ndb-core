733bec70c265c3f24b80e0e36c7154ccf918a8a9
Abhinegi2 <negiabhishek253@gmail.com>
2025-02-10 19:47:24 +0530

    refactor and remove unused code in storybopok

--- src/app/core/basic-datatypes/string/edit-url/edit-url.stories.ts
+++ src/app/core/basic-datatypes/string/edit-url/edit-url.stories.ts
@@ -1,5 +1,5 @@
-import { applicationConfig, Meta, StoryFn } from "@storybook/angular";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { applicationConfig, Meta } from "@storybook/angular";
+import { FormControl } from "@angular/forms";
 import { EditUrlComponent } from "./edit-url.component";
 import { importProvidersFrom } from "@angular/core";
 import { StorybookBaseModule } from "app/utils/storybook-base.module";
@@ -15,27 +15,42 @@ export default {
 } as Meta;
 
 export const Primary = {
+  render: (args) => ({
+    props: {
+      ...args,
+      formControl: new FormControl(
+        "https://github.com/Aam-Digital/ndb-core/issues/2460",
+      ),
+    },
+  }),
   args: {
     label: "URL",
-    formControl: new FormControl(
-      "https://github.com/Aam-Digital/ndb-core/issues/2460",
-    ),
   },
 };
 
 export const DisabledWithClickableLink = {
+  render: (args) => ({
+    props: {
+      ...args,
+      formControl: new FormControl({
+        value: "https://github.com/Aam-Digital/ndb-core/issues/2460",
+        disabled: true,
+      }),
+    },
+  }),
   args: {
     label: "URL",
-    formControl: new FormControl({
-      value: "https://github.com/Aam-Digital/ndb-core/issues/2460",
-      disabled: true,
-    }),
   },
 };
 
 export const InvalidUrl = {
+  render: (args) => ({
+    props: {
+      ...args,
+      formControl: new FormControl("invalid-url"),
+    },
+  }),
   args: {
     label: "URL",
-    formControl: new FormControl("invalid-url"),
   },
 };
