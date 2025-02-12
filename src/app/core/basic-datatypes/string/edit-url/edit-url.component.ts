60f9057b5be4683145479cd82786e278dd343145
Abhinegi2 <negiabhishek253@gmail.com>
2025-02-12 11:43:36 +0530

    added underline + validation

--- src/app/core/basic-datatypes/string/edit-url/edit-url.component.ts
+++ src/app/core/basic-datatypes/string/edit-url/edit-url.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnChanges } from "@angular/core";
 import { EditComponent } from "../../../entity/default-datatype/edit-component";
 import { DynamicComponent } from "../../../config/dynamic-components/dynamic-component.decorator";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -22,31 +22,34 @@ import { NgClass, NgIf } from "@angular/common";
   ],
   standalone: true,
 })
-export class EditUrlComponent extends EditComponent<string> {
-  
-  /**
-   * Checks if the current formControl value is a valid URL.
-   * Returns true only if it is a properly formatted http/https URL.
-   */
-  isValidUrl(value: string | null): boolean {
-    if (!value) return false;
-    try {
-      const url = new URL(value);
-      return url.protocol === "http:" || url.protocol === "https:";
-    } catch (error) {
-      return false;
-    }
+export class EditUrlComponent
+  extends EditComponent<string>
+  implements OnChanges
+{
+  override ngOnChanges() {
+    this.formControl.valueChanges.subscribe((value) => {
+      this.validateUrl(value);
+    });
   }
 
+  /**
+   * Checks if the current formControl value is a valid URL.
+   */
+  validateUrl(value: string): void {
+    const urlPattern =
+      /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,6}\.?)(\/[\w.-]*)*\/?$/i;
+    this.formControl.setErrors(
+      urlPattern.test(value) ? null : { invalid: true },
+    );
+  }
   /**
    * Opens the URL in a new tab only if:
    * - The input field is disabled
    * - The value is a valid URL
    */
   openLinkIfDisabled() {
-    if (this.formControl.disabled && this.isValidUrl(this.formControl.value)) {
+    if (this.formControl.disabled) {
       window.open(this.formControl.value, "_blank");
     }
   }
 }
-
