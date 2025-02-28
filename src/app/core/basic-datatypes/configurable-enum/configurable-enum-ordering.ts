82eea9827c48d89ab86416146262590ca6acb8c9
Sebastian Leidig <sebastian@aam-digital.com>
2025-02-28 16:48:12 +0100

    re-add lost code comments

--- src/app/core/basic-datatypes/configurable-enum/configurable-enum-ordering.ts
+++ src/app/core/basic-datatypes/configurable-enum/configurable-enum-ordering.ts
@@ -12,6 +12,17 @@ export namespace Ordering {
 
   export type Config<T extends ConfigurableEnumValue> = Array<T & HasOrdinal>;
 
+  /**
+   * Extends an array of configurable enum values with an ordinal value so that the
+   * ordinal value of each enum value matches the position of the element in the array.
+   *
+   * Note that this should be used 'early' in the pipeline, i.e. when the `ConfigurableEnumConfig` is just
+   * created. It is technically not incorrect to use this function on any array that contains configurable
+   * enum values, but it doesn't make sense to apply this function to 'scrambled' arrays, i.e. arrays where the
+   * ordering of values is _not_ the natural order of these values.
+   *
+   * @param values The values to impose a total ordering on
+   */
   export function imposeTotalOrdering<T extends ConfigurableEnumValue>(
     values: Array<T>,
   ): Array<EnumValue<T>> {
--- src/app/core/common-components/entities-table/table-row.ts
+++ src/app/core/common-components/entities-table/table-row.ts
@@ -4,7 +4,6 @@ import { EntityFormGroup } from "../entity-form/entity-form.service";
 /**
  * Wrapper to keep additional form data for each row of an entity, required for inline editing.
  */
-
 export interface TableRow<T extends Entity> {
   record: T;
   formGroup?: EntityFormGroup<T>;
