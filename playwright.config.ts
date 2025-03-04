777480a6f736b3a5d168cf59ac25885b09879946
Abhinegi2 <negiabhishek253@gmail.com>
2025-03-04 12:44:37 +0530

    modify timeout + steps of ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,6 +77,7 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://localhost:4200",
+    timeout: 120000, 
     reuseExistingServer: !process.env.CI,
   },
 });
