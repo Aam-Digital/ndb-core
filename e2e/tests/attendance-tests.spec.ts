e7dd8e91bfda8bc7dcecbdf833d86b4010e335b4
Abu Sadaf <sahinakhatoon650@gmail.com>
2025-03-04 15:45:25 +0530

    moved fixed-date code to a separate file

--- e2e/tests/attendance-tests.spec.ts
+++ e2e/tests/attendance-tests.spec.ts
@@ -1,24 +1,7 @@
 import { expect, test } from "@playwright/test";
 import path from "path";
 import { startApp } from "../utils/core-e2e-utils"; // Import the path module for file path operations
-
-async function setFixedDate(page, fixedDate) {
-  const fakeNow = new Date(fixedDate).valueOf(); // Convert the fixed date to a timestamp
-  await page.addInitScript(`{
-    Date = class extends Date {
-      constructor(...args) {
-        if (args.length === 0) {
-          super(${fakeNow}); // Default to the fixed date
-        } else {
-          super(...args); // Handle explicitly provided dates
-        }
-      }
-    }
-    const __DateNowOffset = ${fakeNow} - Date.now(); // Offset for Date.now()
-    const __DateNow = Date.now;
-    Date.now = () => __DateNow() + __DateNowOffset; // Override Date.now()
-  }`);
-}
+import { setFixedDate } from "../utils/fixed-date";
 
 test.describe("Attendance Module", () => {
   test.beforeEach(async ({ page }, testInfo) => {
