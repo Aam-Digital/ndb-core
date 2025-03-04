04d8b1082d6539ae1dec738e0ee157e040426c82
Abhinegi2 <negiabhishek253@gmail.com>
2025-03-04 14:02:09 +0530

    lint fixes

--- src/app/features/de-duplication/bulk-merge-module.ts
+++ src/app/features/de-duplication/bulk-merge-module.ts
@@ -9,11 +9,11 @@ export class BulkMergeModule {
 }
 
 const dynamicComponents: [string, AsyncComponent][] = [
-    [
-      "BulkMergeRecordsComponent",
-      () =>
-        import("./bulk-merge-records/bulk-merge-records.component").then(
-          (c) => c.BulkMergeRecordsComponent,
-        ),
-    ],
-  ];
+  [
+    "BulkMergeRecordsComponent",
+    () =>
+      import("./bulk-merge-records/bulk-merge-records.component").then(
+        (c) => c.BulkMergeRecordsComponent,
+      ),
+  ],
+];
