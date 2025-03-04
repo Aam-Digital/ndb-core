2f1cb6f2ffd56753396dd23be1410b9364ea0982
Abhinegi2 <negiabhishek253@gmail.com>
2025-03-04 12:55:52 +0530

    testing server

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://localhost:4200",
-    timeout: 120000, 
+    timeout: 300000,
     reuseExistingServer: !process.env.CI,
   },
 });
