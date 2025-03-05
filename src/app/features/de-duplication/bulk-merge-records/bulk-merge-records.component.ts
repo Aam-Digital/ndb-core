94026eef27d52a4e90abd863179fbf26eb32c3a0
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-03-05 17:29:53 +0530

    Update src/app/features/de-duplication/bulk-merge-records/bulk-merge-records.component.ts
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/features/de-duplication/bulk-merge-records/bulk-merge-records.component.ts
+++ src/app/features/de-duplication/bulk-merge-records/bulk-merge-records.component.ts
@@ -137,7 +137,7 @@ export class BulkMergeRecordsComponent<E extends Entity> implements OnInit {
     Object.assign(this.mergedEntity, this.mergeForm.formGroup.value);
     if (
       !(await this.confirmationDialog.getConfirmation(
-        $localize`:Merge confirmation title: Are you sure you want to Merge this?`,
+        $localize`:Merge confirmation title: Are you sure you want to merge this?`,
         $localize`:Merge confirmation dialog: Merging of two records will permanently delete the data that is not merged. This action cannot be undone \n(Once the two records are merged, there will be only one record available in the system)`,
       ))
     ) {
