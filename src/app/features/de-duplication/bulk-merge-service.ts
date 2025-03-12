f17bb9acba316d078371eb7f010ba75a4c28ce05
Abhinegi2 <negiabhishek253@gmail.com>
2025-03-12 14:23:07 +0530

    init get&merge related entity setup

--- src/app/features/de-duplication/bulk-merge-service.ts
+++ src/app/features/de-duplication/bulk-merge-service.ts
@@ -6,17 +6,25 @@ import { lastValueFrom } from "rxjs";
 import { BulkMergeRecordsComponent } from "app/features/de-duplication/bulk-merge-records/bulk-merge-records.component";
 import { AlertService } from "app/core/alerts/alert.service";
 import { UnsavedChangesService } from "app/core/entity-details/form/unsaved-changes.service";
+import {
+  CascadingActionResult,
+  CascadingEntityAction,
+} from "app/core/entity/entity-actions/cascading-entity-action";
+import { EntitySchemaService } from "app/core/entity/schema/entity-schema.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class BulkMergeService {
+export class BulkMergeService extends CascadingEntityAction {
   constructor(
-    private entityMapper: EntityMapperService,
+    protected override entityMapper: EntityMapperService,
+    protected override schemaService: EntitySchemaService,
     private matDialog: MatDialog,
     private alert: AlertService,
     private unsavedChangesService: UnsavedChangesService,
-  ) {}
+  ) {
+    super(entityMapper, schemaService);
+  }
 
   /**
    * Opens the merge popup with the selected entities details.
@@ -28,6 +36,9 @@ export class BulkMergeService {
     entitiesToMerge: E[],
     entityType: EntityConstructor,
   ): Promise<void> {
+    for (let e of entitiesToMerge) {
+      this.getrelatedEntities(e);
+    }
     const dialogRef = this.matDialog.open(BulkMergeRecordsComponent, {
       maxHeight: "90vh",
       data: { entityConstructor: entityType, entitiesToMerge: entitiesToMerge },
@@ -40,6 +51,17 @@ export class BulkMergeService {
       this.unsavedChangesService.pending = false;
       this.alert.addInfo($localize`Records merged successfully.`);
     }
+  }
+
+  async getrelatedEntities(entity: Entity): Promise<CascadingActionResult> {
+    console.log(entity, "test");
+    const cascadeResult = await this.cascadeActionToRelatedEntities(
+      entity,
+      (e) => this.getrelatedEntities(e),
+      (e, refField, entity) => this.getrelatedEntities(e), //need to add method which will update the reference entity id with merged id
+    );
+
+    return new CascadingActionResult([entity]).mergeResults(cascadeResult);
   }
 
   /**
