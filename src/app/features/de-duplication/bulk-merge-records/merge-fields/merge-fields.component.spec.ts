2cdbb9b4fb4771cd756e80e5cfe29c5db13b73ec
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-03-25 18:47:18 +0530

    refactor: split merge components to simplify code (#2917)

---
+++ src/app/features/de-duplication/bulk-merge-records/merge-fields/merge-fields.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MergeFieldsComponent } from "./merge-fields.component";
+
+describe("MergeFieldsComponent", () => {
+  let component: MergeFieldsComponent;
+  let fixture: ComponentFixture<MergeFieldsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MergeFieldsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MergeFieldsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
