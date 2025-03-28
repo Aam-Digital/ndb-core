a4a716b612d7a03665086eb3b83c1a7044cbaa92
Abhinegi2 <negiabhishek253@gmail.com>
2025-03-28 16:17:40 +0530

    cleanups: remove unused imports

--- src/app/core/entity/display-entity-type/display-entity-type.component.ts
+++ src/app/core/entity/display-entity-type/display-entity-type.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { EntityTypeLabelPipe } from "app/core/common-components/entity-type-label/entity-type-label.pipe";
 import { ViewDirective } from "../default-datatype/view.directive";
 import { asArray } from "app/utils/asArray";
-import { CommonModule, NgFor } from "@angular/common";
 
 @Component({
   selector: "app-display-entity-type",
   standalone: true,
-  imports: [EntityTypeLabelPipe, CommonModule],
+  imports: [EntityTypeLabelPipe],
   templateUrl: "./display-entity-type.component.html",
   styleUrls: ["./display-entity-type.component.scss"],
 })
