481b615c39d10d262bca8a061890e09fee10b8dd
Abhinegi2 <negiabhishek253@gmail.com>
2025-04-01 11:08:32 +0530

    cleanups

--- src/app/core/entity/display-entity-type/display-entity-type.component.ts
+++ src/app/core/entity/display-entity-type/display-entity-type.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { EntityTypeLabelPipe } from "app/core/common-components/entity-type-label/entity-type-label.pipe";
 import { ViewDirective } from "../default-datatype/view.directive";
 import { asArray } from "app/utils/asArray";
-import { NgFor, NgIf } from "@angular/common";
 
 @Component({
   selector: "app-display-entity-type",
   standalone: true,
-  imports: [EntityTypeLabelPipe, NgIf, NgFor],
+  imports: [EntityTypeLabelPipe],
   templateUrl: "./display-entity-type.component.html",
   styleUrls: ["./display-entity-type.component.scss"],
 })
