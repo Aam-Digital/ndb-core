489a2319776aa2af7e184244ee783bd6422a0583
Sebastian Leidig <sebastian@aam-digital.com>
2025-04-02 11:36:45 +0200

    refactor old code

--- src/app/child-dev-project/notes/notes-related-to-entity/notes-related-to-entity.component.ts
+++ src/app/child-dev-project/notes/notes-related-to-entity/notes-related-to-entity.component.ts
@@ -71,23 +71,9 @@ export class NotesRelatedToEntityComponent
   override createNewRecordFactory() {
     return () => {
       const newNote = super.createNewRecordFactory()();
-      //TODO: generalize this code - possibly by only using relatedEntities to link other records here? see #1501
-      if (this.entity.getType() === ChildSchoolRelation.ENTITY_TYPE) {
-        for (const childId of asArray(
-          (this.entity as ChildSchoolRelation).childId,
-        )) {
-          if (childId) {
-            newNote.addChild(childId);
-          }
-        }
 
-        for (const schooldId of asArray(
-          (this.entity as ChildSchoolRelation).schoolId,
-        )) {
-          if (schooldId) {
-            newNote.addSchool(schooldId);
-          }
-        }
+      if (this.entity.getType() === ChildSchoolRelation.ENTITY_TYPE) {
+        this.specialLinkingForChildSchoolRelation(newNote);
       }
 
       for (const e of [
@@ -101,6 +87,25 @@ export class NotesRelatedToEntityComponent
 
       return newNote;
     };
+  }
+
+  private specialLinkingForChildSchoolRelation(newNote: Note) {
+    //TODO: generalize this code - possibly by only using relatedEntities to link other records here? see #1501
+    for (const childId of asArray(
+      (this.entity as ChildSchoolRelation).childId,
+    )) {
+      if (childId) {
+        newNote.addChild(childId);
+      }
+    }
+
+    for (const schooldId of asArray(
+      (this.entity as ChildSchoolRelation).schoolId,
+    )) {
+      if (schooldId) {
+        newNote.addSchool(schooldId);
+      }
+    }
   }
 
   /**
