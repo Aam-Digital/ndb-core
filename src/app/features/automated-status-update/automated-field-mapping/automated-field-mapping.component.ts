ede8fe00df26d8c917b3eeb08489c6d0e695911a
Abhinegi2 <negiabhishek253@gmail.com>
2025-04-04 11:49:23 +0530

    cleanups

--- src/app/features/automated-status-update/automated-field-mapping/automated-field-mapping.component.ts
+++ src/app/features/automated-status-update/automated-field-mapping/automated-field-mapping.component.ts
@@ -1,15 +1,17 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { EntityMapperService } from 'app/core/entity/entity-mapper/entity-mapper.service';
-import { Entity, EntityConstructor } from 'app/core/entity/model/entity';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { EntityMapperService } from "app/core/entity/entity-mapper/entity-mapper.service";
+import { Entity, EntityConstructor } from "app/core/entity/model/entity";
 
 @Component({
-  selector: 'app-automated-field-mapping',
+  selector: "app-automated-field-mapping",
   imports: [],
-  templateUrl: './automated-field-mapping.component.html',
-  styleUrl: './automated-field-mapping.component.scss'
+  templateUrl: "./automated-field-mapping.component.html",
+  styleUrl: "./automated-field-mapping.component.scss",
 })
-export class AutomatedFieldMappingComponent<E extends Entity> implements OnInit {
+export class AutomatedFieldMappingComponent<E extends Entity>
+  implements OnInit
+{
   entityConstructor: EntityConstructor;
   entitiesToSetAutomateRule: string;
   constructor(
@@ -21,13 +23,14 @@ export class AutomatedFieldMappingComponent<E extends Entity> implements OnInit 
     // private entityFormService: EntityFormService,
     private entityMapperService: EntityMapperService,
   ) {
-
     this.entitiesToSetAutomateRule = data.entity;
   }
 
- async ngOnInit(): Promise<void> {
-    console.log( await this.entityMapperService.loadType( this.entitiesToSetAutomateRule),"okay");
+  async ngOnInit(): Promise<void> {
+    console.log(
+      await this.entityMapperService.loadType(this.entitiesToSetAutomateRule),
+      "okay",
+    );
     // Initialization logic here
   }
-
 }
