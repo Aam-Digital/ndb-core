0a010173eeb118bf8aa8020f972500f229dc7a38
dvdaruri <81395465+dvdaruri@users.noreply.github.com>
2025-04-14 21:30:23 -0400

    Changing it back, accidentally edited master

--- src/app/core/entity-details/entity-details/entity-details.component.ts
+++ src/app/core/entity-details/entity-details/entity-details.component.ts
@@ -19,95 +19,19 @@ import { RouteTarget } from "../../../route-target";
 import { AbstractEntityDetailsComponent } from "../abstract-entity-details/abstract-entity-details.component";
 import { ViewActionsComponent } from "../../common-components/view-actions/view-actions.component";
 import { AblePurePipe } from "@casl/angular";
-import { EntityAbility } from "../../permissions/ability/entity-ability";
-import { Router } from "@angular/router";
-import { EntityRegistry } from "../../entity/database-entity.decorator";
-import { UnsavedChangesService } from "../form/unsaved-changes.service";
-import { EntityMapperService } from "../../entity/entity-mapper/entity-mapper.service";
-import { SessionSubject } from "../../session/auth/session-info";
+
 /**
  * This component can be used to display an entity in more detail.
  * It groups subcomponents in panels.
- * Any component that is registered (has the `DynamicComponent` decorator) can be used as a subcomponent.
- * The subcomponents will be provided with the Entity object and the creating new status, as well as its static config.
- */
-@RouteTarget("EntityDetails")
-@UntilDestroy()
-@Component({
-  selector: "app-entity-details",
-  templateUrl: "./entity-details.component.html",
-  styleUrls: ["./entity-details.component.scss"],
-  imports: [
-    AblePurePipe,
-    MatButtonModule,
-    MatMenuModule,
-    FontAwesomeModule,
-    Angulartics2OnModule,
-    MatTabsModule,
-    TabStateModule,
-    MatTooltipModule,
-    MatProgressBarModule,
-    NgIf,
-    NgForOf,
-    ViewTitleComponent,
-    DynamicComponentDirective,
-    EntityActionsMenuComponent,
-    EntityArchivedInfoComponent,
-    RouterLink,
-    CommonModule,
-    ViewActionsComponent,
-  ],
-})
-export class EntityDetailsComponent
-  extends AbstractEntityDetailsComponent
-  implements OnChanges
-{
-  /**
-   * The configuration for the panels on this details page.
+	@@ -62,6 +67,16 @@ export class EntityDetailsComponent
    */
   @Input() panels: Panel[] = [];
 
-  constructor(
-    entityMapperService: EntityMapperService,
-    entities: EntityRegistry,
-    ability: EntityAbility,
-    router: Router,
-    unsavedChanges: UnsavedChangesService,
-    private session: SessionSubject,
-  ) {
-    super(entityMapperService, entities, ability, router, unsavedChanges);
-  }
   override async ngOnChanges(changes: SimpleChanges) {
     await super.ngOnChanges(changes);
 
-    if (changes.id || changes.entity || changes.panels) {
-      this.initPanels();
-    }
-  }
-
-  private initPanels() {
-    this.panels = this.panels.map((p) => ({
-      title: p.title,
-      components: p.components.map((c) => ({
-        title: c.title,
-        component: c.component,
-        config: this.getPanelConfig(c),
-      })),
+	@@ -81,6 +96,20 @@ export class EntityDetailsComponent
     }));
-  }
-
-  /**
-   * Check if the current user has the required roles for this component
-   * @param config The component configuration to check
-   * @returns true if the user has at least one of the required roles or if no roles are required
-   */
-  private hasRequiredRole(config: any): boolean {
-    if (config?.permittedUserRoles?.length > 0) {
-      const userRoles = this.session.value.roles;
-      return config.permittedUserRoles.some((role) => userRoles.includes(role));
-    } else {
-      return true;
-    }
   }
 
   private getPanelConfig(c: PanelComponent): PanelConfig {
