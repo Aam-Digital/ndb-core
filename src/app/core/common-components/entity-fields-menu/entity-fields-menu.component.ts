bd8cbffd7c0e0a0792c1b87d471271129b8132ca
Abhinegi2 <negiabhishek253@gmail.com>
2025-04-26 17:07:44 +0530

    Refactor entity-fields-menu component to remove unused code and improve field selection functionality

--- src/app/core/common-components/entity-fields-menu/entity-fields-menu.component.ts
+++ src/app/core/common-components/entity-fields-menu/entity-fields-menu.component.ts
@@ -2,26 +2,18 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnChanges,
+  OnInit,
   Optional,
   Output,
-  SimpleChanges,
 } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { EntityFieldLabelComponent } from "../entity-field-label/entity-field-label.component";
 import { EntityConstructor } from "../../entity/model/entity";
-import {
-  ColumnConfig,
-  FormFieldConfig,
-  toFormFieldConfig,
-} from "../entity-form/FormConfig";
+import { ColumnConfig, toFormFieldConfig } from "../entity-form/FormConfig";
 import { EntityFormService } from "../entity-form/entity-form.service";
-import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { BasicAutocompleteComponent } from "../basic-autocomplete/basic-autocomplete.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
 
@@ -29,11 +21,9 @@ import { MatFormFieldModule } from "@angular/material/form-field";
   selector: "app-entity-fields-menu",
   imports: [
     CommonModule,
-    FaIconComponent,
     MatMenuModule,
     MatButtonModule,
     MatTooltipModule,
-    EntityFieldLabelComponent,
     BasicAutocompleteComponent,
     MatFormFieldModule,
     ReactiveFormsModule,
@@ -41,11 +31,9 @@ import { MatFormFieldModule } from "@angular/material/form-field";
   templateUrl: "./entity-fields-menu.component.html",
   styleUrl: "./entity-fields-menu.component.scss",
 })
-export class EntityFieldsMenuComponent implements OnChanges {
-  @Input() icon: IconProp = "search";
+export class EntityFieldsMenuComponent implements OnInit {
   @Input() entityType: EntityConstructor;
   @Input() activeFields: string[] = [];
-
   @Input() set availableFields(value: ColumnConfig[]) {
     this._availableFields = value
       .map((field) => {
@@ -69,26 +57,20 @@ export class EntityFieldsMenuComponent implements OnChanges {
   }
   _availableFields: SimpleDropdownValue[];
 
-  @Output() activeFieldsChange = new EventEmitter<string[]>();
-
-  selectedFieldsControl = new FormControl<FormFieldConfig[]>([]);
+  @Output() activeFieldsChange = new EventEmitter<any[]>();
+  selectedFieldsControl = new FormControl<any[]>([]);
 
   constructor(@Optional() private entityFormService: EntityFormService) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    //     if (changes.activeFields || changes._availableFields) {
-    //       const selectedConfigs = this.activeFields
-    //         .map((id) => this._availableFields.find((f) => f.key === id))
-    //         .filter((f) => !!f);
-    //       this.selectedFieldsControl.setValue(selectedConfigs as any as FormFieldConfig[], {
-    //         emitEvent: false,
-    //       });
-    //       console.log(this.selectedFieldsControl,"selectedFieldsControl");
-    //       this.selectedFieldsControl.valueChanges.subscribe((selectedConfigs) => {
-    // console.log(selectedConfigs,"selectedConfigs");
-    //         this.activeFieldsChange.emit(selectedConfigs);
-    //       });
-    //     }
+  ngOnInit() {
+    this.selectedFieldsControl.setValue(this.activeFields);
+    this.selectedFieldsControl.valueChanges.subscribe((value) => {
+      const newValues = value.filter((v) => !this.activeFields.includes(v));
+      if (newValues.length > 0) {
+        this.activeFields = [...this.activeFields, ...newValues];
+        console.log("this.activeFields", this.activeFields);
+        this.activeFieldsChange.emit(this.activeFields);
+      }
+    });
   }
   objectToLabel = (v: SimpleDropdownValue) => v?.label;
   objectToValue = (v: SimpleDropdownValue) => v?.key;
