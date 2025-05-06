77f985c04352ce01cd0ede5a8c05574a9d52459b
Sebastian Leidig <sebastian@aam-digital.com>
2025-05-06 23:34:37 +0200

    fix lint

--- src/app/core/default-values/x-dynamic-placeholder/admin-default-value-dynamic/admin-default-value-dynamic.component.spec.ts
+++ src/app/core/default-values/x-dynamic-placeholder/admin-default-value-dynamic/admin-default-value-dynamic.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AdminDefaultValueDynamicComponent } from './admin-default-value-dynamic.component';
+import { AdminDefaultValueDynamicComponent } from "./admin-default-value-dynamic.component";
 
-describe('AdminDefaultValueDynamicComponent', () => {
+describe("AdminDefaultValueDynamicComponent", () => {
   let component: AdminDefaultValueDynamicComponent;
   let fixture: ComponentFixture<AdminDefaultValueDynamicComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AdminDefaultValueDynamicComponent]
-    })
-    .compileComponents();
+      imports: [AdminDefaultValueDynamicComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdminDefaultValueDynamicComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
