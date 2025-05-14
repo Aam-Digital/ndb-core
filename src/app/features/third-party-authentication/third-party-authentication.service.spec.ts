02b56d3013f54a3f375d4b1c9bf17eb612665163
Sebastian Leidig <sebastian@aam-digital.com>
2025-05-14 14:05:49 +0200

    fix tests

--- src/app/features/third-party-authentication/third-party-authentication.service.spec.ts
+++ src/app/features/third-party-authentication/third-party-authentication.service.spec.ts
@@ -1,16 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { ThirdPartyAuthenticationService } from './third-party-authentication.service';
+import { ThirdPartyAuthenticationService } from "./third-party-authentication.service";
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from "@angular/common/http/testing";
+import { HttpClient, provideHttpClient } from "@angular/common/http";
 
-describe('ThirdPartyAuthenticationService', () => {
+describe("ThirdPartyAuthenticationService", () => {
   let service: ThirdPartyAuthenticationService;
 
+  let httpTesting: HttpTestingController;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ThirdPartyAuthenticationService);
+
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should not make API request if no session was found", async () => {
+    spyOn(localStorage, "getItem").and.returnValue(null);
+    TestBed.inject(HttpClient);
+
+    httpTesting.expectNone(() => true);
+    const result = await service.getRedirectUrl();
+
+    expect(result).toBeUndefined();
   });
 });
