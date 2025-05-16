ed3ea24e7f8488872d6d946e78d03cd7a5a6f5d7
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-05-16 10:26:38 +0200

    fix: Fix lint warnings and fail on warnings on CI

--- src/app/core/ui/routed-view/routed-view.component.spec.ts
+++ src/app/core/ui/routed-view/routed-view.component.spec.ts
@@ -13,6 +13,7 @@ import { Component } from "@angular/core";
 
 @Component({
   template: ``,
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
   standalone: false,
 })
 class MockComponent {}
