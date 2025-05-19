bcd0f27e0d4bdfed3aea9701435031a848e5c9ee
Sebastian <sebastian@aam-digital.com>
2025-05-19 21:48:01 +0200

    fix: set sentry logging environment (#3010)
    see #3009

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
-/*
- *     This file is part of ndb-core.
- *
- *     ndb-core is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     ndb-core is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import { environment } from "./environments/environment";
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
@@ -30,6 +13,7 @@ if (environment.production) {
 // Initialize remote logging
 Logging.initRemoteLogging({
   dsn: environment.remoteLoggingDsn,
+  environment: environment.production ? "production" : "development",
 });
 
 // Listening to event as soon as possible
