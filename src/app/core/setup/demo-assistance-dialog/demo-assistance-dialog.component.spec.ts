74c0f2c7a40f8bbd61d2e0c54134f0ed2e768861
Abhinegi2 <negiabhishek253@gmail.com>
2025-06-02 15:04:58 +0530

    lint fixes

--- src/app/core/setup/demo-assistance-dialog/demo-assistance-dialog.component.spec.ts
+++ src/app/core/setup/demo-assistance-dialog/demo-assistance-dialog.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DemoAssistanceDialogComponent } from './demo-assistance-dialog.component';
+import { DemoAssistanceDialogComponent } from "./demo-assistance-dialog.component";
 
-describe('DemoAssistanceDialogComponent', () => {
+describe("DemoAssistanceDialogComponent", () => {
   let component: DemoAssistanceDialogComponent;
   let fixture: ComponentFixture<DemoAssistanceDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DemoAssistanceDialogComponent]
-    })
-    .compileComponents();
+      imports: [DemoAssistanceDialogComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DemoAssistanceDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
