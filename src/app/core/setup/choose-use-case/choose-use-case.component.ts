a1132d807d2b500650714c6c887bdf16a0277073
Abhinegi2 <negiabhishek253@gmail.com>
2025-06-05 17:41:40 +0530

    use case selection with locale-based filtering and improve locale handling in demo use cases

--- src/app/core/setup/choose-use-case/choose-use-case.component.ts
+++ src/app/core/setup/choose-use-case/choose-use-case.component.ts
@@ -1,6 +1,11 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, Inject } from "@angular/core";
 import { BaseConfig } from "../base-config";
 import { MatSelectModule } from "@angular/material/select";
+import { WINDOW_TOKEN } from "app/utils/di-tokens";
+import {
+  DEFAULT_LANGUAGE,
+  LANGUAGE_LOCAL_STORAGE_KEY,
+} from "app/core/language/language-statics";
 
 @Component({
   selector: "app-choose-use-case",
@@ -9,12 +14,43 @@ import { MatSelectModule } from "@angular/material/select";
   styleUrls: ["./choose-use-case.component.scss"],
 })
 export class ChooseUseCaseComponent {
-  @Input() demoUseCases: BaseConfig[] = [];
+  private _demoUseCases: BaseConfig[] = [];
+
+  @Input()
+  set demoUseCases(useCases: BaseConfig[]) {
+    this._demoUseCases = useCases || [];
+    this.sortedUseCases = this._demoUseCases.sort((a, b) => {
+      if (a.locale === this.locale && b.locale !== this.locale) return -1;
+      if (a.locale !== this.locale && b.locale === this.locale) return 1;
+      return 0;
+    });
+  }
+
+  get demoUseCases(): BaseConfig[] {
+    return this._demoUseCases;
+  }
+
+  sortedUseCases: BaseConfig[] = [];
+
   @Output() selectionChanged = new EventEmitter<BaseConfig>();
 
   selectedUseCase: BaseConfig;
+  locale: string;
+
+  constructor(@Inject(WINDOW_TOKEN) private window: Window) {
+    this.locale =
+      this.window.localStorage.getItem(LANGUAGE_LOCAL_STORAGE_KEY) ||
+      DEFAULT_LANGUAGE;
+  }
 
   onSelectionChange() {
+    if (this.selectedUseCase?.locale) {
+      this.window.localStorage.setItem(
+        LANGUAGE_LOCAL_STORAGE_KEY,
+        this.selectedUseCase.locale,
+      );
+    }
+
     this.selectionChanged.emit(this.selectedUseCase);
   }
 }
