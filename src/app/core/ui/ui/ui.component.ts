51400eb02603bd5a6e1dbd4889548eb60389e55e
Sebastian Leidig <sebastian@aam-digital.com>
2025-06-16 18:37:00 +0200

    fix multiple opens of assistant

--- src/app/core/ui/ui/ui.component.ts
+++ src/app/core/ui/ui/ui.component.ts
@@ -15,7 +15,7 @@
  *     along with ndb-core.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { Component, signal, ViewChild } from "@angular/core";
+import { Component, Signal, signal, ViewChild } from "@angular/core";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { MatDrawerMode, MatSidenavModule } from "@angular/material/sidenav";
 import { ScreenWidthObserver } from "../../../utils/media/screen-size-observer.service";
@@ -45,8 +45,11 @@ import { NotificationComponent } from "../../../features/notification/notificati
 import { GotoThirdPartySystemComponent } from "../../../features/third-party-authentication/goto-third-party-system/goto-third-party-system.component";
 import { SetupService } from "app/core/setup/setup.service";
 import { AssistantButtonComponent } from "../../setup/assistant-button/assistant-button.component";
-import { filter } from "rxjs/operators";
+import { filter, map } from "rxjs/operators";
 import { BehaviorSubject } from "rxjs";
+import { LoginStateSubject } from "../../session/session-type";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { LoginState } from "../../session/session-states/login-state.enum";
 
 /**
  * The main user interface component as root element for the app structure
@@ -90,6 +93,12 @@ export class UiComponent {
   /** latest version of the site settings*/
   siteSettings = new SiteSettings();
   isDesktop = false;
+  isLoggedIn: Signal<boolean> = toSignal(
+    this.loginState.pipe(
+      map((loginState) => loginState === LoginState.LOGGED_IN),
+    ),
+    { initialValue: false },
+  );
 
   configReady$ = new BehaviorSubject<boolean>(false);
   showPrimaryAction = signal(false);
@@ -100,6 +109,7 @@ export class UiComponent {
     private sessionManager: SessionManagerService,
     private setupService: SetupService,
     private router: Router,
+    private loginState: LoginStateSubject,
   ) {
     this.screenWidthObserver
       .platform()
