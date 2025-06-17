010da2a4350ad55aee3a3152a6452bf40f621298
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2025-06-17 15:22:17 +0530

    Update admin-section-header.component.ts

--- src/app/core/admin/building-blocks/admin-section-header/admin-section-header.component.ts
+++ src/app/core/admin/building-blocks/admin-section-header/admin-section-header.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
