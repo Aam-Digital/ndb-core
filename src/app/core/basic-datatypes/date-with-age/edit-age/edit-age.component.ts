e9efc650557e941c58d8d9ff3b1bf44bcb00a146
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2025-06-17 15:16:37 +0530

    Update edit-age.component.ts

--- src/app/core/basic-datatypes/date-with-age/edit-age/edit-age.component.ts
+++ src/app/core/basic-datatypes/date-with-age/edit-age/edit-age.component.ts
@@ -10,7 +10,6 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { ErrorHintComponent } from "../../../common-components/error-hint/error-hint.component";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { DateWithAge } from "../dateWithAge";
 
