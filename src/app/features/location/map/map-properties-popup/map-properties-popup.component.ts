72cf481fb1c5c90b888c257d6c1bccb437a4265e
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2025-06-17 13:24:44 +0530

    Update map-properties-popup.component.ts

--- src/app/features/location/map/map-properties-popup/map-properties-popup.component.ts
+++ src/app/features/location/map/map-properties-popup/map-properties-popup.component.ts
@@ -7,7 +7,6 @@ import {
 import { DialogCloseComponent } from "../../../../core/common-components/dialog-close/dialog-close.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
-
 import { EntityConstructor } from "../../../../core/entity/model/entity";
 import { EntityRegistry } from "../../../../core/entity/database-entity.decorator";
 import { getLocationProperties } from "../../map-utils";
