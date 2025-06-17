4b80cb34a1486695af352be2f4009c9d2555b920
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2025-06-17 13:15:53 +0530

    Update select-report.component.ts

--- src/app/features/reporting/reporting/select-report/select-report.component.ts
+++ src/app/features/reporting/reporting/select-report/select-report.component.ts
@@ -6,7 +6,6 @@ import {
   Output,
   SimpleChanges,
 } from "@angular/core";
-
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
