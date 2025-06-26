6e515d6778bd61824104b29a7cfb5b147693c221
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-06-26 18:45:05 +0200

    fix: Stable sorting for groups in Entity Count dashboard widget
    Fixes #1068

--- src/app/features/dashboard-widgets/entity-count-dashboard-widget/entity-count-dashboard/entity-count-dashboard.component.spec.ts
+++ src/app/features/dashboard-widgets/entity-count-dashboard-widget/entity-count-dashboard/entity-count-dashboard.component.spec.ts
@@ -12,6 +12,8 @@ import { Note } from "../../../../child-dev-project/notes/model/note";
 import { TestEntity } from "../../../../utils/test-utils/TestEntity";
 import { Entity } from "../../../../core/entity/model/entity";
 import { ConfigurableEnumValue } from "app/core/basic-datatypes/configurable-enum/configurable-enum.types";
+import { ConfigurableEnum } from "app/core/basic-datatypes/configurable-enum/configurable-enum";
+import { ConfigurableEnumService } from "app/core/basic-datatypes/configurable-enum/configurable-enum.service";
 
 describe("EntityCountDashboardComponent", () => {
   let component: EntityCountDashboardComponent;
@@ -28,7 +30,10 @@ describe("EntityCountDashboardComponent", () => {
     entityMapper = mockEntityMapper();
     await TestBed.configureTestingModule({
       imports: [EntityCountDashboardComponent, MockedTestingModule.withState()],
-      providers: [{ provide: EntityMapperService, useValue: entityMapper }],
+      providers: [
+        { provide: EntityMapperService, useValue: entityMapper },
+        { provide: ConfigurableEnumService, useClass: ConfigurableEnumService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EntityCountDashboardComponent);
@@ -54,51 +59,21 @@ describe("EntityCountDashboardComponent", () => {
     expect(component.totalEntities).toBe(3);
   });
 
-  it("should calculate children per center correctly", async () => {
-    const centerA = { id: "a", label: "CenterA" };
-    const centerB = { id: "b", label: "CenterB" };
-    entityMapper.add(createChild(centerA));
-    entityMapper.add(createChild(centerB));
-    entityMapper.add(createChild(centerA));
-
-    await component.ngOnInit();
-
-    const currentlyShownGroupCounts =
-      component.entityGroupCounts[
-        component.groupBy[component.currentGroupIndex]
-      ];
-    expect(currentlyShownGroupCounts.length)
-      .withContext("unexpected number of centersWithProbability")
-      .toBe(2);
-
-    const actualCenterAEntry = currentlyShownGroupCounts.filter(
-      (e) => e.label === centerA.label,
-    )[0];
-    expect(actualCenterAEntry.value)
-      .withContext("child count of CenterA not correct")
-      .toBe(2);
-
-    const actualCenterBEntry = currentlyShownGroupCounts.filter(
-      (e) => e.label === centerB.label,
-    )[0];
-    expect(actualCenterBEntry.value)
-      .withContext("child count of CenterB not correct")
-      .toBe(1);
-  });
-
   it("should groupBy enum values and display label", async () => {
-    const testGroupBy = "test";
-    TestEntity.schema.set(testGroupBy, { dataType: "configurable-enum" });
-    component.groupBy = [testGroupBy];
-
-    const children = [
-      new TestEntity(),
-      new TestEntity(),
-      new TestEntity(),
-      new TestEntity(),
-    ];
     const c1: ConfigurableEnumValue = { label: "foo", id: "01" };
     const c2: ConfigurableEnumValue = { label: "bar", id: "02" };
+    const c3: ConfigurableEnumValue = { label: "qux", id: "03" };
+    const configurableEnum = new ConfigurableEnum("testEnum", [c1, c2, c3]);
+    entityMapper.add(configurableEnum);
+
+    const testGroupBy = "test";
+    TestEntity.schema.set(testGroupBy, {
+      dataType: "configurable-enum",
+      additional: "testEnum",
+    });
+    component.groupBy = [testGroupBy];
+
+    const children = [new TestEntity(), new TestEntity(), new TestEntity()];
     children[0][testGroupBy] = c1;
     children[1][testGroupBy] = c2;
     children[2][testGroupBy] = c1;
@@ -106,24 +81,80 @@ describe("EntityCountDashboardComponent", () => {
 
     await component.ngOnInit();
 
-    const currentlyShownGroupCounts =
+    const groupCounts =
       component.entityGroupCounts[
         component.groupBy[component.currentGroupIndex]
       ];
 
-    expect(currentlyShownGroupCounts).toHaveSize(3);
-    expect(currentlyShownGroupCounts).toContain({
-      label: c1.label,
-      value: 2,
-      id: c1.id,
-      groupedByEntity: undefined,
+    expect(groupCounts).toEqual([
+      {
+        label: c1.label,
+        value: 2,
+        id: c1.id,
+        groupedByEntity: undefined,
+      },
+      {
+        label: c2.label,
+        value: 1,
+        id: c2.id,
+        groupedByEntity: undefined,
+      },
+    ]);
+
+    configurableEnum.values.reverse();
+    await entityMapper.save(configurableEnum);
+    await component.ngOnInit();
+
+    const groupCountsReversed =
+      component.entityGroupCounts[
+        component.groupBy[component.currentGroupIndex]
+      ];
+
+    expect(groupCountsReversed.map((count) => count.label)).toEqual([
+      c2.label,
+      c1.label,
+    ]);
+
+    TestEntity.schema.delete(testGroupBy);
+  });
+
+  it("inlcudes a row for entities with missing configurable enum value", async () => {
+    const c1: ConfigurableEnumValue = { label: "foo", id: "01" };
+    const configurableEnum = new ConfigurableEnum("testEnum", [c1]);
+    entityMapper.add(configurableEnum);
+
+    const testGroupBy = "test";
+    TestEntity.schema.set(testGroupBy, {
+      dataType: "configurable-enum",
+      additional: "testEnum",
     });
-    expect(currentlyShownGroupCounts).toContain({
-      label: c2.label,
-      value: 1,
-      id: c2.id,
-      groupedByEntity: undefined,
-    });
+    component.groupBy = [testGroupBy];
+
+    const children = [new TestEntity(), new TestEntity(), new TestEntity()];
+    children[2][testGroupBy] = c1;
+    entityMapper.addAll(children);
+
+    await component.ngOnInit();
+
+    const groupCounts =
+      component.entityGroupCounts[
+        component.groupBy[component.currentGroupIndex]
+      ];
+
+    expect(groupCounts).toEqual([
+      {
+        label: "",
+        value: 2,
+        id: "",
+        groupedByEntity: undefined,
+      },
+      {
+        label: "foo",
+        value: 1,
+        id: "01",
+        groupedByEntity: undefined,
+      },
+    ]);
 
     TestEntity.schema.delete(testGroupBy);
   });
