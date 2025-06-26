7d0a96bdd63e764d5df9abef249d7bc4cd5ee18a
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-06-26 21:02:24 +0530

    feat(Admin UI): fields in Public Forms can overwrite details like labels just for that form  (#3094)
    closes #2872
    
    ---------
    
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/features/public-form/edit-public-form-columns/edit-public-form-columns.component.ts
+++ src/app/features/public-form/edit-public-form-columns/edit-public-form-columns.component.ts
@@ -7,8 +7,6 @@ import { EntityRegistry } from "app/core/entity/database-entity.decorator";
 import { AdminEntityFormComponent } from "app/core/admin/admin-entity-details/admin-entity-form/admin-entity-form.component";
 import { FormConfig } from "app/core/entity-details/form/form.component";
 import { FieldGroup } from "app/core/entity-details/form/field-group";
-import { AdminEntityService } from "app/core/admin/admin-entity.service";
-import { EntitySchemaField } from "app/core/entity/schema/entity-schema-field";
 import { PublicFormConfig } from "../public-form-config";
 import { migratePublicFormConfig } from "../public-form.component";
 
@@ -25,12 +23,10 @@ export class EditPublicFormColumnsComponent
 {
   entityConstructor: EntityConstructor;
   formConfig: FormConfig;
-  private originalEntitySchemaFields: [string, EntitySchemaField][];
 
   private entities = inject(EntityRegistry);
-  private adminEntityService = inject(AdminEntityService);
 
-  override ngOnInit(): void {
+  override ngOnInit() {
     if (this.entity) {
       this.entityConstructor = this.entities.get(this.entity["entity"]);
 
@@ -43,22 +39,6 @@ export class EditPublicFormColumnsComponent
       this.formControl.valueChanges.subscribe(
         (v) => (this.formConfig = { fieldGroups: v }),
       );
-    }
-
-    this.originalEntitySchemaFields = JSON.parse(
-      JSON.stringify(Array.from(this.entityConstructor.schema.entries())),
-    );
-    if (this.entityForm) {
-      this.entityForm.onFormStateChange.subscribe((event) => {
-        if (event === "saved")
-          this.adminEntityService.setAndSaveEntityConfig(
-            this.entityConstructor,
-          );
-        if (event === "cancelled")
-          this.entityConstructor.schema = new Map(
-            this.originalEntitySchemaFields,
-          );
-      });
     }
   }
 
