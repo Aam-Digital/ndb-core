34231caa830fed36bb856cf90c65576686046b40
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-06-26 11:58:54 +0200

    refactor: Extract demo data generation into static functions
    This change will allow us to generate demo data without depending on DI.

--- src/app/child-dev-project/children/demo-data-generators/demo-child-generator.service.ts
+++ src/app/child-dev-project/children/demo-data-generators/demo-child-generator.service.ts
@@ -10,6 +10,7 @@ import { genders } from "../model/genders";
 import { calculateAge } from "../../../utils/utils";
 import { DateWithAge } from "../../../core/basic-datatypes/date-with-age/dateWithAge";
 import { createEntityOfType } from "../../../core/demo-data/create-entity-of-type";
+import { range } from "lodash-es";
 
 export class DemoChildConfig {
   count: number;
@@ -31,36 +32,48 @@ export class DemoChildGenerator extends DemoDataGenerator<Entity> {
     ];
   }
 
-  static generateEntity(id: string) {
-    const child = createEntityOfType("Child", id);
-    child.name = faker.person.firstName() + " " + faker.person.lastName();
-    child.projectNumber = id;
-    child.religion = faker.helpers.arrayElement(religions);
-    child.gender = faker.helpers.arrayElement(genders.slice(0, 2));
-    child.dateOfBirth = new DateWithAge(
-      faker.date.birthdate({ mode: "age", min: 5, max: 20 }),
-    );
-    child.motherTongue = faker.helpers.arrayElement(languages);
-    child.center = faker.helpers.arrayElement(centersWithProbability);
-    child.phone =
-      "+" +
-      faker.number.int({ min: 10, max: 99 }) +
-      " " +
-      faker.number.int({ min: 10000000, max: 99999999 });
-
-    child.admissionDate = faker.date.past({
-      years: calculateAge(child.dateOfBirth) - 4,
-    });
-
-    child["address"] = faker.geoAddress();
-
-    if (faker.number.int(100) > 90) {
-      DemoChildGenerator.makeChildDropout(child);
-    }
-    return child;
+  constructor(public config: DemoChildConfig) {
+    super();
   }
 
-  private static makeChildDropout(child: Entity & { [key: string]: any }) {
+  generateEntities(): Entity[] {
+    return generateChildren({ count: this.config.count });
+  }
+}
+
+function generateChildren(params: { count: number }): Entity[] {
+  return range(params.count).map((i) => generateChild(String(i + 1)));
+}
+
+/** @deprecated */
+export function generateChild(id: string): Entity;
+export function generateChild(): Entity;
+export function generateChild(id?: string): Entity {
+  id ??= faker.string.alphanumeric(20);
+
+  const child = createEntityOfType("Child", id);
+  child.name = faker.person.firstName() + " " + faker.person.lastName();
+  child.projectNumber = id;
+  child.religion = faker.helpers.arrayElement(religions);
+  child.gender = faker.helpers.arrayElement(genders.slice(0, 2));
+  child.dateOfBirth = new DateWithAge(
+    faker.date.birthdate({ mode: "age", min: 5, max: 20 }),
+  );
+  child.motherTongue = faker.helpers.arrayElement(languages);
+  child.center = faker.helpers.arrayElement(centersWithProbability);
+  child.phone =
+    "+" +
+    faker.number.int({ min: 10, max: 99 }) +
+    " " +
+    faker.number.int({ min: 10000000, max: 99999999 });
+
+  child.admissionDate = faker.date.past({
+    years: calculateAge(child.dateOfBirth) - 4,
+  });
+
+  child["address"] = faker.geoAddress();
+
+  if (faker.number.int(100) > 90) {
     child.dropoutDate = faker.date.between({
       from: child.admissionDate,
       to: faker.defaultRefDate(),
@@ -70,16 +83,5 @@ export class DemoChildGenerator extends DemoDataGenerator<Entity> {
     child.status = $localize`:Child status:Dropout`;
     child.inactive = true;
   }
-
-  constructor(public config: DemoChildConfig) {
-    super();
-  }
-
-  generateEntities(): Entity[] {
-    const data = [];
-    for (let i = 1; i <= this.config.count; i++) {
-      data.push(DemoChildGenerator.generateEntity(String(i)));
-    }
-    return data;
-  }
+  return child;
 }
