2b258e7e26e71719cba6aeb8dbbe9536d43b7ddf
Abhinegi2 <negiabhishek253@gmail.com>
2025-07-01 15:08:58 +0530

    cleanups

--- src/app/utils/reorderable-list.component.spec.ts
+++ src/app/utils/reorderable-list.component.spec.ts
@@ -3,8 +3,8 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReorderableListComponent } from "./reorderable-list.component";
 
 describe("ReorderableListComponent", () => {
-  let component: ReorderableListComponent;
-  let fixture: ComponentFixture<ReorderableListComponent>;
+  let component: ReorderableListComponent<any>;
+  let fixture: ComponentFixture<ReorderableListComponent<any>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
