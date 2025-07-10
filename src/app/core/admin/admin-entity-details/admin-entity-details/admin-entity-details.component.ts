5d7eeec0977a9db9b53e319e826cd85fa7c2a7dc
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-07-10 17:41:43 +0530

    feat(Admin UI): option to add new related-entities component in details view (#3134)
    closes #3101
    
    ---------
    
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/core/admin/admin-entity-details/admin-entity-details/admin-entity-details.component.ts
+++ src/app/core/admin/admin-entity-details/admin-entity-details/admin-entity-details.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import {
   EntityDetailsConfig,
   Panel,
+  PanelComponent,
 } from "../../../entity-details/EntityDetailsConfig";
 import { EntityConstructor } from "../../../entity/model/entity";
 import { DynamicComponent } from "../../../config/dynamic-components/dynamic-component.decorator";
@@ -16,6 +17,8 @@ import { AdminEntityPanelComponentComponent } from "../admin-entity-panel-compon
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { AdminTabsComponent } from "../../building-blocks/admin-tabs/admin-tabs.component";
 import { AdminTabTemplateDirective } from "../../building-blocks/admin-tabs/admin-tab-template.directive";
+import { MatDialog } from "@angular/material/dialog";
+import { WidgetComponentSelectComponent } from "#src/app/core/admin/admin-entity-details/widget-component-select/widget-component-select.component";
 
 @DynamicComponent("AdminEntityDetails")
 @Component({
@@ -44,15 +47,22 @@ export class AdminEntityDetailsComponent {
   @Input() entityConstructor: EntityConstructor;
   @Input() config: EntityDetailsConfig;
 
+  constructor(private dialog: MatDialog) {}
+
   newPanelFactory(): Panel {
     return { title: "New Tab", components: [] };
   }
 
   addComponent(panel: Panel) {
-    panel.components.push({
-      title: $localize`:Default title:New Section`,
-      component: "Form", // TODO: make this configurable
-      config: { fieldGroups: [] },
-    });
+    this.dialog
+      .open(WidgetComponentSelectComponent, {
+        data: { entityType: this.entityConstructor.ENTITY_TYPE },
+      })
+      .afterClosed()
+      .subscribe((sectionConfig: PanelComponent) => {
+        if (sectionConfig) {
+          panel.components.push(sectionConfig);
+        }
+      });
   }
 }
---
+++ src/app/core/admin/admin-entity-details/widget-component-select/widget-component-select.component.ts
@@ -0,0 +1,91 @@
+import { DialogCloseComponent } from "#src/app/core/common-components/dialog-close/dialog-close.component";
+import { PanelComponent } from "#src/app/core/entity-details/EntityDetailsConfig";
+import { EntityRelationsService } from "#src/app/core/entity/entity-mapper/entity-relations.service";
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTooltipModule } from "@angular/material/tooltip";
+
+/**
+ * Admin component to select components to be added to view configs.
+ */
+@Component({
+  selector: "app-widget-component-select",
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    DialogCloseComponent,
+    MatTooltipModule,
+  ],
+  templateUrl: "./widget-component-select.component.html",
+  styleUrl: "./widget-component-select.component.scss",
+})
+export class WidgetComponentSelectComponent implements OnInit {
+  options: WidgetOption[];
+
+  constructor(
+    private entityRelationsService: EntityRelationsService,
+    private dialogRef: MatDialogRef<
+      WidgetComponentSelectComponent,
+      PanelComponent
+    >,
+    @Inject(MAT_DIALOG_DATA) public data: { entityType: string },
+  ) {}
+
+  ngOnInit() {
+    this.options = this.loadAvailableWidgets();
+  }
+
+  private loadAvailableWidgets(): WidgetOption[] {
+    const relatedEntityTypes =
+      this.entityRelationsService.getEntityTypesReferencingType(
+        this.data.entityType,
+      );
+    const hasRelatedEntities = relatedEntityTypes?.length > 0;
+
+    return [
+      {
+        label: $localize`Form Fields Section`,
+        value: {
+          title: $localize`:Default title:New Section`,
+          component: "Form",
+          config: { fieldGroups: [] },
+        },
+      },
+      {
+        label: $localize`Related-Entity Section`,
+        value: {
+          title: $localize`:Default title:New Related Section`,
+          component: "RelatedEntities",
+          config: hasRelatedEntities
+            ? { entityType: relatedEntityTypes[0].entityType.ENTITY_TYPE }
+            : {},
+        },
+        disabled: !hasRelatedEntities
+          ? $localize`Add a field with type that links to this record to enable this section.`
+          : undefined,
+      },
+    ];
+  }
+
+  selectSectionType(opt: PanelComponent) {
+    this.dialogRef.close(opt);
+  }
+}
+
+export interface WidgetOption {
+  label: string;
+  value: PanelComponent;
+
+  /**
+   * If the option is not available in the current context, mark it as disabled
+   * by providing any string value describing the reason (displayed as tooltip).
+   */
+  disabled?: string;
+}
