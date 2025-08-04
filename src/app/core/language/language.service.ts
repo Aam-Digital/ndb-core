eb90224abb2481006f8ce1192514252d678c1eb5
Sebastian Leidig <sebastian@aam-digital.com>
2025-08-04 19:24:37 +0200

    refactor to reuse code

--- src/app/core/language/language.service.ts
+++ src/app/core/language/language.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, LOCALE_ID, inject } from "@angular/core";
+import { inject, Injectable, LOCALE_ID } from "@angular/core";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { LANGUAGE_LOCAL_STORAGE_KEY } from "./language-statics";
 import { WINDOW_TOKEN } from "../../utils/di-tokens";
@@ -6,6 +6,7 @@ import { SiteSettings } from "../site-settings/site-settings";
 import { EntityMapperService } from "../entity/entity-mapper/entity-mapper.service";
 import { SiteSettingsService } from "../site-settings/site-settings.service";
 import { filter } from "rxjs";
+import { UpdatedEntity } from "#src/app/core/entity/model/entity-update";
 
 /**
  * Service that provides the currently active locale and applies a newly selected one.
@@ -21,27 +22,38 @@ export class LanguageService {
   readonly entityMapper = inject(EntityMapperService);
 
   constructor() {
-    //Listen to SiteSettings updates and sync the default language in localStorage
+    this.switchLocaleOnSiteSettingsUpdate();
+  }
+
+  /**
+   * Listen to SiteSettings entity updates
+   * and trigger a switch to the new default language if it has changed.
+   * (Users often do not have a language switcher in the UI to control this otherwise)
+   * @private
+   */
+  private switchLocaleOnSiteSettingsUpdate() {
     this.entityMapper
       .receiveUpdates(SiteSettings)
       .pipe(
         untilDestroyed(this),
-        filter(
-          (update) => update?.entity.getId(true) === SiteSettings.ENTITY_ID,
-        ),
+        filter((u) => u?.entity.getId(true) === SiteSettings.ENTITY_ID),
       )
-      .subscribe((updatedSiteSettings) => {
-        const updatedLanguage =
-          updatedSiteSettings?.entity?.defaultLanguage?.id;
-        const currentLanguage = localStorage.getItem(
-          LANGUAGE_LOCAL_STORAGE_KEY,
-        );
-        if (updatedLanguage !== currentLanguage) {
-          // Override the language in localStorage and reload the app
-          localStorage.setItem(LANGUAGE_LOCAL_STORAGE_KEY, updatedLanguage);
-          this.window.location.reload();
-        }
+      .subscribe((updatedSettings: UpdatedEntity<SiteSettings>) => {
+        const updatedLanguage = updatedSettings?.entity?.defaultLanguage?.id;
+        this.switchLocale(updatedLanguage);
       });
+  }
+
+  /**
+   * Switch the current locale and persist it in localStorage.
+   * @param newLocale
+   */
+  switchLocale(newLocale: string): void {
+    const currentLocale = localStorage.getItem(LANGUAGE_LOCAL_STORAGE_KEY);
+    if (newLocale === currentLocale) return;
+
+    localStorage.setItem(LANGUAGE_LOCAL_STORAGE_KEY, newLocale);
+    this.window.location.reload();
   }
 
   initDefaultLanguage(): void {
