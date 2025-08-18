4cc7076aa41492a2fb0e18905ff6513ca5daaa70
Sebastian <sebastian@aam-digital.com>
2025-08-18 17:16:02 +0200

    Update src/app/core/common-components/entity-select/entity-select.component.ts

--- src/app/core/common-components/entity-select/entity-select.component.ts
+++ src/app/core/common-components/entity-select/entity-select.component.ts
@@ -167,9 +167,7 @@ export class EntitySelectComponent<
     const includeInactive = (entity: E) =>
       this.includeInactive || entity.isActive;
     const includeSelected = (entity: E) =>
-      asArray(this.form.value)
-        .filter((id) => id !== null && id !== undefined && id !== "")
-        .includes(entity.getId());
+      asArray(this.form.value).includes(entity.getId());
 
     const newAvailableEntities = this.allEntities.filter(
       (e) => includeInactive(e) || includeSelected(e),
