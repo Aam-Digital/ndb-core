4f4c03d05b00e734a03cc92f08a68396ee2880e2
Abhinegi2 <negiabhishek253@gmail.com>
2025-08-19 15:51:25 +0530

    updated test cases

--- src/app/core/basic-datatypes/string/edit-email/edit-email.component.spec.ts
+++ src/app/core/basic-datatypes/string/edit-email/edit-email.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { EditEmailComponent } from './edit-email.component';
+import { EditEmailComponent } from "./edit-email.component";
+import { setupEditComponent } from "app/core/entity/default-datatype/edit-component.spec";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-describe('EditEmailComponent', () => {
+describe("EditEmailComponent", () => {
   let component: EditEmailComponent;
   let fixture: ComponentFixture<EditEmailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditEmailComponent]
-    })
-    .compileComponents();
+      imports: [EditEmailComponent, NoopAnimationsModule],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(EditEmailComponent);
     component = fixture.componentInstance;
+    setupEditComponent(component);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should validate a proper email as valid", () => {
+    component.formControl.setValue("test@example.com");
+    fixture.detectChanges();
+
+    expect(component.formControl.valid).toBeTrue();
+    expect(component.formControl.hasError("email")).toBeFalse();
+  });
+
+  it("should mark invalid email as invalid", () => {
+    component.formControl.setValue("invalid-email");
+    fixture.detectChanges();
+
+    expect(component.formControl.valid).toBeFalse();
+    expect(component.formControl.hasError("email")).toBeTrue();
   });
 });
