5a21ff1475d0f83e1e3f2a7b6359fd8658b939d1
Abhinegi2 <negiabhishek253@gmail.com>
2025-08-21 12:46:57 +0530

    updated test

--- src/app/features/email-client/email-client.service.spec.ts
+++ src/app/features/email-client/email-client.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from "@angular/core/testing";
 
 import { EmailClientService } from "./email-client.service";
+import {
+  entityRegistry,
+  EntityRegistry,
+} from "#src/app/core/entity/database-entity.decorator";
 
 describe("EmailClientService", () => {
   let service: EmailClientService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: EntityRegistry, useValue: entityRegistry }],
+    });
     service = TestBed.inject(EmailClientService);
   });
 
