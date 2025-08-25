d59a0bf2654c1c9314315ca4cae9bd0d2b16ea32
Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>
2025-08-25 10:54:03 +0530

    Update src/app/features/email-client/email-client.service.ts
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/app/features/email-client/email-client.service.ts
+++ src/app/features/email-client/email-client.service.ts
@@ -11,6 +11,8 @@ export class EmailClientService {
 
   /**
    * Build a mailto link from an entity's email fields and open the local mail client.
+   * 
+   * If no default email client is available on the device / configured in the browser, then nothing will happen here.
    */
   executeMailtoFromEntity(entity: Entity): boolean {
     const entityType = this.entityRegistry.get(
