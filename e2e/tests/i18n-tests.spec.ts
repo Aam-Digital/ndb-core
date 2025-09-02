2c71c24168e1377bd23260a03b10aa1eb8e7819a
Thomas Scholtes <geigerzaehler@axiom.fm>
2025-09-02 16:25:25 +0200

    test(e2e): Reload dashboard to ensure data is there (#3261)
    closes #3241

--- e2e/tests/i18n-tests.spec.ts
+++ e2e/tests/i18n-tests.spec.ts
@@ -17,7 +17,13 @@ test("Translated and localized app versions (i18n)", async ({ page }) => {
   await page.getByRole("button", { name: "System erkunden" }).click();
   await expect(
     page.getByRole("button", { name: "System erkunden" }),
-  ).toBeHidden();
+  ).not.toBeVisible();
+
+  // FIXME: The dashboard may load before demo data is generated and not display
+  // it. As a workaround we move to a different view and back to the dashboard
+  await page.getByRole("navigation").getByText("Schüler:innen").click();
+  await page.getByRole("navigation").getByText("Dashboard").click();
+  await expect(page.getByText("8 Schüler:innen")).toBeVisible();
 
   await argosScreenshot(page, "i18n-de_dashboard");
 });
