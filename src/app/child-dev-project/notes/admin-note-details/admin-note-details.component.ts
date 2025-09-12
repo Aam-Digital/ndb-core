433bf5a343024bfae91260f7fe65f046300e88ef
Abhinegi2 <negiabhishek253@gmail.com>
2025-09-12 14:41:07 +0530

    lint fixes

--- src/app/child-dev-project/notes/admin-note-details/admin-note-details.component.ts
+++ src/app/child-dev-project/notes/admin-note-details/admin-note-details.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  Input,
-  OnInit,
-  Output,
-  EventEmitter,
-} from "@angular/core";
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
