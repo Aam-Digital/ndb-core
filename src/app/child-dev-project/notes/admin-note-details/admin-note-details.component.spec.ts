9d0e00b1084700dd0a053f5b4ec4d581fd75de97
Abhinegi2 <negiabhishek253@gmail.com>
2025-09-16 16:34:08 +0530

    test case fixes

--- src/app/child-dev-project/notes/admin-note-details/admin-note-details.component.spec.ts
+++ src/app/child-dev-project/notes/admin-note-details/admin-note-details.component.spec.ts
@@ -1,11 +1,21 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { AdminNoteDetailsComponent } from "./admin-note-details.component";
-import { DatabaseEntity } from "../../../core/entity/database-entity.decorator";
+import {
+  DatabaseEntity,
+  EntityRegistry,
+} from "../../../core/entity/database-entity.decorator";
 import { Entity } from "../../../core/entity/model/entity";
+import { EntityForm } from "#src/app/core/common-components/entity-form/entity-form";
+import { EntityFormService } from "#src/app/core/common-components/entity-form/entity-form.service";
+import { FormGroup } from "@angular/forms";
+import { AdminEntityService } from "#src/app/core/admin/admin-entity.service";
+import { SyncStateSubject } from "#src/app/core/session/session-type";
+import { CurrentUserSubject } from "#src/app/core/session/current-user-subject";
 
 describe("AdminNoteDetailsComponent", () => {
   let component: AdminNoteDetailsComponent;
   let fixture: ComponentFixture<AdminNoteDetailsComponent>;
+  let mockFormService: jasmine.SpyObj<EntityFormService>;
 
   @DatabaseEntity("TestNote")
   class TestNote extends Entity {
@@ -13,8 +23,22 @@ describe("AdminNoteDetailsComponent", () => {
   }
 
   beforeEach(async () => {
+    mockFormService = jasmine.createSpyObj("EntityFormService", [
+      "createEntityForm",
+    ]);
+    mockFormService.createEntityForm.and.returnValue(
+      Promise.resolve({
+        formGroup: new FormGroup({}),
+      } as EntityForm<any>),
+    );
     await TestBed.configureTestingModule({
       imports: [AdminNoteDetailsComponent],
+      providers: [
+        { provide: EntityFormService, useValue: mockFormService },
+        SyncStateSubject,
+        CurrentUserSubject,
+        EntityRegistry,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminNoteDetailsComponent);
