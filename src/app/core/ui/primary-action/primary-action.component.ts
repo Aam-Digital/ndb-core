3feedad57445f648b405d23ed3b54001feb7ce35
sadaf <sahinakhatoon650@gmail.com>
2025-11-17 18:03:28 +0530

    made primary action button configurable

--- src/app/core/ui/primary-action/primary-action.component.ts
+++ src/app/core/ui/primary-action/primary-action.component.ts
@@ -5,6 +5,12 @@ import { MatButtonModule } from "@angular/material/button";
 import { Angulartics2Module } from "angulartics2";
 import { DisableEntityOperationDirective } from "../../permissions/permission-directive/disable-entity-operation.directive";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { Router } from "@angular/router";
+import { ConfigService } from "../../config/config.service";
+import { PrimaryActionConfig } from "../../config/primary-action-config";
+import { Todo } from "../../../features/todos/model/todo";
+import { RecurringActivity } from "../../../child-dev-project/attendance/model/recurring-activity";
+import { ActivityAttendance } from "../../../child-dev-project/attendance/model/activity-attendance";
 
 /**
  * The "Primary Action" is always displayed hovering over the rest of the app as a quick action for the user.
@@ -26,17 +32,44 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 })
 export class PrimaryActionComponent {
   private formDialog = inject(FormDialogService);
+  private configService = inject(ConfigService);
+  private router = inject(Router);
 
-  noteConstructor = Note;
+  private defaultConfig: PrimaryActionConfig = {
+    icon: "plus",
+    actionType: "createEntity",
+    entityType: "Note",
+  };
+
+  private entityRegistry: Record<string, any> = {
+    Note,
+    Todo,
+    RecurringActivity,
+    ActivityAttendance,
+  };
+
+  config: PrimaryActionConfig =
+    this.configService.getConfig<PrimaryActionConfig>("primaryAction") ??
+    this.defaultConfig;
+
+  get entityConstructor() {
+    const ctor = this.entityRegistry[this.config.entityType ?? "Note"];
+    return ctor ?? this.entityRegistry["Note"];
+  }
 
   /**
    * The primary action to be triggered when the user clicks the hovering button.
    */
   primaryAction() {
-    this.formDialog.openView(this.createNewNote(), "NoteDetails");
-  }
-
-  private createNewNote() {
-    return new Note();
+    if (this.config.actionType === "createEntity") {
+      const ctor = this.entityRegistry[this.config.entityType ?? "Note"];
+      if (ctor) {
+        this.formDialog.openView(new ctor(), "EntityDetails");
+      } else {
+        this.formDialog.openView(new Note(), "EntityDetails");
+      }
+    } else if (this.config.actionType === "navigate" && this.config.route) {
+      this.router.navigate([this.config.route]);
+    }
   }
 }
