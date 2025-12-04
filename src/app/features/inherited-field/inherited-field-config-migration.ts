3971ce61eb90c11cffc7d12cd53b5d63ba814e24
Abhinegi2 <negiabhishek253@gmail.com>
2025-12-04 17:45:55 +0530

    updayted migration

--- src/app/features/inherited-field/inherited-field-config-migration.ts
+++ src/app/features/inherited-field/inherited-field-config-migration.ts
@@ -1,14 +1,44 @@
 import { ConfigMigration } from "#src/app/core/config/config-migration";
+import { DefaultValueConfigInheritedField } from "./inherited-field-config";
 
 /**
  * Transform DefaultValueConfigInherited and DefaultValueConfigUpdatedFromReferencingEntity configs
  * to DefaultValueConfigInheritedField.
  */
-const migrateInheritedFieldConfig: ConfigMigration = (key, configPart) => {
-  if (configPart) {
+export const migrateInheritedFieldConfig: ConfigMigration = (key, configPart) => {
+  if (key !== "defaultValue" || !configPart?.config) {
     return configPart;
   }
 
-  // field / relatedTriggerField -> relatedSourceField
-  // automatedMapping -> valueMapping
+  const config = configPart.config;
+  
+  if (configPart.mode === "inherited-from-referenced-entity") {
+    const newConfig: DefaultValueConfigInheritedField = {
+      sourceReferenceField: config.localAttribute,
+      sourceValueField: config.field,
+    };
+
+    return {
+      ...configPart,
+      mode: "inherited-field",
+      config: newConfig,
+    };
+  }
+
+  if (configPart.mode === "updated-from-referencing-entity") {
+    const newConfig: DefaultValueConfigInheritedField = {
+      sourceEntityType: config.relatedEntityType,
+      sourceReferenceField: config.relatedReferenceField,
+      sourceValueField: config.relatedTriggerField,
+      valueMapping: config.automatedMapping,
+    };
+
+    return {
+      ...configPart,
+      mode: "inherited-field", 
+      config: newConfig,
+    };
+  }
+
+  return configPart;
 };
