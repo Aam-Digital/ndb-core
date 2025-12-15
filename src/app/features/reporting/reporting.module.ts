7a5fc3df259888da434b1580c91ffa20f644a9c7
Sebastian Leidig <sebastian@aam-digital.com>
2025-12-15 22:10:23 +0100

    fixes

--- src/app/features/reporting/reporting.module.ts
+++ src/app/features/reporting/reporting.module.ts
@@ -1,11 +1,11 @@
 import { inject, NgModule } from "@angular/core";
 import { ComponentRegistry } from "../../dynamic-components";
 import { reportingComponents } from "./reporting-components";
-import { ReportEntity } from "#src/app/features/reporting/report-config";
+import { ReportEntity } from "./report-config";
 
 @NgModule({})
 export class ReportingModule {
-  static databaseEntities = [ReportEntity];
+  static readonly databaseEntities = [ReportEntity];
 
   constructor() {
     const components = inject(ComponentRegistry);
