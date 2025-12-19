6d4d60e430b5d9b6420c35dff95a69932be9201c
Abu Sadaf <sahinakhatoon650@gmail.com>
2025-12-19 17:00:31 +0530

    add emitEvent: false to prevent unnecessary value change events

--- src/app/core/default-values/x-static/admin-default-value-static/admin-default-value-static.component.ts
+++ src/app/core/default-values/x-static/admin-default-value-static/admin-default-value-static.component.ts
@@ -66,7 +66,7 @@ export class AdminDefaultValueStaticComponent
         this.formControl.setValue(this.getInternalValue(newValue), {
           emitEvent: false,
         });
-        this.formControl.updateValueAndValidity();
+        this.formControl.updateValueAndValidity({ emitEvent: false });
       },
     );
   }
