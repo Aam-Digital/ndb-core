3793fa61c555a2fc014f44cf41211759bdb4679b
Abhinegi2 <negiabhishek253@gmail.com>
2026-01-05 16:31:20 +0530

    fix reset issue

--- src/app/core/default-values/x-static/admin-default-value-static/admin-default-value-static.component.ts
+++ src/app/core/default-values/x-static/admin-default-value-static/admin-default-value-static.component.ts
@@ -66,7 +66,7 @@ export class AdminDefaultValueStaticComponent
         this.formControl.setValue(this.getInternalValue(newValue), {
           emitEvent: false,
         });
-        this.formControl.updateValueAndValidity({ emitEvent: false });
+        setTimeout(() => this.formControl.updateValueAndValidity(), 0);
       },
     );
   }
