8f2ef147218ed162b740a70be53bbdd9e85d41ad
Abhinegi2 <negiabhishek253@gmail.com>
2026-01-12 17:34:10 +0530

    added provideZoneChangeDetection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,12 @@
 
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { LOCALE_ID, NgModule, inject } from "@angular/core";
+import {
+  LOCALE_ID,
+  NgModule,
+  inject,
+  provideZoneChangeDetection,
+} from "@angular/core";
 import {
   provideHttpClient,
   withInterceptorsFromDi,
@@ -160,6 +165,7 @@ import { EmailClientServiceModule } from "./features/email-client/email-client.m
   ],
   providers: [
     ...Logging.getAngularTracingProviders(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     { provide: ComponentRegistry, useValue: componentRegistry },
     { provide: EntityRegistry, useValue: entityRegistry },
     { provide: UserAdminService, useClass: KeycloakAdminService },
