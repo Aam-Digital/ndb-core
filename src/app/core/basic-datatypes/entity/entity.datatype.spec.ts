402bd0071c2b6c2d8f78aa17170ad86617f6ee46
Sebastian Leidig <sebastian@aam-digital.com>
2026-01-26 20:10:59 +0100

    fix matching with undefined entity field

--- src/app/core/basic-datatypes/entity/entity.datatype.spec.ts
+++ src/app/core/basic-datatypes/entity/entity.datatype.spec.ts
@@ -3,4 +3,8 @@ import { EntityDatatype } from "./entity.datatype";
 
 describe("Schema data type: entity", () => {
   testDatatype(EntityDatatype, "1", "1", "User");
+
+  // keep undefined and null unchanged
+  testDatatype(EntityDatatype, undefined, undefined, "User");
+  testDatatype(EntityDatatype, null, null, "User");
 });
