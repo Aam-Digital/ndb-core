e58ff482ed20f737097d3b8b7cf347eaa53d18d8
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2026-02-18 16:03:26 +0530

    Update src/app/core/common-components/entity-form/dynamic-form-validators/dynamic-validators.service.ts
    Co-authored-by: Abhishek Negi <108608673+Abhinegi2@users.noreply.github.com>

--- src/app/core/common-components/entity-form/dynamic-form-validators/dynamic-validators.service.ts
+++ src/app/core/common-components/entity-form/dynamic-form-validators/dynamic-validators.service.ts
@@ -217,6 +217,7 @@ export class DynamicValidatorsService {
           this.entityMapper
             .loadType(value)
             // TODO: extend this to allow checking for any configurable property (e.g. Child.name rather than only id)
+            
             .then((entities) => entities.map((entity) => entity.getId())),
         normalize: false,
         errorKey: "uniqueId",
