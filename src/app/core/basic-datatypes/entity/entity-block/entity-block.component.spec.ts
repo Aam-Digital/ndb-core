9d543681529a95e15143d3422d94e046338d3623
Sebastian Leidig <sebastian@aam-digital.com>
2026-02-21 19:16:52 +0100

    refactor: use signal and onPush for EntityBlock

--- src/app/core/basic-datatypes/entity/entity-block/entity-block.component.spec.ts
+++ src/app/core/basic-datatypes/entity/entity-block/entity-block.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+} from "@angular/core/testing";
 
 import { EntityBlockComponent } from "./entity-block.component";
 import { EntityMapperService } from "../../../entity/entity-mapper/entity-mapper.service";
@@ -41,45 +46,53 @@ describe("EntityBlockComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should load entity if only id is given", async () => {
-    component.entityId = testEntity.getId();
-    await component.ngOnInit();
+  it("should load entity if only id is given", fakeAsync(() => {
+    fixture.componentRef.setInput("entityId", testEntity.getId());
+    fixture.detectChanges();
+    flush();
+    fixture.detectChanges();
 
-    expect(component.entity).toEqual(testEntity);
-  });
+    expect(component.entityResource.value()).toEqual(testEntity);
+  }));
 
-  it("should load the block config for tooltip when available", async () => {
-    component.entity = testEntity;
-    await component.ngOnInit();
+  it("should load the block config for tooltip when available", fakeAsync(() => {
+    fixture.componentRef.setInput("entity", testEntity);
+    fixture.detectChanges();
+    flush();
+    fixture.detectChanges();
 
-    expect(component.entityBlockConfig).toEqual(
+    expect(component.entityBlockConfig()).toEqual(
       TestEntity.toBlockDetailsAttributes,
     );
-  });
+  }));
 
-  it("should navigate to the details page of the entity", () => {
-    component.entity = new TestEntity("1");
+  it("should navigate to the details page of the entity", fakeAsync(() => {
+    fixture.componentRef.setInput("entity", new TestEntity("1"));
+    fixture.detectChanges();
+    flush();
+    fixture.detectChanges();
 
     component.showDetailsPage();
 
     expect(mockRouter.navigate).toHaveBeenCalledWith([TestEntity.route, "1"]);
-  });
+  }));
 
-  it("should log a warning if entity cannot be loaded", async () => {
+  it("should log a warning if entity cannot be loaded", fakeAsync(() => {
     const logSpy = spyOn(Logging, "debug");
 
-    component.entityId = "Entity:404";
     mockEntityMapper.load.and.rejectWith(
       new DatabaseException(new Error(), "Entity not found"),
     );
-
-    await component.ngOnInit();
+    fixture.componentRef.setInput("entityId", "Entity:404");
+    fixture.detectChanges();
+    flush();
+    fixture.detectChanges();
 
     expect(logSpy).toHaveBeenCalledWith(
       jasmine.stringContaining("Could not find entity"),
       "Entity:404",
       jasmine.anything(),
     );
-    expect(component.entity).toBeUndefined();
-  });
+    expect(component.entityResource.value()).toBeUndefined();
+  }));
 });
