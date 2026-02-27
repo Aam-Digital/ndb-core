d9e10e487fa96925472303ea7e8c23414c9fce62
Abhinegi2 <negiabhishek253@gmail.com>
2026-02-27 13:39:58 +0530

    merge conflic fixed

--- src/app/features/attendance/model/attendance-item.ts
+++ src/app/features/attendance/model/attendance-item.ts
@@ -41,8 +41,6 @@ export class AttendanceItem {
 
   @DatabaseField() remarks: string;
 
-  @DatabaseField() participant: string;
-
   constructor(
     status: AttendanceStatusType = NullAttendanceStatusType,
     remarks: string = "",
