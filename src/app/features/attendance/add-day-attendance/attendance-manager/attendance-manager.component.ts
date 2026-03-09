a469046409b8559e6ed490f90dd2397b4af3cd9d
Sebastian Leidig <sebastian@aam-digital.com>
2026-03-09 11:26:57 +0100

    make attendance-manager configurable

--- src/app/features/attendance/add-day-attendance/attendance-manager/attendance-manager.component.ts
+++ src/app/features/attendance/add-day-attendance/attendance-manager/attendance-manager.component.ts
@@ -1,18 +1,36 @@
-import { Component, inject } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+  Signal,
+} from "@angular/core";
 import { ComingSoonDialogService } from "#src/app/features/coming-soon/coming-soon-dialog.service";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterLink } from "@angular/router";
 import { ViewTitleComponent } from "#src/app/core/common-components/view-title/view-title.component";
 import { RouteTarget } from "#src/app/route-target";
+import { EntityConstructor } from "#src/app/core/entity/model/entity";
+import { DisableEntityOperationDirective } from "#src/app/core/permissions/permission-directive/disable-entity-operation.directive";
 
 @RouteTarget("AttendanceManager")
 @Component({
   selector: "app-attendance-manager",
   templateUrl: "./attendance-manager.component.html",
   styleUrls: ["./attendance-manager.component.scss"],
-  imports: [MatCardModule, MatButtonModule, RouterLink, ViewTitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    RouterLink,
+    ViewTitleComponent,
+    DisableEntityOperationDirective,
+  ],
 })
 export class AttendanceManagerComponent {
   comingSoonDialog = inject(ComingSoonDialogService);
+
+  activityTypes: Signal<EntityConstructor[]> = signal([]);
+  hasPermissionsToRecordEvent: Signal<boolean> = signal(true);
 }
