01c55009444297a303f9210aee29cf525d0a1fa8
Abu Sadaf <sahinakhatoon650@gmail.com>
2026-04-16 18:46:35 +0530

    ensure "Complete Task" button is clickable in readonly mode

--- src/app/core/entity/entity-field-edit/dynamic-edit/dynamic-edit.component.ts
+++ src/app/core/entity/entity-field-edit/dynamic-edit/dynamic-edit.component.ts
@@ -2,7 +2,7 @@ import { FormFieldConfig } from "#src/app/core/common-components/entity-form/For
 import { DynamicComponentDirective } from "#src/app/core/config/dynamic-components/dynamic-component.directive";
 import { Entity } from "#src/app/core/entity/model/entity";
 import { Component, Input } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { MatFormFieldControl } from "@angular/material/form-field";
 import { CustomFormControlDirective } from "../../../common-components/basic-autocomplete/custom-form-control.directive";
 import { EditComponent } from "./edit-component.interface";
@@ -20,6 +20,28 @@ export class DynamicEditComponent
   extends CustomFormControlDirective<any>
   implements EditComponent
 {
+  private static readonly TODO_COMPLETION_EDIT_COMPONENT = "EditTodoCompletion";
+
   @Input() formFieldConfig: FormFieldConfig;
   @Input() entity: Entity;
+
+  override get disabled(): boolean {
+    const control = this.ngControl?.control as FormControl<unknown> | null;
+
+    // Keep Todo "Complete Task" button interactive in readonly forms while unfinished.
+    if (
+      this.formFieldConfig?.editComponent ===
+        DynamicEditComponent.TODO_COMPLETION_EDIT_COMPONENT &&
+      control?.disabled &&
+      !control.value
+    ) {
+      return false;
+    }
+
+    return super.disabled;
+  }
+
+  override set disabled(value: boolean) {
+    super.disabled = value;
+  }
 }
