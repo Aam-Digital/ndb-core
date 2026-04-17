e101ad0eca956d50d33f4d9b854228ecddb636be
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2026-04-17 23:00:15 +0530

    fix(Import): show inheritance warning in import mapping and confirmation (#3855)
    closes #3683

--- src/app/core/basic-datatypes/entity/entity-import-config/entity-import-config.component.ts
+++ src/app/core/basic-datatypes/entity/entity-import-config/entity-import-config.component.ts
@@ -14,6 +14,8 @@ import { MatButtonModule } from "@angular/material/button";
 import { EntityConstructor } from "../../../entity/model/entity";
 import { HelpButtonComponent } from "../../../common-components/help-button/help-button.component";
 import { DynamicComponent } from "../../../config/dynamic-components/dynamic-component.decorator";
+import { HintBoxComponent } from "../../../common-components/hint-box/hint-box.component";
+import { isInheritanceSourceReferenceField } from "../../../import/import-inheritance-warning.util";
 
 /**
  * Configuration UI for the EntityDatatype's import mapping function.
@@ -31,6 +33,7 @@ import { DynamicComponent } from "../../../config/dynamic-components/dynamic-com
     MatDialogModule,
     MatButtonModule,
     HelpButtonComponent,
+    HintBoxComponent,
   ],
 })
 export class EntityImportConfigComponent {
@@ -42,9 +45,16 @@ export class EntityImportConfigComponent {
   entity: EntityConstructor;
   propertyForm = new FormControl("");
   availableProperties: { property: string; label: string }[] = [];
+  showInheritanceImportHint = false;
 
   constructor() {
     const propertyName = this.data.col.propertyName;
+
+    this.showInheritanceImportHint = isInheritanceSourceReferenceField(
+      this.data.entityType,
+      propertyName,
+    );
+
     const entityName = this.data.entityType.schema.get(propertyName).additional;
     this.entity = this.entities.get(entityName);
     this.availableProperties = [...this.entity.schema.entries()]
