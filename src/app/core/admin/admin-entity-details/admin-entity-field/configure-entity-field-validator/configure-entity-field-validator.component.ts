8a8c1972b9092fe5d2fcd896d1588ad799242ea4
Abu Sadaf <116058905+sadaf895@users.noreply.github.com>
2026-05-13 20:03:53 +0530

    feat: add date and age range validators to entity field configuration (#3892)
    closes #3871

--- src/app/core/admin/admin-entity-details/admin-entity-field/configure-entity-field-validator/configure-entity-field-validator.component.ts
+++ src/app/core/admin/admin-entity-details/admin-entity-field/configure-entity-field-validator/configure-entity-field-validator.component.ts
@@ -6,6 +6,7 @@ import {
   Output,
   inject,
   ChangeDetectionStrategy,
+  DestroyRef,
 } from "@angular/core";
 import { MatInputModule } from "@angular/material/input";
 import {
@@ -15,25 +16,33 @@ import {
   ReactiveFormsModule,
 } from "@angular/forms";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { EntitySchemaField } from "app/core/entity/schema/entity-schema-field";
 import { FormValidatorConfig } from "app/core/common-components/entity-form/dynamic-form-validators/form-validator-config";
 import { HelpButtonComponent } from "../../../../common-components/help-button/help-button.component";
+import { EditDateComponent } from "../../../../basic-datatypes/date/edit-date/edit-date.component";
+import { EditMonthComponent } from "../../../../basic-datatypes/month/edit-month/edit-month.component";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "app-configure-entity-field-validator",
   imports: [
     MatInputModule,
+    MatFormFieldModule,
     FormsModule,
     MatCheckboxModule,
     ReactiveFormsModule,
     HelpButtonComponent,
+    EditDateComponent,
+    EditMonthComponent,
   ],
   templateUrl: "./configure-entity-field-validator.component.html",
   styleUrl: "./configure-entity-field-validator.component.scss",
 })
 export class ConfigureEntityFieldValidatorComponent implements OnInit {
   private fb = inject(FormBuilder);
+  private readonly destroyRef = inject(DestroyRef);
 
   validatorForm: FormGroup;
 
@@ -51,12 +60,41 @@ export class ConfigureEntityFieldValidatorComponent implements OnInit {
     this.init();
   }
 
+  get isDateLikeValidatorType(): boolean {
+    return ["date", "date-only", "date-with-age", "month"].includes(
+      this.entitySchemaField?.dataType,
+    );
+  }
+
+  get isDateWithAgeType(): boolean {
+    return this.entitySchemaField?.dataType === "date-with-age";
+  }
+
+  get isMonthType(): boolean {
+    return this.entitySchemaField?.dataType === "month";
+  }
+
+  private normalizeDateControl(controlName: string): void {
+    const ctrl = this.validatorForm.get(controlName);
+    const val = ctrl?.value;
+    if (typeof val === "string" || typeof val === "number") {
+      const parsed = new Date(val as any);
+      if (!Number.isNaN(parsed.getTime())) {
+        ctrl?.setValue(parsed, { emitEvent: false });
+      }
+    }
+  }
+
   private init() {
     if (this.entitySchemaField.validators) {
       this.validatorForm = this.fb.group({
         required: [this.entitySchemaField.validators.required],
         min: [this.entitySchemaField.validators.min],
         max: [this.entitySchemaField.validators.max],
+        minAge: [this.entitySchemaField.validators.minAge],
+        maxAge: [this.entitySchemaField.validators.maxAge],
+        minDate: [this.entitySchemaField.validators.minDate],
+        maxDate: [this.entitySchemaField.validators.maxDate],
         regex: [this.entitySchemaField.validators.pattern],
         uniqueId: [this.entitySchemaField.validators.uniqueId],
         readonlyAfterSet: [this.entitySchemaField.validators.readonlyAfterSet],
@@ -66,19 +104,30 @@ export class ConfigureEntityFieldValidatorComponent implements OnInit {
         required: [false],
         min: [null],
         max: [null],
+        minAge: [null],
+        maxAge: [null],
+        minDate: [null],
+        maxDate: [null],
         regex: [""],
         uniqueId: [""],
         readonlyAfterSet: [false],
       });
     }
 
-    this.validatorForm.valueChanges.subscribe((value) => {
-      const rawValues = this.validatorForm.getRawValue();
-      const cleanedValues =
-        this.removeDefaultValuesFromValidatorConfig(rawValues);
+    // Normalize any string/timestamp date values to Date objects so EditDate component receives a Date
+    this.normalizeDateControl("minDate");
+    this.normalizeDateControl("maxDate");
 
-      this.entityValidatorChanges.emit(cleanedValues);
-    });
+    // Emit validator changes when form values change
+    this.validatorForm.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        const rawValues = this.validatorForm.getRawValue();
+        const cleanedValues =
+          this.removeDefaultValuesFromValidatorConfig(rawValues);
+
+        this.entityValidatorChanges.emit(cleanedValues);
+      });
   }
 
   /**
