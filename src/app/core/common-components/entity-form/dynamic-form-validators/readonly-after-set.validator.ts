66f7d2d50111701d08b4395e1fafdb28b5f15017
Abhishek Negi <negiabhishek253@gmail.com>
2026-05-29 13:55:39 +0530

    fix(Admin UI: fix multiple bugs in public form admin Configure Pre-filled Values tab (#4018)
    fixes #4003
    fixes #4014

--- src/app/core/common-components/entity-form/dynamic-form-validators/readonly-after-set.validator.ts
+++ src/app/core/common-components/entity-form/dynamic-form-validators/readonly-after-set.validator.ts
@@ -1,9 +1,9 @@
 import {
-  FormControl,
+  AbstractControl,
   FormControlStatus,
   ValidationErrors,
+  ValidatorFn,
 } from "@angular/forms";
-import { AsyncPromiseValidatorFn } from "./validator-types";
 import { Entity } from "../../../entity/model/entity";
 
 /**
@@ -12,27 +12,37 @@ import { Entity } from "../../../entity/model/entity";
  * readonlyAfterSet: true
  */
 export function buildReadonlyValidator(entity: Entity): {
-  async: true;
-  fn: AsyncPromiseValidatorFn;
+  fn: ValidatorFn;
 } {
   let keepDisabled: boolean = !entity.isNew;
+  let subscribed = false;
 
   return {
-    async: true,
-    fn: async (control: FormControl): Promise<ValidationErrors | null> => {
-      control.statusChanges.subscribe((status: FormControlStatus) => {
-        if (status === "DISABLED") {
-          // after the form was disabled once, keep this disabled now
-          keepDisabled = true;
-        }
+    fn: (control: AbstractControl): ValidationErrors | null => {
+      if (!subscribed) {
+        subscribed = true;
+        control.statusChanges.subscribe((status: FormControlStatus) => {
+          if (status === "DISABLED") {
+            keepDisabled = true;
+          }
 
-        if (keepDisabled) {
-          control.disable({
-            onlySelf: true,
-            emitEvent: false,
-          });
-        }
-      });
+          if (keepDisabled) {
+            control.disable({
+              onlySelf: true,
+              emitEvent: false,
+            });
+          }
+        });
+      }
+
+      if (keepDisabled && !control.disabled) {
+        // Defer to avoid mutating control state during the validation cycle
+        Promise.resolve().then(() => {
+          if (keepDisabled) {
+            control.disable({ onlySelf: true, emitEvent: false });
+          }
+        });
+      }
 
       return null;
     },
