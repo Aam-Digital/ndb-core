df52b7f766d63facd31f99dc5109847c82e675b7
Simon <simon@aam-digital.com>
2026-08-21 14:44:40 +0200

    fix notes-related-to-entity to correctly use loaderMethod

--- src/app/child-dev-project/notes/notes-related-to-entity/notes-related-to-entity.component.ts
+++ src/app/child-dev-project/notes/notes-related-to-entity/notes-related-to-entity.component.ts
@@ -4,6 +4,7 @@ import {
   inject,
   input,
   ChangeDetectionStrategy,
+  InputSignal,
 } from "@angular/core";
 import { Note } from "../model/note";
 import { FormDialogService } from "../../../core/form-dialog/form-dialog.service";
@@ -38,6 +39,8 @@ export class NotesRelatedToEntityComponent extends RelatedEntitiesComponent<Note
   override loaderMethod = input<LoaderMethod>(
     LoaderMethod.NotesRelatedToEntity,
   );
+  /** set this to make sure that loader method is used (it will not be used if multiple relation properties are found) */
+  override property = input("children")
 
   protected override getDefaultColumns(): FormFieldConfig[] {
     return structuredClone(
