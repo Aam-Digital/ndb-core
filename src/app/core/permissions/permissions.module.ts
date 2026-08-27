a8c00d86be9a2bf802924de41fbc18454ba46274
Simon <therealslimv@yahoo.de>
2026-08-27 16:12:29 +0200

    fix: permission enforcer does not purge in online-only mode (#4328)
    Co-authored-by: Simon <simon@aam-digital.com>

--- src/app/core/permissions/permissions.module.ts
+++ src/app/core/permissions/permissions.module.ts
@@ -1,9 +1,15 @@
-import { NgModule, inject } from "@angular/core";
+import { Injector, NgModule, inject } from "@angular/core";
 import { UserRoleGuard } from "./permission-guard/user-role.guard";
 import { PureAbility } from "@casl/ability";
 import { EntityAbility } from "./ability/entity-ability";
 import { AbilityService } from "./ability/ability.service";
 import { EntityPermissionGuard } from "./permission-guard/entity-permission.guard";
+import { PermissionEnforcerService } from "./permission-enforcer/permission-enforcer.service";
+import { LocalPermissionEnforcerService } from "./permission-enforcer/local-permission-enforcer.service";
+import { NoopPermissionEnforcerService } from "./permission-enforcer/noop-permission-enforcer.service";
+import { SessionType } from "../session/session-type";
+import { environment } from "../../../environments/environment";
+import { serviceProvider } from "../../utils/utils";
 
 @NgModule({
   providers: [
@@ -15,6 +21,14 @@ import { EntityPermissionGuard } from "./permission-guard/entity-permission.guar
       provide: PureAbility,
       useExisting: EntityAbility,
     },
+    LocalPermissionEnforcerService,
+    NoopPermissionEnforcerService,
+    serviceProvider(PermissionEnforcerService, (injector: Injector) =>
+      // No local data to purge in online-mode
+      environment.session_type === SessionType.online
+        ? injector.get(NoopPermissionEnforcerService)
+        : injector.get(LocalPermissionEnforcerService),
+    ),
   ],
 })
 export class PermissionsModule {
