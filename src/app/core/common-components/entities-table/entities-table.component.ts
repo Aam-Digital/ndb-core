f30b86709023608be3a21bc4ae3fcdb202795564
Simon <simon@aam-digital.com>
2026-09-18 12:21:27 +0200

    undone code changes

--- src/app/core/common-components/entities-table/entities-table.component.ts
+++ src/app/core/common-components/entities-table/entities-table.component.ts
@@ -236,7 +236,7 @@ export class EntitiesTableComponent<T extends Entity>
     });
     effect(() => {
       this.recordsDataSource().dataFilter();
-      this.recordsDataSource().selectableRows();
+      this.recordsDataSource().displayedData();
       if (untracked(this.selectedRecords)?.length > 0) {
         // reset selection if filter or input data changes
         this.selectedRecords.set([]);
@@ -255,7 +255,7 @@ export class EntitiesTableComponent<T extends Entity>
     // Connect selection store
     this.selectionStore.connect({
       selectedRecords: this.selectedRecords,
-      selectableRows: this.recordsDataSource().selectableRows,
+      selectableRows: this.recordsDataSource().displayedData,
       currentPageRows: this.recordsDataSource().renderedRows,
     });
   }
